fberlakovich/r2c-webkit

commit ebb1bd054fc8365a594dfdfd4a888726ad194dee

2007-11-30  Alp Toker  <alp@atoker.com>
        Reviewed by Adam Roben.

        http://bugs.webkit.org/show_bug.cgi?id=15691
        [GTK] Public API does not follow GTK+ conventions

        Refactor the WebKit/GTK+ public API. Changes:
          WebKitPage -> WebKitWebView
          WebKitFrame -> WebKitWebFrame

        Public API source and header names have been updated to mirror the API
        changes.

        The API is now kept in WebKit/gtk/WebView to match other ports in the
        same class such as Mac and Win.

        API/ABI-breaking change.


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@28273 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKit/gtk/WebView/webkitprivate.cpp
@@ -26,16 +26,73 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef WEBKIT_GLOBAL_H
-#define WEBKIT_GLOBAL_H
+#include "config.h"
 
-#include "webkitgtkdefines.h"
+#include "webkitprivate.h"
+#include "ChromeClientGtk.h"
+#include "FrameLoader.h"
+#include "FrameLoaderClientGtk.h"
+#include "NotImplemented.h"
 
-G_BEGIN_DECLS
+using namespace WebCore;
 
-WEBKIT_API void
-webkit_init (void);
+namespace WebKit {
+void apply(WebKitSettings*, WebCore::Settings*)
+{
+    notImplemented();
+}
 
-G_END_DECLS
+WebKitSettings* create(WebCore::Settings*)
+{
+    notImplemented();
+    return 0;
+}
 
-#endif
+WebKitWebFrame* getFrameFromView(WebKitWebView* webView)
+{
+    return webkit_web_view_get_main_frame(webView);
+}
+
+WebKitWebView* getViewFromFrame(WebKitWebFrame* frame)
+{
+    return webkit_web_frame_get_web_view(frame);
+}
+
+WebCore::Frame* core(WebKitWebFrame* frame)
+{
+    if (!frame)
+        return 0;
+
+    WebKitWebFramePrivate* frame_data = WEBKIT_WEB_FRAME_GET_PRIVATE(frame);
+    return frame_data ? frame_data->frame : 0;
+}
+
+WebKitWebFrame* kit(WebCore::Frame* coreFrame)
+{
+    if (!coreFrame)
+        return 0;
+
+    ASSERT(coreFrame->loader());
+    WebKit::FrameLoaderClient* client = static_cast<WebKit::FrameLoaderClient*>(coreFrame->loader()->client());
+    return client ? client->webFrame() : 0;
+}
+
+WebCore::Page* core(WebKitWebView* webView)
+{
+    if (!webView)
+        return 0;
+
+    WebKitWebViewPrivate* webViewData = WEBKIT_WEB_VIEW_GET_PRIVATE(webView);
+    return webViewData ? webViewData->corePage : 0;
+}
+
+WebKitWebView* kit(WebCore::Page* corePage)
+{
+    if (!corePage)
+        return 0;
+
+    ASSERT(webView->chrome());
+    WebKit::ChromeClient* client = static_cast<WebKit::ChromeClient*>(corePage->chrome()->client());
+    return client ? client->webPage() : 0;
+}
+}

WebCore/WebCore.pro
@@ -146,7 +146,7 @@ gtk-port {
     $$PWD/loader/gtk \
     $$PWD/page/gtk \
     $$PWD/../WebKit/gtk/WebCoreSupport \
-    $$PWD/../WebKit/gtk/Api
+    $$PWD/../WebKit/gtk/WebView
 
     DEPENDPATH += \
     platform/gtk \
@@ -164,7 +164,7 @@ gtk-port {
     loader/gtk \
     page/gtk \
     ../WebKit/gtk/WebCoreSupport \
-    ../WebKit/gtk/Api
+    ../WebKit/gtk/WebView
 }
 
 INCLUDEPATH +=  $$PWD \
@@ -939,13 +939,13 @@ qt-port {
 gtk-port {
     HEADERS += \
         ../WebCore/platform/gtk/ClipboardGtk.h \
-        ../WebKit/gtk/Api/webkitgtkdefines.h \
-        ../WebKit/gtk/Api/webkitgtkframe.h \
-        ../WebKit/gtk/Api/webkitgtkglobal.h \
-        ../WebKit/gtk/Api/webkitgtknetworkrequest.h \
-        ../WebKit/gtk/Api/webkitgtkpage.h \
-        ../WebKit/gtk/Api/webkitgtkprivate.h \
-        ../WebKit/gtk/Api/webkitgtksettings.h \
+        ../WebKit/gtk/WebView/webkitdefines.h \
+        ../WebKit/gtk/WebView/webkitglobal.h \
+        ../WebKit/gtk/WebView/webkitnetworkrequest.h \
+        ../WebKit/gtk/WebView/webkitprivate.h \
+        ../WebKit/gtk/WebView/webkitsettings.h \
+        ../WebKit/gtk/WebView/webkitwebframe.h \
+        ../WebKit/gtk/WebView/webkitwebview.h \
         ../WebKit/gtk/WebCoreSupport/ChromeClientGtk.h \
         ../WebKit/gtk/WebCoreSupport/ContextMenuClientGtk.h \
         ../WebKit/gtk/WebCoreSupport/DragClientGtk.h \
@@ -1016,12 +1016,12 @@ gtk-port {
         platform/image-decoders/bmp/BMPImageDecoder.cpp \
         platform/image-decoders/ico/ICOImageDecoder.cpp \
         platform/image-decoders/xbm/XBMImageDecoder.cpp \
-        ../WebKit/gtk/Api/webkitgtkframe.cpp \
-        ../WebKit/gtk/Api/webkitgtkglobal.cpp \
-        ../WebKit/gtk/Api/webkitgtknetworkrequest.cpp \
-        ../WebKit/gtk/Api/webkitgtkpage.cpp \
-        ../WebKit/gtk/Api/webkitgtkprivate.cpp \
-        ../WebKit/gtk/Api/webkitgtksettings.cpp \
+        ../WebKit/gtk/WebView/webkitglobal.cpp \
+        ../WebKit/gtk/WebView/webkitnetworkrequest.cpp \
+        ../WebKit/gtk/WebView/webkitprivate.cpp \
+        ../WebKit/gtk/WebView/webkitsettings.cpp \
+        ../WebKit/gtk/WebView/webkitwebframe.cpp \
+        ../WebKit/gtk/WebView/webkitwebview.cpp \
         ../WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp \
         ../WebKit/gtk/WebCoreSupport/ContextMenuClientGtk.cpp \
         ../WebKit/gtk/WebCoreSupport/DragClientGtk.cpp \
@@ -1681,7 +1681,7 @@ gtk-port {
     target.path = $$WEBKIT_LIB_DIR
     INSTALLS += target
 
-    include($$PWD/../WebKit/gtk/Api/headers.pri)
+    include($$PWD/../WebKit/gtk/WebView/headers.pri)
     headers.files = $$WEBKIT_API_HEADERS
     headers.path = $$WEBKIT_INC_DIR
     INSTALLS += headers
@@ -1701,7 +1701,7 @@ gtk-port {
         QMAKE_PKGCONFIG_INSTALL_REPLACE += lib_replace
     }
 
-    GENMARSHALS = ../WebKit/gtk/Api/webkitgtk-marshal.list
+    GENMARSHALS = ../WebKit/gtk/WebView/webkit-marshal.list
     GENMARSHALS_PREFIX = webkit_marshal
 
     #

WebKit.pri
@@ -17,7 +17,7 @@ qt-port:!building-libs {
 gtk-port:!building-libs {
     QMAKE_LIBDIR = $$OUTPUT_DIR/lib $$QMAKE_LIBDIR
     LIBS += -lWebKitGtk
-    DEPENDPATH += $$PWD/WebKit/gtk/Api $$PWD/WebKit/gtk/WebCoreSupport
+    DEPENDPATH += $$PWD/WebKit/gtk/WebView $$PWD/WebKit/gtk/WebCoreSupport
 }
 
 gtk-port {
@@ -44,6 +44,9 @@ gtk-port {
 
     # This set of warnings is borrowed from the Mac build
     QMAKE_CXXFLAGS += -Wall -W -Wcast-align -Wchar-subscripts -Wformat-security -Wmissing-format-attribute -Wpointer-arith -Wwrite-strings -Wno-format-y2k -Wno-unused-parameter -Wundef
+
+    DEPENDPATH += $$PWD/JavaScriptCore/API
+    INCLUDEPATH += $$PWD
 }
 
 DEFINES += USE_SYSTEM_MALLOC
@@ -60,7 +63,7 @@ gtk-port:INCLUDEPATH += \
     $$BASE_DIR/WebCore/platform/graphics/cairo \
     $$BASE_DIR/WebCore/loader/gtk \
     $$BASE_DIR/WebCore/page/gtk \
-    $$BASE_DIR/WebKit/gtk/Api \
+    $$BASE_DIR/WebKit/gtk/WebView \
     $$BASE_DIR/WebKit/gtk/WebCoreSupport
 INCLUDEPATH += \
     $$BASE_DIR/JavaScriptCore/ \

WebKit/gtk/Api/headers.pri
@@ -1,7 +1,0 @@
-WEBKIT_API_HEADERS = $$PWD/webkitgtkdefines.h \
-                     $$PWD/webkitgtkframe.h \
-                     $$PWD/webkitgtkframedata.h \
-                     $$PWD/webkitgtkglobal.h \
-                     $$PWD/webkitgtknetworkrequest.h \
-                     $$PWD/webkitgtkpage.h \
-                     $$PWD/webkitgtksettings.h

WebKit/gtk/Api/webkitgtkframe.h
@@ -1,105 +1,0 @@
-/*
- * Copyright (C) 2007 Holger Hans Peter Freyther
- * Copyright (C) 2007 Alp Toker <alp@atoker.com>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer. 
- * 2.  Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution. 
- * 3.  Neither the name of Apple, Inc. ("Apple") nor the names of
- *     its contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission. 
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#ifndef WEBKIT_FRAME_H
-#define WEBKIT_FRAME_H
-
-#include <glib-object.h>
-#include <gdk/gdk.h>
-
-#include "webkitgtkdefines.h"
-
-G_BEGIN_DECLS
-
-#define WEBKIT_TYPE_FRAME            (webkit_frame_get_type())
-#define WEBKIT_FRAME(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), WEBKIT_TYPE_FRAME, WebKitFrame))
-#define WEBKIT_FRAME_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),  WEBKIT_TYPE_FRAME, WebKitFrameClass))
-#define WEBKIT_IS_FRAME(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), WEBKIT_TYPE_FRAME))
-#define WEBKIT_IS_FRAME_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),  WEBKIT_TYPE_FRAME))
-#define WEBKIT_FRAME_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj),  WEBKIT_TYPE_FRAME, WebKitFrameClass))
-
-
-struct _WebKitFrame {
-    GObject parent;
-};
-
-struct _WebKitFrameClass {
-    GObjectClass parent;
-
-    void (*title_changed) (WebKitFrame* frame, gchar* title, gchar* location);
-
-    /*
-     * protected virtual methods
-     */
-    void (*mouse_move_event)  (WebKitFrame* frame, GdkEvent* move_event);
-    void (*mouse_press_event) (WebKitFrame* frame, GdkEvent* press_event);
-    void (*mouse_release_event) (WebKitFrame* frame, GdkEvent* mouse_release_event);
-    void (*mouse_double_click_event) (WebKitFrame* frame, GdkEvent* double_click_event);
-    void (*mouse_wheel_event) (WebKitFrame* frame, GdkEvent* wheel_event);
-};
-
-WEBKIT_API GType
-webkit_frame_get_type (void);
-
-WEBKIT_API WebKitFrame*
-webkit_frame_new (WebKitPage* page);
-
-WEBKIT_API WebKitPage*
-webkit_frame_get_page (WebKitFrame* frame);
-
-WEBKIT_API const gchar*
-webkit_frame_get_name (WebKitFrame* frame);
-
-WEBKIT_API const gchar*
-webkit_frame_get_title (WebKitFrame* frame);
-
-WEBKIT_API const gchar*
-webkit_frame_get_location (WebKitFrame* frame);
-
-WEBKIT_API WebKitFrame*
-webkit_frame_get_parent (WebKitFrame* frame);
-
-WEBKIT_API void
-webkit_frame_load_request (WebKitFrame* frame, WebKitNetworkRequest* request);
-
-WEBKIT_API void
-webkit_frame_stop_loading (WebKitFrame* frame);
-
-WEBKIT_API void
-webkit_frame_reload (WebKitFrame* frame);
-
-WEBKIT_API WebKitFrame*
-webkit_frame_find_frame (WebKitFrame* frame, const gchar* name);
-
-WEBKIT_API JSGlobalContextRef
-webkit_frame_get_global_context (WebKitFrame* frame);
-
-G_END_DECLS
-
-#endif

WebKit/gtk/Api/webkitgtkpage.h
@@ -1,135 +1,0 @@
-/*
- * Copyright (C) 2007 Holger Hans Peter Freyther
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer. 
- * 2.  Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution. 
- * 3.  Neither the name of Apple, Inc. ("Apple") nor the names of
- *     its contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission. 
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#ifndef WEBKIT_PAGE_H
-#define WEBKIT_PAGE_H
-
-#include <gtk/gtk.h>
-
-#include "webkitgtkdefines.h"
-#include "webkitgtksettings.h"
-
-G_BEGIN_DECLS
-
-#define WEBKIT_TYPE_PAGE            (webkit_page_get_type())
-#define WEBKIT_PAGE(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), WEBKIT_TYPE_PAGE, WebKitPage))
-#define WEBKIT_PAGE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),  WEBKIT_TYPE_PAGE, WebKitPageClass))
-#define WEBKIT_IS_PAGE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), WEBKIT_TYPE_PAGE))
-#define WEBKIT_IS_PAGE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),  WEBKIT_TYPE_PAGE))
-#define WEBKIT_PAGE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj),  WEBKIT_TYPE_PAGE, WebKitPageClass))
-
-
-typedef enum {
-    WEBKIT_NAVIGATION_RESPONSE_ACCEPT,
-    WEBKIT_NAVIGATION_RESPONSE_IGNORE,
-    WEBKIT_NAVIGATION_RESPONSE_DOWNLOAD
-} WebKitNavigationResponse;
-
-
-
-struct _WebKitPage {
-    GtkContainer parent;
-};
-
-struct _WebKitPageClass {
-    GtkContainerClass parent;
-
-    /*
-     * default handler/virtual methods
-     * DISCUSS: create_page needs a request and should we make this a signal with default handler? this would
-     * require someone doing a g_signal_stop_emission_by_name
-     * WebUIDelegate has nothing for create_frame, WebPolicyDelegate as well...
-     */
-    WebKitPage*  (*create_page)  (WebKitPage* page);
-
-    /*
-     * TODO: FIXME: Create something like WebPolicyDecisionListener_Protocol instead
-     */
-    WebKitNavigationResponse (*navigation_requested) (WebKitPage* page, WebKitFrame* frame, WebKitNetworkRequest* request);
-
-    void (*window_object_cleared) (WebKitPage* page, WebKitFrame* frame, JSGlobalContextRef context, JSObjectRef window_object);
-    gchar*   (*choose_file) (WebKitPage* page, WebKitFrame* frame, const gchar* old_file);
-    gboolean (*script_alert) (WebKitPage* page, WebKitFrame* frame, const gchar* alert_message);
-    gboolean (*script_confirm) (WebKitPage* page, WebKitFrame* frame, const gchar* confirm_message, gboolean* did_confirm);
-    gboolean (*script_prompt) (WebKitPage* page, WebKitFrame* frame, const gchar* message, const gchar* default_value, gchar** value);
-    gboolean (*console_message) (WebKitPage* page, const gchar* message, unsigned int line_number, const gchar* source_id);
-
-    /*
-     * internal
-     */
-    void   (*set_scroll_adjustments) (WebKitPage*, GtkAdjustment*, GtkAdjustment*);
-};
-
-WEBKIT_API GType
-webkit_page_get_type (void);
-
-WEBKIT_API GtkWidget*
-webkit_page_new (void);
-
-WEBKIT_API gboolean
-webkit_page_can_go_backward (WebKitPage* page);
-
-WEBKIT_API gboolean
-webkit_page_can_go_forward (WebKitPage* page);
-
-WEBKIT_API void
-webkit_page_go_backward (WebKitPage* page);
-
-WEBKIT_API void
-webkit_page_go_forward (WebKitPage* page);
-
-WEBKIT_API void
-webkit_page_stop_loading (WebKitPage* page);
-
-WEBKIT_API void
-webkit_page_open (WebKitPage* page, const gchar* uri);
-
-WEBKIT_API void
-webkit_page_reload (WebKitPage *page);
-
-WEBKIT_API void
-webkit_page_load_string (WebKitPage* page, const gchar* content, const gchar* content_mime_type, const gchar* content_encoding, const gchar* base_uri);
-
-WEBKIT_API void
-webkit_page_load_html_string (WebKitPage* page, const gchar* content, const gchar* base_uri);
-
-WEBKIT_API WebKitFrame*
-webkit_page_get_main_frame (WebKitPage* page);
-
-WEBKIT_API void
-webkit_page_execute_script (WebKitPage* page, const gchar* script);
-
-WEBKIT_API gboolean
-webkit_page_get_editable (WebKitPage* page);
-
-WEBKIT_API void
-webkit_page_set_editable (WebKitPage* page, gboolean flag);
-
-G_END_DECLS
-
-#endif

WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
@@ -35,15 +35,15 @@
 #include "CString.h"
 #include "HitTestResult.h"
 #include "KURL.h"
-#include "webkitgtkpage.h"
-#include "webkitgtkprivate.h"
+#include "webkitwebview.h"
+#include "webkitprivate.h"
 #include "NotImplemented.h"
 #include "WindowFeatures.h"
 
 using namespace WebCore;
 
 namespace WebKit {
-ChromeClient::ChromeClient(WebKitPage* page)
+ChromeClient::ChromeClient(WebKitWebView* page)
     : m_webPage(page)
 {
 }
@@ -93,12 +93,12 @@ Page* ChromeClient::createWindow(Frame*, const FrameLoadRequest&, const WindowFe
         return 0;
     } else {
         /* TODO: FrameLoadRequest is not used */
-        WebKitPage* page = WEBKIT_PAGE_GET_CLASS(m_webPage)->create_page(m_webPage);
+        WebKitWebView* page = WEBKIT_WEB_VIEW_GET_CLASS(m_webPage)->create_web_view(m_webPage);
         if (!page)
             return 0;
 
-        WebKitPagePrivate *privateData = WEBKIT_PAGE_GET_PRIVATE(WEBKIT_PAGE(page));
-        return privateData->page;
+        WebKitWebViewPrivate *privateData = WEBKIT_WEB_VIEW_GET_PRIVATE(WEBKIT_WEB_VIEW(page));
+        return privateData->corePage;
     }
 }
 

WebKit/gtk/WebCoreSupport/ChromeClientGtk.h
@@ -32,14 +32,14 @@
 #include "ChromeClient.h"
 #include "KURL.h"
 
-typedef struct _WebKitPage WebKitPage;
+typedef struct _WebKitWebView WebKitWebView;
 
 namespace WebKit {
 
     class ChromeClient : public WebCore::ChromeClient {
     public:
-        ChromeClient(WebKitPage*);
-        WebKitPage* webPage() const { return m_webPage; }
+        ChromeClient(WebKitWebView*);
+        WebKitWebView* webPage() const { return m_webPage; }
 
         virtual void chromeDestroyed();
 
@@ -106,7 +106,7 @@ namespace WebKit {
         virtual unsigned long long requestQuotaIncreaseForDatabaseOperation(WebCore::Frame*, const WebCore::SecurityOriginData&, const WebCore::String&, unsigned long long);
 
     private:
-        WebKitPage* m_webPage;
+        WebKitWebView* m_webPage;
         WebCore::KURL m_hoveredLinkURL;
     };
 }

commit 0de524067df5b65d8df0ea4f2e6b7ad9a1b9e07d

2007-11-30  Jan Michael Alonzo  <jmalonzo@unpluggable.com>
        Fix for GTK+ Debug build breakage introduced in r28273.

        * WebView/webkitprivate.cpp:
        (WebKit::kit):


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@28274 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKit/gtk/WebView/webkitprivate.cpp
@@ -91,7 +91,7 @@ WebKitWebView* kit(WebCore::Page* corePage)
     if (!corePage)
         return 0;
 
-    ASSERT(webView->chrome());
+    ASSERT(corePage->chrome());
     WebKit::ChromeClient* client = static_cast<WebKit::ChromeClient*>(corePage->chrome()->client());
     return client ? client->webPage() : 0;
 }