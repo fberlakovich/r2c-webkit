fberlakovich/r2c-webkit

commit 52b1d8cc031a75a278a2bd83ed05180a90f12987

2009-03-19  Simon Fraser  <simon.fraser@apple.com>
        Reviewed by Dave Hyatt

        https://bugs.webkit.org/show_bug.cgi?id=24686

        When hit testing a RenderLayer whose parent lives in a preserves-3D hierarchy,
        we need to compare the computed z-offset with the depth-test z-offset before
        deciding that such a RenderLayer was hit. This fixes an issue, tested by the
        3d-point-mapping-overlapping.html test, where the child of a transformed element
        is found by hit testing, even when some other element with greater Z overlaps
        them both.

        Improved the code by adding a utility method, isHitCandidate(), which computes and tests
        z-depth when necessary.

        Tests: transforms/3d/point-mapping/3d-point-mapping-coplanar.html
               transforms/3d/point-mapping/3d-point-mapping-overlapping.html

        * rendering/RenderLayer.cpp:
        (WebCore::isHitCandidate):
        (WebCore::RenderLayer::hitTestLayer):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@41840 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/rendering/RenderLayer.cpp
@@ -2188,6 +2188,31 @@ PassRefPtr<HitTestingTransformState> RenderLayer::createLocalTransformState(Rend
     return transformState;
 }
 
+
+static bool isHitCandidate(const RenderLayer* hitLayer, bool canDepthSort, double* zOffset, const HitTestingTransformState* transformState)
+{
+    if (!hitLayer)
+        return false;
+
+    // The hit layer is depth-sorting with other layers, so just say that it was hit.
+    if (canDepthSort)
+        return true;
+    
+    // We need to look at z-depth to decide if this layer was hit.
+    if (zOffset) {
+        ASSERT(transformState);
+        // This is actually computing our z, but that's OK because the hitLayer is coplanar with us.
+        double childZOffset = computeZOffset(*transformState);
+        if (childZOffset > *zOffset) {
+            *zOffset = childZOffset;
+            return true;
+        }
+        return false;
+    }
+
+    return true;
+}
+
 // hitTestPoint and hitTestRect are relative to rootLayer.
 // A 'flattening' layer is one preserves3D() == false.
 // transformState.m_accumulatedTransform holds the transform from the containing flattening layer.
@@ -2300,17 +2325,14 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
     // Begin by walking our list of positive layers from highest z-index down to the lowest z-index.
     if (m_posZOrderList) {
         for (int i = m_posZOrderList->size() - 1; i >= 0; --i) {
-            RenderLayer* hitLayer = m_posZOrderList->at(i)->hitTestLayer(rootLayer, this, request, result, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
-            if (hitLayer) {
-                if (depthSortDescendants) {
-                    // Don't return yet. Keep a ref to the frontmost layer, and keep looking.
-                    candidateLayer = hitLayer;
-                } else {
-                    // Our container needs to know the z of the hit layer. This is actually computing our z, but that's OK because the hitLayer is coplanar with us.
-                    if (zOffset && !zOffsetForDescendantsPtr)
-                        *zOffset = computeZOffset(*unflattenedTransformState);
+            HitTestResult tempResult(result.point());
+            RenderLayer* hitLayer = m_posZOrderList->at(i)->hitTestLayer(rootLayer, this, request, tempResult, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
+            if (isHitCandidate(hitLayer, depthSortDescendants, zOffset, unflattenedTransformState.get())) {
+                result = tempResult;
+                if (!depthSortDescendants)
                     return hitLayer;
-                }
+
+                candidateLayer = hitLayer;
             }
         }
     }
@@ -2321,17 +2343,15 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
             RenderLayer* currLayer = m_normalFlowList->at(i);
             if (!currLayer->isSelfPaintingLayer())
                 continue;
+
+            HitTestResult tempResult(result.point());
             RenderLayer* hitLayer = currLayer->hitTestLayer(rootLayer, this, request, result, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
-            if (hitLayer) {
-                if (depthSortDescendants) {
-                    // Don't return yet. Keep a ref to the frontmost layer, and keep looking.
-                    candidateLayer = hitLayer;
-                } else {
-                    // Our container needs to know the z of the hit layer. This is actually computing our z, but that's OK because the hitLayer is coplanar with us.
-                    if (zOffset && !zOffsetForDescendantsPtr)
-                        *zOffset = computeZOffset(*unflattenedTransformState);
+            if (isHitCandidate(hitLayer, depthSortDescendants, zOffset, unflattenedTransformState.get())) {
+                result = tempResult;
+                if (!depthSortDescendants)
                     return hitLayer;
-                }
+
+                candidateLayer = hitLayer;
             }
         }
     }
@@ -2340,38 +2360,27 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
     if (fgRect.contains(hitTestPoint)) {
         // Hit test with a temporary HitTestResult, because we onlyl want to commit to 'result' if we know we're frontmost.
         HitTestResult tempResult(result.point());
-        if (hitTestContents(request, tempResult, layerBounds, hitTestPoint, HitTestDescendants)) {
-            if (zOffsetForContentsPtr) {
-                double foregroundZOffset = computeZOffset(*unflattenedTransformState);
-                if (foregroundZOffset > *zOffsetForContentsPtr) {
-                    *zOffsetForContentsPtr = foregroundZOffset;
-                    result = tempResult;
-                    if (depthSortDescendants)
-                        candidateLayer = this;
-                    else
-                        return this;
-                }
-            } else {
-                result = tempResult;
+        if (hitTestContents(request, tempResult, layerBounds, hitTestPoint, HitTestDescendants) &&
+            isHitCandidate(this, false, zOffsetForContentsPtr, unflattenedTransformState.get())) {
+            result = tempResult;
+            if (!depthSortDescendants)
                 return this;
-            }
+            // Foreground can depth-sort with descendant layers, so keep this as a candidate.
+            candidateLayer = this;
         }
     }
 
     // Now check our negative z-index children.
     if (m_negZOrderList) {
         for (int i = m_negZOrderList->size() - 1; i >= 0; --i) {
+            HitTestResult tempResult(result.point());
             RenderLayer* hitLayer = m_negZOrderList->at(i)->hitTestLayer(rootLayer, this, request, result, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
-            if (hitLayer) {
-                if (depthSortDescendants) {
-                    // Don't return yet. Keep a ref to the frontmost layer, and keep looking.
-                    candidateLayer = hitLayer;
-                } else {
-                    // Our container needs to know the z of the hit layer. This is actually computing our z, but that's OK because the hitLayer is coplanar with us.
-                    if (zOffset && !zOffsetForDescendantsPtr)
-                        *zOffset = computeZOffset(*unflattenedTransformState);
+            if (isHitCandidate(hitLayer, depthSortDescendants, zOffset, unflattenedTransformState.get())) {
+                result = tempResult;
+                if (!depthSortDescendants)
                     return hitLayer;
-                }
+
+                candidateLayer = hitLayer;
             }
         }
     }
@@ -2382,18 +2391,10 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
 
     if (bgRect.contains(hitTestPoint)) {
         HitTestResult tempResult(result.point());
-        if (hitTestContents(request, tempResult, layerBounds, hitTestPoint, HitTestSelf)) {
-            if (zOffsetForContentsPtr) {
-                double backgroundZOffset = computeZOffset(*unflattenedTransformState);
-                if (backgroundZOffset > *zOffsetForContentsPtr) {
-                    *zOffsetForContentsPtr = backgroundZOffset;
-                    result = tempResult;
-                    return this;
-                }
-            } else {
-                result = tempResult;
-                return this;
-            }
+        if (hitTestContents(request, tempResult, layerBounds, hitTestPoint, HitTestSelf) &&
+            isHitCandidate(this, false, zOffsetForContentsPtr, unflattenedTransformState.get())) {
+            result = tempResult;
+            return this;
         }
     }
     

LayoutTests/platform/mac/transforms/3d/point-mapping/3d-point-mapping-coplanar-expected.txt
@@ -1,0 +1,37 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (0,0) size 800x600 [border: (1px solid #000000)]
+      RenderBlock (anonymous) at (1,1) size 798x342
+        RenderBlock {DIV} at (20,20) size 302x302 [border: (1px solid #000000)]
+        RenderText {#text} at (0,0) size 0x0
+      RenderBlock {P} at (1,359) size 798x18
+        RenderText {#text} at (0,0) size 366x18
+          text run at (0,0) width 366: "When hit-testing coplanar elements, document order wins."
+layer at (30,400) size 306x72
+  RenderBlock (positioned) {DIV} at (30,400) size 306x72
+    RenderInline {SPAN} at (0,0) size 298x18 [color=#008000]
+      RenderText {#text} at (0,0) size 298x18
+        text run at (0,0) width 298: "PASS: event at (44, 44) hit box1 at offset (2, 2)"
+    RenderBR {BR} at (298,14) size 0x0
+    RenderInline {SPAN} at (0,0) size 298x18 [color=#008000]
+      RenderText {#text} at (0,18) size 298x18
+        text run at (0,18) width 298: "PASS: event at (70, 59) hit box2 at offset (2, 2)"
+    RenderBR {BR} at (298,32) size 0x0
+    RenderInline {SPAN} at (0,0) size 306x18 [color=#008000]
+      RenderText {#text} at (0,36) size 306x18
+        text run at (0,36) width 306: "PASS: event at (70, 101) hit box3 at offset (2, 2)"
+    RenderBR {BR} at (306,50) size 0x0
+    RenderInline {SPAN} at (0,0) size 306x18 [color=#008000]
+      RenderText {#text} at (0,54) size 306x18
+        text run at (0,54) width 306: "PASS: event at (70, 144) hit box4 at offset (2, 2)"
+    RenderBR {BR} at (306,68) size 0x0
+layer at (42,42) size 260x260
+  RenderBlock (relative positioned) {DIV} at (21,21) size 260x260 [bgcolor=#DDDDDD] [border: (1px solid #000000)]
+layer at (73,63) size 200x100
+  RenderBlock (positioned) {DIV} at (31,21) size 200x100 [bgcolor=#AAAAAA] [border: (1px solid #000000)]
+layer at (73,103) size 200x100
+  RenderBlock (positioned) {DIV} at (31,61) size 200x100 [bgcolor=#AAAAAA] [border: (1px solid #000000)]
+layer at (73,143) size 200x100
+  RenderBlock (positioned) {DIV} at (31,101) size 200x100 [bgcolor=#AAAAAA] [border: (1px solid #000000)]

LayoutTests/platform/mac/transforms/3d/point-mapping/3d-point-mapping-overlapping-expected.txt
@@ -1,0 +1,38 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (0,0) size 800x600 [border: (1px solid #000000)]
+      RenderBlock (anonymous) at (1,1) size 798x342
+        RenderText {#text} at (0,0) size 0x0
+      RenderBlock {P} at (1,359) size 798x18
+        RenderText {#text} at (0,0) size 602x18
+          text run at (0,0) width 602: "The green overlay is translated in Z by 100px, so should hit test in front relative to the blue box."
+layer at (30,400) size 345x72
+  RenderBlock (positioned) {DIV} at (30,400) size 345x72
+    RenderInline {SPAN} at (0,0) size 322x18 [color=#008000]
+      RenderText {#text} at (0,0) size 322x18
+        text run at (0,0) width 322: "PASS: event at (285, 50) hit box2 at offset (198, 2)"
+    RenderBR {BR} at (322,14) size 0x0
+    RenderInline {SPAN} at (0,0) size 322x18 [color=#008000]
+      RenderText {#text} at (0,18) size 322x18
+        text run at (0,18) width 322: "PASS: event at (174, 108) hit box3 at offset (50, 2)"
+    RenderBR {BR} at (322,32) size 0x0
+    RenderInline {SPAN} at (0,0) size 329x18 [color=#008000]
+      RenderText {#text} at (0,36) size 329x18
+        text run at (0,36) width 329: "PASS: event at (61, 50) hit overlay at offset (39, 28)"
+    RenderBR {BR} at (329,50) size 0x0
+    RenderInline {SPAN} at (0,0) size 345x18 [color=#008000]
+      RenderText {#text} at (0,54) size 345x18
+        text run at (0,54) width 345: "PASS: event at (119, 108) hit overlay at offset (97, 86)"
+    RenderBR {BR} at (345,68) size 0x0
+layer at (21,21) size 302x302
+  RenderBlock (relative positioned) {DIV} at (20,20) size 302x302 [border: (1px solid #000000)]
+layer at (42,42) size 260x260
+  RenderBlock (positioned) {DIV} at (21,21) size 260x260 [bgcolor=#DDDDDD] [border: (1px solid #000000)]
+layer at (73,63) size 200x201
+  RenderBlock (positioned) {DIV} at (31,21) size 200x200 [bgcolor=#AAAAAA] [border: (1px solid #000000)]
+layer at (124,114) size 100x100
+  RenderBlock (relative positioned) {DIV} at (51,51) size 100x100 [bgcolor=#0000FF]
+layer at (22,22) size 150x300
+  RenderBlock (positioned) {DIV} at (1,1) size 150x300 [bgcolor=#0080004C]

LayoutTests/transforms/3d/point-mapping/3d-point-mapping-coplanar.html
@@ -1,0 +1,98 @@
+<html>
+<head>
+  <title>Hit test coplanar elements</title>
+  <script src="point-mapping-helpers.js" type="text/javascript" charset="utf-8"></script>
+  
+  <script type="text/javascript" charset="utf-8">
+    
+    function test()
+    {
+      dispatchEvent(44, 44, 'box1', 2, 2);
+      dispatchEvent(70, 59, 'box2', 2, 2);
+      dispatchEvent(70, 101, 'box3', 2, 2);
+      dispatchEvent(70, 144, 'box4', 2, 2);
+    }
+    
+  </script>
+  <style type="text/css" media="screen">
+  
+    body {
+      margin: 0;
+      border: 1px solid black;
+      cursor: crosshair;
+    }
+
+    .test {
+      display: inline-block;
+      height: 300px;
+      width: 300px;
+      border: 1px solid black;
+      margin: 20px;
+    }
+
+    .box {
+      height: 200px;
+      width: 200px;
+      -webkit-box-sizing: border-box;
+      background-color: #DDD;
+      border: 1px solid black;
+    }
+
+    .box:hover {
+      outline: 3px solid orange;
+    }
+    
+    .container {
+      position: relative;
+      height: 260px;
+      width: 260px;
+      margin: 20px;
+      border: 1px solid black;
+      -webkit-box-sizing: border-box;
+      -webkit-perspective: 400;
+    }
+    
+    .transformed {
+      position: absolute;
+      top: 20px;
+      left: 30px;
+      height: 100px;
+      width: 200px;
+      border: 1px solid black;
+      background-color: #AAA;
+      -webkit-box-sizing: border-box;
+      -webkit-transform: translateZ(20px);
+    }
+    
+    #results {
+      position: absolute;
+      left: 30px;
+      top: 400px;
+    }
+    
+    #mousepos {
+      position: absolute;
+      left: 30px;
+      top: 700px;
+      color: gray;
+      font-size: smaller;
+    }
+  </style>
+</head>
+<body onclick="clicked(event)">
+
+<div id="results"></div>
+<div class="test">
+  <!-- Simple transformed div in perpsective -->
+  <div class="container box" id="box1">
+    <div class="transformed box" id="box2"></div>
+    <div class="transformed box" id="box3" style="top: 60px;"></div>
+    <div class="transformed box" id="box4" style="top: 100px;"></div>
+  </div>
+</div>
+<p>When hit-testing coplanar elements, document order wins.</p>
+
+<div id="mousepos"></div>
+
+</body>
+</html>

LayoutTests/transforms/3d/point-mapping/3d-point-mapping-overlapping.html
@@ -1,0 +1,117 @@
+<html>
+<head>
+  <title>Hit test overlapping 3d elements</title>
+  <script src="point-mapping-helpers.js" type="text/javascript" charset="utf-8"></script>
+  
+  <script type="text/javascript" charset="utf-8">
+    
+    function test()
+    {
+      dispatchEvent(285, 50, 'box2', 198, 2);
+      dispatchEvent(174, 108, 'box3', 50, 2);
+
+      dispatchEvent(61, 50, 'overlay', 39, 28);
+      dispatchEvent(119, 108, 'overlay', 97, 86);
+    }
+    
+  </script>
+  <style type="text/css" media="screen">
+  
+    body {
+      margin: 0;
+      border: 1px solid black;
+      cursor: crosshair;
+    }
+
+    .test {
+      display: inline-block;
+      position: relative;
+      height: 300px;
+      width: 300px;
+      border: 1px solid black;
+      margin: 20px;
+    }
+
+    .box {
+      height: 200px;
+      width: 200px;
+      -webkit-box-sizing: border-box;
+      background-color: #DDD;
+      border: 1px solid black;
+    }
+
+    .box:hover {
+      outline: 3px solid orange;
+    }
+    
+    .container {
+      position: absolute;
+      height: 260px;
+      width: 260px;
+      margin: 20px;
+      border: 1px solid black;
+      -webkit-box-sizing: border-box;
+      -webkit-perspective: 400;
+    }
+    
+    .transformed {
+      position: absolute;
+      top: 20px;
+      left: 30px;
+      height: 200px;
+      width: 200px;
+      border: 1px solid black;
+      background-color: #AAA;
+      -webkit-box-sizing: border-box;
+      -webkit-transform: translateZ(50px);
+    }
+    
+    #overlay {
+      position: absolute;
+      height: 300px;
+      width: 150px;
+      background-color: rgba(0, 128, 0, 0.3);
+      -webkit-transform: translateZ(100px);
+    }
+    
+    .inner {
+      position: relative;
+      background-color: blue;
+      height: 100px;
+      width: 100px;
+      margin: 50px;
+    }
+
+    #results {
+      position: absolute;
+      left: 30px;
+      top: 400px;
+    }
+    
+    #mousepos {
+      position: absolute;
+      left: 30px;
+      top: 700px;
+      color: gray;
+      font-size: smaller;
+    }
+  </style>
+</head>
+<body onclick="clicked(event)">
+
+<div id="results"></div>
+<div class="test">
+  <!-- Simple transformed div in perpsective -->
+  <div class="container box" id="box1">
+    <div class="transformed box" id="box2">
+      <div class="inner" id="box3"></div>
+    </div>
+  </div>
+  <div id="overlay"></div>
+</div>
+<p>The green overlay is translated in Z by 100px, so should hit test in front relative to the blue box.</p>
+
+<div id="mousepos"></div>
+
+</body>
+</html>

commit 0020b88b14f52f912cfe3328a4fb75675ead93e4

2009-03-22  Simon Fraser  <simon.fraser@apple.com>
        Reviewed by Dan Bernstein

        https://bugs.webkit.org/show_bug.cgi?id=24743

        Fix hit testing regression from r41840. We need to pass the temporary
        HitTestResult when testing sublayers, then only copy to 'result' when
        the layer is known to have been hit.

        Test: fast/layers/zindex-hit-test.html

        * rendering/RenderLayer.cpp:
        (WebCore::RenderLayer::hitTestLayer):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@41900 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/rendering/RenderLayer.cpp
@@ -2345,7 +2345,7 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
                 continue;
 
             HitTestResult tempResult(result.point());
-            RenderLayer* hitLayer = currLayer->hitTestLayer(rootLayer, this, request, result, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
+            RenderLayer* hitLayer = currLayer->hitTestLayer(rootLayer, this, request, tempResult, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
             if (isHitCandidate(hitLayer, depthSortDescendants, zOffset, unflattenedTransformState.get())) {
                 result = tempResult;
                 if (!depthSortDescendants)
@@ -2374,7 +2374,7 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
     if (m_negZOrderList) {
         for (int i = m_negZOrderList->size() - 1; i >= 0; --i) {
             HitTestResult tempResult(result.point());
-            RenderLayer* hitLayer = m_negZOrderList->at(i)->hitTestLayer(rootLayer, this, request, result, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
+            RenderLayer* hitLayer = m_negZOrderList->at(i)->hitTestLayer(rootLayer, this, request, tempResult, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
             if (isHitCandidate(hitLayer, depthSortDescendants, zOffset, unflattenedTransformState.get())) {
                 result = tempResult;
                 if (!depthSortDescendants)

LayoutTests/fast/layers/zindex-hit-test-expected.txt
@@ -1,0 +1,2 @@
+Div with z-index: -100
+Hit testing found div "main": PASS

LayoutTests/fast/layers/zindex-hit-test.html
@@ -1,0 +1,44 @@
+<!DOCTYPE html>
+<head>
+  <style type="text/css">
+  #main {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    width: 200px;
+    height: 100px;
+    z-index: -100;
+    background-color: #eee;
+  }
+  #main:hover {
+    background-color: orange;
+  }
+  
+  #results {
+    margin-top: 150px;
+  }
+  </style>
+  <script type="text/javascript" charset="utf-8">
+    if (window.layoutTestController)
+      layoutTestController.dumpAsText();
+
+    function runTest()
+    {
+      var hitElement = document.elementFromPoint(50, 50);
+      var results = document.getElementById('results');
+      var main = document.getElementById('main');
+      if (hitElement == main)
+        results.innerHTML = 'Hit testing found div "main": PASS';
+      else
+        results.innerHTML = 'Hit testing did not find div "main": FAIL';
+    }
+  </script>
+</head>
+<body onload="runTest()">
+    <div id="main">
+      Div with z-index: -100
+    </div>
+    <div id="results">
+    </div>
+</body>
+</html>