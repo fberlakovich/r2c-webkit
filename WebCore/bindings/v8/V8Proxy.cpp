fberlakovich/r2c-webkit

commit 1aaa57c5e862385a79307bfa3ba5e6910bd42a0d

2009-11-26  Søren Gjesse  <sgjesse@chromium.org>
        Reviewed by Pavel Feldman.

        [V8] Avoid using JavaScript objects as context data
        https://bugs.webkit.org/show_bug.cgi?id=31873

        Change the context "data" from a JavaScript object holding the two properties type and value to
        a string holding type and value separated by a comma.

        * bindings/v8/V8Proxy.cpp:
        (WebCore::V8Proxy::setInjectedScriptContextDebugId):
        (WebCore::V8Proxy::setContextDebugId):
        (WebCore::V8Proxy::contextDebugId):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51407 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/bindings/v8/V8Proxy.cpp
@@ -54,12 +54,14 @@
 #include "WorkerContextExecutionProxy.h"
 
 #include <algorithm>
+#include <stdio.h>
 #include <utility>
 #include <v8.h>
 #include <v8-debug.h>
 #include <wtf/Assertions.h>
 #include <wtf/OwnArrayPtr.h>
 #include <wtf/StdLibExtras.h>
+#include <wtf/StringExtras.h>
 #include <wtf/UnusedParam.h>
 
 namespace WebCore {
@@ -68,9 +70,6 @@ v8::Persistent<v8::Context> V8Proxy::m_utilityContext;
 
 // Static list of registered extensions
 V8Extensions V8Proxy::m_extensions;
-
-const char* V8Proxy::kContextDebugDataType = "type";
-const char* V8Proxy::kContextDebugDataValue = "value";
 
 void batchConfigureAttributes(v8::Handle<v8::ObjectTemplate> instance, 
                               v8::Handle<v8::ObjectTemplate> proto, 
@@ -387,24 +386,16 @@ bool V8Proxy::setInjectedScriptContextDebugId(v8::Handle<v8::Context> targetCont
 {
     // Setup context id for JS debugger.
     v8::Context::Scope contextScope(targetContext);
-    v8::Handle<v8::Object> contextData = v8::Object::New();
-    if (contextData.IsEmpty())
-        return false;
-
     if (m_context.IsEmpty())
         return false;
-    v8::Handle<v8::Value> windowContextData = m_context->GetData();
-    if (windowContextData->IsObject()) {
-        v8::Handle<v8::String> propertyName = v8::String::New(kContextDebugDataValue);
-        if (propertyName.IsEmpty())
-            return false;
-        contextData->Set(propertyName, v8::Object::Cast(*windowContextData)->Get(propertyName));
-    }
-    v8::Handle<v8::String> propertyName = v8::String::New(kContextDebugDataType);
-    if (propertyName.IsEmpty())
+    int debugId = contextDebugId(m_context);
+    if (debugId == -1)
         return false;
-    contextData->Set(propertyName, v8::String::New("injected"));
-    targetContext->SetData(contextData);
+
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "injected,%d", debugId);
+    targetContext->SetData(v8::String::New(buffer));
+
     return true;
 }
 
@@ -1366,20 +1357,22 @@ bool V8Proxy::setContextDebugId(int debugId)
         return false;
 
     v8::Context::Scope contextScope(m_context);
-    v8::Handle<v8::Object> contextData = v8::Object::New();
-    contextData->Set(v8::String::New(kContextDebugDataType), v8::String::New("page"));
-    contextData->Set(v8::String::New(kContextDebugDataValue), v8::Integer::New(debugId));
-    m_context->SetData(contextData);
+
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "page,%d", debugId);
+    m_context->SetData(v8::String::New(buffer));
+
     return true;
 }
 
 int V8Proxy::contextDebugId(v8::Handle<v8::Context> context)
 {
     v8::HandleScope scope;
-    if (!context->GetData()->IsObject())
+    if (!context->GetData()->IsString())
         return -1;
-    v8::Handle<v8::Value> data = context->GetData()->ToObject()->Get( v8::String::New(kContextDebugDataValue));
-    return data->IsInt32() ? data->Int32Value() : -1;
+    v8::String::AsciiValue ascii(context->GetData());
+    char* comma = strnstr(*ascii, ",", ascii.length());
+    return atoi(comma + 1);
 }
 
 v8::Handle<v8::Value> V8Proxy::getHiddenObjectPrototype(v8::Handle<v8::Context> context)

WebCore/bindings/v8/V8Proxy.h
@@ -365,9 +365,6 @@ namespace WebCore {
         void updateDocumentWrapper(v8::Handle<v8::Value> wrapper);
 
     private:
-        static const char* kContextDebugDataType;
-        static const char* kContextDebugDataValue;
-
         void setSecurityToken();
         void clearDocumentWrapper();
 

commit 082e022e935977556ed4329c872e9f2559413606

2009-12-09  Søren Gjesse  <sgjesse@chromium.org>
        Reviewed by Pavel Feldman.

        [V8] Isolated world reuse failure
        https://bugs.webkit.org/show_bug.cgi?id=32306

        Fixed bug introduced in r51407: (http://trac.webkit.org/changeset/51407) which caused layout test
        LayoutTests/http/tests/security/isolatedWorld/world-reuse.html to fail.

        * bindings/v8/V8Proxy.cpp:
        (WebCore::V8Proxy::setInjectedScriptContextDebugId):
        (WebCore::V8Proxy::contextDebugId):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51912 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/bindings/v8/V8Proxy.cpp
@@ -389,11 +389,12 @@ bool V8Proxy::setInjectedScriptContextDebugId(v8::Handle<v8::Context> targetCont
     if (m_context.IsEmpty())
         return false;
     int debugId = contextDebugId(m_context);
-    if (debugId == -1)
-        return false;
 
     char buffer[32];
-    snprintf(buffer, sizeof(buffer), "injected,%d", debugId);
+    if (debugId == -1)
+        snprintf(buffer, sizeof(buffer), "injected");
+    else
+        snprintf(buffer, sizeof(buffer), "injected,%d", debugId);
     targetContext->SetData(v8::String::New(buffer));
 
     return true;
@@ -1378,6 +1379,8 @@ int V8Proxy::contextDebugId(v8::Handle<v8::Context> context)
         return -1;
     v8::String::AsciiValue ascii(context->GetData());
     char* comma = strnstr(*ascii, ",", ascii.length());
+    if (!comma)
+        return -1;
     return atoi(comma + 1);
 }
 