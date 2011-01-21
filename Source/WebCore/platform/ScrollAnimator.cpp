fberlakovich/r2c-webkit

commit 4876ff46df3e9d727ab4c15b5336c123ffc2f830

Cleanup Scrollbar/ScrollbarClient relationship https://bugs.webkit.org/show_bug.cgi?id=52779
Reviewed by Dave Hyatt.

Source/WebCore: 

Pipe all scrolling through the ScrollbarClient/ScrollAnimator
rather than through the Scrollbar. The Scrollbar now is just
a "view" on the scroll position of the scrollable area it is
attached to.

There are now two ways to scroll a scrollable area:
- ScrollbarClient::scroll()
- ScrollbarClient::scrollToOffsetWithoutAnimation()

Both of these go through the ScrollAnimator (updating its state
or starting an animation). The ScrollAnimator, in turn, now calls
ScrollbarClient::setScrollOffsetFromAnimation, which tells the
Scrollbars to pull a new offset (via Scrollbar::offsetDidChange)
and tells the class that derives from ScrollbarClient to scroll
its contents (via ScrollbarClient::setScrollOffset).

* WebCore.xcodeproj/project.pbxproj:
Move Scrollbar.cpp to the right place.

* accessibility/AccessibilityScrollbar.cpp:
(WebCore::AccessibilityScrollbar::setValue):
Initiate the scroll through the scrollbar client, rather than the
scrollbar itself.

* page/FrameView.cpp:
(WebCore::FrameView::scrollTo):
* page/FrameView.h:
Condense the two valueChanged overrides to a single override of the
scrollTo function.

* platform/ScrollAnimator.cpp:
(WebCore::ScrollAnimator::scroll):
(WebCore::ScrollAnimator::scrollToOffsetWithoutAnimation):
(WebCore::ScrollAnimator::currentPosition):
(WebCore::ScrollAnimator::notityPositionChanged):
* platform/ScrollAnimator.h:
* platform/ScrollAnimatorWin.cpp:
(WebCore::ScrollAnimatorWin::scrollToOffsetWithoutAnimation):
(WebCore::ScrollAnimatorWin::animateScroll):
* platform/ScrollAnimatorWin.h:
* platform/mac/ScrollAnimatorMac.h:
* platform/mac/ScrollAnimatorMac.mm:
(WebCore::ScrollAnimatorMac::scrollToOffsetWithoutAnimation):
(WebCore::ScrollAnimatorMac::immediateScrollToPoint):
Change setScrollPositionAndStopAnimation to scrollToOffsetWithoutAnimation

Source/WebCore/platform/ScrollAnimator.cpp
@@ -31,6 +31,7 @@
 #include "config.h"
 #include "ScrollAnimator.h"
 
+#include "FloatPoint.h"
 #include "ScrollbarClient.h"
 #include <algorithm>
 
@@ -61,16 +62,27 @@ bool ScrollAnimator::scroll(ScrollbarOrientation orientation, ScrollGranularity,
     if (*currentPos == newPos)
         return false;
     *currentPos = newPos;
-    m_client->setScrollOffsetFromAnimation(IntPoint(m_currentPosX, m_currentPosY));
+
+    notityPositionChanged();
+
     return true;
 }
 
-void ScrollAnimator::setScrollPositionAndStopAnimation(ScrollbarOrientation orientation, float pos)
+void ScrollAnimator::scrollToOffsetWithoutAnimation(const FloatPoint& offset)
 {
-    if (orientation == HorizontalScrollbar)
-        m_currentPosX = pos;
-    else
-        m_currentPosY = pos;
+    m_currentPosX = offset.x();
+    m_currentPosY = offset.y();
+    notityPositionChanged();
+}
+
+FloatPoint ScrollAnimator::currentPosition() const
+{
+    return FloatPoint(m_currentPosX, m_currentPosY);
+}
+
+void ScrollAnimator::notityPositionChanged()
+{
+    m_client->setScrollOffsetFromAnimation(IntPoint(m_currentPosX, m_currentPosY));
 }
 
 } // namespace WebCore

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -11415,7 +11415,7 @@
 		BCA8CA5D11E4E6D100812FB7 /* BackForwardListImpl.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = BackForwardListImpl.cpp; sourceTree = "<group>"; };
 		BCA8CA5E11E4E6D100812FB7 /* BackForwardListImpl.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = BackForwardListImpl.h; sourceTree = "<group>"; };
 		BCA979161215D055005C485C /* ImageBufferData.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ImageBufferData.h; sourceTree = "<group>"; };
-		BCAA90C20A7EBA60008B1229 /* Scrollbar.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; name = Scrollbar.cpp; path = platform/Scrollbar.cpp; sourceTree = SOURCE_ROOT; };
+		BCAA90C20A7EBA60008B1229 /* Scrollbar.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = Scrollbar.cpp; sourceTree = "<group>"; };
 		BCACF3BA1072921A00C0C8A3 /* UserContentURLPattern.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = UserContentURLPattern.cpp; sourceTree = "<group>"; };
 		BCACF3BB1072921A00C0C8A3 /* UserContentURLPattern.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = UserContentURLPattern.h; sourceTree = "<group>"; };
 		BCAE1FA512939DB7004CB026 /* ScrollAnimatorMac.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ScrollAnimatorMac.h; sourceTree = "<group>"; };
@@ -13890,7 +13890,6 @@
 				1CE24F960D7CAF0E007E04C2 /* SchedulePairMac.mm */,
 				BCAE1FA512939DB7004CB026 /* ScrollAnimatorMac.h */,
 				BC51156D12B1749C00C96754 /* ScrollAnimatorMac.mm */,
-				BCAA90C20A7EBA60008B1229 /* Scrollbar.cpp */,
 				BC8B853C0E7C7F1100AB6984 /* ScrollbarThemeMac.h */,
 				BCEF869E0E844E9D00A85CD5 /* ScrollbarThemeMac.mm */,
 				9353676A09AED88B00D35CD6 /* ScrollViewMac.mm */,
@@ -18278,6 +18277,7 @@
 				5162C7F311F77EFB00612EFE /* SchemeRegistry.h */,
 				CA3BF67B10D99BAE00E6CE53 /* ScrollAnimator.cpp */,
 				CA3BF67D10D99BAE00E6CE53 /* ScrollAnimator.h */,
+				BCAA90C20A7EBA60008B1229 /* Scrollbar.cpp */,
 				BC7B2AF80450824100A8000F /* Scrollbar.h */,
 				BC9BC64B0E7C4889008B9849 /* ScrollbarClient.cpp */,
 				BC9BC64C0E7C4889008B9849 /* ScrollbarClient.h */,

Source/WebCore/accessibility/AccessibilityScrollbar.cpp
@@ -97,9 +97,11 @@ void AccessibilityScrollbar::setValue(float value)
     if (!m_scrollbar)
         return;
     
+    if (!m_scrollbar->client())
+        return;
+
     float newValue = value * m_scrollbar->maximum();
-    
-    m_scrollbar->setValue(newValue, Scrollbar::NotFromScrollAnimator);    
+    m_scrollbar->client()->scrollToOffsetWithoutAnimation(m_scrollbar->orientation(), newValue);
 }
     
 } // namespace WebCore

Source/WebCore/page/FrameView.cpp
@@ -1956,20 +1956,12 @@ bool FrameView::isActive() const
     return page && page->focusController()->isActive();
 }
 
-void FrameView::valueChanged(Scrollbar* bar)
+void FrameView::scrollTo(const IntSize& newOffset)
 {
-    // Figure out if we really moved.
     IntSize offset = scrollOffset();
-    ScrollView::valueChanged(bar);
+    ScrollView::scrollTo(newOffset);
     if (offset != scrollOffset())
         scrollPositionChanged();
-    frame()->loader()->client()->didChangeScrollOffset();
-}
-
-void FrameView::valueChanged(const IntSize& scrollDelta)
-{
-    ScrollView::valueChanged(scrollDelta);
-    frame()->eventHandler()->sendScrollEvent();
     frame()->loader()->client()->didChangeScrollOffset();
 }
 

Source/WebCore/page/FrameView.h
@@ -295,11 +295,11 @@ private:
     virtual IntPoint convertFromContainingView(const IntPoint&) const;
 
     // ScrollBarClient interface
-    virtual void valueChanged(Scrollbar*);
-    virtual void valueChanged(const IntSize&);
     virtual void invalidateScrollbarRect(Scrollbar*, const IntRect&);
     virtual bool isActive() const;
     virtual void getTickmarks(Vector<IntRect>&) const;
+
+    virtual void scrollTo(const IntSize&);
 
     void deferredRepaintTimerFired(Timer<FrameView>*);
     void doDeferredRepaints();

Source/WebCore/platform/ScrollAnimator.h
@@ -35,6 +35,7 @@
 
 namespace WebCore {
 
+class FloatPoint;
 class ScrollbarClient;
 
 class ScrollAnimator {
@@ -50,16 +51,18 @@ public:
     // The base class implementation always scrolls immediately, never animates.
     virtual bool scroll(ScrollbarOrientation, ScrollGranularity, float step, float multiplier);
 
-    // Stops any animation in the given direction and updates the ScrollAnimator
-    // with the current scroll position.  This does not cause a callback to the
-    // ScrollbarClient.
-    virtual void setScrollPositionAndStopAnimation(ScrollbarOrientation, float);
+    virtual void scrollToOffsetWithoutAnimation(const FloatPoint&);
+
+    FloatPoint currentPosition() const;
 
 protected:
+    void notityPositionChanged();
+
     ScrollbarClient* m_client;
     float m_currentPosX; // We avoid using a FloatPoint in order to reduce
     float m_currentPosY; // subclass code complexity.
 };
 
 } // namespace WebCore
-#endif
+
+#endif // ScrollAnimator_h

Source/WebCore/platform/ScrollAnimatorWin.cpp
@@ -34,6 +34,7 @@
 
 #include "ScrollAnimatorWin.h"
 
+#include "FloatPoint.h"
 #include "ScrollbarClient.h"
 #include "ScrollbarTheme.h"
 #include <algorithm>
@@ -177,17 +178,24 @@ bool ScrollAnimatorWin::scroll(ScrollbarOrientation orientation, ScrollGranulari
     return true;
 }
 
-void ScrollAnimatorWin::setScrollPositionAndStopAnimation(ScrollbarOrientation orientation, float pos)
+void ScrollAnimatorWin::scrollToOffsetWithoutAnimation(const FloatPoint& offset)
 {
-    PerAxisData* data = (orientation == HorizontalScrollbar) ? &m_horizontalData : &m_verticalData;
-    stopAnimationTimerIfNeeded(data);
-    *data->m_currentPos = pos;
-    data->m_desiredPos = pos;
-    data->m_currentVelocity = 0;
-    data->m_desiredVelocity = 0;
+    stopAnimationTimerIfNeeded(&m_horizontalData);
+    stopAnimationTimerIfNeeded(&m_verticalData);
+
+    *m_horizontalData.m_currentPos = offset.x();
+    m_horizontalData.m_desiredPos = offset.x();
+    m_horizontalData.m_currentVelocity = 0;
+    m_horizontalData.m_desiredVelocity = 0;
+
+    *m_verticalData.m_currentPos = offset.y();
+    m_verticalData.m_desiredPos = offset.y();
+    m_verticalData.m_currentVelocity = 0;
+    m_verticalData.m_desiredVelocity = 0;
+
+    notityPositionChanged();
 }
 
-// static
 double ScrollAnimatorWin::accelerationTime()
 {
     // We elect to use ScrollbarTheme::nativeTheme()->autoscrollTimerDelay() as
@@ -293,7 +301,8 @@ void ScrollAnimatorWin::animateScroll(PerAxisData* data)
         data->m_animationTimer.startOneShot(animationTimerDelay);
         data->m_lastAnimationTime = WTF::currentTime();
     }
-    m_client->setScrollOffsetFromAnimation(IntPoint(*m_horizontalData.m_currentPos, *m_verticalData.m_currentPos));
+
+    notityPositionChanged();
 }
 
 } // namespace WebCore

Source/WebCore/platform/ScrollAnimatorWin.h
@@ -44,7 +44,7 @@ public:
     virtual ~ScrollAnimatorWin();
 
     virtual bool scroll(ScrollbarOrientation, ScrollGranularity, float step, float multiplier);
-    virtual void setScrollPositionAndStopAnimation(ScrollbarOrientation, float);
+    virtual void scrollToOffsetWithoutAnimation(const FloatPoint&);
 
 private:
     struct PerAxisData {
@@ -69,8 +69,8 @@ private:
     PerAxisData m_verticalData;
 };
 
-}
+} // namespace WebCore
 
 #endif // ENABLE(SMOOTH_SCROLLING)
 
-#endif
+#endif // ScrollAnimatorWin_h

commit 1c322d5db784d7a459ebf28db7a1f0a8e24b3f3c

2011-01-21  Tony Chang  <tony@chromium.org>
        Reviewed by Sam Weinig.

        reduce number of FrameLoaderClient::didChangeScrollOffset calls
        https://bugs.webkit.org/show_bug.cgi?id=52915

        Only notify of changes in scroll offset when there actually is a change.
        This regressed in r76291.

        Covered by Chromium browser_tests.

        * platform/ScrollAnimator.cpp:
        (WebCore::ScrollAnimator::scrollToOffsetWithoutAnimation):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@76395 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/ScrollAnimator.cpp
@@ -71,9 +71,11 @@ bool ScrollAnimator::scroll(ScrollbarOrientation orientation, ScrollGranularity,
 
 void ScrollAnimator::scrollToOffsetWithoutAnimation(const FloatPoint& offset)
 {
-    m_currentPosX = offset.x();
-    m_currentPosY = offset.y();
-    notityPositionChanged();
+    if (m_currentPosX != offset.x() || m_currentPosY != offset.y()) {
+        m_currentPosX = offset.x();
+        m_currentPosY = offset.y();
+        notityPositionChanged();
+    }
 }
 
 FloatPoint ScrollAnimator::currentPosition() const