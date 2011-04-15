fberlakovich/r2c-webkit

commit 3312ed311b53e34eafbe27d86b43f02fddddcaa8

2011-04-15  Alice Boxhall  <aboxhall@chromium.org>
        Reviewed by Ryosuke Niwa.

        Text selection changes unexpectedly when dragging out of the <input>
        https://bugs.webkit.org/show_bug.cgi?id=55552

        Tests that dragging outside of a contenteditable, input or textarea selects to the end of the
        element, rather than jumping back to the beginning.

        * editing/selection/resources/select-out-of-floated-editable.js: Added.
        (log):
        (else.window.onmouseup):
        (getSelectionStart):
        (getSelectionEnd):
        (checkSelection):
        * editing/selection/select-out-of-editable-expected.txt: Added.
        * editing/selection/select-out-of-editable.html: Added.
        * editing/selection/select-out-of-floated-contenteditable-expected.txt: Added.
        * editing/selection/select-out-of-floated-contenteditable.html: Added.
        * editing/selection/select-out-of-floated-input-expected.txt: Added.
        * editing/selection/select-out-of-floated-input.html: Added.
        * editing/selection/select-out-of-floated-textarea-expected.txt: Added.
        * editing/selection/select-out-of-floated-textarea.html: Added.
2011-04-15  Alice Boxhall  <aboxhall@chromium.org>

        Reviewed by Ryosuke Niwa.

        Text selection changes unexpectedly when dragging out of the <input>
        https://bugs.webkit.org/show_bug.cgi?id=55552

        Tests: editing/selection/select-out-of-editable.html
               editing/selection/select-out-of-floated-contenteditable.html
               editing/selection/select-out-of-floated-input.html
               editing/selection/select-out-of-floated-textarea.html

        * page/EventHandler.cpp:
        (WebCore::selectionExtentRespectingEditingBoundary): When dragging from an editable element, check that
        the endpoint is not outside the element. If it is, translate the point into a local point within
        the editable element.

LayoutTests/editing/selection/resources/select-out-of-floated-editable.js
@@ -1,0 +1,47 @@
+function log(msg) {
+    document.getElementById('console').appendChild(document.createTextNode(msg + '\n'));
+}
+
+if (window.layoutTestController && window.eventSender) {
+    layoutTestController.dumpAsText();
+
+    var x = floatedEditable.offsetLeft + (floatedEditable.offsetWidth / 2);
+    var y = floatedEditable.offsetTop + (floatedEditable.offsetHeight / 2);
+    eventSender.mouseMoveTo(x, y);
+    eventSender.mouseDown();
+    x = floatedEditable.offsetLeft + floatedEditable.offsetWidth + 5;
+    eventSender.mouseMoveTo(x, y);
+    eventSender.leapForward(250);
+    eventSender.mouseUp();
+
+    checkSelection();
+} else {
+    window.onmouseup = function() {
+        window.setTimeout(function() {
+            log('Input selection start: ' + getSelectionStart(floatedEditable) + ', end: ' +
+                getSelectionEnd(floatedEditable));
+            checkSelection();
+        }, 0);  // Without a timeout the selection is inaccurately printed
+    }
+}
+
+function getSelectionStart(element) {
+    return element.isContentEditable ? window.getSelection().baseOffset : element.selectionStart;
+}
+
+function getSelectionEnd(element) {
+    return element.isContentEditable ? window.getSelection().extentOffset : element.selectionEnd;
+}
+
+function checkSelection() {
+    var inputText = floatedEditable.isContentEditable ? floatedEditable.textContent : floatedEditable.value;
+    var selectionStart = getSelectionStart(floatedEditable);
+    var selectionEnd = getSelectionEnd(floatedEditable);
+
+    var selectionStartsFromMiddle = selectionStart > 0 && selectionStart < inputText.length;
+    var selectionGoesToEnd = selectionEnd == inputText.length;
+    if (selectionStartsFromMiddle && selectionGoesToEnd)
+        result.innerHTML = '<span style="padding: 5px; background-color: green">SUCCESS</span>';
+    else
+        result.innerHTML = '<span style="padding: 5px; background-color: red">FAIL</span>';
+}

LayoutTests/editing/selection/select-out-of-editable-expected.txt
@@ -1,0 +1,4 @@
+hello world
+Dragging to the left: PASS
+Dragging to the left: PASS
+

LayoutTests/editing/selection/select-out-of-editable.html
@@ -1,0 +1,61 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
+<p id="description">This test ensures that when editable text is selected by a drag that ends outside of the editable region, selection is extended to the either end of the text. See the <a href="https://bugs.webkit.org/show_bug.cgi?id=52986">bug 52986</a> for more details.</p> 
+<div id="test" contenteditable style="width: 200px; margin: 20px; border: solid 1px; font-size: x-large;">hello world</div> 
+<script> 
+ 
+function selectTextByDrag(div, endX, endY) {
+    window.getSelection().removeAllRanges();
+ 
+    eventSender.mouseMoveTo(div.offsetLeft + (div.offsetWidth / 2) , div.offsetTop + (div.offsetHeight / 2));
+    eventSender.mouseDown();
+ 
+    eventSender.leapForward(200);
+ 
+    // Without this, drag doesn't start selection.
+    eventSender.mouseMoveTo(div.offsetLeft + (div.offsetWidth / 3) , div.offsetTop + (div.offsetHeight / 3));
+    eventSender.mouseDown();
+ 
+    eventSender.leapForward(200);
+ 
+    eventSender.mouseMoveTo(endX, endY);
+    eventSender.mouseUp();
+}
+ 
+var log = '';
+ 
+function assertEqual(actual, expected) {
+    if (actual == expected)
+        log += 'PASS';
+    else
+        log += 'FAIL - expected ' + expected + ' but got ' + actual;
+    log += '\n';
+}
+ 
+if (!window.layoutTestController || !window.eventSender)
+    document.writeln("This test requires eventSender since the bug does not reproduce reliably when tested manually.");
+else {
+    layoutTestController.dumpAsText();
+    var description = document.getElementById('description');
+    description.style.display = 'none';
+ 
+    var div = document.getElementById('test');
+ 
+    log += 'Dragging to the left: ';
+    selectTextByDrag(div, div.offsetLeft - 10, div.offsetTop - 10);
+    assertEqual(window.getSelection().getRangeAt(0).startOffset, 0);
+ 
+    log += 'Dragging to the left: ';
+    selectTextByDrag(div, div.offsetLeft + 300, div.offsetTop + 100);
+    assertEqual(window.getSelection().getRangeAt(0).endOffset, div.innerText.length);
+ 
+    // Having the log in DOM will interfere with the test so append it at last.
+    var console = document.createElement('pre');
+    document.body.appendChild(console);
+    console.innerHTML = log;
+}
+ 
+</script> 
+</body> 
+</html> 

LayoutTests/editing/selection/select-out-of-floated-contenteditable-expected.txt
@@ -1,0 +1,6 @@
+Some text before the floats. Drag from the middle. Some floated text in a div. Some text after the floats.
+To test manually, drag from the middle of the editable div to the right, into the non-floated text. The selection should go to the end of the input element and not jump to the beginning.
+
+SUCCESS
+
+

LayoutTests/editing/selection/select-out-of-floated-contenteditable.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html>
+Some text before the floats.
+<div contenteditable=true id="floatedEditable" style="float: left;">
+  Drag from the middle.
+</div>
+<div style="width: 100%; float: left;">
+  Some floated text in a div.
+</div>
+Some text after the floats.
+<p style="padding-top: 1em;">
+To test manually, drag from the middle of the editable div to the
+right, into the non-floated text. The selection should go to the end
+of the input element and not jump to the beginning.
+</p>
+<p id="result"></p>
+<pre id="console"></pre>
+<script src="resources/select-out-of-floated-editable.js"></script>
+

LayoutTests/editing/selection/select-out-of-floated-input-expected.txt
@@ -1,0 +1,6 @@
+Some text before the floats. Some floated text in a div. Some text after the floats.
+To test manually, drag from the middle of the input element to the right, into the non-floated text. The selection should go to the end of the input element and not jump to the beginning.
+
+SUCCESS
+
+

LayoutTests/editing/selection/select-out-of-floated-input.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html>
+Some text before the floats.
+<div style="float: left;">
+  <input type="text" id="floatedEditable" value="Drag from the middle.">
+</div>
+<div style="width: 100%; float: left;">
+  Some floated text in a div.
+</div>
+Some text after the floats.
+<p style="padding-top: 1em;">
+To test manually, drag from the middle of the input element to the
+right, into the non-floated text. The selection should go to the end
+of the input element and not jump to the beginning.
+</p>
+<p id="result"></p>
+<pre id="console"></pre>
+<script src="resources/select-out-of-floated-editable.js"></script>
+

LayoutTests/editing/selection/select-out-of-floated-textarea-expected.txt
@@ -1,0 +1,6 @@
+Some text before the floats. Some floated text in a div. Some text after the floats.
+To test manually, drag from the middle of the textarea to the right, into the non-floated text. The selection should go to the end of the input element and not jump to the beginning.
+
+SUCCESS
+
+

commit 1cda94b9cced6d5fbc6f8d810adccf84c2301bc5

[Windows Tests] fast/forms/input-text-drag-down.html and editing/selection/select-from-textfield-outwards.html failing since r83967. https://bugs.webkit.org/show_bug.cgi?id=58664
Adding Windows-specific expected failing results to get the bots green.

* platform/win/editing/selection/select-from-textfield-outwards-expected.checksum: Added.
* platform/win/editing/selection/select-from-textfield-outwards-expected.png: Added.
* platform/win/editing/selection/select-from-textfield-outwards-expected.txt: Added.
* platform/win/fast/forms/input-text-drag-down-expected.checksum: Added.
* platform/win/fast/forms/input-text-drag-down-expected.png: Added.
* platform/win/fast/forms/input-text-drag-down-expected.txt: Added.



git-svn-id: https://svn.webkit.org/repository/webkit/trunk@83976 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/win/editing/selection/select-from-textfield-outwards-expected.checksum
@@ -1,0 +1,1 @@
+686c1deedd2005fad95057604c195c22

LayoutTests/platform/win/editing/selection/select-from-textfield-outwards-expected.txt
@@ -1,0 +1,38 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x576
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 104x18
+          text run at (0,0) width 104: "This is a test for "
+        RenderInline {I} at (0,0) size 755x36
+          RenderInline {A} at (0,0) size 348x18 [color=#0000EE]
+            RenderText {#text} at (104,0) size 348x18
+              text run at (104,0) width 348: "http://bugzilla.opendarwin.org/show_bug.cgi?id=9312"
+          RenderText {#text} at (452,0) size 755x36
+            text run at (452,0) width 4: " "
+            text run at (456,0) width 299: "REGRESSION: Selection bug in new text fields"
+            text run at (0,18) width 210: "when selecting past the first letter"
+        RenderText {#text} at (210,18) size 4x18
+          text run at (210,18) width 4: "."
+      RenderBlock {HR} at (0,52) size 784x2 [border: (1px inset #000000)]
+      RenderBlock {P} at (0,70) size 784x54
+        RenderText {#text} at (0,0) size 778x54
+          text run at (0,0) width 778: "Curabitur pretium, quam quis semper malesuada, est libero feugiat libero, vel fringilla orci nibh sed neque. Quisque eu nulla"
+          text run at (0,18) width 732: "non nisi molestie accumsan. Etiam tellus urna, laoreet ac, laoreet non, suscipit sed, sapien. Phasellus vehicula, sem at"
+          text run at (0,36) width 541: "posuere vehicula, augue nibh molestie nisl, nec ullamcorper lacus ante vulputate pede."
+      RenderBlock (anonymous) at (0,140) size 784x92
+        RenderTextControl {INPUT} at (20,20) size 282x52 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (0,0) size 0x0
+      RenderBlock {P} at (0,248) size 784x54
+        RenderText {#text} at (0,0) size 769x54
+          text run at (0,0) width 754: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris viverra augue vitae purus."
+          text run at (0,18) width 769: "Morbi sed sem. Donec dui nisi, ultrices non, pretium quis, hendrerit non, est. Donec tellus. Donec eget dui id eros pharetra"
+          text run at (0,36) width 733: "rutrum. Suspendisse sodales lectus sit amet nulla. Morbi tortor arcu, convallis blandit, elementum eu, aliquet a, tellus."
+layer at (40,180) size 258x28
+  RenderBlock {DIV} at (12,12) size 258x28
+    RenderText {#text} at (1,0) size 222x28
+      text run at (1,0) width 222: "Lorem ipsum dolor"
+selection start: position 6 of child 0 {#text} of child 0 {DIV} of child 7 {INPUT} of body
+selection end:   position 11 of child 0 {#text} of child 0 {DIV} of child 7 {INPUT} of body

LayoutTests/platform/win/fast/forms/input-text-drag-down-expected.checksum
@@ -1,0 +1,1 @@
+eca89e811437a13c18399a8243559967

LayoutTests/platform/win/fast/forms/input-text-drag-down-expected.txt
@@ -1,0 +1,17 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x576
+      RenderBlock (anonymous) at (0,0) size 784x23
+        RenderTextControl {INPUT} at (2,2) size 125x19 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (0,0) size 0x0
+      RenderBlock {P} at (0,39) size 784x18
+        RenderText {#text} at (0,0) size 732x18
+          text run at (0,0) width 732: "Tests drag-selecting down. If the test succeeds, the text from the center to the end of the text field should be selected."
+layer at (13,13) size 119x13 scrollWidth 126
+  RenderBlock {DIV} at (3,3) size 119x13
+    RenderText {#text} at (1,0) size 124x13
+      text run at (1,0) width 124: "This is a bunch of text."
+selection start: position 7 of child 0 {#text} of child 0 {DIV} of child 0 {INPUT} of body
+selection end:   position 8 of child 0 {#text} of child 0 {DIV} of child 0 {INPUT} of body