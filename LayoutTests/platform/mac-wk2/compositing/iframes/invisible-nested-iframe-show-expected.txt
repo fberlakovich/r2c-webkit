fberlakovich/r2c-webkit

commit a1a965f082414b21c80e5dfe52406af127f76e21

Bug 58696 - DFG JIT split handling of vars/temporaries
Reviewed by Sam Weinig.

Presently all callee registers are treated as having single block scope,
since the DFG JIT can only compile single block functions. In order to
expand the JIT to support control flow we will need to change to retaining
locals (but not temporaries) across basic block boundaries.

* dfg/DFGByteCodeParser.cpp:
(JSC::DFG::ByteCodeParser::ByteCodeParser):
(JSC::DFG::ByteCodeParser::get):
(JSC::DFG::ByteCodeParser::set):
(JSC::DFG::ByteCodeParser::getVariable):
(JSC::DFG::ByteCodeParser::setVariable):
(JSC::DFG::ByteCodeParser::getTemporary):
(JSC::DFG::ByteCodeParser::setTemporary):
(JSC::DFG::ByteCodeParser::getArgument):
(JSC::DFG::ByteCodeParser::getInt32Constant):
(JSC::DFG::ByteCodeParser::getDoubleConstant):
(JSC::DFG::ByteCodeParser::getJSConstant):
(JSC::DFG::ByteCodeParser::constantUndefined):
(JSC::DFG::ByteCodeParser::constantNull):
(JSC::DFG::ByteCodeParser::one):
(JSC::DFG::ByteCodeParser::parseBlock):
(JSC::DFG::ByteCodeParser::parse):
(JSC::DFG::parse):
* dfg/DFGNode.h:
* dfg/DFGScoreBoard.h:
(JSC::DFG::ScoreBoard::ScoreBoard):
(JSC::DFG::ScoreBoard::~ScoreBoard):
(JSC::DFG::ScoreBoard::allocate):
(JSC::DFG::ScoreBoard::use):



git-svn-id: https://svn.webkit.org/repository/webkit/trunk@84037 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
@@ -55,31 +55,33 @@ public:
         , m_constantUndefined(UINT_MAX)
         , m_constantNull(UINT_MAX)
         , m_constant1(UINT_MAX)
+        , m_constants(codeBlock->numberOfConstantRegisters())
+        , m_arguments(codeBlock->m_numParameters)
+        , m_variables(codeBlock->m_numVars)
+        , m_temporaries(codeBlock->m_numCalleeRegisters - codeBlock->m_numVars)
     {
-        unsigned numberOfConstants = codeBlock->numberOfConstantRegisters();
-        m_constantRecords.grow(numberOfConstants);
-
-        unsigned numberOfParameters = codeBlock->m_numParameters;
-        m_arguments.grow(numberOfParameters);
-        for (unsigned i = 0; i < numberOfParameters; ++i)
+        for (unsigned i = 0; i < m_arguments.size(); ++i)
             m_arguments[i] = NoNode;
-
-        unsigned numberOfRegisters = codeBlock->m_numCalleeRegisters;
-        m_calleeRegisters.grow(numberOfRegisters);
-        for (unsigned i = 0; i < numberOfRegisters; ++i)
-            m_calleeRegisters[i] = NoNode;
+        for (unsigned i = 0; i < m_variables.size(); ++i)
+            m_variables[i] = NoNode;
+        for (unsigned i = 0; i < m_temporaries.size(); ++i)
+            m_temporaries[i] = NoNode;
     }
 
+    // Parse a full CodeBlock of bytecode.
     bool parse();
 
 private:
+    // Parse a single basic block of bytecode instructions.
+    bool parseBlock();
+
     // Get/Set the operands/result of a bytecode instruction.
     NodeIndex get(int operand)
     {
         // Is this a constant?
         if (operand >= FirstConstantRegisterIndex) {
             unsigned constant = operand - FirstConstantRegisterIndex;
-            ASSERT(constant < m_constantRecords.size());
+            ASSERT(constant < m_constants.size());
             return getJSConstant(constant);
         }
 
@@ -90,9 +92,15 @@ private:
             return getArgument(argument);
         }
 
-        // Must be a local or temporary.
-        ASSERT((unsigned)operand < m_calleeRegisters.size());
-        return getRegister((unsigned)operand);
+        // Is this a variable?
+        unsigned numVariables = m_variables.size();
+        if ((unsigned)operand < numVariables)
+            return getVariable((unsigned)operand);
+        
+        // Must be a temporary.
+        unsigned temporary = (unsigned)operand - numVariables;
+        ASSERT(temporary < m_temporaries.size());
+        return getTemporary(temporary);
     }
     void set(int operand, NodeIndex value)
     {
@@ -103,15 +111,23 @@ private:
             return setArgument(argument, value);
         }
 
-        // Must be a local or temporary.
-        ASSERT((unsigned)operand < m_calleeRegisters.size());
-        return setRegister((unsigned)operand, value);
+        // Is this a variable?
+        unsigned numVariables = m_variables.size();
+        if ((unsigned)operand < numVariables) {
+            setVariable((unsigned)operand, value);
+            return;
+        }
+        
+        // Must be a temporary.
+        unsigned temporary = (unsigned)operand - numVariables;
+        ASSERT(temporary < m_temporaries.size());
+        setTemporary(temporary, value);
     }
 
-    // Used in implementing get/set, above, where the operand is a local or temporary.
-    NodeIndex getRegister(unsigned operand)
+    // Used in implementing get/set, above, where the operand is a local variable.
+    NodeIndex getVariable(unsigned operand)
     {
-        NodeIndex index = m_calleeRegisters[operand];
+        NodeIndex index = m_variables[operand];
         if (index != NoNode)
             return index;
         // We have not yet seen a definition for this value in this block.
@@ -121,9 +137,25 @@ private:
         //     function f() { var x; return x; }
         return constantUndefined();
     }
-    void setRegister(int operand, NodeIndex value)
+    void setVariable(int operand, NodeIndex value)
     {
-        m_calleeRegisters[operand] = value;
+        m_variables[operand] = value;
+    }
+
+    // Used in implementing get/set, above, where the operand is a temporary.
+    NodeIndex getTemporary(unsigned operand)
+    {
+        NodeIndex index = m_temporaries[operand];
+        if (index != NoNode)
+            return index;
+        
+        // Detect a read of an temporary that is not a yet defined within this block (e.g. use of ?:).
+        m_parseFailed = true;
+        return constantUndefined();
+    }
+    void setTemporary(int operand, NodeIndex value)
+    {
+        m_temporaries[operand] = value;
     }
 
     // Used in implementing get/set, above, where the operand is an argument.
@@ -132,8 +164,7 @@ private:
         NodeIndex index = m_arguments[argument];
         if (index != NoNode)
             return index;
-        NodeIndex resultIndex = (NodeIndex)m_graph.size();
-        m_graph.append(Node(Argument, m_currentIndex, OpInfo(argument)));
+        NodeIndex resultIndex = addToGraph(Argument, OpInfo(argument));
         return m_arguments[argument] = resultIndex;
     }
     void setArgument(int operand, NodeIndex value)
@@ -237,35 +268,32 @@ private:
     // Used in implementing get, above, where the operand is a constant.
     NodeIndex getInt32Constant(int32_t value, unsigned constant)
     {
-        NodeIndex index = m_constantRecords[constant].asInt32;
+        NodeIndex index = m_constants[constant].asInt32;
         if (index != NoNode)
             return index;
-        NodeIndex resultIndex = (NodeIndex)m_graph.size();
-        m_graph.append(Node(Int32Constant, m_currentIndex, OpInfo(constant)));
+        NodeIndex resultIndex = addToGraph(Int32Constant, OpInfo(constant));
         m_graph[resultIndex].setInt32Constant(value);
-        m_constantRecords[constant].asInt32 = resultIndex;
+        m_constants[constant].asInt32 = resultIndex;
         return resultIndex;
     }
     NodeIndex getDoubleConstant(double value, unsigned constant)
     {
-        NodeIndex index = m_constantRecords[constant].asNumeric;
+        NodeIndex index = m_constants[constant].asNumeric;
         if (index != NoNode)
             return index;
-        NodeIndex resultIndex = (NodeIndex)m_graph.size();
-        m_graph.append(Node(DoubleConstant, m_currentIndex, OpInfo(constant)));
+        NodeIndex resultIndex = addToGraph(DoubleConstant, OpInfo(constant));
         m_graph[resultIndex].setDoubleConstant(value);
-        m_constantRecords[constant].asNumeric = resultIndex;
+        m_constants[constant].asNumeric = resultIndex;
         return resultIndex;
     }
     NodeIndex getJSConstant(unsigned constant)
     {
-        NodeIndex index = m_constantRecords[constant].asJSValue;
+        NodeIndex index = m_constants[constant].asJSValue;
         if (index != NoNode)
             return index;
 
-        NodeIndex resultIndex = (NodeIndex)m_graph.size();
-        m_graph.append(Node(JSConstant, m_currentIndex, OpInfo(constant)));
-        m_constantRecords[constant].asJSValue = resultIndex;
+        NodeIndex resultIndex = addToGraph(JSConstant, OpInfo(constant));
+        m_constants[constant].asJSValue = resultIndex;
         return resultIndex;
     }
 
@@ -323,11 +351,11 @@ private:
                     return getJSConstant(m_constantUndefined);
             }
 
-            // Add undefined to the CodeBlock's constants, and add a corresponding slot in m_constantRecords.
-            ASSERT(m_constantRecords.size() == numberOfConstants);
+            // Add undefined to the CodeBlock's constants, and add a corresponding slot in m_constants.
+            ASSERT(m_constants.size() == numberOfConstants);
             m_codeBlock->addConstant(jsUndefined());
-            m_constantRecords.append(ConstantRecord());
-            ASSERT(m_constantRecords.size() == m_codeBlock->numberOfConstantRegisters());
+            m_constants.append(ConstantRecord());
+            ASSERT(m_constants.size() == m_codeBlock->numberOfConstantRegisters());
         }
 
         // m_constantUndefined must refer to an entry in the CodeBlock's constant pool that has the value 'undefined'.
@@ -348,11 +376,11 @@ private:
                     return getJSConstant(m_constantNull);
             }
 
-            // Add null to the CodeBlock's constants, and add a corresponding slot in m_constantRecords.
-            ASSERT(m_constantRecords.size() == numberOfConstants);
+            // Add null to the CodeBlock's constants, and add a corresponding slot in m_constants.
+            ASSERT(m_constants.size() == numberOfConstants);
             m_codeBlock->addConstant(jsNull());
-            m_constantRecords.append(ConstantRecord());
-            ASSERT(m_constantRecords.size() == m_codeBlock->numberOfConstantRegisters());
+            m_constants.append(ConstantRecord());
+            ASSERT(m_constants.size() == m_codeBlock->numberOfConstantRegisters());
         }
 
         // m_constantNull must refer to an entry in the CodeBlock's constant pool that has the value 'null'.
@@ -373,11 +401,11 @@ private:
                     return getDoubleConstant(1, m_constant1);
             }
 
-            // Add the value 1 to the CodeBlock's constants, and add a corresponding slot in m_constantRecords.
-            ASSERT(m_constantRecords.size() == numberOfConstants);
+            // Add the value 1 to the CodeBlock's constants, and add a corresponding slot in m_constants.
+            ASSERT(m_constants.size() == numberOfConstants);
             m_codeBlock->addConstant(jsNumber(1));
-            m_constantRecords.append(ConstantRecord());
-            ASSERT(m_constantRecords.size() == m_codeBlock->numberOfConstantRegisters());
+            m_constants.append(ConstantRecord());
+            ASSERT(m_constants.size() == m_codeBlock->numberOfConstantRegisters());
         }
 
         // m_constant1 must refer to an entry in the CodeBlock's constant pool that has the integer value 1.
@@ -441,12 +469,13 @@ private:
         NodeIndex asNumeric;
         NodeIndex asJSValue;
     };
-    Vector <ConstantRecord, 32> m_constantRecords;
+    Vector <ConstantRecord, 32> m_constants;
 
     // Track the index of the node whose result is the current value for every
     // register value in the bytecode - argument, local, and temporary.
     Vector <NodeIndex, 32> m_arguments;
-    Vector <NodeIndex, 32> m_calleeRegisters;
+    Vector <NodeIndex, 32> m_variables;
+    Vector <NodeIndex, 32> m_temporaries;
 
     // These maps are used to unique ToNumber and ToInt32 operations.
     typedef HashMap<NodeIndex, NodeIndex> UnaryOpMap;
@@ -462,7 +491,7 @@ private:
     m_currentIndex += OPCODE_LENGTH(name); \
     return !m_parseFailed
 
-bool ByteCodeParser::parse()
+bool ByteCodeParser::parseBlock()
 {
     AliasTracker aliases(m_graph);
 
@@ -799,17 +828,15 @@ bool ByteCodeParser::parse()
     }
 }
 
-bool parse(Graph& graph, JSGlobalData* globalData, CodeBlock* codeBlock)
+bool ByteCodeParser::parse()
 {
-    // Call ByteCodeParser::parse to build the dataflow for the basic block at 'startIndex'.
-    ByteCodeParser state(globalData, codeBlock, graph);
-    if (!state.parse())
+    if (!parseBlock())
         return false;
 
     // Assign VirtualRegisters.
-    ScoreBoard scoreBoard(graph);
-    Node* nodes = graph.begin();
-    size_t size = graph.size();
+    ScoreBoard scoreBoard(m_graph, m_variables.size());
+    Node* nodes = m_graph.begin();
+    size_t size = m_graph.size();
     for (size_t i = 0; i < size; ++i) {
         Node& node = nodes[i];
         if (node.refCount) {
@@ -831,13 +858,27 @@ bool parse(Graph& graph, JSGlobalData* globalData, CodeBlock* codeBlock)
     // 'm_numCalleeRegisters' is the number of locals and temporaries allocated
     // for the function (and checked for on entry). Since we perform a new and
     // different allocation of temporaries, more registers may now be required.
-    if ((unsigned)codeBlock->m_numCalleeRegisters < scoreBoard.allocatedCount())
-        codeBlock->m_numCalleeRegisters = scoreBoard.allocatedCount();
+    unsigned calleeRegisters = scoreBoard.allocatedCount() + m_variables.size();
+    if ((unsigned)m_codeBlock->m_numCalleeRegisters < calleeRegisters)
+        m_codeBlock->m_numCalleeRegisters = calleeRegisters;
 
 #if DFG_DEBUG_VERBOSE
-    graph.dump(codeBlock);
+    m_graph.dump(m_codeBlock);
 #endif
+
     return true;
+}
+
+bool parse(Graph& graph, JSGlobalData* globalData, CodeBlock* codeBlock)
+{
+#if DFG_DEBUG_LOCAL_DISBALE
+    UNUSED_PARAM(graph);
+    UNUSED_PARAM(globalData);
+    UNUSED_PARAM(codeBlock);
+    return false;
+#else
+    return ByteCodeParser(globalData, codeBlock, graph).parse();
+#endif
 }
 
 } } // namespace JSC::DFG

Source/JavaScriptCore/dfg/DFGNode.h
@@ -38,6 +38,8 @@
 #define DFG_CONSISTENCY_CHECK 0
 // Emit a breakpoint into the head of every generated function, to aid debugging in GDB.
 #define DFG_JIT_BREAK_ON_ENTRY 0
+// Disable the DFG JIT without having to touch Platform.h!
+#define DFG_DEBUG_LOCAL_DISBALE 0
 
 
 namespace JSC { namespace DFG {

Source/JavaScriptCore/dfg/DFGScoreBoard.h
@@ -42,8 +42,9 @@ namespace JSC { namespace DFG {
 // another node.
 class ScoreBoard {
 public:
-    ScoreBoard(Graph& graph)
+    ScoreBoard(Graph& graph, uint32_t firstTemporary)
         : m_graph(graph)
+        , m_firstTemporary(firstTemporary)
     {
     }
 
@@ -58,7 +59,7 @@ public:
         // * By setting m_used to a non-zero value after checking it, we are checking that all
         //   entries in m_free are unique (otherwise the second test of m_used will fail).
         for (size_t i = 0; i < m_free.size(); ++i) {
-            VirtualRegister virtualRegister = m_free[i];
+            uint32_t virtualRegister = m_free[i];
             ASSERT(!m_used[virtualRegister]);
             m_used[virtualRegister] = 1;
         }
@@ -70,17 +71,17 @@ public:
         // Do we have any VirtualRegsiters in the free list, that were used by
         // prior nodes, but are now available?
         if (!m_free.isEmpty()) {
-            VirtualRegister result = m_free.last();
+            uint32_t index = m_free.last();
             m_free.removeLast();
             // Use count must have hit zero for it to have been added to the free list!
-            ASSERT(!m_used[result]);
-            return result;
+            ASSERT(!m_used[index]);
+            return (VirtualRegister)(m_firstTemporary + index);
         }
 
         // Allocate a new VirtualRegister, and add a corresponding entry to m_used.
         size_t next = allocatedCount();
         m_used.append(0);
-        return (VirtualRegister)next;
+        return (VirtualRegister)(m_firstTemporary + next);
     }
 
     // Increment the usecount for the VirtualRegsiter associated with 'child',
@@ -92,7 +93,7 @@ public:
 
         // Find the virtual register number for this child, increment its use count.
         Node& node = m_graph[child];
-        VirtualRegister index = node.virtualRegister;
+        uint32_t index = node.virtualRegister - m_firstTemporary;
         if (node.refCount == ++m_used[index]) {
             // If the use count in the scoreboard reaches the use count for the node,
             // then this was its last use; the virtual register is now free.
@@ -111,6 +112,9 @@ public:
 private:
     // The graph, so we can get refCounts for nodes, to determine when values are dead.
     Graph& m_graph;
+    // The first VirtualRegsiter to be used as a temporary.
+    uint32_t m_firstTemporary;
+    
     // For every virtual register that has been allocated (either currently alive, or in
     // the free list), we keep a count of the number of remaining uses until it is dead
     // (0, in the case of entries in the free list). Since there is an entry for every
@@ -118,7 +122,7 @@ private:
     // next available VirtualRegister number.
     Vector<uint32_t, 64> m_used;
     // A free list of VirtualRegsiters no longer alive.
-    Vector<VirtualRegister, 64> m_free;
+    Vector<uint32_t, 64> m_free;
 };
 
 } } // namespace JSC::DFG

commit 0c1fc3d37dc63c91d40335f5845756288fe50ea8

[Snow Leopard Intel WebKit2 Release Tests] editing/undo/undo-iframe-location-change.html and compositing/iframes/invisible-nested-iframe-show.html failing since r84037. https://bugs.webkit.org/show_bug.cgi?id=58990
Add failing expected results for SL WK2 to get the bots green.

These tests are already Skipped on Windows or have their own expected results.

* platform/mac-wk2/compositing/iframes/invisible-nested-iframe-show-expected.txt: Added.
* platform/mac-wk2/editing/undo/undo-iframe-location-change-expected.txt: Added.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@84783 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac-wk2/compositing/iframes/invisible-nested-iframe-show-expected.txt
@@ -1,0 +1,18 @@
+
+(GraphicsLayer
+  (bounds 800.00 600.00)
+  (children 1
+    (GraphicsLayer
+      (position -12.00 -12.00)
+      (bounds 812.00 612.00)
+      (children 1
+        (GraphicsLayer
+          (position 30.00 214.00)
+          (bounds 210.00 210.00)
+          (drawsContent 1)
+        )
+      )
+    )
+  )
+)
+

LayoutTests/platform/mac-wk2/editing/undo/undo-iframe-location-change-expected.txt
@@ -1,0 +1,2 @@
+
+Failure, Undo was still enabled after the location changed (but at least we didn't crash!)