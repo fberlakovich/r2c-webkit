fberlakovich/r2c-webkit

commit f00cc269ef3321d31ce0c68870c09e10a4bdee09

2011-06-15  Nikolas Zimmermann  <nzimmermann@rim.com>
        Reviewed by David Hyatt.

        CSS 2.1: Support replaced elements with relative intrinsic sizes
        https://bugs.webkit.org/show_bug.cgi?id=15849

        CSS 2.1 failure: html4/replaced-intrinsic-ratio-001
        https://bugs.webkit.org/show_bug.cgi?id=53099

        Imported all CSS 2.1 tests named *replaced* and *intrinsic* (except the still failing background-intrinsic-*) tests.
        All of these pass now, except block-non-replaced-width-008.htm which is unrelated to this patch.

        * css2.1/20110323: Added.
        * css2.1/20110323/absolute-non-replaced-height-001.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-002.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-003.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-004.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-005.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-006.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-007.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-008.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-009.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-010.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-011.htm: Added.
        * css2.1/20110323/absolute-non-replaced-height-012.htm: Added.
        * css2.1/20110323/absolute-non-replaced-max-height-001.htm: Added.
        * css2.1/20110323/absolute-non-replaced-max-height-002.htm: Added.
        * css2.1/20110323/absolute-non-replaced-max-height-003.htm: Added.
        * css2.1/20110323/absolute-non-replaced-max-height-004.htm: Added.
        * css2.1/20110323/absolute-non-replaced-max-height-005.htm: Added.
        * css2.1/20110323/absolute-non-replaced-max-height-006.htm: Added.
        * css2.1/20110323/absolute-non-replaced-max-height-007.htm: Added.

LayoutTests/platform/mac/svg/zoom/page/zoom-replaced-intrinsic-ratio-001-expected.txt
@@ -1,0 +1,62 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x469
+  RenderBlock {HTML} at (0,0) size 800x469
+    RenderBody {BODY} at (4,4) size 140x457 [border: (1px dashed #C0C0C0)]
+      RenderBlock {P} at (1,10) size 138x27
+        RenderText {#text} at (0,0) size 135x28
+          text run at (0,0) width 133: "The following six blue boxes must"
+          text run at (0,9) width 135: "be of the same width. There must be"
+          text run at (0,18) width 26: "no red."
+      RenderBlock {P} at (10,46) size 120x27 [bgcolor=#008000] [border: (9px solid #0000FF)]
+        RenderText {#text} at (9,9) size 2x10
+          text run at (9,9) width 2: " "
+      RenderBlock {P} at (1,128) size 138x9
+        RenderText {#text} at (0,0) size 12x10
+          text run at (0,0) width 12: "      "
+        RenderText {#text} at (0,0) size 0x0
+        RenderEmbeddedObject {OBJECT} at (9,0) size 120x44 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+          layer at (0,0) size 102x26
+            RenderView at (0,0) size 102x26
+          layer at (0,0) size 102x26
+            RenderSVGRoot {svg} at (0,0) size 102x26
+              RenderSVGPath {rect} at (0,0) size 102x26 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+              RenderSVGPath {path} at (15,5) size 72x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]
+      RenderBlock {P} at (1,192) size 138x9
+        RenderEmbeddedObject {OBJECT} at (9,0) size 120x44 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+          layer at (0,0) size 102x26
+            RenderView at (0,0) size 102x26
+          layer at (0,0) size 102x26
+            RenderSVGRoot {svg} at (0,0) size 102x26
+              RenderSVGPath {rect} at (0,0) size 102x26 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+              RenderSVGPath {path} at (15,5) size 72x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]
+      RenderTable at (1,256) size 138x45
+        RenderTableSection (anonymous) at (0,0) size 138x45
+          RenderTableRow (anonymous) at (0,0) size 138x45
+            RenderTableCell {P} at (0,0) size 138x45 [r=0 c=0 rs=1 cs=1]
+              RenderEmbeddedObject {OBJECT} at (9,0) size 120x44 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+                layer at (0,0) size 102x26
+                  RenderView at (0,0) size 102x26
+                layer at (0,0) size 102x26
+                  RenderSVGRoot {svg} at (0,0) size 102x26
+                    RenderSVGPath {rect} at (0,0) size 102x26 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+                    RenderSVGPath {path} at (15,5) size 72x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]
+      RenderTable {TABLE} at (1,356) size 138x45
+        RenderTableSection {TBODY} at (0,0) size 138x45
+          RenderTableRow {TR} at (0,0) size 138x45
+            RenderTableCell {TD} at (0,0) size 138x45 [r=0 c=0 rs=1 cs=1]
+              RenderEmbeddedObject {OBJECT} at (9,0) size 120x44 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+                layer at (0,0) size 102x26
+                  RenderView at (0,0) size 102x26
+                layer at (0,0) size 102x26
+                  RenderSVGRoot {svg} at (0,0) size 102x26
+                    RenderSVGPath {rect} at (0,0) size 102x26 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+                    RenderSVGPath {path} at (15,5) size 72x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]
+      RenderBlock (floating) {P} at (1,456) size 138x9
+        RenderEmbeddedObject {OBJECT} at (9,0) size 120x44 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+          layer at (0,0) size 102x26
+            RenderView at (0,0) size 102x26
+          layer at (0,0) size 102x26
+            RenderSVGRoot {svg} at (0,0) size 102x26
+              RenderSVGPath {rect} at (0,0) size 102x26 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+              RenderSVGPath {path} at (15,5) size 72x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]

commit 1c97d18a77a622a8cea83ef6488fc8e04fe29b0d

[WebKit2 Tests] svg/zoom/page/zoom-replaced-intrinsic-ratio-001.html failing since introduction in r88913. https://bugs.webkit.org/show_bug.cgi?id=63277
Add the (failing?) expected results in order to get the WK2 bots green.

* platform/mac-wk2/svg: Added.
* platform/mac-wk2/svg/zoom: Added.
* platform/mac-wk2/svg/zoom/page: Added.
* platform/mac-wk2/svg/zoom/page/zoom-replaced-intrinsic-ratio-001-expected.txt: Added.



git-svn-id: https://svn.webkit.org/repository/webkit/trunk@89607 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac-wk2/svg/zoom/page/zoom-replaced-intrinsic-ratio-001-expected.txt
@@ -1,0 +1,62 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x461
+  RenderBlock {HTML} at (0,0) size 800x461
+    RenderBody {BODY} at (3,3) size 137x450 [border: (1px dashed #C0C0C0)]
+      RenderBlock {P} at (1,10) size 135x27
+        RenderText {#text} at (0,0) size 135x28
+          text run at (0,0) width 133: "The following six blue boxes must"
+          text run at (0,9) width 135: "be of the same width. There must be"
+          text run at (0,18) width 26: "no red."
+      RenderBlock {P} at (10,46) size 117x27 [bgcolor=#008000] [border: (9px solid #0000FF)]
+        RenderText {#text} at (9,9) size 2x10
+          text run at (9,9) width 2: " "
+      RenderBlock {P} at (1,127) size 135x9
+        RenderText {#text} at (0,0) size 12x10
+          text run at (0,0) width 12: "      "
+        RenderText {#text} at (0,0) size 0x0
+        RenderEmbeddedObject {OBJECT} at (9,0) size 117x43 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+          layer at (0,0) size 99x25
+            RenderView at (0,0) size 99x25
+          layer at (0,0) size 99x25
+            RenderSVGRoot {svg} at (0,0) size 99x25
+              RenderSVGPath {rect} at (0,0) size 99x25 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+              RenderSVGPath {path} at (14,4) size 71x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]
+      RenderBlock {P} at (1,190) size 135x9
+        RenderEmbeddedObject {OBJECT} at (9,0) size 117x43 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+          layer at (0,0) size 99x25
+            RenderView at (0,0) size 99x25
+          layer at (0,0) size 99x25
+            RenderSVGRoot {svg} at (0,0) size 99x25
+              RenderSVGPath {rect} at (0,0) size 99x25 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+              RenderSVGPath {path} at (14,4) size 71x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]
+      RenderTable at (1,253) size 135x44
+        RenderTableSection (anonymous) at (0,0) size 135x44
+          RenderTableRow (anonymous) at (0,0) size 135x44
+            RenderTableCell {P} at (0,0) size 135x44 [r=0 c=0 rs=1 cs=1]
+              RenderEmbeddedObject {OBJECT} at (9,0) size 117x43 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+                layer at (0,0) size 99x25
+                  RenderView at (0,0) size 99x25
+                layer at (0,0) size 99x25
+                  RenderSVGRoot {svg} at (0,0) size 99x25
+                    RenderSVGPath {rect} at (0,0) size 99x25 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+                    RenderSVGPath {path} at (14,4) size 71x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]
+      RenderTable {TABLE} at (1,351) size 135x44
+        RenderTableSection {TBODY} at (0,0) size 135x44
+          RenderTableRow {TR} at (0,0) size 135x44
+            RenderTableCell {TD} at (0,0) size 135x44 [r=0 c=0 rs=1 cs=1]
+              RenderEmbeddedObject {OBJECT} at (9,0) size 117x43 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+                layer at (0,0) size 99x25
+                  RenderView at (0,0) size 99x25
+                layer at (0,0) size 99x25
+                  RenderSVGRoot {svg} at (0,0) size 99x25
+                    RenderSVGPath {rect} at (0,0) size 99x25 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+                    RenderSVGPath {path} at (14,4) size 71x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]
+      RenderBlock (floating) {P} at (1,449) size 135x9
+        RenderEmbeddedObject {OBJECT} at (9,0) size 117x43 [bgcolor=#FF0000] [border: (9px solid #0000FF)]
+          layer at (0,0) size 99x25
+            RenderView at (0,0) size 99x25
+          layer at (0,0) size 99x25
+            RenderSVGRoot {svg} at (0,0) size 99x25
+              RenderSVGPath {rect} at (0,0) size 99x25 [stroke={[type=SOLID] [color=#008000] [stroke width=12.00]}] [fill={[type=SOLID] [color=#00FF00]}] [x=0.00] [y=0.00] [width=1000.00] [height=250.00]
+              RenderSVGPath {path} at (14,4) size 71x16 [fill={[type=SOLID] [color=#008000]}] [data="M 500 50 L 150 200 L 850 200 Z"]