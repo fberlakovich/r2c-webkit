fberlakovich/r2c-webkit

commit a93a0c77ca9d1fc07dac26836812940e0453e863

2011-06-07  Lucas De Marchi  <lucas.demarchi@profusion.mobi>
        Reviewed by Antonio Gomes.

        [EFL] Remove Ecore_X bits from PlatformScreenEfl
        https://bugs.webkit.org/show_bug.cgi?id=61649

        Remove the need for Ecore_X and some X calls by always using
        new functions available in EFL to get the screen size.

        * Source/cmake/FindEFL.cmake: Bump ecore and ecore-evas versions.
2011-06-07  Lucas De Marchi  <lucas.demarchi@profusion.mobi>

        Reviewed by Antonio Gomes.

        [EFL] Remove Ecore_X bits from PlatformScreenEfl
        https://bugs.webkit.org/show_bug.cgi?id=61649

        Remove the need for Ecore_X and some X calls by always using
        new functions available in EFL to get the screen size.

        No new tests since the same functionality are done inside EFL now.

        * platform/efl/PlatformScreenEfl.cpp:
        (WebCore::screenRect):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@88245 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/efl/PlatformScreenEfl.cpp
@@ -34,15 +34,11 @@
 #include "config.h"
 #include "PlatformScreen.h"
 
+#include <Ecore_Evas.h>
 #include "NotImplemented.h"
 #include "PlatformString.h"
 #include "Widget.h"
 #include <wtf/text/CString.h>
-
-#ifdef HAVE_ECORE_X
-#include <Ecore_X.h>
-#include <X11/Xlib.h>
-#endif
 
 namespace WebCore {
 
@@ -66,17 +62,10 @@ bool screenIsMonochrome(Widget*)
 
 FloatRect screenRect(Widget* widget)
 {
-    int x = 0, y = 0, w = 0, h = 0;
+    int x, y, w, h;
+    Evas* e = widget->evas();
 
-#ifdef HAVE_ECORE_X
-    Ecore_X_Display* display = ecore_x_display_get();
-    int def = DefaultScreen(display);
-    Screen* screen = ScreenOfDisplay(display, def);
-    x = 0;
-    y = 0;
-    w = screen->width;
-    h = screen->height;
-#endif
+    ecore_evas_screen_geometry_get(ecore_evas_ecore_evas_get(e), &x, &y, &w, &h);
 
     return FloatRect(x, y, w, h);
 }

Source/cmake/FindEFL.cmake
@@ -6,9 +6,9 @@ PKG_CHECK_MODULES (ECORE_EVAS REQUIRED ecore-evas>=1.0.0)
 PKG_CHECK_MODULES (EFLDEPS REQUIRED
   eina>=1.0.0
   evas>=1.0.0
-  ecore>=1.0.0
+  ecore>=1.0.999.59763
   ecore-file>=1.0.0
-  ecore-evas>=1.0.0
+  ecore-evas>=1.0.999.59763
   edje>=1.0.0)
 PKG_CHECK_MODULES (EINA REQUIRED eina>=1.0.0)
 PKG_CHECK_MODULES (ECORE_X ecore-x>=1.0.0)

commit 0ab76a5431f2e8b45a5aa993eeafeb43a8645998

[EFL] Account for the invalid pointer case in WebCore::screenRect() https://bugs.webkit.org/show_bug.cgi?id=64543
Patch by Raphael Kubo da Costa <kubo@profusion.mobi> on 2011-07-19
Reviewed by Antonio Gomes.

Fix a regression introduced in r88245: it assumes the pointer passed
to screenRect() is always valid.
fast/frames/crash-removed-iframe.html proves this is not always true,
so we need to do some sanity check on the pointer before using it.
No new tests as this has been caught by the current test suite.

* platform/efl/PlatformScreenEfl.cpp:
(WebCore::screenRect):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@91247 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/efl/PlatformScreenEfl.cpp
@@ -63,6 +63,9 @@ bool screenIsMonochrome(Widget*)
 
 FloatRect screenRect(Widget* widget)
 {
+    if (!widget)
+        return FloatRect();
+
     int x, y, w, h;
     Evas* e = widget->evas();
 