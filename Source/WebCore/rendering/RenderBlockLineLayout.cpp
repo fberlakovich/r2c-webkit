fberlakovich/r2c-webkit

commit 6636371cff3ab7207315632851f3dffe8020f7d1

<rdar://problem/8832814> With large line-height, column break can happen anywhere in the inter-line gap, causing staggered lines.
https://bugs.webkit.org/show_bug.cgi?id=67202 Handle positive leading when paginating lines.
        
Technically we should paginate lines based solely off their line top and line bottom with
leading included. However there are two issues with always doing so. The first is that overflow
can cause lines to overlap, and the second is that negative leading can cause lines to overlap.

Since we're incapable of dealing with overlap until we stop clipping column boxes and allow them
to have a form of specialized overflow, we still have to at least factor in overflow and ignore
negative leading for now. However we can at least honor positive leading when the lines don't
overlap. This patch provides basic support for paginating when positive leading is involved.

Reviewed by Dan Bernstein.

Added fast/multicol/positive-leading.html.

Source/WebCore: 

* rendering/RenderBlock.cpp:
(WebCore::RenderBlock::markLinesDirtyInBlockRange):
Renamed blockLogicalHeight() to lineBottomWithLeading(). The value is the same.

(WebCore::RenderBlock::adjustLinePositionForPagination):
Grow the top and extent of the line to include our leading when deciding how to paginate
the line.

* rendering/RenderBlockLineLayout.cpp:
(WebCore::RenderBlock::computeBlockDirectionPositionsForLine):
(WebCore::RenderBlock::layoutRunsAndFloatsInRange):
(WebCore::RenderBlock::linkToEndLineIfNeeded):
(WebCore::RenderBlock::checkFloatsInCleanLine):
(WebCore::RenderBlock::determineStartPosition):
(WebCore::RenderBlock::determineEndPosition):
(WebCore::RenderBlock::matchedEndLine):
Renamed blockLogicalHeight() to lineBottomWithLeading(). The value is the same.

* rendering/RootInlineBox.cpp:
(WebCore::RootInlineBox::RootInlineBox):
(WebCore::RootInlineBox::adjustPosition):
(WebCore::RootInlineBox::alignBoxesInBlockDirection):
* rendering/RootInlineBox.h:

Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -665,7 +665,6 @@ void RenderBlock::computeBlockDirectionPositionsForLine(RootInlineBox* lineBox, 
                                                         VerticalPositionCache& verticalPositionCache)
 {
     setLogicalHeight(lineBox->alignBoxesInBlockDirection(logicalHeight(), textBoxDataMap, verticalPositionCache));
-    lineBox->setBlockLogicalHeight(logicalHeight());
 
     // Now make sure we place replaced render objects correctly.
     for (BidiRun* r = firstRun; r; r = r->next()) {
@@ -1078,7 +1077,7 @@ void RenderBlock::layoutRunsAndFloatsInRange(LineLayoutState& layoutState, Inlin
                             continue;
                         }
 
-                        setLogicalHeight(lineBox->blockLogicalHeight());
+                        setLogicalHeight(lineBox->lineBottomWithLeading());
                     }
                 }
             }
@@ -1145,7 +1144,7 @@ void RenderBlock::linkToEndLineIfNeeded(LineLayoutState& layoutState)
                     }
                 }
             }
-            setLogicalHeight(lastRootBox()->blockLogicalHeight());
+            setLogicalHeight(lastRootBox()->lineBottomWithLeading());
         } else {
             // Delete all the remaining lines.
             deleteLineRange(layoutState, renderArena(), layoutState.endLine());
@@ -1169,7 +1168,6 @@ void RenderBlock::linkToEndLineIfNeeded(LineLayoutState& layoutState)
             IntRect logicalLayoutOverflow(0, blockLogicalHeight, 1, bottomLayoutOverflow - blockLogicalHeight);
             IntRect logicalVisualOverflow(0, blockLogicalHeight, 1, bottomVisualOverflow - blockLogicalHeight);
             trailingFloatsLineBox->setOverflowFromLogicalRects(logicalLayoutOverflow, logicalVisualOverflow, trailingFloatsLineBox->lineTop(), trailingFloatsLineBox->lineBottom());
-            trailingFloatsLineBox->setBlockLogicalHeight(logicalHeight());
         }
 
         const FloatingObjectSet& floatingObjectSet = m_floatingObjects->set();
@@ -1311,7 +1309,7 @@ void RenderBlock::checkFloatsInCleanLine(RootInlineBox* line, Vector<FloatWithRe
                                                                  : max(floats[floatIndex].rect.width(), newSize.width());
             floatHeight = min(floatHeight, numeric_limits<int>::max() - floatTop);
             line->markDirty();
-            markLinesDirtyInBlockRange(line->blockLogicalHeight(), floatTop + floatHeight, line);
+            markLinesDirtyInBlockRange(line->lineBottomWithLeading(), floatTop + floatHeight, line);
             floats[floatIndex].rect.setSize(newSize);
             dirtiedByFloat = true;
         }
@@ -1422,7 +1420,7 @@ RootInlineBox* RenderBlock::determineStartPosition(LineLayoutState& layoutState,
     layoutState.lineInfo().setPreviousLineBrokeCleanly(!last || last->endsWithBreak());
 
     if (last) {
-        setLogicalHeight(last->blockLogicalHeight());
+        setLogicalHeight(last->lineBottomWithLeading());
         resolver.setPosition(InlineIterator(this, last->lineBreakObj(), last->lineBreakPos()));
         resolver.setStatus(last->lineBreakBidiStatus());
     } else {
@@ -1465,7 +1463,7 @@ void RenderBlock::determineEndPosition(LineLayoutState& layoutState, RootInlineB
     RootInlineBox* prev = last->prevRootBox();
     cleanLineStart = InlineIterator(this, prev->lineBreakObj(), prev->lineBreakPos());
     cleanLineBidiStatus = prev->lineBreakBidiStatus();
-    layoutState.setEndLineLogicalTop(prev->blockLogicalHeight());
+    layoutState.setEndLineLogicalTop(prev->lineBottomWithLeading());
 
     for (RootInlineBox* line = last; line; line = line->nextRootBox())
         line->extractLine(); // Disconnect all line boxes from their render objects while preserving
@@ -1491,7 +1489,7 @@ bool RenderBlock::matchedEndLine(LineLayoutState& layoutState, const InlineBidiR
         while (RootInlineBox* nextLine = lastLine->nextRootBox())
             lastLine = nextLine;
 
-        int logicalBottom = lastLine->blockLogicalHeight() + abs(delta);
+        int logicalBottom = lastLine->lineBottomWithLeading() + abs(delta);
 
         const FloatingObjectSet& floatingObjectSet = m_floatingObjects->set();
         FloatingObjectSetIterator end = floatingObjectSet.end();
@@ -1517,7 +1515,7 @@ bool RenderBlock::matchedEndLine(LineLayoutState& layoutState, const InlineBidiR
 
             // Set our logical top to be the block height of endLine.
             if (result)
-                layoutState.setEndLineLogicalTop(line->blockLogicalHeight());
+                layoutState.setEndLineLogicalTop(line->lineBottomWithLeading());
 
             int delta = logicalHeight() - layoutState.endLineLogicalTop();
             if (delta && m_floatingObjects) {
@@ -1528,7 +1526,7 @@ bool RenderBlock::matchedEndLine(LineLayoutState& layoutState, const InlineBidiR
                 while (RootInlineBox* nextLine = lastLine->nextRootBox())
                     lastLine = nextLine;
 
-                int logicalBottom = lastLine->blockLogicalHeight() + abs(delta);
+                int logicalBottom = lastLine->lineBottomWithLeading() + abs(delta);
 
                 const FloatingObjectSet& floatingObjectSet = m_floatingObjects->set();
                 FloatingObjectSetIterator end = floatingObjectSet.end();

LayoutTests/fast/multicol/positive-leading.html
@@ -1,0 +1,23 @@
+<div style="text-align: justify; text-decoration: underline; border: solid; width: 500px; -webkit-columns: 3; -moz-column-count: 3; font-size: 12px; line-height: 2; height: 499px;">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare, nunc eget condimentum tempor,
+augue ligula accumsan eros, sed consequat sapien orci id libero. Curabitur tristique, augue in aliquet
+accumsan, nibh nisi lobortis odio, et suscipit est ligula eu nisi. Nunc aliquet lobortis porta. Donec
+venenatis consectetur eleifend. Etiam sed tempor quam. Pellentesque gravida gravida eros venenatis
+suscipit. Aenean diam enim, suscipit id pellentesque vel, hendrerit et metus. Suspendisse felis erat,
+hendrerit a congue in, euismod ac erat. Donec pulvinar venenatis tortor, a lobortis erat scelerisque
+et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+Aliquam in orci id felis mollis porta ac et orci. Donec viverra, eros ac mattis dapibus, quam ante
+porta lectus, accumsan pellentesque nibh metus non felis. Vivamus sed eros quis magna scelerisque
+ultrices ac eget velit.
+
+Nunc et odio ut velit posuere viverra. Integer eu mi quis velit dapibus tempus at ut purus. Quisque
+congue mollis mauris a hendrerit. Nam viverra est at orci interdum a ullamcorper mi dictum. In id
+sapien augue. Suspendisse potenti. Vestibulum turpis purus, convallis ut sagittis quis, semper vel
+mauris. Curabitur vitae felis turpis. Fusce augue nibh, consectetur non vestibulum et, bibendum sit
+amet lectus. Nulla at imperdiet metus. Donec non est velit, non fringilla ipsum. Fusce elementum neque
+a nibh egestas sodales. Mauris ut lorem hendrerit metus interdum malesuada. Nam tempus sodales neque
+vitae bibendum. Vestibulum ac est sit amet dolor vehicula semper at sed ante. Nunc at neque urna, in
+volutpat orci. Praesent eu velit neque, quis rutrum metus. Nunc eu ipsum sit amet ligula mattis
+porttitor sit amet id felis. Integer volutpat aliquam odio et feugiat.
+
+</div>

LayoutTests/platform/mac/fast/multicol/positive-leading-expected.txt
@@ -1,0 +1,68 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+layer at (8,8) size 506x505
+  RenderBlock {DIV} at (0,0) size 506x505 [border: (3px solid #000000)]
+    RenderText {#text} at (3,8) size 158x1444
+      text run at (3,8) width 158: "Lorem ipsum dolor sit amet,"
+      text run at (3,32) width 158: "consectetur adipiscing elit."
+      text run at (3,56) width 158: "Nullam ornare, nunc eget"
+      text run at (3,80) width 158: "condimentum tempor, augue"
+      text run at (3,104) width 158: "ligula accumsan eros, sed"
+      text run at (3,128) width 158: "consequat sapien orci id libero."
+      text run at (3,152) width 158: "Curabitur tristique, augue in"
+      text run at (3,176) width 158: "aliquet accumsan, nibh nisi"
+      text run at (3,200) width 158: "lobortis odio, et suscipit est ligula"
+      text run at (3,224) width 158: "eu nisi. Nunc aliquet lobortis"
+      text run at (3,248) width 158: "porta. Donec venenatis"
+      text run at (3,272) width 158: "consectetur eleifend. Etiam sed"
+      text run at (3,296) width 158: "tempor quam. Pellentesque"
+      text run at (3,320) width 158: "gravida gravida eros venenatis"
+      text run at (3,344) width 158: "suscipit. Aenean diam enim,"
+      text run at (3,368) width 158: "suscipit id pellentesque vel,"
+      text run at (3,392) width 158: "hendrerit et metus. Suspendisse"
+      text run at (3,416) width 158: "felis erat, hendrerit a congue in,"
+      text run at (3,440) width 158: "euismod ac erat. Donec pulvinar"
+      text run at (3,464) width 158: "venenatis tortor, a lobortis erat"
+      text run at (3,507) width 158: "scelerisque et. Class aptent taciti"
+      text run at (3,531) width 158: "sociosqu ad litora torquent per"
+      text run at (3,555) width 158: "conubia nostra, per inceptos"
+      text run at (3,579) width 158: "himenaeos. Aliquam in orci id"
+      text run at (3,603) width 158: "felis mollis porta ac et orci."
+      text run at (3,627) width 158: "Donec viverra, eros ac mattis"
+      text run at (3,651) width 158: "dapibus, quam ante porta lectus,"
+      text run at (3,675) width 158: "accumsan pellentesque nibh"
+      text run at (3,699) width 158: "metus non felis. Vivamus sed"
+      text run at (3,723) width 158: "eros quis magna scelerisque"
+      text run at (3,747) width 116: "ultrices ac eget velit. "
+      text run at (119,747) width 42: "Nunc et"
+      text run at (3,771) width 158: "odio ut velit posuere viverra."
+      text run at (3,795) width 158: "Integer eu mi quis velit dapibus"
+      text run at (3,819) width 158: "tempus at ut purus. Quisque"
+      text run at (3,843) width 158: "congue mollis mauris a hendrerit."
+      text run at (3,867) width 158: "Nam viverra est at orci interdum"
+      text run at (3,891) width 158: "a ullamcorper mi dictum. In id"
+      text run at (3,915) width 158: "sapien augue. Suspendisse"
+      text run at (3,939) width 158: "potenti. Vestibulum turpis purus,"
+      text run at (3,963) width 158: "convallis ut sagittis quis, semper"
+      text run at (3,1006) width 158: "vel mauris. Curabitur vitae felis"
+      text run at (3,1030) width 158: "turpis. Fusce augue nibh,"
+      text run at (3,1054) width 158: "consectetur non vestibulum et,"
+      text run at (3,1078) width 158: "bibendum sit amet lectus. Nulla"
+      text run at (3,1102) width 158: "at imperdiet metus. Donec non"
+      text run at (3,1126) width 158: "est velit, non fringilla ipsum."
+      text run at (3,1150) width 158: "Fusce elementum neque a nibh"
+      text run at (3,1174) width 158: "egestas sodales. Mauris ut lorem"
+      text run at (3,1198) width 158: "hendrerit metus interdum"
+      text run at (3,1222) width 158: "malesuada. Nam tempus sodales"
+      text run at (3,1246) width 158: "neque vitae bibendum."
+      text run at (3,1270) width 158: "Vestibulum ac est sit amet dolor"
+      text run at (3,1294) width 158: "vehicula semper at sed ante."
+      text run at (3,1318) width 158: "Nunc at neque urna, in volutpat"
+      text run at (3,1342) width 158: "orci. Praesent eu velit neque, quis"
+      text run at (3,1366) width 158: "rutrum metus. Nunc eu ipsum sit"
+      text run at (3,1390) width 158: "amet ligula mattis porttitor sit"
+      text run at (3,1414) width 158: "amet id felis. Integer volutpat"
+      text run at (3,1438) width 110: "aliquam odio et feugiat."

Source/WebCore/rendering/RenderBlock.cpp
@@ -3649,12 +3649,12 @@ void RenderBlock::markLinesDirtyInBlockRange(LayoutUnit logicalTop, LayoutUnit l
 
     RootInlineBox* lowestDirtyLine = lastRootBox();
     RootInlineBox* afterLowest = lowestDirtyLine;
-    while (lowestDirtyLine && lowestDirtyLine->blockLogicalHeight() >= logicalBottom && logicalBottom < numeric_limits<LayoutUnit>::max()) {
+    while (lowestDirtyLine && lowestDirtyLine->lineBottomWithLeading() >= logicalBottom && logicalBottom < numeric_limits<LayoutUnit>::max()) {
         afterLowest = lowestDirtyLine;
         lowestDirtyLine = lowestDirtyLine->prevRootBox();
     }
 
-    while (afterLowest && afterLowest != highest && (afterLowest->blockLogicalHeight() >= logicalTop || afterLowest->blockLogicalHeight() < 0)) {
+    while (afterLowest && afterLowest != highest && (afterLowest->lineBottomWithLeading() >= logicalTop || afterLowest->lineBottomWithLeading() < 0)) {
         afterLowest->markDirty();
         afterLowest = afterLowest->prevRootBox();
     }
@@ -6191,7 +6191,7 @@ void RenderBlock::adjustLinePositionForPagination(RootInlineBox* lineBox, Layout
     // the line on the top of the next page will appear too far down relative to the same kind of line at the top
     // of the first column.
     //
-    // The rendering we would like to see is one where the lineTop is at the top of the column, and any line overflow
+    // The rendering we would like to see is one where the lineTopWithLeading is at the top of the column, and any line overflow
     // simply spills out above the top of the column.  This effect would match what happens at the top of the first column.
     // We can't achieve this rendering, however, until we stop columns from clipping to the column bounds (thus allowing
     // for overflow to occur), and then cache visible overflow for each column rect.
@@ -6199,14 +6199,17 @@ void RenderBlock::adjustLinePositionForPagination(RootInlineBox* lineBox, Layout
     // Furthermore, the paint we have to do when a column has overflow has to be special.  We need to exclude
     // content that paints in a previous column (and content that paints in the following column).
     //
+    // For now we'll at least honor the lineTopWithLeading when paginating if it is above the logical top overflow. This will
+    // at least make positive leading work in typical cases.
+    //
     // FIXME: Another problem with simply moving lines is that the available line width may change (because of floats).
     // Technically if the location we move the line to has a different line width than our old position, then we need to dirty the
     // line and all following lines.
     LayoutState* layoutState = view()->layoutState();
     LayoutUnit pageLogicalHeight = layoutState->m_pageLogicalHeight;
     LayoutRect logicalVisualOverflow = lineBox->logicalVisualOverflowRect(lineBox->lineTop(), lineBox->lineBottom());
-    LayoutUnit logicalOffset = logicalVisualOverflow.y();
-    LayoutUnit lineHeight = logicalVisualOverflow.maxY() - logicalOffset;
+    LayoutUnit logicalOffset = min(lineBox->lineTopWithLeading(), logicalVisualOverflow.y());
+    LayoutUnit lineHeight = max(lineBox->lineBottomWithLeading(), logicalVisualOverflow.maxY()) - logicalOffset;
     if (layoutState->m_columnInfo)
         layoutState->m_columnInfo->updateMinimumColumnHeight(lineHeight);
     logicalOffset += delta;

Source/WebCore/rendering/RootInlineBox.cpp
@@ -48,8 +48,9 @@ RootInlineBox::RootInlineBox(RenderBlock* block)
     , m_lineBreakPos(0)
     , m_lineTop(0)
     , m_lineBottom(0)
+    , m_lineTopWithLeading(0)
+    , m_lineBottomWithLeading(0)
     , m_paginationStrut(0)
-    , m_blockLogicalHeight(0)
     , m_baselineType(AlphabeticBaseline)
     , m_hasAnnotationsBefore(false)
     , m_hasAnnotationsAfter(false)
@@ -203,10 +204,11 @@ bool RootInlineBox::nodeAtPoint(const HitTestRequest& request, HitTestResult& re
 void RootInlineBox::adjustPosition(float dx, float dy)
 {
     InlineFlowBox::adjustPosition(dx, dy);
-    int blockDirectionDelta = isHorizontal() ? dy : dx; // The block direction delta will always be integral.
+    LayoutUnit blockDirectionDelta = isHorizontal() ? dy : dx; // The block direction delta is a LayoutUnit.
     m_lineTop += blockDirectionDelta;
     m_lineBottom += blockDirectionDelta;
-    m_blockLogicalHeight += blockDirectionDelta;
+    m_lineTopWithLeading += blockDirectionDelta;
+    m_lineBottomWithLeading += blockDirectionDelta;
 }
 
 void RootInlineBox::childRemoved(InlineBox* box)
@@ -258,7 +260,10 @@ LayoutUnit RootInlineBox::alignBoxesInBlockDirection(LayoutUnit heightOfBlock, G
                                lineTopIncludingMargins, lineBottomIncludingMargins, hasAnnotationsBefore, hasAnnotationsAfter, baselineType());
     m_hasAnnotationsBefore = hasAnnotationsBefore;
     m_hasAnnotationsAfter = hasAnnotationsAfter;
-    setLineTopBottomPositions(lineTop, lineBottom);
+    
+    maxHeight = max<LayoutUnit>(0, maxHeight); // FIXME: Is this really necessary?
+
+    setLineTopBottomPositions(lineTop, lineBottom, heightOfBlock, heightOfBlock + maxHeight);
 
     int annotationsAdjustment = beforeAnnotationsAdjustment();
     if (annotationsAdjustment) {
@@ -267,8 +272,6 @@ LayoutUnit RootInlineBox::alignBoxesInBlockDirection(LayoutUnit heightOfBlock, G
         adjustBlockDirectionPosition(annotationsAdjustment);
         heightOfBlock += annotationsAdjustment;
     }
-
-    maxHeight = max<LayoutUnit>(0, maxHeight);
 
     return heightOfBlock + maxHeight;
 }

Source/WebCore/rendering/RootInlineBox.h
@@ -50,6 +50,9 @@ public:
     LayoutUnit lineTop() const { return m_lineTop; }
     LayoutUnit lineBottom() const { return m_lineBottom; }
 
+    LayoutUnit lineTopWithLeading() const { return m_lineTopWithLeading; }
+    LayoutUnit lineBottomWithLeading() const { return m_lineBottomWithLeading; }
+    
     int paginationStrut() const { return m_paginationStrut; }
     void setPaginationStrut(int s) { m_paginationStrut = s; }
 
@@ -60,10 +63,12 @@ public:
     int blockDirectionPointInLine() const { return max(lineTop(), selectionTop()); }
 
     LayoutUnit alignBoxesInBlockDirection(LayoutUnit heightOfBlock, GlyphOverflowAndFallbackFontsMap&, VerticalPositionCache&);
-    void setLineTopBottomPositions(LayoutUnit top, LayoutUnit bottom)
+    void setLineTopBottomPositions(LayoutUnit top, LayoutUnit bottom, LayoutUnit topWithLeading, LayoutUnit bottomWithLeading)
     { 
         m_lineTop = top; 
-        m_lineBottom = bottom; 
+        m_lineBottom = bottom;
+        m_lineTopWithLeading = topWithLeading;
+        m_lineBottomWithLeading = bottomWithLeading;
     }
 
     virtual RenderLineBoxList* rendererLineBoxes() const;
@@ -74,9 +79,6 @@ public:
 
     unsigned lineBreakPos() const { return m_lineBreakPos; }
     void setLineBreakPos(unsigned p) { m_lineBreakPos = p; }
-
-    int blockLogicalHeight() const { return m_blockLogicalHeight; }
-    void setBlockLogicalHeight(int h) { m_blockLogicalHeight = h; }
 
     bool endsWithBreak() const { return m_endsWithBreak; }
     void setEndsWithBreak(bool b) { m_endsWithBreak = b; }
@@ -186,14 +188,14 @@ private:
     LayoutUnit m_lineTop;
     LayoutUnit m_lineBottom;
 
+    LayoutUnit m_lineTopWithLeading;
+    LayoutUnit m_lineBottomWithLeading;
+
     int m_paginationStrut;
 
     // Floats hanging off the line are pushed into this vector during layout. It is only
     // good for as long as the line has not been marked dirty.
     OwnPtr<Vector<RenderBox*> > m_floats;
-
-    // The logical height of the block at the end of this line.  This is where the next line starts.
-    int m_blockLogicalHeight;
 
     // Whether or not this line uses alphabetic or ideographic baselines by default.
     unsigned m_baselineType : 1; // FontBaseline

Source/WebCore/rendering/svg/SVGRootInlineBox.cpp
@@ -196,8 +196,7 @@ void SVGRootInlineBox::layoutRootBox(const LayoutRect& childRect)
     setY(0);
     setLogicalWidth(widthBlock);
     setLogicalHeight(heightBlock);
-    setBlockLogicalHeight(heightBlock);
-    setLineTopBottomPositions(0, heightBlock);
+    setLineTopBottomPositions(0, heightBlock, 0, heightBlock);
 }
 
 InlineBox* SVGRootInlineBox::closestLeafChildForPosition(const LayoutPoint& point)

commit 16225969d655d4ffc281031eb392e241d0734096

https://bugs.webkit.org/show_bug.cgi?id=67286
REGRESSION: css2.1/t090204-display-change-01-b-ao.html fails after r94084.

Make sure that the trailing floats line box explicitly sets the line top with leading and line bottom with
leading to just be the block height.

Reviewed by Dan Bernstein.

* rendering/RenderBlockLineLayout.cpp:
(WebCore::RenderBlock::linkToEndLineIfNeeded):



git-svn-id: https://svn.webkit.org/repository/webkit/trunk@94693 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -1168,8 +1168,9 @@ void RenderBlock::linkToEndLineIfNeeded(LineLayoutState& layoutState)
             trailingFloatsLineBox->setConstructed();
             GlyphOverflowAndFallbackFontsMap textBoxDataMap;
             VerticalPositionCache verticalPositionCache;
-            trailingFloatsLineBox->alignBoxesInBlockDirection(logicalHeight(), textBoxDataMap, verticalPositionCache);
             int blockLogicalHeight = logicalHeight();
+            trailingFloatsLineBox->alignBoxesInBlockDirection(blockLogicalHeight, textBoxDataMap, verticalPositionCache);
+            trailingFloatsLineBox->setLineTopBottomPositions(blockLogicalHeight, blockLogicalHeight, blockLogicalHeight, blockLogicalHeight);
             IntRect logicalLayoutOverflow(0, blockLogicalHeight, 1, bottomLayoutOverflow - blockLogicalHeight);
             IntRect logicalVisualOverflow(0, blockLogicalHeight, 1, bottomVisualOverflow - blockLogicalHeight);
             trailingFloatsLineBox->setOverflowFromLogicalRects(logicalLayoutOverflow, logicalVisualOverflow, trailingFloatsLineBox->lineTop(), trailingFloatsLineBox->lineBottom());