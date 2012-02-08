fberlakovich/r2c-webkit

commit 527d69f9819eaf2759c275fc23e4eb97c7388ceb

Kill CSSMappedAttributeDeclaration. <http://webkit.org/b/77820>
Reviewed by Antti Koivisto.

Replace all use of CSSMappedAttributeDeclaration by StylePropertySet.
Moved the setNeedsStyleRecalc() calls from CSSMappedAttributeDeclaration
to the add/remove-CSS-property helpers in StyledElement.

This removes one step of indirection for attribute styles and reduces
the size of elements that have presentational attributes by one pointer.

* CMakeLists.txt:
* GNUmakefile.list.am:
* Target.pri:
* WebCore.gypi:
* WebCore.order:
* WebCore.vcproj/WebCore.vcproj:
* WebCore.xcodeproj/project.pbxproj:
* dom/CSSMappedAttributeDeclaration.cpp: Removed.
* dom/CSSMappedAttributeDeclaration.h: Removed.
* dom/DOMAllInOne.cpp:

    Remove CSSMappedAttributeDeclaration.{cpp,h}

* css/CSSParser.h:
* css/CSSParser.cpp:
(WebCore::parseSimpleLengthValue):

    Remove CSSParser::parseMappedAttributeValue(), we now use parseValue()
    directly instead. We lose the benefit of caching new CSSValues in the
    document's CSSValuePool but this optimization can be added back later.

* css/CSSStyleSelector.cpp:
(WebCore::CSSStyleSelector::matchAllRules):
* dom/ElementAttributeData.h:
(ElementAttributeData):
* dom/NamedNodeMap.cpp:
(WebCore::NamedNodeMap::ensureAttributeStyle):
* dom/NamedNodeMap.h:
(WebCore::NamedNodeMap::attributeStyle):
(NamedNodeMap):
* dom/StyledElement.cpp:
(WebCore::StyledElement::removeCSSProperties):
(WebCore::StyledElement::addCSSProperty):
(WebCore::StyledElement::addCSSImageProperty):
(WebCore::StyledElement::addCSSLength):
(WebCore::StyledElement::addCSSColor):
* dom/StyledElement.h:
(WebCore):
(WebCore::StyledElement::attributeStyle):
(WebCore::StyledElement::ensureAttributeStyle):
* inspector/InspectorCSSAgent.cpp:
(WebCore::InspectorCSSAgent::buildArrayForAttributeStyles):


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@106756 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/StyledElement.cpp
@@ -25,6 +25,7 @@
 #include "StyledElement.h"
 
 #include "Attribute.h"
+#include "CSSImageValue.h"
 #include "CSSStyleSelector.h"
 #include "CSSStyleSheet.h"
 #include "CSSValueKeywords.h"
@@ -121,49 +122,56 @@ void StyledElement::parseMappedAttribute(Attribute* attr)
 
 void StyledElement::removeCSSProperties(int id1, int id2, int id3, int id4, int id5, int id6, int id7, int id8)
 {
-    CSSMappedAttributeDeclaration* style = attributeStyle();
+    StylePropertySet* style = attributeStyle();
     if (!style)
         return;
 
+    setNeedsStyleRecalc(FullStyleChange);
+
     ASSERT(id1 != CSSPropertyInvalid);
-    style->removeMappedProperty(this, id1);
+    style->removeProperty(id1);
 
     if (id2 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id2);
+    style->removeProperty(id2);
     if (id3 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id3);
+    style->removeProperty(id3);
     if (id4 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id4);
+    style->removeProperty(id4);
     if (id5 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id5);
+    style->removeProperty(id5);
     if (id6 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id6);
+    style->removeProperty(id6);
     if (id7 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id7);
+    style->removeProperty(id7);
     if (id8 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id8);
+    style->removeProperty(id8);
 }
 
 void StyledElement::addCSSProperty(int id, const String &value)
 {
-    ensureAttributeStyle()->setMappedProperty(this, id, value);
+    if (!ensureAttributeStyle()->setProperty(id, value))
+        removeCSSProperty(id);
+    else
+        setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSProperty(int id, int value)
 {
-    ensureAttributeStyle()->setMappedProperty(this, id, value);
+    ensureAttributeStyle()->setProperty(id, value);
+    setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSImageProperty(int id, const String& url)
 {
-    ensureAttributeStyle()->setMappedImageProperty(this, id, url);
+    ensureAttributeStyle()->setProperty(CSSProperty(id, CSSImageValue::create(url)));
+    setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSLength(int id, const String &value)
@@ -192,12 +200,12 @@ void StyledElement::addCSSLength(int id, const String &value)
         }
 
         if (l != v->length()) {
-            ensureAttributeStyle()->setMappedLengthProperty(this, id, v->substring(0, l));
+            addCSSProperty(id, v->substring(0, l));
             return;
         }
     }
 
-    ensureAttributeStyle()->setMappedLengthProperty(this, id, value);
+    addCSSProperty(id, value);
 }
 
 static String parseColorStringWithCrazyLegacyRules(const String& colorString)
@@ -272,11 +280,11 @@ void StyledElement::addCSSColor(int id, const String& attributeValue)
     // If the string is a named CSS color or a 3/6-digit hex color, use that.
     Color parsedColor(colorString);
     if (parsedColor.isValid()) {
-        ensureAttributeStyle()->setMappedProperty(this, id, colorString);
+        addCSSProperty(id, colorString);
         return;
     }
 
-    ensureAttributeStyle()->setMappedProperty(this, id, parseColorStringWithCrazyLegacyRules(colorString));
+    addCSSProperty(id, parseColorStringWithCrazyLegacyRules(colorString));
 }
 
 void StyledElement::copyNonAttributeProperties(const Element* sourceElement)

Source/WebCore/CMakeLists.txt
@@ -520,7 +520,6 @@ SET(WebCore_SOURCES
     dom/Comment.cpp
     dom/CompositionEvent.cpp
     dom/ContainerNode.cpp
-    dom/CSSMappedAttributeDeclaration.cpp
     dom/CustomEvent.cpp
     dom/DatasetDOMStringMap.cpp
     dom/DecodedDataDocumentParser.cpp

Source/WebCore/GNUmakefile.list.am
@@ -1474,8 +1474,6 @@ webcore_sources += \
 	Source/WebCore/dom/ContainerNode.cpp \
 	Source/WebCore/dom/ContainerNode.h \
 	Source/WebCore/dom/CrossThreadTask.h \
-	Source/WebCore/dom/CSSMappedAttributeDeclaration.cpp \
-	Source/WebCore/dom/CSSMappedAttributeDeclaration.h \
 	Source/WebCore/dom/CustomEvent.cpp \
 	Source/WebCore/dom/CustomEvent.h \
 	Source/WebCore/dom/DatasetDOMStringMap.cpp \

Source/WebCore/Target.pri
@@ -484,7 +484,6 @@ SOURCES += \
     dom/Comment.cpp \
     dom/CompositionEvent.cpp \
     dom/ContainerNode.cpp \
-    dom/CSSMappedAttributeDeclaration.cpp \
     dom/CustomEvent.cpp \
     dom/DecodedDataDocumentParser.cpp \
     dom/DeviceMotionController.cpp \
@@ -1596,7 +1595,6 @@ HEADERS += \
     dom/Clipboard.h \
     dom/Comment.h \
     dom/ContainerNode.h \
-    dom/CSSMappedAttributeDeclaration.h \
     dom/CustomEvent.h \
     dom/default/PlatformMessagePortChannel.h \
     dom/DeviceMotionClient.h \

Source/WebCore/WebCore.gypi
@@ -575,7 +575,6 @@
             'dom/ActiveDOMObject.h',
             'dom/Attr.h',
             'dom/Attribute.h',
-            'dom/CSSMappedAttributeDeclaration.h',
             'dom/CharacterData.h',
             'dom/CheckedRadioButtons.h',
             'dom/Clipboard.h',
@@ -4928,7 +4927,6 @@
             'dom/BeforeUnloadEvent.h',
             'dom/CDATASection.cpp',
             'dom/CDATASection.h',
-            'dom/CSSMappedAttributeDeclaration.cpp',
             'dom/CharacterData.cpp',
             'dom/CheckedRadioButtons.cpp',
             'dom/ChildListMutationScope.cpp',

Source/WebCore/WebCore.order
@@ -4915,7 +4915,6 @@ __ZN7WebCore16HTMLTableElement29additionalAttributeStyleDeclsERN3WTF6VectorIPNS_
 __ZN7WebCore13StyledElement22getMappedAttributeDeclENS_20MappedAttributeEntryERKNS_13QualifiedNameERKN3WTF12AtomicStringE
 __ZN7WebCore26CSSMutableStyleDeclaration11setPropertyEiibb
 __ZN7WebCore26CSSMutableStyleDeclaration19setPropertyInternalERKNS_11CSSPropertyEPS1_
-__ZN7WebCore13StyledElement22setMappedAttributeDeclENS_20MappedAttributeEntryERKNS_13QualifiedNameERKN3WTF12AtomicStringEPNS_29CSSMappedAttributeDeclarationE
 __ZN3WTF6VectorIPN7WebCore26CSSMutableStyleDeclarationELm0EE14expandCapacityEm
 __ZN7WebCore11RenderStyle26setHorizontalBorderSpacingEs
 __ZN7WebCore11RenderStyle24setVerticalBorderSpacingEs
@@ -5366,7 +5365,6 @@ __ZNK7WebCore25AccessibilityRenderObject16linkedUIElementsERN3WTF6VectorINS1_6Re
 __ZNK7WebCore25AccessibilityRenderObject18ariaFlowToElementsERN3WTF6VectorINS1_6RefPtrINS_19AccessibilityObjectEEELm0EEE
 __ZN3WTF6VectorINS_6RefPtrIN7WebCore19AccessibilityObjectEEELm0EED1Ev
 __ZN7WebCore16HTMLTableElementD0Ev
-__ZN7WebCore29CSSMappedAttributeDeclarationD0Ev
 __ZN7WebCore13StyledElement25removeMappedAttributeDeclENS_20MappedAttributeEntryERKNS_13QualifiedNameERKN3WTF12AtomicStringE
 __ZN7WebCore26CSSMutableStyleDeclarationD2Ev
 __ZN7WebCore17HTMLSelectElementD0Ev
@@ -7918,10 +7916,6 @@ __ZNK7WebCore17CSSPrimitiveValue14getDoubleValueEt
 __ZNK7WebCore17CSSPrimitiveValue22getDoubleValueInternalENS0_9UnitTypesEPd
 __ZN7WebCore23HTMLTableCaptionElementD0Ev
 __ZN7WebCore13StyledElement11addCSSColorEPNS_9AttributeEiRKN3WTF6StringE
-__ZN3WTF9HashTableIN7WebCore18MappedAttributeKeyESt4pairIS2_PNS1_29CSSMappedAttributeDeclarationEENS_18PairFirstExtractorIS6_EENS1_19MappedAttributeHashENS_14PairHashTraitsINS1_24MappedAttributeKeyTraitsENS_10HashTraitsIS5_EEEESB_E16lookupForWritingIS2_NS_22IdentityHashTranslatorIS2_S6_S9_EEEES3_IPS6_bERKT_
-__ZN3WTF9HashTableIN7WebCore18MappedAttributeKeyESt4pairIS2_PNS1_29CSSMappedAttributeDeclarationEENS_18PairFirstExtractorIS6_EENS1_19MappedAttributeHashENS_14PairHashTraitsINS1_24MappedAttributeKeyTraitsENS_10HashTraitsIS5_EEEESB_E3addIS2_S5_NS_17HashMapTranslatorIS6_SE_S9_EEEES3_INS_17HashTableIteratorIS2_S6_S8_S9_SE_SB_EEbERKT_RKT0_
-__ZN3WTF9HashTableIN7WebCore18MappedAttributeKeyESt4pairIS2_PNS1_29CSSMappedAttributeDeclarationEENS_18PairFirstExtractorIS6_EENS1_19MappedAttributeHashENS_14PairHashTraitsINS1_24MappedAttributeKeyTraitsENS_10HashTraitsIS5_EEEESB_E6lookupIS2_NS_22IdentityHashTranslatorIS2_S6_S9_EEEEPS6_RKT_
-__ZN3WTF9HashTableIN7WebCore18MappedAttributeKeyESt4pairIS2_PNS1_29CSSMappedAttributeDeclarationEENS_18PairFirstExtractorIS6_EENS1_19MappedAttributeHashENS_14PairHashTraitsINS1_24MappedAttributeKeyTraitsENS_10HashTraitsIS5_EEEESB_E6rehashEi
 __ZN7WebCore16HTMLStyleElement20parseMappedAttributeEPNS_9AttributeE
 __ZN7WebCore15FontFamilyValueC1ERKN3WTF6StringE
 __ZN7WebCore15FontFamilyValueC2ERKN3WTF6StringE

Source/WebCore/WebCore.vcproj/WebCore.vcproj
@@ -46642,62 +46642,6 @@
 				>
 			</File>
 			<File
-				RelativePath="..\dom\CSSMappedAttributeDeclaration.cpp"
-				>
-				<FileConfiguration
-					Name="Debug|Win32"
-					ExcludedFromBuild="true"
-					>
-					<Tool
-						Name="VCCLCompilerTool"
-					/>
-				</FileConfiguration>
-				<FileConfiguration
-					Name="Release|Win32"
-					ExcludedFromBuild="true"
-					>
-					<Tool
-						Name="VCCLCompilerTool"
-					/>
-				</FileConfiguration>
-				<FileConfiguration
-					Name="Debug_Cairo_CFLite|Win32"
-					ExcludedFromBuild="true"
-					>
-					<Tool
-						Name="VCCLCompilerTool"
-					/>
-				</FileConfiguration>
-				<FileConfiguration
-					Name="Release_Cairo_CFLite|Win32"
-					ExcludedFromBuild="true"
-					>
-					<Tool
-						Name="VCCLCompilerTool"
-					/>
-				</FileConfiguration>
-				<FileConfiguration
-					Name="Debug_All|Win32"
-					ExcludedFromBuild="true"
-					>
-					<Tool
-						Name="VCCLCompilerTool"
-					/>
-				</FileConfiguration>
-				<FileConfiguration
-					Name="Production|Win32"
-					ExcludedFromBuild="true"
-					>
-					<Tool
-						Name="VCCLCompilerTool"
-					/>
-				</FileConfiguration>
-			</File>
-			<File
-				RelativePath="..\dom\CSSMappedAttributeDeclaration.h"
-				>
-			</File>
-			<File
 				RelativePath="..\dom\CustomEvent.cpp"
 				>
 				<FileConfiguration

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -3972,8 +3972,6 @@
 		A8C4A80009D563270003AC8D /* Node.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A8C4A7EE09D563270003AC8D /* Node.cpp */; };
 		A8C4A80709D563270003AC8D /* Element.h in Headers */ = {isa = PBXBuildFile; fileRef = A8C4A7F509D563270003AC8D /* Element.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		A8C4A80809D563270003AC8D /* Element.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A8C4A7F609D563270003AC8D /* Element.cpp */; };
-		A8C4A80909D563270003AC8D /* CSSMappedAttributeDeclaration.h in Headers */ = {isa = PBXBuildFile; fileRef = A8C4A7F709D563270003AC8D /* CSSMappedAttributeDeclaration.h */; settings = {ATTRIBUTES = (Private, ); }; };
-		A8C4A80A09D563270003AC8D /* CSSMappedAttributeDeclaration.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A8C4A7F809D563270003AC8D /* CSSMappedAttributeDeclaration.cpp */; };
 		A8C4A80B09D563270003AC8D /* Attribute.h in Headers */ = {isa = PBXBuildFile; fileRef = A8C4A7F909D563270003AC8D /* Attribute.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		A8C4A80C09D563270003AC8D /* Attribute.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A8C4A7FA09D563270003AC8D /* Attribute.cpp */; };
 		A8C4A80D09D563270003AC8D /* Attr.h in Headers */ = {isa = PBXBuildFile; fileRef = A8C4A7FB09D563270003AC8D /* Attr.h */; settings = {ATTRIBUTES = (Private, ); }; };
@@ -10693,8 +10691,6 @@
 		A8C4A7EE09D563270003AC8D /* Node.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = Node.cpp; sourceTree = "<group>"; };
 		A8C4A7F509D563270003AC8D /* Element.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; path = Element.h; sourceTree = "<group>"; };
 		A8C4A7F609D563270003AC8D /* Element.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = Element.cpp; sourceTree = "<group>"; };
-		A8C4A7F709D563270003AC8D /* CSSMappedAttributeDeclaration.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; path = CSSMappedAttributeDeclaration.h; sourceTree = "<group>"; };
-		A8C4A7F809D563270003AC8D /* CSSMappedAttributeDeclaration.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = CSSMappedAttributeDeclaration.cpp; sourceTree = "<group>"; };
 		A8C4A7F909D563270003AC8D /* Attribute.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; path = Attribute.h; sourceTree = "<group>"; };
 		A8C4A7FA09D563270003AC8D /* Attribute.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = Attribute.cpp; sourceTree = "<group>"; };
 		A8C4A7FB09D563270003AC8D /* Attr.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; path = Attr.h; sourceTree = "<group>"; };
@@ -20406,8 +20402,6 @@
 				A81872110977D3C0005826D9 /* ContainerNode.h */,
 				E1A1470711102B1500EEC0F3 /* ContainerNodeAlgorithms.h */,
 				2E3FAA0811A36BF100576624 /* CrossThreadTask.h */,
-				A8C4A7F809D563270003AC8D /* CSSMappedAttributeDeclaration.cpp */,
-				A8C4A7F709D563270003AC8D /* CSSMappedAttributeDeclaration.h */,
 				62CD32561157E57C0063B0A7 /* CustomEvent.cpp */,
 				62CD32571157E57C0063B0A7 /* CustomEvent.h */,
 				62CD32581157E57C0063B0A7 /* CustomEvent.idl */,
@@ -21290,7 +21284,6 @@
 				A80E6D010A1989CA007FB8C5 /* CSSInheritedValue.h in Headers */,
 				A80E6D080A1989CA007FB8C5 /* CSSInitialValue.h in Headers */,
 				BC772E131331620C001EC9CE /* CSSLineBoxContainValue.h in Headers */,
-				A8C4A80909D563270003AC8D /* CSSMappedAttributeDeclaration.h in Headers */,
 				A80E6D030A1989CA007FB8C5 /* CSSMediaRule.h in Headers */,
 				A80E6DFC0A199067007FB8C5 /* StylePropertySet.h in Headers */,
 				A80E734E0A199C77007FB8C5 /* CSSNamespace.h in Headers */,
@@ -24695,7 +24688,6 @@
 				A80E6CF00A1989CA007FB8C5 /* CSSInheritedValue.cpp in Sources */,
 				A80E6D020A1989CA007FB8C5 /* CSSInitialValue.cpp in Sources */,
 				BC772E16133162C2001EC9CE /* CSSLineBoxContainValue.cpp in Sources */,
-				A8C4A80A09D563270003AC8D /* CSSMappedAttributeDeclaration.cpp in Sources */,
 				A80E6CFC0A1989CA007FB8C5 /* CSSMediaRule.cpp in Sources */,
 				A80E6DFB0A199067007FB8C5 /* StylePropertySet.cpp in Sources */,
 				F98FFF4411A2676200F548E8 /* CSSOMUtils.cpp in Sources */,

commit 02262c7f2fe8bf5ebafe7c4d319fe3243923eaca

REGRESSION(r106668-r106889): Chromium page cycler tests (Intl2) performance regressions. <http://webkit.org/b/78068>
Reviewed by Ryosuke Niwa.

Create CSS_IDENT values for attribute styles in the document's CSSValuePool.
This regressed in r106756 and I suspect it'll fix up the cycler regression.

* dom/StyledElement.cpp:
(WebCore::StyledElement::addCSSProperty):


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@107029 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/StyledElement.cpp
@@ -29,6 +29,7 @@
 #include "CSSStyleSelector.h"
 #include "CSSStyleSheet.h"
 #include "CSSValueKeywords.h"
+#include "CSSValuePool.h"
 #include "Color.h"
 #include "ClassList.h"
 #include "ContentSecurityPolicy.h"
@@ -142,9 +143,9 @@ void StyledElement::addCSSProperty(int id, const String &value)
         removeCSSProperty(id);
 }
 
-void StyledElement::addCSSProperty(int id, int value)
+void StyledElement::addCSSProperty(int propertyID, int identifier)
 {
-    ensureAttributeStyle()->setProperty(id, value);
+    ensureAttributeStyle()->setProperty(CSSProperty(propertyID, document()->cssValuePool()->createIdentifierValue(identifier)));
 }
 
 void StyledElement::addCSSImageProperty(int id, const String& url)