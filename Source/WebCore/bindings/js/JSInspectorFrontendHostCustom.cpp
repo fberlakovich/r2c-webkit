fberlakovich/r2c-webkit

commit 5a8271d22ab463c4f03f7ba0ce0556abc4bc7c18

Web Inspector: Support hierarchical context menus https://bugs.webkit.org/show_bug.cgi?id=86847
Reviewed by Pavel Feldman.

This patch makes use of the WebMenuItemInfo SubMenu type to expose the capability of building submenu items
in the Web Inspector's context menu. ContextMenuItems are also passed/stored by reference/value rather than pointer
in order to be consistent with the PlatformMenuDescription typedef.

* bindings/js/JSInspectorFrontendHostCustom.cpp:
(WebCore::populateContextMenuItems): Enable submenu item population.
(WebCore):
(WebCore::JSInspectorFrontendHost::showContextMenu): Extract the menu population part into populateContextMenuItems().
* bindings/v8/custom/V8InspectorFrontendHostCustom.cpp:
(WebCore::populateContextMenuItems): Enable submenu item population.
(WebCore):
(WebCore::V8InspectorFrontendHost::showContextMenuCallback): Extract the menu population part into populateContextMenuItems().
* inspector/InspectorFrontendHost.cpp:
(WebCore::FrontendMenuProvider::create): Use reference instead of pointer for ContextMenuItems.
(WebCore::FrontendMenuProvider::FrontendMenuProvider): Use reference instead of pointer for ContextMenuItems.
(WebCore::FrontendMenuProvider::populateContextMenu): Use reference instead of pointer for ContextMenuItems.
(WebCore::FrontendMenuProvider::contextMenuCleared):
(FrontendMenuProvider):
(WebCore::InspectorFrontendHost::showContextMenu): Use reference instead of pointer for ContextMenuItems.
* inspector/InspectorFrontendHost.h:
(InspectorFrontendHost):
* inspector/front-end/ContextMenu.js: Support the tree-like structure of context menus.
(WebInspector.ContextMenuItem):
(WebInspector.ContextMenuItem.prototype.id):
(WebInspector.ContextMenuItem.prototype.type):
(WebInspector.ContextMenuItem.prototype._buildDescriptor):
(WebInspector.ContextSubMenuItem):
(WebInspector.ContextSubMenuItem.prototype.appendItem):
(WebInspector.ContextSubMenuItem.prototype.appendSubMenuItem):

Source/WebCore/bindings/js/JSInspectorFrontendHostCustom.cpp
@@ -85,16 +85,9 @@ JSValue JSInspectorFrontendHost::port(ExecState* execState)
     return jsString(execState, port);
 }
 
-JSValue JSInspectorFrontendHost::showContextMenu(ExecState* exec)
-{
-    if (exec->argumentCount() < 2)
-        return jsUndefined();
 #if ENABLE(CONTEXT_MENUS)
-    Event* event = toEvent(exec->argument(0));
-
-    JSArray* array = asArray(exec->argument(1));
-    Vector<ContextMenuItem*> items;
-
+static void populateContextMenuItems(ExecState* exec, JSArray* array, ContextMenu& menu)
+{
     for (size_t i = 0; i < array->length(); ++i) {
         JSObject* item = asObject(array->getIndex(i));
         JSValue label = item->get(exec, Identifier(exec, "label"));
@@ -102,25 +95,54 @@ JSValue JSInspectorFrontendHost::showContextMenu(ExecState* exec)
         JSValue id = item->get(exec, Identifier(exec, "id"));
         JSValue enabled = item->get(exec, Identifier(exec, "enabled"));
         JSValue checked = item->get(exec, Identifier(exec, "checked"));
+        JSValue subItems = item->get(exec, Identifier(exec, "subItems"));
         if (!type.isString())
             continue;
 
         String typeString = ustringToString(type.toString(exec)->value(exec));
         if (typeString == "separator") {
-            items.append(new ContextMenuItem(SeparatorType,
-                                             ContextMenuItemCustomTagNoAction,
-                                             String()));
+            ContextMenuItem item(SeparatorType,
+                                 ContextMenuItemCustomTagNoAction,
+                                 String());
+            menu.appendItem(item);
+        } else if (typeString == "subMenu" && subItems.inherits(&JSArray::s_info)) {
+            ContextMenu subMenu;
+            JSArray* subItemsArray = asArray(subItems);
+            populateContextMenuItems(exec, subItemsArray, subMenu);
+            ContextMenuItem item(SubmenuType,
+                                 ContextMenuItemCustomTagNoAction,
+                                 ustringToString(label.toString(exec)->value(exec)),
+                                 &subMenu);
+            menu.appendItem(item);
         } else {
             ContextMenuAction typedId = static_cast<ContextMenuAction>(ContextMenuItemBaseCustomTag + id.toInt32(exec));
-            ContextMenuItem* menuItem = new ContextMenuItem((typeString == "checkbox" ? CheckableActionType : ActionType), typedId, ustringToString(label.toString(exec)->value(exec)));
+            ContextMenuItem menuItem((typeString == "checkbox" ? CheckableActionType : ActionType), typedId, ustringToString(label.toString(exec)->value(exec)));
             if (!enabled.isUndefined())
-                menuItem->setEnabled(enabled.toBoolean());
+                menuItem.setEnabled(enabled.toBoolean());
             if (!checked.isUndefined())
-                menuItem->setChecked(checked.toBoolean());
-            items.append(menuItem);
+                menuItem.setChecked(checked.toBoolean());
+            menu.appendItem(menuItem);
         }
     }
+}
+#endif
 
+JSValue JSInspectorFrontendHost::showContextMenu(ExecState* exec)
+{
+#if ENABLE(CONTEXT_MENUS)
+    if (exec->argumentCount() < 2)
+        return jsUndefined();
+    Event* event = toEvent(exec->argument(0));
+
+    JSArray* array = asArray(exec->argument(1));
+    ContextMenu menu;
+    populateContextMenuItems(exec, array, menu);
+
+#if !USE(CROSS_PLATFORM_CONTEXT_MENUS)
+    Vector<ContextMenuItem> items = contextMenuItemVector(menu.platformDescription());
+#else
+    Vector<ContextMenuItem> items = menu.items();
+#endif
     impl()->showContextMenu(event, items);
 #endif
     return jsUndefined();

Source/WebCore/bindings/v8/custom/V8InspectorFrontendHostCustom.cpp
@@ -68,6 +68,45 @@ v8::Handle<v8::Value> V8InspectorFrontendHost::portCallback(const v8::Arguments&
     return v8::Undefined();
 }
 
+static void populateContextMenuItems(v8::Local<v8::Array>& itemArray, ContextMenu& menu)
+{
+    for (size_t i = 0; i < itemArray->Length(); ++i) {
+        v8::Local<v8::Object> item = v8::Local<v8::Object>::Cast(itemArray->Get(i));
+        v8::Local<v8::Value> type = item->Get(v8::String::New("type"));
+        v8::Local<v8::Value> id = item->Get(v8::String::New("id"));
+        v8::Local<v8::Value> label = item->Get(v8::String::New("label"));
+        v8::Local<v8::Value> enabled = item->Get(v8::String::New("enabled"));
+        v8::Local<v8::Value> checked = item->Get(v8::String::New("checked"));
+        v8::Local<v8::Value> subItems = item->Get(v8::String::New("subItems"));
+        if (!type->IsString())
+            continue;
+        String typeString = toWebCoreStringWithNullCheck(type);
+        if (typeString == "separator") {
+            ContextMenuItem item(ContextMenuItem(SeparatorType,
+                                 ContextMenuItemCustomTagNoAction,
+                                 String()));
+            menu.appendItem(item);
+        } else if (typeString == "subMenu" && subItems->IsArray()) {
+            ContextMenu subMenu;
+            v8::Local<v8::Array> subItemsArray = v8::Local<v8::Array>::Cast(subItems);
+            populateContextMenuItems(subItemsArray, subMenu);
+            ContextMenuItem item(SubmenuType,
+                                 ContextMenuItemCustomTagNoAction,
+                                 toWebCoreStringWithNullCheck(label),
+                                 &subMenu);
+            menu.appendItem(item);
+        } else {
+            ContextMenuAction typedId = static_cast<ContextMenuAction>(ContextMenuItemBaseCustomTag + id->ToInt32()->Value());
+            ContextMenuItem menuItem((typeString == "checkbox" ? CheckableActionType : ActionType), typedId, toWebCoreStringWithNullCheck(label));
+            if (checked->IsBoolean())
+                menuItem.setChecked(checked->ToBoolean()->Value());
+            if (enabled->IsBoolean())
+                menuItem.setEnabled(enabled->ToBoolean()->Value());
+            menu.appendItem(menuItem);
+        }
+    }
+}
+
 v8::Handle<v8::Value> V8InspectorFrontendHost::showContextMenuCallback(const v8::Arguments& args)
 {
     if (args.Length() < 2)
@@ -82,34 +121,15 @@ v8::Handle<v8::Value> V8InspectorFrontendHost::showContextMenuCallback(const v8:
         return v8::Undefined();
 
     v8::Local<v8::Array> array = v8::Local<v8::Array>::Cast(args[1]);
-    Vector<ContextMenuItem*> items;
-
-    for (size_t i = 0; i < array->Length(); ++i) {
-        v8::Local<v8::Object> item = v8::Local<v8::Object>::Cast(array->Get(i));
-        v8::Local<v8::Value> type = item->Get(v8::String::New("type"));
-        v8::Local<v8::Value> id = item->Get(v8::String::New("id"));
-        v8::Local<v8::Value> label = item->Get(v8::String::New("label"));
-        v8::Local<v8::Value> enabled = item->Get(v8::String::New("enabled"));
-        v8::Local<v8::Value> checked = item->Get(v8::String::New("checked"));
-        if (!type->IsString())
-            continue;
-        String typeString = toWebCoreStringWithNullCheck(type);
-        if (typeString == "separator") {
-            items.append(new ContextMenuItem(SeparatorType,
-                                             ContextMenuItemCustomTagNoAction,
-                                             String()));
-        } else {
-            ContextMenuAction typedId = static_cast<ContextMenuAction>(ContextMenuItemBaseCustomTag + id->ToInt32()->Value());
-            ContextMenuItem* menuItem = new ContextMenuItem((typeString == "checkbox" ? CheckableActionType : ActionType), typedId, toWebCoreStringWithNullCheck(label));
-            if (checked->IsBoolean())
-                menuItem->setChecked(checked->ToBoolean()->Value());
-            if (enabled->IsBoolean())
-                menuItem->setEnabled(enabled->ToBoolean()->Value());
-            items.append(menuItem);
-        }
-    }
+    ContextMenu menu;
+    populateContextMenuItems(array, menu);
 
     InspectorFrontendHost* frontendHost = V8InspectorFrontendHost::toNative(args.Holder());
+#if !USE(CROSS_PLATFORM_CONTEXT_MENUS)
+    Vector<ContextMenuItem> items = contextMenuItemVector(menu.platformDescription());
+#else
+    Vector<ContextMenuItem> items = menu.items();
+#endif
     frontendHost->showContextMenu(event, items);
 
     return v8::Undefined();

Source/WebCore/inspector/InspectorFrontendHost.cpp
@@ -60,7 +60,7 @@ namespace WebCore {
 #if ENABLE(CONTEXT_MENUS)
 class FrontendMenuProvider : public ContextMenuProvider {
 public:
-    static PassRefPtr<FrontendMenuProvider> create(InspectorFrontendHost* frontendHost, ScriptObject webInspector, const Vector<ContextMenuItem*>& items)
+    static PassRefPtr<FrontendMenuProvider> create(InspectorFrontendHost* frontendHost, ScriptObject webInspector, const Vector<ContextMenuItem>& items)
     {
         return adoptRef(new FrontendMenuProvider(frontendHost, webInspector, items));
     }
@@ -72,7 +72,7 @@ public:
     }
     
 private:
-    FrontendMenuProvider(InspectorFrontendHost* frontendHost, ScriptObject webInspector,  const Vector<ContextMenuItem*>& items)
+    FrontendMenuProvider(InspectorFrontendHost* frontendHost, ScriptObject webInspector, const Vector<ContextMenuItem>& items)
         : m_frontendHost(frontendHost)
         , m_webInspector(webInspector)
         , m_items(items)
@@ -87,7 +87,7 @@ private:
     virtual void populateContextMenu(ContextMenu* menu)
     {
         for (size_t i = 0; i < m_items.size(); ++i)
-            menu->appendItem(*m_items[i]);
+            menu->appendItem(m_items[i]);
     }
     
     virtual void contextMenuItemSelected(ContextMenuItem* item)
@@ -110,13 +110,12 @@ private:
 
             m_frontendHost->m_menuProvider = 0;
         }
-        deleteAllValues(m_items);
         m_items.clear();
     }
 
     InspectorFrontendHost* m_frontendHost;
     ScriptObject m_webInspector;
-    Vector<ContextMenuItem*> m_items;
+    Vector<ContextMenuItem> m_items;
 };
 #endif
 
@@ -258,7 +257,7 @@ void InspectorFrontendHost::sendMessageToBackend(const String& message)
 }
 
 #if ENABLE(CONTEXT_MENUS)
-void InspectorFrontendHost::showContextMenu(Event* event, const Vector<ContextMenuItem*>& items)
+void InspectorFrontendHost::showContextMenu(Event* event, const Vector<ContextMenuItem>& items)
 {
     ASSERT(m_frontendPage);
     ScriptState* frontendScriptState = scriptStateFromPage(debuggerWorld(), m_frontendPage);

Source/WebCore/inspector/InspectorFrontendHost.h
@@ -80,7 +80,7 @@ public:
     void append(const String& url, const String& content);
 
     // Called from [Custom] implementations.
-    void showContextMenu(Event*, const Vector<ContextMenuItem*>& items);
+    void showContextMenu(Event*, const Vector<ContextMenuItem>& items);
     void sendMessageToBackend(const String& message);
 
     String loadResourceSynchronously(const String& url);

Source/WebCore/inspector/front-end/ContextMenu.js
@@ -30,34 +30,77 @@
 
 /**
  * @constructor
+ * @param {WebInspector.ContextSubMenuItem} topLevelMenu
+ * @param {string} type
+ * @param {string=} label
+ * @param {boolean=} disabled
+ * @param {boolean=} checked
  */
-WebInspector.ContextMenu = function() {
-    this._items = [];
-    this._handlers = {};
+WebInspector.ContextMenuItem = function(topLevelMenu, type, label, disabled, checked)
+{
+    this._type = type;
+    this._label = label;
+    this._disabled = disabled;
+    this._checked = checked;
+    this._contextMenu = topLevelMenu;
+    if (type === "item" || type === "checkbox")
+        this._id = topLevelMenu.nextId();
 }
 
-WebInspector.ContextMenu.prototype = {
-    show: function(event)
+WebInspector.ContextMenuItem.prototype = {
+    id: function()
     {
-        // Remove trailing separator.
-        while (this._items.length > 0 && !("id" in this._items[this._items.length - 1]))
-            this._items.splice(this._items.length - 1, 1);
-
-        if (this._items.length) {
-            WebInspector._contextMenu = this;
-            InspectorFrontendHost.showContextMenu(event, this._items);
-        }
-        event.consume();
+        return this._id;
     },
 
+    type: function()
+    {
+        return this._type;
+    },
+
+    _buildDescriptor: function()
+    {
+        switch (this._type) {
+        case "item":
+            return { type: "item", id: this._id, label: this._label, enabled: !this._disabled };
+        case "separator":
+            return { type: "separator" };
+        case "checkbox":
+            return { type: "checkbox", id: this._id, label: this._label, checked: !!this._checked, enabled: !this._disabled };
+        }
+    }
+}
+
+/**
+ * @constructor
+ * @extends {WebInspector.ContextMenuItem}
+ * @param topLevelMenu
+ * @param {string=} label
+ * @param {boolean=} disabled
+ */
+WebInspector.ContextSubMenuItem = function(topLevelMenu, label, disabled)
+{
+    WebInspector.ContextMenuItem.call(this, topLevelMenu, "subMenu", label, disabled);
+    this._items = [];
+}
+
+WebInspector.ContextSubMenuItem.prototype = {
     /**
      * @param {boolean=} disabled
      */
     appendItem: function(label, handler, disabled)
     {
-        var id = this._items.length;
-        this._items.push({type: "item", id: id, label: label, enabled: !disabled});
-        this._handlers[id] = handler;
+        var item = new WebInspector.ContextMenuItem(this._contextMenu, "item", label, disabled);
+        this._items.push(item);
+        this._contextMenu._setHandler(item.id(), handler);
+        return item;
+    },
+
+    appendSubMenuItem: function(label, disabled)
+    {
+        var item = new WebInspector.ContextSubMenuItem(this._contextMenu, label, disabled);
+        this._items.push(item);
+        return item;
     },
 
     /**
@@ -65,19 +108,22 @@ WebInspector.ContextMenu.prototype = {
      */
     appendCheckboxItem: function(label, handler, checked, disabled)
     {
-        var id = this._items.length;
-        this._items.push({type: "checkbox", id: id, label: label, checked: !!checked, enabled: !disabled});
-        this._handlers[id] = handler;
+        var item = new WebInspector.ContextMenuItem(this._contextMenu, "checkbox", label, disabled, checked);
+        this._items.push(item);
+        this._contextMenu._setHandler(item.id(), handler);
+        return item;
     },
 
     appendSeparator: function()
     {
         // No separator dupes allowed.
         if (this._items.length === 0)
-            return;
-        if (!("id" in this._items[this._items.length - 1]))
-            return;
-        this._items.push({type: "separator"});
+            return null;
+        if (this._items[this._items.length - 1].type() === "separator")
+            return null;
+        var item = new WebInspector.ContextMenuItem(this._contextMenu, "separator");
+        this._items.push(item);
+        return item;
     },
 
     /**
@@ -86,6 +132,58 @@ WebInspector.ContextMenu.prototype = {
     isEmpty: function()
     {
         return !this._items.length;
+    },
+
+    _buildDescriptor: function()
+    {
+        var result = { type: "subMenu", label: this._label, enabled: !this._disabled, subItems: [] };
+        for (var i = 0; i < this._items.length; ++i)
+            result.subItems.push(this._items[i]._buildDescriptor());
+        return result;
+    }
+}
+
+WebInspector.ContextSubMenuItem.prototype.__proto__ = WebInspector.ContextMenuItem.prototype;
+
+/**
+ * @constructor
+ * @extends {WebInspector.ContextSubMenuItem}
+ */
+WebInspector.ContextMenu = function() {
+    WebInspector.ContextSubMenuItem.call(this, this, "");
+    this._handlers = {};
+    this._id = 0;
+}
+
+WebInspector.ContextMenu.prototype = {
+    nextId: function()
+    {
+        return this._id++;
+    },
+
+    show: function(event)
+    {
+        var menuObject = this._buildDescriptor();
+
+        if (menuObject.length) {
+            WebInspector._contextMenu = this;
+            InspectorFrontendHost.showContextMenu(event, menuObject);
+        }
+        event.consume();
+    },
+
+    _setHandler: function(id, handler)
+    {
+        if (handler)
+            this._handlers[id] = handler;
+    },
+
+    _buildDescriptor: function()
+    {
+        var result = [];
+        for (var i = 0; i < this._items.length; ++i)
+            result.push(this._items[i]._buildDescriptor());
+        return result;
     },
 
     _itemSelected: function(id)
@@ -107,6 +205,8 @@ WebInspector.ContextMenu.prototype = {
         }
     }
 }
+
+WebInspector.ContextMenu.prototype.__proto__ = WebInspector.ContextSubMenuItem.prototype;
 
 /**
  * @interface

commit 35f81516fdcf792a71822ecc915776214b1b33e7

BUILD FIX: Unused variable with ENABLE(CONTEXT_MENUS) off
This broke in r118374.

* bindings/js/JSInspectorFrontendHostCustom.cpp:
(WebCore::JSInspectorFrontendHost::showContextMenu): Add
UNUSED_PARAM(exec) when ENABLE(CONTEXT_MENUS) is off.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119407 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/bindings/js/JSInspectorFrontendHostCustom.cpp
@@ -144,6 +144,8 @@ JSValue JSInspectorFrontendHost::showContextMenu(ExecState* exec)
     Vector<ContextMenuItem> items = menu.items();
 #endif
     impl()->showContextMenu(event, items);
+#else
+    UNUSED_PARAM(exec);
 #endif
     return jsUndefined();
 }