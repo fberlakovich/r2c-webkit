fberlakovich/r2c-webkit

commit f0f3e73b71ecc4ae91fcc42f2dfd400eba89ce0d

FocusController::advanceFocus spends a lot of time in HTMLMapElement::imageElement https://bugs.webkit.org/show_bug.cgi?id=122313
Reviewed by Andreas Kling.

The bug was caused by HTMLMapElement::imageElement traversing the entire document to look for
the image element associated with a given map element. Because HTMLCollection used to find the
image element is not cached, it traversed the entire document on every area element we visit,
resulting in O(n^2) behavior.

Fixed the bug by adding the name-to-image-element map on document to avoid the traversal in
HTMLMapElement::imageElement.

* dom/Document.cpp:
(WebCore::Document::addImageElementByLowercasedUsemap): Added.
(WebCore::Document::removeImageElementByLowercasedUsemap): Added.
(WebCore::Document::imageElementByLowercasedUsemap): Added.

* dom/Document.h: Added m_imagesByUsemap.

* dom/DocumentOrderedMap.cpp:
(WebCore::keyMatchesLowercasedUsemap): Added.
(WebCore::DocumentOrderedMap::getElementByLowercasedUsemap): Added.

* dom/DocumentOrderedMap.h:
* html/HTMLImageElement.cpp:
(WebCore::HTMLImageElement::parseAttribute): Update the name-to-usemap map. The code to parse
the usemap attribute and strip # was moved from HTMLMapElement::imageElement.
(WebCore::HTMLImageElement::insertedInto): Ditto.
(WebCore::HTMLImageElement::removedFrom): Ditto.
(WebCore::HTMLImageElement::matchesLowercasedUsemap): Added; called by DocumentOrderedMap.

* html/HTMLImageElement.h:

* html/HTMLMapElement.cpp:
(WebCore::HTMLMapElement::imageElement): Call Document::imageElementByUsemap instead of
looking through all image elements in the document.


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@156925 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/HTMLImageElement.cpp
@@ -123,9 +123,27 @@ void HTMLImageElement::parseAttribute(const QualifiedName& name, const AtomicStr
     } else if (name == srcAttr || name == srcsetAttr) {
         m_bestFitImageURL = bestFitSourceForImageAttributes(document().deviceScaleFactor(), fastGetAttribute(srcAttr), fastGetAttribute(srcsetAttr));
         m_imageLoader.updateFromElementIgnoringPreviousError();
-    } else if (name == usemapAttr)
+    } else if (name == usemapAttr) {
         setIsLink(!value.isNull() && !shouldProhibitLinks(this));
-    else if (name == onbeforeloadAttr)
+
+        if (m_lowercasedUsemap == value)
+            return;
+
+        if (!m_lowercasedUsemap.isNull())
+            document().removeImageElementByLowercasedUsemap(m_lowercasedUsemap, *this);
+
+        // The HTMLImageElement's useMap() value includes the '#' symbol at the beginning, which has to be stripped off.
+        // FIXME: We should check that the first character is '#'.
+        // FIXME: HTML5 specification says we should strip any leading string before '#'.
+        // FIXME: HTML5 specification says we should ignore usemap attributes without #.
+        if (value.length() > 1)
+            m_lowercasedUsemap = value.string().substring(1).lower();
+        else
+            m_lowercasedUsemap = nullAtom;
+
+        if (!m_lowercasedUsemap.isNull())
+            document().addImageElementByLowercasedUsemap(m_lowercasedUsemap, *this);
+    } else if (name == onbeforeloadAttr)
         setAttributeEventListener(eventNames().beforeloadEvent, name, value);
     else if (name == compositeAttr) {
         // FIXME: images don't support blend modes in their compositing attribute.
@@ -206,6 +224,9 @@ Node::InsertionNotificationRequest HTMLImageElement::insertedInto(ContainerNode&
             m_form->registerImgElement(this);
     }
 
+    if (insertionPoint->inDocument() && !m_lowercasedUsemap.isNull())
+        document().addImageElementByLowercasedUsemap(m_lowercasedUsemap, *this);
+
     // If we have been inserted from a renderer-less document,
     // our loader may have not fetched the image, so do it now.
     if (insertionPoint.inDocument() && !m_imageLoader.image())
@@ -218,6 +239,10 @@ void HTMLImageElement::removedFrom(ContainerNode& insertionPoint)
 {
     if (m_form)
         m_form->removeImgElement(this);
+
+    if (insertionPoint->inDocument() && !m_lowercasedUsemap.isNull())
+        document().removeImageElementByLowercasedUsemap(m_lowercasedUsemap, *this);
+
     m_form = 0;
     HTMLElement::removedFrom(insertionPoint);
 }
@@ -291,6 +316,12 @@ bool HTMLImageElement::isURLAttribute(const Attribute& attribute) const
         || attribute.name() == longdescAttr
         || (attribute.name() == usemapAttr && attribute.value().string()[0] != '#')
         || HTMLElement::isURLAttribute(attribute);
+}
+
+bool HTMLImageElement::matchesLowercasedUsemap(const AtomicStringImpl& name) const
+{
+    ASSERT(const_cast<AtomicStringImpl&>(name).lower() == &name);
+    return m_lowercasedUsemap.impl() == &name;
 }
 
 const AtomicString& HTMLImageElement::alt() const

Source/WebCore/dom/Document.cpp
@@ -84,6 +84,7 @@
 #include "HTMLFrameOwnerElement.h"
 #include "HTMLHeadElement.h"
 #include "HTMLIFrameElement.h"
+#include "HTMLImageElement.h"
 #include "HTMLLinkElement.h"
 #include "HTMLMediaElement.h"
 #include "HTMLNameCollection.h"
@@ -704,6 +705,21 @@ void Document::invalidateAccessKeyMap()
 {
     m_accessKeyMapValid = false;
     m_elementsByAccessKey.clear();
+}
+
+void Document::addImageElementByLowercasedUsemap(const AtomicString& name, HTMLImageElement& element)
+{
+    return m_imagesByUsemap.add(name.impl(), &element);
+}
+
+void Document::removeImageElementByLowercasedUsemap(const AtomicString& name, HTMLImageElement& element)
+{
+    return m_imagesByUsemap.remove(name.impl(), &element);
+}
+
+HTMLImageElement* Document::imageElementByLowercasedUsemap(const AtomicString& name) const
+{
+    return m_imagesByUsemap.getElementByLowercasedUsemap(name.impl(), this);
 }
 
 SelectorQueryCache& Document::selectorQueryCache()

Source/WebCore/dom/Document.h
@@ -103,6 +103,7 @@ class HTMLElement;
 class HTMLFrameOwnerElement;
 class HTMLHeadElement;
 class HTMLIFrameElement;
+class HTMLImageElement;
 class HTMLMapElement;
 class HTMLNameCollection;
 class HTMLScriptElement;
@@ -256,6 +257,10 @@ public:
 
     Element* getElementByAccessKey(const String& key);
     void invalidateAccessKeyMap();
+
+    void addImageElementByLowercasedUsemap(const AtomicString&, HTMLImageElement&);
+    void removeImageElementByLowercasedUsemap(const AtomicString&, HTMLImageElement&);
+    HTMLImageElement* imageElementByLowercasedUsemap(const AtomicString&) const;
 
     SelectorQueryCache& selectorQueryCache();
 
@@ -1449,6 +1454,8 @@ private:
 
     HashMap<StringImpl*, Element*, CaseFoldingHash> m_elementsByAccessKey;
     bool m_accessKeyMapValid;
+
+    DocumentOrderedMap m_imagesByUsemap;
 
     OwnPtr<SelectorQueryCache> m_selectorQueryCache;
 

Source/WebCore/dom/DocumentOrderedMap.cpp
@@ -33,6 +33,7 @@
 
 #include "Element.h"
 #include "ElementTraversal.h"
+#include "HTMLImageElement.h"
 #include "HTMLLabelElement.h"
 #include "HTMLMapElement.h"
 #include "HTMLNameCollection.h"
@@ -61,6 +62,12 @@ inline bool keyMatchesMapName(const AtomicStringImpl* key, Element* element)
 inline bool keyMatchesLowercasedMapName(const AtomicStringImpl* key, Element* element)
 {
     return isHTMLMapElement(element) && toHTMLMapElement(element)->getName().lower().impl() == key;
+}
+
+inline bool keyMatchesLowercasedUsemap(const AtomicStringImpl* key, Element* element)
+{
+    // FIXME: HTML5 specification says we should match both image and object elements.
+    return isHTMLImageElement(element) && toHTMLImageElement(element)->matchesLowercasedUsemap(*key);
 }
 
 inline bool keyMatchesLabelForAttribute(const AtomicStringImpl* key, Element* element)
@@ -167,6 +174,11 @@ Element* DocumentOrderedMap::getElementByMapName(const AtomicStringImpl* key, co
 Element* DocumentOrderedMap::getElementByLowercasedMapName(const AtomicStringImpl* key, const TreeScope* scope) const
 {
     return get<keyMatchesLowercasedMapName>(key, scope);
+}
+
+HTMLImageElement* DocumentOrderedMap::getElementByLowercasedUsemap(const AtomicStringImpl* key, const TreeScope* scope) const
+{
+    return toHTMLImageElement(get<keyMatchesLowercasedUsemap>(key, scope));
 }
 
 Element* DocumentOrderedMap::getElementByLabelForAttribute(const AtomicStringImpl* key, const TreeScope* scope) const

Source/WebCore/dom/DocumentOrderedMap.h
@@ -39,6 +39,7 @@
 namespace WebCore {
 
 class Element;
+class HTMLImageElement;
 class TreeScope;
 
 class DocumentOrderedMap {
@@ -56,6 +57,7 @@ public:
     Element* getElementByName(const AtomicStringImpl*, const TreeScope*) const;
     Element* getElementByMapName(const AtomicStringImpl*, const TreeScope*) const;
     Element* getElementByLowercasedMapName(const AtomicStringImpl*, const TreeScope*) const;
+    HTMLImageElement* getElementByLowercasedUsemap(const AtomicStringImpl*, const TreeScope*) const;
     Element* getElementByLabelForAttribute(const AtomicStringImpl*, const TreeScope*) const;
     Element* getElementByWindowNamedItem(const AtomicStringImpl*, const TreeScope*) const;
     Element* getElementByDocumentNamedItem(const AtomicStringImpl*, const TreeScope*) const;

Source/WebCore/html/HTMLImageElement.h
@@ -59,6 +59,8 @@ public:
 
     void setLoadManually(bool loadManually) { m_imageLoader.setLoadManually(loadManually); }
 
+    bool matchesLowercasedUsemap(const AtomicStringImpl&) const;
+
     const AtomicString& alt() const;
 
     void setHeight(int);
@@ -117,6 +119,7 @@ private:
     HTMLFormElement* m_form;
     CompositeOperator m_compositeOperator;
     AtomicString m_bestFitImageURL;
+    AtomicString m_lowercasedUsemap;
 };
 
 ELEMENT_TYPE_CASTS(HTMLImageElement)

Source/WebCore/html/HTMLMapElement.cpp
@@ -79,20 +79,7 @@ bool HTMLMapElement::mapMouseEvent(LayoutPoint location, const LayoutSize& size,
 
 HTMLImageElement* HTMLMapElement::imageElement()
 {
-    RefPtr<HTMLCollection> images = document().images();
-    for (unsigned i = 0; Node* curr = images->item(i); i++) {
-        if (!isHTMLImageElement(curr))
-            continue;
-        
-        // The HTMLImageElement's useMap() value includes the '#' symbol at the beginning,
-        // which has to be stripped off.
-        HTMLImageElement* imageElement = toHTMLImageElement(curr);
-        String useMapName = imageElement->getAttribute(usemapAttr).string().substring(1);
-        if (equalIgnoringCase(useMapName, m_name))
-            return imageElement;
-    }
-    
-    return 0;    
+    return document().imageElementByLowercasedUsemap(m_name.lower());
 }
 
 void HTMLMapElement::parseAttribute(const QualifiedName& name, const AtomicString& value)

commit 491c0f6969d06290dd5c53bdd988da5d7315895e

Fix an assertion failure introduced in r156925.
Add HTMLImageElement to the document-wide map only if it's in the document.

* html/HTMLImageElement.cpp:
(WebCore::HTMLImageElement::parseAttribute):


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@156950 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/HTMLImageElement.cpp
@@ -126,10 +126,7 @@ void HTMLImageElement::parseAttribute(const QualifiedName& name, const AtomicStr
     } else if (name == usemapAttr) {
         setIsLink(!value.isNull() && !shouldProhibitLinks(this));
 
-        if (m_lowercasedUsemap == value)
-            return;
-
-        if (!m_lowercasedUsemap.isNull())
+        if (inDocument() && !m_lowercasedUsemap.isNull())
             document().removeImageElementByLowercasedUsemap(*m_lowercasedUsemap.impl(), *this);
 
         // The HTMLImageElement's useMap() value includes the '#' symbol at the beginning, which has to be stripped off.
@@ -141,7 +138,7 @@ void HTMLImageElement::parseAttribute(const QualifiedName& name, const AtomicStr
         else
             m_lowercasedUsemap = nullAtom;
 
-        if (!m_lowercasedUsemap.isNull())
+        if (inDocument() && !m_lowercasedUsemap.isNull())
             document().addImageElementByLowercasedUsemap(*m_lowercasedUsemap.impl(), *this);
     } else if (name == onbeforeloadAttr)
         setAttributeEventListener(eventNames().beforeloadEvent, name, value);