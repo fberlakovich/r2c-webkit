fberlakovich/r2c-webkit

commit cfc779d69372f2b113b53c9ceae83627c5f7a5b9

Use unique_ptr instead of deleteAllValues in XPath https://bugs.webkit.org/show_bug.cgi?id=121082
Reviewed by Anders Carlsson.

* dom/Attr.h: Made the namespaceURI function public so it can be called by
code in XPath. Since the class is FINAL, calls won't do virtual dispatch.

* xml/XPathExpression.cpp:
(WebCore::XPathExpression::XPathExpression): Added. Use std::move.
(WebCore::XPathExpression::createExpression): Use the new Parser::parseStatement
and more std::move.
(WebCore::XPathExpression::~XPathExpression): Removed now-unneeded delete.
* xml/XPathExpression.h: Removed unused create function. Use std::unique_ptr.

* xml/XPathExpressionNode.cpp:
(WebCore::XPath::Expression::evaluationContext): Use NeverDestroyed instead of
DEFINE_STATIC_LOCAL.
(WebCore::XPath::Expression::setSubexpressions): Added. Used to set all the
subexpressions at once rather than adding one at a time.

* xml/XPathExpressionNode.h: Use plain unsigned instead of unsigned long.
Eliminated the ParseNode class, used only by the parser to delete objects,
which we now do with %destructor. Made more functions protected. Fixed the
capitalization of the word "subexpression". Made m_subexpressions be a Vector
of std::unique_ptr.

* xml/XPathFunctions.cpp: Marked all the classes FINAL and all their virtual
functions OVERRIDE. Reduced some of the boilerplate.
(WebCore::XPath::Function::setArguments): Passed in the name so we don't have
to store the function names in all the function objects.
(WebCore::XPath::FunId::evaluate): Use std::move instead of Value::adopt.
(WebCore::XPath::FunLocalName::evaluate): Use emptyString instead of "".
(WebCore::XPath::FunNamespaceURI::evaluate): Ditto.
(WebCore::XPath::FunName::evaluate): Ditto.
(WebCore::XPath::FunSubstringBefore::evaluate): Ditto.
(WebCore::XPath::FunSubstringAfter::evaluate): Ditto.
(WebCore::XPath::FunSubstring::evaluate): Ditto.
(WebCore::XPath::FunLang::evaluate): Use Value(false) so we can make the
constructor that takes a boolean explicit.

Source/WebCore/xml/XPathStep.h
@@ -27,8 +27,8 @@
 #ifndef XPathStep_h
 #define XPathStep_h
 
-#include "XPathExpressionNode.h"
-#include "XPathNodeSet.h"
+#include <wtf/Vector.h>
+#include <wtf/text/AtomicString.h>
 
 namespace WebCore {
 
@@ -36,10 +36,10 @@ class Node;
 
 namespace XPath {
 
-class Predicate;
-        
-class Step : public ParseNode {
-    WTF_MAKE_NONCOPYABLE(Step);
+class Expression;
+class NodeSet;
+
+class Step {
     WTF_MAKE_FAST_ALLOCATED;
 public:
     enum Axis {
@@ -49,57 +49,73 @@ public:
         ParentAxis, PrecedingAxis, PrecedingSiblingAxis,
         SelfAxis
     };
-           
+
     class NodeTest {
         WTF_MAKE_FAST_ALLOCATED;
     public:
-        enum Kind {
-            TextNodeTest, CommentNodeTest, ProcessingInstructionNodeTest, AnyNodeTest, NameTest
-        };
+        enum Kind { TextNodeTest, CommentNodeTest, ProcessingInstructionNodeTest, AnyNodeTest, NameTest };
 
-        NodeTest(Kind kind) : m_kind(kind) { }
-        NodeTest(Kind kind, const String& data) : m_kind(kind), m_data(data) { }
-        NodeTest(Kind kind, const String& data, const String& namespaceURI) : m_kind(kind), m_data(data), m_namespaceURI(namespaceURI) { }
-                
-        Kind kind() const { return m_kind; }
-        const AtomicString& data() const { return m_data; }
-        const AtomicString& namespaceURI() const { return m_namespaceURI; }
-        Vector<Predicate*>& mergedPredicates() { return m_mergedPredicates; }
-        const Vector<Predicate*>& mergedPredicates() const { return m_mergedPredicates; }
-                
+        explicit NodeTest(Kind kind) : m_kind(kind) { }
+        NodeTest(Kind kind, const AtomicString& data) : m_kind(kind), m_data(data) { }
+        NodeTest(Kind kind, const AtomicString& data, const AtomicString& namespaceURI) : m_kind(kind), m_data(data), m_namespaceURI(namespaceURI) { }
+
+#if COMPILER(MSVC)
+        NodeTest(const NodeTest&);
+        void operator=(const NodeTest&);
+
+        NodeTest(NodeTest&& other)
+            : m_kind(other.m_kind)
+            , m_data(std::move(other.m_data))
+            , m_namespaceURI(std::move(other.m_namespaceURI))
+            , m_mergedPredicates(std::move(m_mergedPredicates))
+        {
+        }
+        NodeTest& operator=(NodeTest&& other)
+        {
+            m_kind = other.m_kind;
+            m_data = std::move(other.m_data);
+            m_namespaceURI = std::move(other.m_namespaceURI);
+            m_mergedPredicates = std::move(m_mergedPredicates);
+            return *this;
+        }
+#endif
+
     private:
+        friend class Step;
+        friend void optimizeStepPair(Step&, Step&, bool&);
+        friend bool nodeMatchesBasicTest(Node&, Axis, const NodeTest&);
+        friend bool nodeMatches(Node&, Axis, const NodeTest&);
+
         Kind m_kind;
         AtomicString m_data;
         AtomicString m_namespaceURI;
-
-        // When possible, we merge some or all predicates with node test for better performance.
-        Vector<Predicate*> m_mergedPredicates;
+        Vector<std::unique_ptr<Expression>> m_mergedPredicates;
     };
 
-    Step(Axis, const NodeTest&, const Vector<Predicate*>& predicates = Vector<Predicate*>());
+    Step(Axis, NodeTest);
+    Step(Axis, NodeTest, Vector<std::unique_ptr<Expression>>);
     ~Step();
 
     void optimize();
 
-    void evaluate(Node* context, NodeSet&) const;
+    void evaluate(Node& context, NodeSet&) const;
 
     Axis axis() const { return m_axis; }
-    const NodeTest& nodeTest() const { return m_nodeTest; }
 
 private:
-    friend void optimizeStepPair(Step*, Step*, bool&);
+    friend void optimizeStepPair(Step&, Step&, bool&);
+
     bool predicatesAreContextListInsensitive() const;
 
     void parseNodeTest(const String&);
-    void nodesInAxis(Node* context, NodeSet&) const;
-    String namespaceFromNodetest(const String& nodeTest) const;
+    void nodesInAxis(Node& context, NodeSet&) const;
 
     Axis m_axis;
     NodeTest m_nodeTest;
-    Vector<Predicate*> m_predicates;
+    Vector<std::unique_ptr<Expression>> m_predicates;
 };
 
-void optimizeStepPair(Step*, Step*, bool& dropSecondStep);
+void optimizeStepPair(Step&, Step&, bool& dropSecondStep);
 
 } // namespace XPath
 

Source/WebCore/dom/Attr.h
@@ -64,6 +64,8 @@ public:
     void attachToElement(Element*);
     void detachFromElementWithValue(const AtomicString&);
 
+    virtual const AtomicString& namespaceURI() const OVERRIDE { return m_name.namespaceURI(); }
+
 private:
     Attr(Element*, const QualifiedName&);
     Attr(Document&, const QualifiedName&, const AtomicString& value);
@@ -74,7 +76,6 @@ private:
     virtual NodeType nodeType() const OVERRIDE { return ATTRIBUTE_NODE; }
 
     virtual const AtomicString& localName() const OVERRIDE { return m_name.localName(); }
-    virtual const AtomicString& namespaceURI() const OVERRIDE { return m_name.namespaceURI(); }
     virtual const AtomicString& prefix() const OVERRIDE { return m_name.prefix(); }
 
     virtual void setPrefix(const AtomicString&, ExceptionCode&) OVERRIDE;

Source/WebCore/xml/XPathExpression.cpp
@@ -41,21 +41,22 @@ namespace WebCore {
 
 using namespace XPath;
     
+inline XPathExpression::XPathExpression(std::unique_ptr<XPath::Expression> expression)
+    : m_topExpression(std::move(expression))
+{
+}
+
 PassRefPtr<XPathExpression> XPathExpression::createExpression(const String& expression, XPathNSResolver* resolver, ExceptionCode& ec)
 {
-    RefPtr<XPathExpression> expr = XPathExpression::create();
-    Parser parser;
+    auto parsedExpression = Parser::parseStatement(expression, resolver, ec);
+    if (!parsedExpression)
+        return nullptr;
 
-    expr->m_topExpression = parser.parseStatement(expression, resolver, ec);
-    if (!expr->m_topExpression)
-        return 0;
-
-    return expr.release();
+    return adoptRef(new XPathExpression(std::move(parsedExpression)));
 }
 
 XPathExpression::~XPathExpression()
 {
-    delete m_topExpression;
 }
 
 PassRefPtr<XPathResult> XPathExpression::evaluate(Node* contextNode, unsigned short type, XPathResult*, ExceptionCode& ec)
@@ -71,20 +72,20 @@ PassRefPtr<XPathResult> XPathExpression::evaluate(Node* contextNode, unsigned sh
     evaluationContext.position = 1;
     evaluationContext.hadTypeConversionError = false;
     RefPtr<XPathResult> result = XPathResult::create(&contextNode->document(), m_topExpression->evaluate());
-    evaluationContext.node = 0; // Do not hold a reference to the context node, as this may prevent the whole document from being destroyed in time.
+    evaluationContext.node = nullptr; // Do not hold a reference to the context node, as this may prevent the whole document from being destroyed in time.
 
     if (evaluationContext.hadTypeConversionError) {
         // It is not specified what to do if type conversion fails while evaluating an expression, and INVALID_EXPRESSION_ERR is not exactly right
         // when the failure happens in an otherwise valid expression because of a variable. But XPathEvaluator does not support variables, so it's close enough.
         ec = XPathException::INVALID_EXPRESSION_ERR;
-        return 0;
+        return nullptr;
     }
 
     if (type != XPathResult::ANY_TYPE) {
         ec = 0;
         result->convertTo(type, ec);
         if (ec)
-            return 0;
+            return nullptr;
     }
 
     return result;

Source/WebCore/xml/XPathExpression.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2005 Frerich Raabe <raabe@kde.org>
- * Copyright (C) 2006 Apple Computer, Inc.
+ * Copyright (C) 2006, 2013 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,7 +29,6 @@
 
 #include <wtf/Forward.h>
 #include <wtf/RefCounted.h>
-#include <wtf/PassRefPtr.h>
 
 namespace WebCore {
 
@@ -45,16 +44,15 @@ namespace WebCore {
 
     class XPathExpression : public RefCounted<XPathExpression> {
     public:
-        static PassRefPtr<XPathExpression> create() { return adoptRef(new XPathExpression); }
+        static PassRefPtr<XPathExpression> createExpression(const String& expression, XPathNSResolver*, ExceptionCode&);
         ~XPathExpression();
         
-        static PassRefPtr<XPathExpression> createExpression(const String& expression, XPathNSResolver*, ExceptionCode&);
         PassRefPtr<XPathResult> evaluate(Node* contextNode, unsigned short type, XPathResult*, ExceptionCode&);
             
     private:
-        XPathExpression() { }
+        explicit XPathExpression(std::unique_ptr<XPath::Expression>);
 
-        XPath::Expression* m_topExpression;
+        std::unique_ptr<XPath::Expression> m_topExpression;
     };
 
 }

Source/WebCore/xml/XPathExpressionNode.cpp
@@ -28,6 +28,7 @@
 #include "XPathExpressionNode.h"
 
 #include "Node.h"
+#include <wtf/NeverDestroyed.h>
 #include <wtf/StdLibExtras.h>
 
 namespace WebCore {
@@ -35,8 +36,8 @@ namespace XPath {
 
 EvaluationContext& Expression::evaluationContext()
 {
-    DEFINE_STATIC_LOCAL(EvaluationContext, evaluationContext, ());
-    return evaluationContext;
+    static NeverDestroyed<EvaluationContext> context;
+    return context;
 }
 
 Expression::Expression()
@@ -46,9 +47,15 @@ Expression::Expression()
 {
 }
 
-Expression::~Expression()
+void Expression::setSubexpressions(Vector<std::unique_ptr<Expression>> subexpressions)
 {
-    deleteAllValues(m_subExpressions);
+    ASSERT(m_subexpressions.isEmpty());
+    m_subexpressions = std::move(subexpressions);
+    for (unsigned i = 0; i < m_subexpressions.size(); ++i) {
+        m_isContextNodeSensitive |= m_subexpressions[i]->m_isContextNodeSensitive;
+        m_isContextPositionSensitive |= m_subexpressions[i]->m_isContextPositionSensitive;
+        m_isContextSizeSensitive |= m_subexpressions[i]->m_isContextSizeSensitive;
+    }
 }
 
 } // namespace XPath

Source/WebCore/xml/XPathExpressionNode.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2005 Frerich Raabe <raabe@kde.org>
- * Copyright (C) 2006, 2009, 2013 Apple Inc.
+ * Copyright (C) 2006, 2009, 2013 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,66 +27,56 @@
 #ifndef XPathExpressionNode_h
 #define XPathExpressionNode_h
 
-#include "Node.h"
 #include "XPathValue.h"
-#include <wtf/HashMap.h>
-#include <wtf/Vector.h>
-#include <wtf/text/StringHash.h>
 
 namespace WebCore {
 namespace XPath {
 
 struct EvaluationContext {
-    WTF_MAKE_FAST_ALLOCATED;
-public:
     RefPtr<Node> node;
-    unsigned long size;
-    unsigned long position;
+    unsigned size;
+    unsigned position;
     HashMap<String, String> variableBindings;
 
     bool hadTypeConversionError;
 };
 
-class ParseNode {
-public:
-    virtual ~ParseNode() { }
-};
-
-class Expression : public ParseNode {
+class Expression {
     WTF_MAKE_NONCOPYABLE(Expression); WTF_MAKE_FAST_ALLOCATED;
 public:
     static EvaluationContext& evaluationContext();
 
-    virtual ~Expression();
-
-    void addSubExpression(Expression* expr)
-    {
-        m_subExpressions.append(expr);
-        m_isContextNodeSensitive |= expr->m_isContextNodeSensitive;
-        m_isContextPositionSensitive |= expr->m_isContextPositionSensitive;
-        m_isContextSizeSensitive |= expr->m_isContextSizeSensitive;
-    }
-
-    bool isContextNodeSensitive() const { return m_isContextNodeSensitive; }
-    void setIsContextNodeSensitive(bool value) { m_isContextNodeSensitive = value; }
-
-    bool isContextPositionSensitive() const { return m_isContextPositionSensitive; }
-    void setIsContextPositionSensitive(bool value) { m_isContextPositionSensitive = value; }
-
-    bool isContextSizeSensitive() const { return m_isContextSizeSensitive; }
-    void setIsContextSizeSensitive(bool value) { m_isContextSizeSensitive = value; }
+    virtual ~Expression() { }
 
     virtual Value evaluate() const = 0;
     virtual Value::Type resultType() const = 0;
 
+    bool isContextNodeSensitive() const { return m_isContextNodeSensitive; }
+    bool isContextPositionSensitive() const { return m_isContextPositionSensitive; }
+    bool isContextSizeSensitive() const { return m_isContextSizeSensitive; }
+
 protected:
     Expression();
 
-    unsigned subExpressionCount() const { return m_subExpressions.size(); }
-    const Expression& subExpression(unsigned i) const { return *m_subExpressions[i]; }
+    unsigned subexpressionCount() const { return m_subexpressions.size(); }
+    const Expression& subexpression(unsigned i) const { return *m_subexpressions[i]; }
+
+    void addSubexpression(std::unique_ptr<Expression> expression)
+    {
+        m_isContextNodeSensitive |= expression->m_isContextNodeSensitive;
+        m_isContextPositionSensitive |= expression->m_isContextPositionSensitive;
+        m_isContextSizeSensitive |= expression->m_isContextSizeSensitive;
+        m_subexpressions.append(std::move(expression));
+    }
+
+    void setSubexpressions(Vector<std::unique_ptr<Expression>>);
+
+    void setIsContextNodeSensitive(bool value) { m_isContextNodeSensitive = value; }
+    void setIsContextPositionSensitive(bool value) { m_isContextPositionSensitive = value; }
+    void setIsContextSizeSensitive(bool value) { m_isContextSizeSensitive = value; }
 
 private:
-    Vector<Expression*> m_subExpressions;
+    Vector<std::unique_ptr<Expression>> m_subexpressions;
 
     // Evaluation details that can be used for optimization.
     bool m_isContextNodeSensitive;

Source/WebCore/xml/XPathFunctions.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2005 Frerich Raabe <raabe@kde.org>
- * Copyright (C) 2006, 2009 Apple Inc.
+ * Copyright (C) 2006, 2009, 2013 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,20 +34,17 @@ namespace XPath {
 
 class Function : public Expression {
 public:
-    void setArguments(const Vector<Expression*>&);
-    void setName(const String& name) { m_name = name; }
+    static std::unique_ptr<Function> create(const String& name);
+    static std::unique_ptr<Function> create(const String& name, Vector<std::unique_ptr<Expression>> arguments);
 
 protected:
-    unsigned argumentCount() const { return subExpressionCount(); }
-    const Expression& argument(int pos) const { return subExpression(pos); }
-
-    String name() const { return m_name; }
+    unsigned argumentCount() const { return subexpressionCount(); }
+    const Expression& argument(unsigned i) const { return subexpression(i); }
 
 private:
-    String m_name;
+    static std::unique_ptr<Function> create(const String& name, unsigned numArguments);
+    void setArguments(const String& name, Vector<std::unique_ptr<Expression>>);
 };
-
-Function* createFunction(const String& name, const Vector<Expression*>& args = Vector<Expression*>());
 
 } // namespace XPath
 } // namespace WebCore

commit 057f06d8a1c6c3d97294c841cd9fb0e38f98304d

[Windows] Fix for fast/xpath test regressions caused by r157205. https://bugs.webkit.org/show_bug.cgi?id=122658.
Reviewed by Darin Adler.

* xml/XPathStep.h:
(WebCore::XPath::Step::NodeTest::NodeTest):
(WebCore::XPath::Step::NodeTest::operator=):
We were incorrectly setting the predicate list to null instead of the predicate list of the NodeTest being passed in.


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@157324 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/xml/XPathStep.h
@@ -67,7 +67,7 @@ public:
             : m_kind(other.m_kind)
             , m_data(std::move(other.m_data))
             , m_namespaceURI(std::move(other.m_namespaceURI))
-            , m_mergedPredicates(std::move(m_mergedPredicates))
+            , m_mergedPredicates(std::move(other.m_mergedPredicates))
         {
         }
         NodeTest& operator=(NodeTest&& other)
@@ -75,7 +75,7 @@ public:
             m_kind = other.m_kind;
             m_data = std::move(other.m_data);
             m_namespaceURI = std::move(other.m_namespaceURI);
-            m_mergedPredicates = std::move(m_mergedPredicates);
+            m_mergedPredicates = std::move(other.m_mergedPredicates);
             return *this;
         }
 #endif