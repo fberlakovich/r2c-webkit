fberlakovich/r2c-webkit

commit fe057109aa9fc42436af366d338db54c3c6328fe

Remove AtomicallyInitializedStatic https://bugs.webkit.org/show_bug.cgi?id=126864
Reviewed by Darin Adler.

* wtf/Threading.h:
* wtf/ThreadingPthreads.cpp:
(WTF::initializeThreading):
* wtf/ThreadingWin.cpp:
(WTF::initializeThreading):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@161852 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/ThreadingPthreads.cpp
@@ -98,8 +98,6 @@ private:
 
 typedef HashMap<ThreadIdentifier, std::unique_ptr<PthreadState>> ThreadMap;
 
-static Mutex* atomicallyInitializedStaticMutex;
-
 void unsafeThreadWasDetached(ThreadIdentifier);
 void threadDidExit(ThreadIdentifier);
 void threadWasJoined(ThreadIdentifier);
@@ -112,14 +110,16 @@ static Mutex& threadMapMutex()
 
 void initializeThreading()
 {
-    if (atomicallyInitializedStaticMutex)
+    static bool isInitialized;
+
+    if (isInitialized)
         return;
 
     WTF::double_conversion::initialize();
     // StringImpl::empty() does not construct its static string in a threadsafe fashion,
     // so ensure it has been initialized from here.
     StringImpl::empty();
-    atomicallyInitializedStaticMutex = new Mutex;
+
     threadMapMutex();
     initializeRandomNumberGenerator();
     ThreadIdentifierData::initializeOnce();
@@ -127,17 +127,6 @@ void initializeThreading()
     wtfThreadData();
     s_dtoaP5Mutex = new Mutex;
     initializeDates();
-}
-
-void lockAtomicallyInitializedStaticMutex()
-{
-    ASSERT(atomicallyInitializedStaticMutex);
-    atomicallyInitializedStaticMutex->lock();
-}
-
-void unlockAtomicallyInitializedStaticMutex()
-{
-    atomicallyInitializedStaticMutex->unlock();
 }
 
 static ThreadMap& threadMap()

Source/WTF/wtf/Threading.h
@@ -69,12 +69,6 @@
 #include <wtf/ThreadSafeRefCounted.h>
 #include <wtf/ThreadingPrimitives.h>
 
-// For portability, we do not use thread-safe statics natively supported by some compilers (e.g. gcc).
-#define AtomicallyInitializedStatic(T, name) \
-    WTF::lockAtomicallyInitializedStaticMutex(); \
-    static T name; \
-    WTF::unlockAtomicallyInitializedStaticMutex();
-
 namespace WTF {
 
 typedef uint32_t ThreadIdentifier;
@@ -99,9 +93,6 @@ void initializeCurrentThreadInternal(const char* threadName);
 WTF_EXPORT_PRIVATE ThreadIdentifier currentThread();
 WTF_EXPORT_PRIVATE int waitForThreadCompletion(ThreadIdentifier);
 WTF_EXPORT_PRIVATE void detachThread(ThreadIdentifier);
-
-WTF_EXPORT_PRIVATE void lockAtomicallyInitializedStaticMutex();
-WTF_EXPORT_PRIVATE void unlockAtomicallyInitializedStaticMutex();
 
 } // namespace WTF
 

Source/WTF/wtf/ThreadingWin.cpp
@@ -148,19 +148,6 @@ void initializeCurrentThreadInternal(const char* szThreadName)
 #endif
 }
 
-static Mutex* atomicallyInitializedStaticMutex;
-
-void lockAtomicallyInitializedStaticMutex()
-{
-    ASSERT(atomicallyInitializedStaticMutex);
-    atomicallyInitializedStaticMutex->lock();
-}
-
-void unlockAtomicallyInitializedStaticMutex()
-{
-    atomicallyInitializedStaticMutex->unlock();
-}
-
 static Mutex& threadMapMutex()
 {
     static Mutex mutex;
@@ -169,19 +156,22 @@ static Mutex& threadMapMutex()
 
 void initializeThreading()
 {
-    if (atomicallyInitializedStaticMutex)
+    static bool isInitialized;
+    
+    if (isInitialized)
         return;
 
     WTF::double_conversion::initialize();
     // StringImpl::empty() does not construct its static string in a threadsafe fashion,
     // so ensure it has been initialized from here.
     StringImpl::empty();
-    atomicallyInitializedStaticMutex = new Mutex;
     threadMapMutex();
     initializeRandomNumberGenerator();
     wtfThreadData();
     s_dtoaP5Mutex = new Mutex;
     initializeDates();
+    
+    isInitialized = true;
 }
 
 static HashMap<DWORD, HANDLE>& threadMap()

commit 30f40039e3fe5342ac27d7ee8667c106453f4354

Fix an assertion failure in initializeDates() when launching Safari, which was introduced in r161852.
Reviewed by Andy Estes.

* wtf/ThreadingPthreads.cpp:
(WTF::initializeThreading): Set isInitialized to true.



git-svn-id: https://svn.webkit.org/repository/webkit/trunk@161855 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/ThreadingPthreads.cpp
@@ -115,6 +115,8 @@ void initializeThreading()
     if (isInitialized)
         return;
 
+    isInitialized = true;
+
     WTF::double_conversion::initialize();
     // StringImpl::empty() does not construct its static string in a threadsafe fashion,
     // so ensure it has been initialized from here.