fberlakovich/r2c-webkit

commit ba59e7ebdd28946a53cbd2613a643faac924ce1b

HTMLMediaElement::potentiallyPlaying can be more simple. https://bugs.webkit.org/show_bug.cgi?id=111
Patch by Piotr Grad <p.grad@samsung.com> on 2014-01-14
Reviewed by Eric Carlson.

Tests: No new tests, just refactorings.

Expression is simplified because the first 'if' statement depends on the same condition
as pausedToBuffer value.

* html/HTMLMediaElement.cpp:
(WebCore::HTMLMediaElement::parseAttribute):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@161973 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/HTMLMediaElement.cpp
@@ -4107,12 +4107,8 @@ PassRefPtr<TimeRanges> HTMLMediaElement::seekable() const
 
 bool HTMLMediaElement::potentiallyPlaying() const
 {
-    // "pausedToBuffer" means the media engine's rate is 0, but only because it had to stop playing
-    // when it ran out of buffered data. A movie is this state is "potentially playing", modulo the
-    // checks in couldPlayIfEnoughData().
-    bool pausedToBuffer = m_readyStateMaximum >= HAVE_FUTURE_DATA && m_readyState < HAVE_FUTURE_DATA;
-    
-    if (!pausedToBuffer && m_readyState < HAVE_FUTURE_DATA)
+    // When media engine ran out of buffered data it's not playing.
+    if (m_readyStateMaximum < HAVE_FUTURE_DATA && m_readyState < HAVE_FUTURE_DATA)
         return false;
 
     return couldPlayIfEnoughData() && !isBlockedOnMediaController();

commit 172d5c1f93fed5aa87d0105ca9fce415b15304f1

[iOS] preload=none prevents play() https://bugs.webkit.org/show_bug.cgi?id=127702
Reviewed by Jer Noble.

No new tests, covered by existing tests.

* html/HTMLMediaElement.cpp:
(WebCore::HTMLMediaElement::potentiallyPlaying): Cleanup logic to make it simpler to understand,
    fix logic error introduced in r161973
(WebCore::HTMLMediaElement::updatePlayState): Conditionalize some iOS-only code on whether or not
    we are using the plug-in proxy.


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@162870 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/HTMLMediaElement.cpp
@@ -4108,11 +4108,16 @@ PassRefPtr<TimeRanges> HTMLMediaElement::seekable() const
 
 bool HTMLMediaElement::potentiallyPlaying() const
 {
-    // When media engine ran out of buffered data it's not playing.
-    if (m_readyStateMaximum < HAVE_FUTURE_DATA && m_readyState < HAVE_FUTURE_DATA)
+    if (isBlockedOnMediaController())
+        return false;
+    
+    if (!couldPlayIfEnoughData())
         return false;
 
-    return couldPlayIfEnoughData() && !isBlockedOnMediaController();
+    if (m_readyState >= HAVE_FUTURE_DATA)
+        return true;
+
+    return m_readyStateMaximum >= HAVE_FUTURE_DATA && m_readyState < HAVE_FUTURE_DATA;
 }
 
 bool HTMLMediaElement::couldPlayIfEnoughData() const
@@ -4242,11 +4247,13 @@ void HTMLMediaElement::updatePlayState()
     bool shouldBePlaying = potentiallyPlaying();
     bool playerPaused = m_player->paused();
 
-#if PLATFORM(IOS)
-    if (shouldBePlaying && !m_requestingPlay && !m_player->readyForPlayback())
-        shouldBePlaying = false;
-    else if (!shouldBePlaying && m_requestingPlay && m_player->readyForPlayback())
-        shouldBePlaying = true;
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    if (shouldUseVideoPluginProxy()) {
+        if (shouldBePlaying && !m_requestingPlay && !m_player->readyForPlayback())
+            shouldBePlaying = false;
+        else if (!shouldBePlaying && m_requestingPlay && m_player->readyForPlayback())
+            shouldBePlaying = true;
+    }
 #endif
 
     LOG(Media, "HTMLMediaElement::updatePlayState - shouldBePlaying = %s, playerPaused = %s", 

Source/WebCore/platform/audio/ios/MediaSessionManagerIOS.mm
@@ -73,6 +73,7 @@ MediaSessionManageriOS::MediaSessionManageriOS()
     :MediaSessionManager()
     , m_objcObserver(adoptNS([[WebMediaSessionHelper alloc] initWithCallback:this]))
 {
+    resetRestrictions();
 }
 
 void MediaSessionManageriOS::resetRestrictions()