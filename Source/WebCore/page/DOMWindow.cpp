fberlakovich/r2c-webkit

commit a673c42fb216d2f1baf850931ab31b07af9a27d9

[iOS] WebKit1 scroll position is incorrect https://bugs.webkit.org/show_bug.cgi?id=129905
Patch by Benjamin Poulain <bpoulain@apple.com> on 2014-03-07
Reviewed by Simon Fraser.

* page/DOMWindow.cpp:
(WebCore::DOMWindow::scrollX):
(WebCore::DOMWindow::scrollY):
A new optimization was avoiding layout if the position is zero. This was using the scrollX/Y instead
of actualScrollX/Y which caused us to always bail out instead of returning the value to JavaScript.

* platform/ScrollView.h:
(WebCore::ScrollView::actualScrollX):
(WebCore::ScrollView::actualScrollY):
(WebCore::ScrollView::actualScrollPosition):
This was accidentally changed to the iOS compat document visible rect at some point.


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165288 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/DOMWindow.cpp
@@ -1192,16 +1192,18 @@ int DOMWindow::scrollX() const
     if (!view)
         return 0;
 
-    if (!view->scrollX())
+    int scrollX;
+#if PLATFORM(IOS)
+    scrollX = view->actualScrollX();
+#else
+    scrollX = view->scrollX();
+#endif
+    if (!scrollX)
         return 0;
 
     m_frame->document()->updateLayoutIgnorePendingStylesheets();
 
-#if PLATFORM(IOS)
-    return static_cast<int>(view->actualScrollX() / (m_frame->pageZoomFactor() * m_frame->frameScaleFactor()));
-#else
-    return view->mapFromLayoutToCSSUnits(view->scrollX());
-#endif
+    return view->mapFromLayoutToCSSUnits(scrollX);
 }
 
 int DOMWindow::scrollY() const
@@ -1213,16 +1215,18 @@ int DOMWindow::scrollY() const
     if (!view)
         return 0;
 
-    if (!view->scrollY())
+    int scrollY;
+#if PLATFORM(IOS)
+    scrollY = view->actualScrollY();
+#else
+    scrollY = view->scrollY();
+#endif
+    if (!scrollY)
         return 0;
 
     m_frame->document()->updateLayoutIgnorePendingStylesheets();
 
-#if PLATFORM(IOS)
-    return static_cast<int>(view->actualScrollY() / (m_frame->pageZoomFactor() * m_frame->frameScaleFactor()));
-#else
-    return view->mapFromLayoutToCSSUnits(view->scrollY());
-#endif
+    return view->mapFromLayoutToCSSUnits(scrollY);
 }
 
 bool DOMWindow::closed() const

Source/WebCore/platform/ScrollView.h
@@ -212,10 +212,10 @@ public:
     int scrollY() const { return scrollPosition().y(); }
 
 #if PLATFORM(IOS)
-    int actualScrollX() const { return visibleContentRect().x(); }
-    int actualScrollY() const { return visibleContentRect().y(); }
+    int actualScrollX() const { return unobscuredContentRect().x(); }
+    int actualScrollY() const { return unobscuredContentRect().y(); }
     // FIXME: maybe fix scrollPosition() on iOS to return the actual scroll position.
-    IntPoint actualScrollPosition() const { return visibleContentRect().location(); }
+    IntPoint actualScrollPosition() const { return unobscuredContentRect().location(); }
 #endif
 
     // scrollOffset() anchors its (0,0) point at the top end of the header if this ScrollableArea

commit 2a925441568e1b21ec6985918b7ba5f5710e69a8

Fix the regression introduced by r165288 https://bugs.webkit.org/show_bug.cgi?id=129934
Patch by Benjamin Poulain <bpoulain@apple.com> on 2014-03-07
Reviewed by Ryosuke Niwa.

* page/DOMWindow.cpp:
(WebCore::DOMWindow::scrollX):
(WebCore::DOMWindow::scrollY):
We need to return the new value after layout, not the value from the dirty tree.


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165301 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/DOMWindow.cpp
@@ -1203,7 +1203,11 @@ int DOMWindow::scrollX() const
 
     m_frame->document()->updateLayoutIgnorePendingStylesheets();
 
-    return view->mapFromLayoutToCSSUnits(scrollX);
+#if PLATFORM(IOS)
+    return view->mapFromLayoutToCSSUnits(view->actualScrollX());
+#else
+    return view->mapFromLayoutToCSSUnits(view->scrollX());
+#endif
 }
 
 int DOMWindow::scrollY() const
@@ -1226,7 +1230,11 @@ int DOMWindow::scrollY() const
 
     m_frame->document()->updateLayoutIgnorePendingStylesheets();
 
-    return view->mapFromLayoutToCSSUnits(scrollY);
+#if PLATFORM(IOS)
+    return view->mapFromLayoutToCSSUnits(view->actualScrollY());
+#else
+    return view->mapFromLayoutToCSSUnits(view->scrollY());
+#endif
 }
 
 bool DOMWindow::closed() const