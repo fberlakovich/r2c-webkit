fberlakovich/r2c-webkit

commit 5124eea033df11011a912b100451810a036af9af

Refactor Vibration algorithm to use only one timer. https://bugs.webkit.org/show_bug.cgi?id=130059
Patch by Jinwoo Jeong <jw00.jeong@samsung.com> on 2014-03-13
Reviewed by Darin Adler.

Source/WebCore:

Currently Vibration is using two timers,
one is to start a vibration and another is to check termination of a vibration.
But they do not work in same time, if one of them is fired, then anothor will start.
Thus this patch removes one of them, and manages vibration states by enumeration.

Also, this patch implement the missing part of the algorithm,
which check the maximum length of the vibration pattern and the maximum duration of the vibration.

Lastly, this patch removes unused methods from Vibration.

* Modules/vibration/Vibration.cpp:
(WebCore::Vibration::Vibration):
(WebCore::Vibration::vibrate):
(WebCore::Vibration::cancelVibration):
Removed stopVibration() and its contents moved to cancelVibration().
(WebCore::Vibration::timerFired): Combined timerStartFired() and timerStopFired().
Removed suspendVibration() and resumeVibration(), which is never called since r.
* Modules/vibration/Vibration.h: Added new enumertaion to specify states of Vibration.
(WebCore::Vibration::isVibrating):

Source/WebKit2:

This patch adds a vibration unit test regression after r161257.
It consists of two short term requests to check
that Vibration could work properly by sequential requests.

Indeed this patch tightens condition of existing tests.

* UIProcess/API/efl/tests/test_ewk2_view.cpp:
(TEST_F):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165598 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Modules/vibration/Vibration.cpp
@@ -26,11 +26,15 @@
 
 namespace WebCore {
 
+// Maximum number of entries in a vibration pattern.
+const unsigned MaxVibrationPatternLength = 99;
+// Maximum duration of a vibration in 10 seconds.
+const unsigned MaxVibrationDuration = 10000;
+
 Vibration::Vibration(VibrationClient* client)
     : m_vibrationClient(client)
-    , m_timerStart(this, &Vibration::timerStartFired)
-    , m_timerStop(this, &Vibration::timerStopFired)
-    , m_isVibrating(false)
+    , m_timer(this, &Vibration::timerFired)
+    , m_state(State::Idle)
 {
 }
 
@@ -46,80 +50,74 @@ PassOwnPtr<Vibration> Vibration::create(VibrationClient* client)
 
 bool Vibration::vibrate(const VibrationPattern& pattern)
 {
-    size_t length = pattern.size();
+    VibrationPattern& sanitized = const_cast<VibrationPattern&>(pattern);
+    size_t length = sanitized.size();
 
-    m_pattern = pattern;
-    if (length && !(length % 2))
-        m_pattern.removeLast();
+    // If the pattern is too long then truncate it.
+    if (length > MaxVibrationPatternLength) {
+        sanitized.shrink(MaxVibrationPatternLength);
+        length = MaxVibrationPatternLength;
+    }
 
-    // Pre-exsiting instance need to be canceled when vibration is called.
-    // And if time is 0, vibration have to be canceled also.
-    if (m_isVibrating || (m_pattern.size() == 1 && !m_pattern[0]))
+    // If the length of pattern is even and is not zero, then remove the last entry in pattern.
+    if (length && !(length % 2)) {
+        sanitized.removeLast();
+        length--;
+    }
+
+    // If any pattern entry is too long then truncate it.
+    for (auto& duration : sanitized)
+        duration = std::min(duration, MaxVibrationDuration);
+
+    // Pre-exisiting instance need to be canceled when vibrate() is called.
+    if (isVibrating())
         cancelVibration();
 
-    if (!m_pattern.size())
+    // If pattern is an empty list, then return true and terminate these steps.
+    if (!length || (length == 1 && !sanitized[0])) {
+        cancelVibration();
         return true;
+    }
 
-    m_timerStart.startOneShot(0);
-    m_isVibrating = true;
+    m_pattern = sanitized;
+
+    m_timer.startOneShot(0);
     return true;
 }
 
 void Vibration::cancelVibration()
 {
     m_pattern.clear();
-    if (m_isVibrating)
-        stopVibration();
+    if (isVibrating()) {
+        m_timer.stop();
+        m_state = State::Idle;
+        m_vibrationClient->cancelVibration();
+    }
 }
 
-void Vibration::suspendVibration()
+void Vibration::timerFired(Timer<Vibration>* timer)
 {
-    if (!m_isVibrating)
+    ASSERT_UNUSED(timer, timer == &m_timer);
+
+    m_timer.stop();
+
+    if (m_pattern.isEmpty()) {
+        m_state = State::Idle;
         return;
+    }
 
-    m_pattern.insert(0, m_timerStop.nextFireInterval());
-    stopVibration();
-}
-
-void Vibration::resumeVibration()
-{
-    m_timerStart.startOneShot(0);
-    m_isVibrating = true;
-}
-
-void Vibration::stopVibration()
-{
-    m_timerStart.stop();
-    m_timerStop.stop();
-    m_vibrationClient->cancelVibration();
-    m_isVibrating = false;
-}
-
-void Vibration::timerStartFired(Timer<Vibration>* timer)
-{
-    ASSERT_UNUSED(timer, timer == &m_timerStart);
-
-    m_timerStart.stop();
-
-    if (!m_pattern.isEmpty()) {
+    switch (m_state) {
+    case State::Vibrating:
+        m_state = State::Waiting;
+        break;
+    case State::Waiting:
+    case State::Idle:
+        m_state = State::Vibrating;
         m_vibrationClient->vibrate(m_pattern[0]);
-        m_timerStop.startOneShot(m_pattern[0] / 1000.0);
-        m_pattern.remove(0);
+        break;
     }
-}
-
-void Vibration::timerStopFired(Timer<Vibration>* timer)
-{
-    ASSERT_UNUSED(timer, timer == &m_timerStop);
-
-    m_timerStop.stop();
-
-    if (!m_pattern.isEmpty()) {
-        m_timerStart.startOneShot(m_pattern[0] / 1000.0);
-        m_pattern.remove(0);
-        if (m_pattern.isEmpty())
-            m_isVibrating = false;
-    }
+    m_timer.startOneShot(m_pattern[0] / 1000.0);
+    m_pattern.remove(0);
 }
 
 const char* Vibration::supplementName()

Source/WebCore/Modules/vibration/Vibration.h
@@ -40,26 +40,22 @@ public:
     static PassOwnPtr<Vibration> create(VibrationClient*);
 
     bool vibrate(const VibrationPattern&);
+    // FIXME: When a visibilitychange event is dispatched while vibrating, the vibration should be canceled.
     void cancelVibration();
 
-    // FIXME : Add suspendVibration() and resumeVibration() to the page visibility feature, when the document.hidden attribute is changed.
-    void suspendVibration();
-    void resumeVibration();
-    void timerStartFired(Timer<Vibration>*);
-    void timerStopFired(Timer<Vibration>*);
+    void timerFired(Timer<Vibration>*);
 
     static const char* supplementName();
     static Vibration* from(Page* page) { return static_cast<Vibration*>(Supplement<Page>::from(page, supplementName())); }
 
-    bool isVibrating() { return m_isVibrating; }
+    bool isVibrating() { return m_state != State::Idle; }
 
 private:
-    void stopVibration();
+    enum class State { Idle, Vibrating, Waiting };
 
     VibrationClient* m_vibrationClient;
-    Timer<Vibration> m_timerStart;
-    Timer<Vibration> m_timerStop;
-    bool m_isVibrating;
+    Timer<Vibration> m_timer;
+    State m_state;
     VibrationPattern m_pattern;
 };
 

Source/WebKit2/UIProcess/API/efl/tests/test_ewk2_view.cpp
@@ -928,23 +928,34 @@ TEST_F(EWK2ViewTest, ewk_context_vibration_client_callbacks_set)
     // Vibrate for 5 seconds.
     loadVibrationHTMLString(webView(), "5000", &data);
     waitUntilTrue(data.testFinished);
-    ASSERT_TRUE(data.didReceiveVibrate);
+    ASSERT_TRUE(data.didReceiveVibrate && !data.didReceiveCancelVibration);
 
     // Cancel any existing vibrations.
     loadVibrationHTMLString(webView(), "0", &data);
     waitUntilTrue(data.testFinished);
-    ASSERT_TRUE(data.didReceiveCancelVibration);
+    ASSERT_TRUE(!data.didReceiveVibrate && data.didReceiveCancelVibration);
 
     // This case the pattern will cause the device to vibrate for 200 ms, be still for 100 ms, and then vibrate for 5000 ms.
     loadVibrationHTMLString(webView(), "[200, 100, 5000]", &data);
     waitUntilTrue(data.testFinished);
     ASSERT_EQ(2, data.vibrateCalledCount);
-    ASSERT_TRUE(data.didReceiveVibrate);
+    ASSERT_TRUE(data.didReceiveVibrate && !data.didReceiveCancelVibration);
 
     // Cancel outstanding vibration pattern.
     loadVibrationHTMLString(webView(), "[0]", &data);
     waitUntilTrue(data.testFinished);
-    ASSERT_TRUE(data.didReceiveCancelVibration);
+    ASSERT_TRUE(!data.didReceiveVibrate && data.didReceiveCancelVibration);
+
+    // Check that vibration works properly by continuous request.
+    data.expectedVibrationTime = 5;
+    loadVibrationHTMLString(webView(), "5", &data);
+    waitUntilTrue(data.testFinished);
+    ASSERT_TRUE(data.didReceiveVibrate && !data.didReceiveCancelVibration);
+
+    loadVibrationHTMLString(webView(), "5", &data);
+    waitUntilTrue(data.testFinished);
+    ASSERT_EQ(1, data.vibrateCalledCount);
+    ASSERT_TRUE(data.didReceiveVibrate && !data.didReceiveCancelVibration);
 
     // Stop listening for vibration events, by calling the function with null for the callbacks.
     evas_object_smart_callback_del(webView(), "vibrate", onVibrate);

commit 22b7f8ffa11aeeb7fd5c43137c0bbf95ed7f4d62

[EFL] vibration/cancelVibration-during-pattern-vibrating.html is failing after r165598 https://bugs.webkit.org/show_bug.cgi?id=130333
Patch by Krzysztof Wolanski <k.wolanski@samsung.com> on 2014-03-19
Reviewed by Gyuyoung Kim.

Added setting m_state when calling Vibration::vibrate. It was missing after
refactoring Vibration (r165598).

* Modules/vibration/Vibration.cpp:
(WebCore::Vibration::vibrate):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165882 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Modules/vibration/Vibration.cpp
@@ -82,6 +82,7 @@ bool Vibration::vibrate(const VibrationPattern& pattern)
     m_pattern = sanitized;
 
     m_timer.startOneShot(0);
+    m_state = State::Waiting;
     return true;
 }
 