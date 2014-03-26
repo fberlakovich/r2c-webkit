fberlakovich/r2c-webkit

commit 63f6c5208086b0d7963c3caf7ac3a2de223a7b36

Web Inspector: AXI: Expose focused/focusable state in the Accessibility Node Inspector https://bugs.webkit.org/show_bug.cgi?id=129779
Patch by Diego Pino Garcia <dpino@igalia.com> on 2014-03-13
Reviewed by Timothy Hatcher.

Source/WebCore:

* inspector/InspectorDOMAgent.cpp: Set property "focused".
(WebCore::InspectorDOMAgent::buildObjectForAccessibilityProperties):
* inspector/protocol/DOM.json: Add property "focused" to
AccessibilityProperties.

Source/WebInspectorUI:

* Localizations/en.lproj/localizedStrings.js: Add label "Focused".
* UserInterface/Models/DOMNode.js: Set property "focused" in
accessibilityProperties.
(WebInspector.DOMNode.prototype.accessibilityProperties): Add row for
property "focused".
* UserInterface/Views/DOMNodeDetailsSidebarPanel.js:
(WebInspector.DOMNodeDetailsSidebarPanel):
(WebInspector.DOMNodeDetailsSidebarPanel.prototype._refreshAccessibility):
Refresh value of property "focused" in UI.

LayoutTests:

* inspector-protocol/dom/getAccessibilityPropertiesForNode-expected.txt:
Updated test.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165590 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/inspector/InspectorDOMAgent.cpp
@@ -1418,6 +1418,7 @@ PassRefPtr<TypeBuilder::DOM::AccessibilityProperties> InspectorDOMAgent::buildOb
     bool exists = false;
     bool expanded = false;
     bool disabled = false;
+    bool focused = false;
     bool ignored = true;
     bool ignoredByDefault = false;
     String invalid = "false"; // String values: true, false, spelling, grammar, etc.
@@ -1432,6 +1433,7 @@ PassRefPtr<TypeBuilder::DOM::AccessibilityProperties> InspectorDOMAgent::buildOb
     bool supportsExpanded = false;
     bool supportsPressed = false;
     bool supportsRequired = false;
+    bool supportsFocused = false;
 
     if (AXObjectCache* axObjectCache = node->document().axObjectCache()) {
         if (AccessibilityObject* axObject = axObjectCache->getOrCreate(node)) {
@@ -1454,6 +1456,10 @@ PassRefPtr<TypeBuilder::DOM::AccessibilityProperties> InspectorDOMAgent::buildOb
             if (supportsExpanded)
                 expanded = axObject->isExpanded();
             
+            supportsFocused = toElement(node)->isFocusable();
+            if (supportsFocused)
+                focused = axObject->isFocused();
+
             ignored = axObject->accessibilityIsIgnored();
             ignoredByDefault = axObject->accessibilityIsIgnoredByDefault();
             invalid = axObject->invalidStatus();
@@ -1490,6 +1496,8 @@ PassRefPtr<TypeBuilder::DOM::AccessibilityProperties> InspectorDOMAgent::buildOb
             value->setDisabled(disabled);
         if (supportsExpanded)
             value->setExpanded(expanded);
+        if (supportsFocused)
+            value->setFocused(focused);
         if (ignored)
             value->setIgnored(ignored);
         if (ignoredByDefault)

LayoutTests/inspector-protocol/dom/getAccessibilityPropertiesForNode-expected.txt
@@ -85,6 +85,7 @@ Total elements to be tested: 50.
     exists: true
     label: 
     role: 
+    focused: false
     ignored: true
     ignoredByDefault: true
     hidden: true
@@ -123,6 +124,7 @@ Total elements to be tested: 50.
     label: 
     role: radio
     checked: true
+    focused: false
     required: false
 
 <input type="radio">
@@ -130,6 +132,7 @@ Total elements to be tested: 50.
     label: 
     role: radio
     checked: false
+    focused: false
     required: false
 
 <div role="checkbox" aria-checked="false">unchecked</div>
@@ -165,6 +168,7 @@ Total elements to be tested: 50.
     label: 
     role: checkbox
     checked: true
+    focused: false
     required: false
 
 <input type="checkbox">
@@ -172,6 +176,7 @@ Total elements to be tested: 50.
     label: 
     role: checkbox
     checked: false
+    focused: false
     required: false
 
 <div role="textbox" tabindex="0" aria-disabled="true">disabled</div>
@@ -179,6 +184,7 @@ Total elements to be tested: 50.
     label: 
     role: 
     disabled: true
+    focused: false
     readonly: true
     required: false
 
@@ -193,6 +199,7 @@ Total elements to be tested: 50.
     exists: true
     label: 
     role: 
+    focused: false
     readonly: true
     required: false
 
@@ -200,6 +207,7 @@ Total elements to be tested: 50.
     exists: true
     label: 
     role: 
+    focused: false
     readonly: true
     required: false
 
@@ -207,6 +215,7 @@ Total elements to be tested: 50.
     exists: true
     label: 
     role: 
+    focused: false
     invalid: true
     required: false
 
@@ -214,6 +223,7 @@ Total elements to be tested: 50.
     exists: true
     label: 
     role: 
+    focused: false
     invalid: spelling
     required: false
 
@@ -221,34 +231,40 @@ Total elements to be tested: 50.
     exists: true
     label: 
     role: 
+    focused: false
     required: true
 
 <input required="">
     exists: true
     label: 
     role: 
+    focused: false
     required: true
 
 <input>
     exists: true
     label: 
     role: 
+    focused: false
     required: false
 
 <input type="button">
     exists: true
     label: 
     role: button
+    focused: false
 
 <button></button>
     exists: true
     label: 
     role: button
+    focused: false
 
 <div role="button" tabindex="0" aria-pressed="false">Not Pressed.</div>
     exists: true
     label: 
     role: 
+    focused: false
     pressed: false
     required: false
 
@@ -256,6 +272,7 @@ Total elements to be tested: 50.
     exists: true
     label: 
     role: 
+    focused: false
     pressed: false
     required: false
 
@@ -264,11 +281,13 @@ Total elements to be tested: 50.
     label: 
     role: button
     disabled: true
+    focused: false
 
 <div role="button" tabindex="0"></div>
     exists: true
     label: 
     role: button
+    focused: false
 
 <span aria-hidden="true"></span>
     exists: true

Source/WebInspectorUI/UserInterface/Models/DOMNode.js
@@ -451,6 +451,7 @@ WebInspector.DOMNode.prototype = {
                     disabled: accessibilityProperties.disabled,
                     exists: accessibilityProperties.exists,
                     expanded: accessibilityProperties.expanded,
+                    focused: accessibilityProperties.focused,
                     ignored: accessibilityProperties.ignored,
                     ignoredByDefault: accessibilityProperties.ignoredByDefault,
                     invalid: accessibilityProperties.invalid,

Source/WebInspectorUI/UserInterface/Views/DOMNodeDetailsSidebarPanel.js
@@ -61,6 +61,7 @@ WebInspector.DOMNodeDetailsSidebarPanel = function() {
         this._accessibilityNodeCheckedRow = new WebInspector.DetailsSectionSimpleRow(WebInspector.UIString("Checked"));
         this._accessibilityNodeDisabledRow = new WebInspector.DetailsSectionSimpleRow(WebInspector.UIString("Disabled"));
         this._accessibilityNodeExpandedRow = new WebInspector.DetailsSectionSimpleRow(WebInspector.UIString("Expanded"));
+        this._accessibilityNodeFocusedRow = new WebInspector.DetailsSectionSimpleRow(WebInspector.UIString("Focused"));
         this._accessibilityNodeIgnoredRow = new WebInspector.DetailsSectionSimpleRow(WebInspector.UIString("Ignored"));
         this._accessibilityNodeInvalidRow = new WebInspector.DetailsSectionSimpleRow(WebInspector.UIString("Invalid"));
         this._accessibilityNodeLabelRow = new WebInspector.DetailsSectionSimpleRow(WebInspector.UIString("Label"));
@@ -290,6 +291,7 @@ WebInspector.DOMNodeDetailsSidebarPanel.prototype = {
 
                 var disabled = booleanValueToLocalizedStringIfTrue("disabled");
                 var expanded = booleanValueToLocalizedStringIfPropertyDefined("expanded");
+                var focused = booleanValueToLocalizedStringIfPropertyDefined("focused");
                 
                 var ignored = "";
                 if (accessibilityProperties.ignored) {
@@ -327,6 +329,7 @@ WebInspector.DOMNodeDetailsSidebarPanel.prototype = {
                 this._accessibilityNodeCheckedRow.value = checked;
                 this._accessibilityNodeDisabledRow.value = disabled;
                 this._accessibilityNodeExpandedRow.value = expanded;
+                this._accessibilityNodeFocusedRow.value = focused;
                 this._accessibilityNodeIgnoredRow.value = ignored;
                 this._accessibilityNodeInvalidRow.value = invalid;
                 this._accessibilityNodeLabelRow.value = label;
@@ -339,6 +342,7 @@ WebInspector.DOMNodeDetailsSidebarPanel.prototype = {
                 // Display order, not alphabetical as above.
                 this._accessibilityGroup.rows = [
                     // Global properties for all elements.
+                    this._accessibilityNodeFocusedRow,
                     this._accessibilityNodeIgnoredRow,
                     this._accessibilityNodeRoleRow,
                     this._accessibilityNodeLabelRow,

commit 69329d3851ca2aa057f5e2cfc4d40ff5e85ec0c8

Web Inspector: AXI: crash when inspecting "bar" text node in getAccessibilityPropertiesForNode layout test https://bugs.webkit.org/show_bug.cgi?id=130290
Patch by James Craig <jcraig@apple.com> on 2014-03-26
Reviewed by Timothy Hatcher.

Source/WebCore:

Test: inspector-protocol/dom/getAccessibilityPropertiesForNode.html

Fixing regression from r165590. http://webkit.org/b/129779
Verify isElementNode to avoid calling toElement on document or text nodes.

* inspector/InspectorDOMAgent.cpp:
(WebCore::InspectorDOMAgent::buildObjectForAccessibilityProperties):

LayoutTests:

Updating test to include properties for document node.

* inspector-protocol/dom/getAccessibilityPropertiesForNode-expected.txt:
* inspector-protocol/dom/getAccessibilityPropertiesForNode.html:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@166295 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/inspector/InspectorDOMAgent.cpp
@@ -1504,9 +1504,11 @@ PassRefPtr<TypeBuilder::DOM::AccessibilityProperties> InspectorDOMAgent::buildOb
                 }
             }
             
-            supportsFocused = toElement(node)->isFocusable();
-            if (supportsFocused)
-                focused = axObject->isFocused();
+            if (node->isElementNode()) {
+                supportsFocused = toElement(node)->isFocusable();
+                if (supportsFocused)
+                    focused = axObject->isFocused();
+            }
 
             ignored = axObject->accessibilityIsIgnored();
             ignoredByDefault = axObject->accessibilityIsIgnoredByDefault();

LayoutTests/inspector-protocol/dom/getAccessibilityPropertiesForNode-expected.txt
@@ -1,5 +1,12 @@
 Checking Web Inspector protocol for the Accessibility Node Inspector.
 
+#document
+    exists: true
+    label: 
+    role: 
+    childNodeIds.length: 19
+
+
 Total elements to be tested: 57.
 
 <li role="treeitem" aria-expanded="undefined">expanded</li>
@@ -445,6 +452,4 @@ Total elements to be tested: 57.
     exists: false
     label: 
     role: 
-
-
 

LayoutTests/inspector-protocol/dom/getAccessibilityPropertiesForNode.html
@@ -113,28 +113,35 @@ function cleanup() {
 function test() {
 
     var examples = [];
+    var documentNodeId = null;
+
+    function onGotDocument(response) {
+        InspectorTest.checkForError(response);
+        documentNodeId = response.result.root.nodeId;
+        InspectorTest.sendCommand("DOM.getAccessibilityPropertiesForNode", {"nodeId": documentNodeId}, onGotAccessibilityPropertiesForDocumentNode);
+    }
+
+    function onGotAccessibilityPropertiesForDocumentNode(response) {
+        InspectorTest.checkForError(response);
+        InspectorTest.log("#document");
+        logAccessibilityProperties(response.result.properties);
+        InspectorTest.log("\n");
+        InspectorTest.sendCommand("DOM.querySelectorAll", {"nodeId": documentNodeId, "selector": ".ex"}, onGotQuerySelectorAll);
+    }
+
+    function onGotQuerySelectorAll(response) {
+        InspectorTest.checkForError(response);
+        examples = response.result.nodeIds;
+        InspectorTest.log("Total elements to be tested: " + examples.length + ".");
+        loop();
+    }
 
     function loop() {
         if (examples.length) {
             InspectorTest.sendCommand("DOM.getOuterHTML", {"nodeId": examples[examples.length-1]}, onGotOuterHTML);
         } else {
-            InspectorTest.log("\n");
-            InspectorTest.sendCommand("Runtime.evaluate", {"expression": "cleanup()"}, function(){
-                InspectorTest.completeTest();
-            });
+            finishTest();
         }
-    };
-
-    function onGotDocument(response) {
-        InspectorTest.checkForError(response);
-        InspectorTest.sendCommand("DOM.querySelectorAll", {"nodeId": response.result.root.nodeId, "selector": ".ex"}, onQuerySelectorAll);
-    }
-
-    function onQuerySelectorAll(response) {
-        InspectorTest.checkForError(response);
-        examples = response.result.nodeIds;
-        InspectorTest.log("Total elements to be tested: " + examples.length + ".");
-        loop();
     }
 
     function onGotOuterHTML(response) {
@@ -143,13 +150,19 @@ function test() {
         outerHTML = outerHTML.replace(/ class="ex"/g, ""); // remove any duplicated, unnecessary class attributes
         outerHTML = outerHTML.replace(/;base64,.*?"/, "...\""); // suppress full data URIs in output
         InspectorTest.log("\n" + outerHTML);
-        InspectorTest.sendCommand("DOM.getAccessibilityPropertiesForNode", {"nodeId": examples[examples.length-1]}, onGotAXProps);
+        InspectorTest.sendCommand("DOM.getAccessibilityPropertiesForNode", {"nodeId": examples[examples.length-1]}, onGotAccessibilityProperties);
     }
 
-    function onGotAXProps(response) {
+    function onGotAccessibilityProperties(response) {
         InspectorTest.checkForError(response);
-        for (var key in response.result.properties) {
-            var value = response.result.properties[key];
+        logAccessibilityProperties(response.result.properties);
+        examples.pop();
+        loop();
+    }
+
+    function logAccessibilityProperties(properties) {
+        for (var key in properties) {
+            var value = properties[key];
             switch (key){
             case "nodeId":
                 continue;
@@ -169,9 +182,15 @@ function test() {
                 InspectorTest.log("    " + key + ": " + value);
             }
         }
-        examples.pop();
-        loop();
     }
+
+    function finishTest() {
+        InspectorTest.sendCommand("Runtime.evaluate", {"expression": "cleanup()"}, function(){
+            InspectorTest.completeTest();
+        });
+    }
+
+
 
     InspectorTest.sendCommand("DOM.getDocument", {}, onGotDocument);
 