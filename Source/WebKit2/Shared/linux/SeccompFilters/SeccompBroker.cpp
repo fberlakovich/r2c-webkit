fberlakovich/r2c-webkit

commit 374d7dfb73ee778978a62375b549cccd63715faf

Rename shared() static member functions to singleton() for singleton classes. https://bugs.webkit.org/show_bug.cgi?id=141088
Reviewed by Ryosuke Niwa and Benjamin Poulain.

Rename shared() static member functions to singleton() for singleton
classes as per the recent coding style change.
Source/JavaScriptCore:

* inspector/remote/RemoteInspector.h:
* inspector/remote/RemoteInspector.mm:
(Inspector::RemoteInspector::singleton):
(Inspector::RemoteInspector::start):
(Inspector::RemoteInspector::shared): Deleted.
* inspector/remote/RemoteInspectorDebuggable.cpp:
(Inspector::RemoteInspectorDebuggable::~RemoteInspectorDebuggable):
(Inspector::RemoteInspectorDebuggable::init):
(Inspector::RemoteInspectorDebuggable::update):
(Inspector::RemoteInspectorDebuggable::setRemoteDebuggingAllowed):
(Inspector::RemoteInspectorDebuggable::pauseWaitingForAutomaticInspection):
(Inspector::RemoteInspectorDebuggable::unpauseForInitializedInspector):
* inspector/remote/RemoteInspectorDebuggableConnection.mm:
(Inspector::RemoteInspectorDebuggableConnection::setup):
(Inspector::RemoteInspectorDebuggableConnection::sendMessageToFrontend):

Source/WebKit:

* Storage/WebDatabaseProvider.cpp:
(WebDatabaseProvider::singleton):
(WebDatabaseProvider::shared): Deleted.
* Storage/WebDatabaseProvider.h:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@179409 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Shared/linux/SeccompFilters/SeccompBroker.cpp
@@ -140,7 +140,7 @@ static void SIGSYSHandler(int signal, siginfo_t* info, void* data)
     if (!ucontext)
         CRASH();
 
-    SeccompBrokerClient* client = &SeccompBrokerClient::shared();
+    SeccompBrokerClient* client = &SeccompBrokerClient::singleton();
 
     if (client->handleIfOpeningOnlineCPUCount(&ucontext->uc_mcontext))
         return;

commit 35d8afb34ac44d824b65c65fe950e7689432c9ad

Build with seccomp filters broken since r179409 https://bugs.webkit.org/show_bug.cgi?id=141206
Patch by Michael Catanzaro <mcatanzaro@igalia.com> on 2015-02-04
Reviewed by Darin Adler.

Fix build by renaming SeccompBrokerClient::shared to SeccompBrokerClient::singleton.

* Shared/linux/SeccompFilters/SeccompBroker.cpp:
(WebKit::SeccompBrokerClient::singleton):
(WebKit::SeccompBroker::initialize):
(WebKit::SeccompBrokerClient::shared):
(WebKit::SeccompBroker::runLoop):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@179605 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Shared/linux/SeccompFilters/SeccompBroker.cpp
@@ -52,7 +52,7 @@ namespace WebKit {
 
 class SeccompBrokerClient {
 public:
-    static SeccompBrokerClient& shared(int socket = -1);
+    static SeccompBrokerClient& singleton(int socket = -1);
     ~SeccompBrokerClient();
 
     void dispatch(Syscall*) const;
@@ -174,7 +174,7 @@ static void registerSIGSYSHandler()
         CRASH();
 }
 
-SeccompBrokerClient& SeccompBrokerClient::shared(int socket)
+SeccompBrokerClient& SeccompBrokerClient::singleton(int socket)
 {
     DEPRECATED_DEFINE_STATIC_LOCAL(SeccompBrokerClient, brokerClient, (socket));
 
@@ -290,7 +290,7 @@ void SeccompBroker::initialize()
     pid_t pid = fork();
     if (pid) { // Sandboxed process.
         close(sockets[1]);
-        SeccompBrokerClient::shared(sockets[0]);
+        SeccompBrokerClient::singleton(sockets[0]);
         registerSIGSYSHandler();
     } else { // Broker.
         // TODO: The broker should setup seccomp filters