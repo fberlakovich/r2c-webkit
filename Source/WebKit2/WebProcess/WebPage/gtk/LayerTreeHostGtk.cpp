fberlakovich/r2c-webkit

commit 79957264d0cf4460530d686e834c3de0c4282bf9

REGRESSION(r177075): WebProcess crashes when entering accelerating compositing mode before the WebView is realized https://bugs.webkit.org/show_bug.cgi?id=142079
Reviewed by Žan Doberšek.

The problem is that the texture mapper and native window handler
are initialized when the LayerTreeHost is initialized, assuming
the UI process has already sent the native window handler to the
web process, but that doesn't always happen since we moved the
redirected window creation to realize in r177075.

* WebProcess/WebPage/DrawingArea.h:
(WebKit::DrawingArea::nativeSurfaceHandleForCompositing): Deleted.
* WebProcess/WebPage/DrawingAreaImpl.cpp:
(WebKit::DrawingAreaImpl::enterAcceleratedCompositingMode): Call
LayerTreeHost::setNativeSurfaceHandleForCompositing if we
already have a native window handle at this point.
(WebKit::DrawingAreaImpl::setNativeSurfaceHandleForCompositing):
Call LayerTreeHost::setNativeSurfaceHandleForCompositing also when
not using threaded compositing.
* WebProcess/WebPage/LayerTreeHost.h:
* WebProcess/WebPage/gtk/LayerTreeHostGtk.cpp:
(WebKit::LayerTreeHostGtk::makeContextCurrent): Helper function to
ensure a context and making it current.
(WebKit::LayerTreeHostGtk::ensureTextureMapper): Ensure a texture
is created for the current context.
(WebKit::LayerTreeHostGtk::initialize): Use makeContextCurrent()
and ensureTextureMapper(), and remove the LayerTreeContext
initialization since that's is now always initialized in
setNativeSurfaceHandleForCompositing().
(WebKit::LayerTreeHostGtk::compositeLayersToContext): Use
makeContextCurrent() helper function and also call
ensureTextureMapper() just in case the texture could not be
created during initialization because the native window handle was
not yet available.
(WebKit::LayerTreeHostGtk::flushAndRenderLayers): Use makeContextCurrent().
(WebKit::LayerTreeHostGtk::setNativeSurfaceHandleForCompositing):
Initialize the LayerTreeContext.
(WebKit::LayerTreeHostGtk::glContext): Deleted.

Source/WebKit2/WebProcess/WebPage/gtk/LayerTreeHostGtk.cpp
@@ -78,17 +78,29 @@ LayerTreeHostGtk::LayerTreeHostGtk(WebPage* webPage)
 {
 }
 
-GLContext* LayerTreeHostGtk::glContext()
+bool LayerTreeHostGtk::makeContextCurrent()
 {
-    if (m_context)
-        return m_context.get();
+    if (!m_context) {
+        if (!m_layerTreeContext.contextID)
+            return false;
 
-    uint64_t windowHandle = m_webPage->drawingArea()->nativeSurfaceHandleForCompositing();
-    if (!windowHandle)
-        return 0;
+        m_context = GLContext::createContextForWindow(m_layerTreeContext.contextID, GLContext::sharingContext());
+        if (!m_context)
+            return false;
+    }
 
-    m_context = GLContext::createContextForWindow(windowHandle, GLContext::sharingContext());
-    return m_context.get();
+    return m_context->makeContextCurrent();
+}
+
+void LayerTreeHostGtk::ensureTextureMapper()
+{
+    if (m_textureMapper)
+        return;
+
+    ASSERT(m_isValid);
+    m_textureMapper = TextureMapper::create(TextureMapper::OpenGLMode);
+    static_cast<TextureMapperGL*>(m_textureMapper.get())->setEnableEdgeDistanceAntialiasing(true);
+    downcast<GraphicsLayerTextureMapper>(*m_rootLayer).layer().setTextureMapper(m_textureMapper.get());
 }
 
 void LayerTreeHostGtk::initialize()
@@ -113,19 +125,11 @@ void LayerTreeHostGtk::initialize()
     m_rootLayer->addChild(m_nonCompositedContentLayer.get());
     m_nonCompositedContentLayer->setNeedsDisplay();
 
-    m_layerTreeContext.contextID = m_webPage->drawingArea()->nativeSurfaceHandleForCompositing();
-
-    GLContext* context = glContext();
-    if (!context)
+    // The creation of the TextureMapper needs an active OpenGL context.
+    if (!makeContextCurrent())
         return;
 
-    // The creation of the TextureMapper needs an active OpenGL context.
-    context->makeContextCurrent();
-
-    m_textureMapper = TextureMapper::create(TextureMapper::OpenGLMode);
-    static_cast<TextureMapperGL*>(m_textureMapper.get())->setEnableEdgeDistanceAntialiasing(true);
-    downcast<GraphicsLayerTextureMapper>(*m_rootLayer).layer().setTextureMapper(m_textureMapper.get());
-
+    ensureTextureMapper();
     scheduleLayerFlush();
 }
 
@@ -289,9 +293,10 @@ bool LayerTreeHostGtk::flushPendingLayerChanges()
 
 void LayerTreeHostGtk::compositeLayersToContext(CompositePurpose purpose)
 {
-    GLContext* context = glContext();
-    if (!context || !context->makeContextCurrent())
+    if (!makeContextCurrent())
         return;
+
+    ensureTextureMapper();
 
     // The window size may be out of sync with the page size at this point, and getting
     // the viewport parameters incorrect, means that the content will be misplaced. Thus
@@ -308,7 +313,7 @@ void LayerTreeHostGtk::compositeLayersToContext(CompositePurpose purpose)
     downcast<GraphicsLayerTextureMapper>(*m_rootLayer).layer().paint();
     m_textureMapper->endPainting();
 
-    context->swapBuffers();
+    m_context->swapBuffers();
 }
 
 void LayerTreeHostGtk::flushAndRenderLayers()
@@ -321,8 +326,7 @@ void LayerTreeHostGtk::flushAndRenderLayers()
             return;
     }
 
-    GLContext* context = glContext();
-    if (!context || !context->makeContextCurrent())
+    if (!makeContextCurrent())
         return;
 
     if (!flushPendingLayerChanges())
@@ -378,6 +382,11 @@ void LayerTreeHostGtk::setViewOverlayRootLayer(WebCore::GraphicsLayer* viewOverl
     m_viewOverlayRootLayer = viewOverlayRootLayer;
     if (m_viewOverlayRootLayer)
         m_rootLayer->addChild(m_viewOverlayRootLayer);
+}
+
+void LayerTreeHostGtk::setNativeSurfaceHandleForCompositing(uint64_t handle)
+{
+    m_layerTreeContext.contextID = handle;
 }
 
 } // namespace WebKit

Source/WebKit2/WebProcess/WebPage/gtk/LayerTreeHostGtk.h
@@ -61,6 +61,8 @@ protected:
     virtual void deviceOrPageScaleFactorChanged() override;
     virtual void pageBackgroundTransparencyChanged() override;
 
+    virtual void setNativeSurfaceHandleForCompositing(uint64_t) override;
+
 private:
     // LayerTreeHost
     virtual const LayerTreeContext& layerTreeContext() override;
@@ -84,7 +86,8 @@ private:
 
     void layerFlushTimerFired();
 
-    WebCore::GLContext* glContext();
+    bool makeContextCurrent();
+    void ensureTextureMapper();
 
     LayerTreeContext m_layerTreeContext;
     bool m_isValid;

Source/WebKit2/WebProcess/WebPage/DrawingArea.h
@@ -124,10 +124,6 @@ public:
 
     virtual void attachViewOverlayGraphicsLayer(WebCore::Frame*, WebCore::GraphicsLayer*) { }
 
-#if USE(TEXTURE_MAPPER_GL) && PLATFORM(GTK)
-    uint64_t nativeSurfaceHandleForCompositing() { return m_nativeSurfaceHandleForCompositing; }
-#endif
-
 protected:
     DrawingArea(DrawingAreaType, WebPage&);
 

Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -456,6 +456,8 @@ void DrawingAreaImpl::enterAcceleratedCompositingMode(GraphicsLayer* graphicsLay
     ASSERT(!m_layerTreeHost);
 
     m_layerTreeHost = LayerTreeHost::create(&m_webPage);
+    if (m_nativeSurfaceHandleForCompositing)
+        m_layerTreeHost->setNativeSurfaceHandleForCompositing(m_nativeSurfaceHandleForCompositing);
     if (!m_inUpdateBackingStoreState)
         m_layerTreeHost->setShouldNotifyAfterNextScheduledLayerFlush(true);
 
@@ -677,10 +679,8 @@ void DrawingAreaImpl::setNativeSurfaceHandleForCompositing(uint64_t handle)
     m_nativeSurfaceHandleForCompositing = handle;
     m_webPage.corePage()->settings().setAcceleratedCompositingEnabled(true);
 
-#if USE(COORDINATED_GRAPHICS_THREADED)
     if (m_layerTreeHost)
         m_layerTreeHost->setNativeSurfaceHandleForCompositing(handle);
-#endif
 }
 #endif
 

Source/WebKit2/WebProcess/WebPage/LayerTreeHost.h
@@ -93,6 +93,10 @@ public:
     virtual void scheduleAnimation() = 0;
 #endif
 
+#if USE(TEXTURE_MAPPER_GL) && PLATFORM(GTK)
+    virtual void setNativeSurfaceHandleForCompositing(uint64_t) = 0;
+#endif
+
     virtual void setViewOverlayRootLayer(WebCore::GraphicsLayer*) = 0;
 
 protected:

commit 17ba5b650477daceefd9c2e8b6af07249c38f9bf

REGRESSION(r180924): ASSERTION FAILED: !from.isEmpty() in WebCore::TransformationMatrix::rectToRect https://bugs.webkit.org/show_bug.cgi?id=142345
Reviewed by Martin Robinson.

This was caused by r180924 that postpones the creation of the
TextureMapper, which could cause that a layer has not yet a size
when TextureMapper::paint() is called. This patch moves the
creation of the TextureMapper to
LayerTreeHostGtk::setNativeSurfaceHandleForCompositing(), so that
it's created as soon as it's possible to create. This method is
called by the drawing area right after creating the
LayerTreeHostGtk if it already have a handler, or when the handle
is received from the UI process.

* WebProcess/WebPage/gtk/LayerTreeHostGtk.cpp:
(WebKit::LayerTreeHostGtk::initialize): Remove the
nsureTextureMapper call because at this point the layer context ID
is always 0, so it's impossible to create the TextureMapper.
(WebKit::LayerTreeHostGtk::compositeLayersToContext): Remove the
ensureTextureMapper call from here too, since at this point, if we
have a context, we should also have a TextureMapper. Add an ASSERT
right before using the TextureMapper.
(WebKit::LayerTreeHostGtk::setNativeSurfaceHandleForCompositing):
Create the TextureMapper here.
(WebKit::LayerTreeHostGtk::ensureTextureMapper): Deleted.
* WebProcess/WebPage/gtk/LayerTreeHostGtk.h:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@181138 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/WebProcess/WebPage/gtk/LayerTreeHostGtk.cpp
@@ -92,17 +92,6 @@ bool LayerTreeHostGtk::makeContextCurrent()
     return m_context->makeContextCurrent();
 }
 
-void LayerTreeHostGtk::ensureTextureMapper()
-{
-    if (m_textureMapper)
-        return;
-
-    ASSERT(m_isValid);
-    m_textureMapper = TextureMapper::create(TextureMapper::OpenGLMode);
-    static_cast<TextureMapperGL*>(m_textureMapper.get())->setEnableEdgeDistanceAntialiasing(true);
-    downcast<GraphicsLayerTextureMapper>(*m_rootLayer).layer().setTextureMapper(m_textureMapper.get());
-}
-
 void LayerTreeHostGtk::initialize()
 {
     m_rootLayer = GraphicsLayer::create(graphicsLayerFactory(), *this);
@@ -124,13 +113,6 @@ void LayerTreeHostGtk::initialize()
 
     m_rootLayer->addChild(m_nonCompositedContentLayer.get());
     m_nonCompositedContentLayer->setNeedsDisplay();
-
-    // The creation of the TextureMapper needs an active OpenGL context.
-    if (!makeContextCurrent())
-        return;
-
-    ensureTextureMapper();
-    scheduleLayerFlush();
 }
 
 LayerTreeHostGtk::~LayerTreeHostGtk()
@@ -296,8 +278,6 @@ void LayerTreeHostGtk::compositeLayersToContext(CompositePurpose purpose)
     if (!makeContextCurrent())
         return;
 
-    ensureTextureMapper();
-
     // The window size may be out of sync with the page size at this point, and getting
     // the viewport parameters incorrect, means that the content will be misplaced. Thus
     // we set the viewport parameters directly from the window size.
@@ -309,6 +289,7 @@ void LayerTreeHostGtk::compositeLayersToContext(CompositePurpose purpose)
         glClear(GL_COLOR_BUFFER_BIT);
     }
 
+    ASSERT(m_textureMapper);
     m_textureMapper->beginPainting();
     downcast<GraphicsLayerTextureMapper>(*m_rootLayer).layer().paint();
     m_textureMapper->endPainting();
@@ -344,7 +325,7 @@ void LayerTreeHostGtk::flushAndRenderLayers()
 
 void LayerTreeHostGtk::scheduleLayerFlush()
 {
-    if (!m_layerFlushSchedulingEnabled)
+    if (!m_layerFlushSchedulingEnabled || !m_textureMapper)
         return;
 
     // We use a GLib timer because otherwise GTK+ event handling during dragging can starve WebCore timers, which have a lower priority.
@@ -387,6 +368,18 @@ void LayerTreeHostGtk::setViewOverlayRootLayer(WebCore::GraphicsLayer* viewOverl
 void LayerTreeHostGtk::setNativeSurfaceHandleForCompositing(uint64_t handle)
 {
     m_layerTreeContext.contextID = handle;
+
+    // The creation of the TextureMapper needs an active OpenGL context.
+    if (!makeContextCurrent())
+        return;
+
+    ASSERT(m_isValid);
+    ASSERT(!m_textureMapper);
+    m_textureMapper = TextureMapper::create(TextureMapper::OpenGLMode);
+    static_cast<TextureMapperGL*>(m_textureMapper.get())->setEnableEdgeDistanceAntialiasing(true);
+    downcast<GraphicsLayerTextureMapper>(*m_rootLayer).layer().setTextureMapper(m_textureMapper.get());
+
+    scheduleLayerFlush();
 }
 
 } // namespace WebKit

Source/WebKit2/WebProcess/WebPage/gtk/LayerTreeHostGtk.h
@@ -87,7 +87,6 @@ private:
     void layerFlushTimerFired();
 
     bool makeContextCurrent();
-    void ensureTextureMapper();
 
     LayerTreeContext m_layerTreeContext;
     bool m_isValid;