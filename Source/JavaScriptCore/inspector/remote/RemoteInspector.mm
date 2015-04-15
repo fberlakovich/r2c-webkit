fberlakovich/r2c-webkit

commit ff78bb80bff5f7b0b3a9c1f539286f593fc70a9e

Web Inspector: ASSERT mainThreadPthread launching remote debuggable JSContext app with Debug JavaScriptCore https://bugs.webkit.org/show_bug.cgi?id=141189
Reviewed by Michael Saboff.

* inspector/remote/RemoteInspector.mm:
(Inspector::RemoteInspector::singleton):
Ensure we call WTF::initializeMainThread() on the main thread so that
we can perform automatic String <-> NSString conversions.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@179562 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/inspector/remote/RemoteInspector.mm
@@ -84,9 +84,12 @@ RemoteInspector& RemoteInspector::singleton()
     static dispatch_once_t once;
     dispatch_once(&once, ^{
         if (canAccessWebInspectorMachPort()) {
-            JSC::initializeThreading();
-            if (RemoteInspector::startEnabled)
-                shared.get().start();
+            dispatch_async(dispatch_get_main_queue(), ^{
+                WTF::initializeMainThread();
+                JSC::initializeThreading();
+                if (RemoteInspector::startEnabled)
+                    shared.get().start();
+            });
         }
     });
 

commit 0208924de5e744c272e83423f2e59bb06357d016

Web Inspector: Auto-attach fails after r179562, initialization too late after dispatch https://bugs.webkit.org/show_bug.cgi?id=143682
Reviewed by Timothy Hatcher.

* inspector/remote/RemoteInspector.mm:
(Inspector::RemoteInspector::singleton):
If we are on the main thread, run the initialization immediately.
Otherwise dispatch to the main thread. This way if the first JSContext
was created on the main thread it can get auto-attached if applicable.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@182825 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/inspector/remote/RemoteInspector.mm
@@ -84,12 +84,17 @@ RemoteInspector& RemoteInspector::singleton()
     static dispatch_once_t once;
     dispatch_once(&once, ^{
         if (canAccessWebInspectorMachPort()) {
-            dispatch_async(dispatch_get_main_queue(), ^{
+            dispatch_block_t initialize = ^{
                 WTF::initializeMainThread();
                 JSC::initializeThreading();
                 if (RemoteInspector::startEnabled)
                     shared.get().start();
-            });
+            };
+
+            if ([NSThread isMainThread])
+                initialize();
+            else
+                dispatch_async(dispatch_get_main_queue(), initialize);
         }
     });
 