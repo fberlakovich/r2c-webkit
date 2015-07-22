fberlakovich/r2c-webkit

commit 4ead76d9a2fe2628fba8105687d76e995d95255e

Adopt AVPlayerLayerView https://bugs.webkit.org/show_bug.cgi?id=146862
Patch by Jeremy Jones <jeremyj@apple.com> on 2015-07-20
Source/WebCore:

Reviewed by Jer Noble.

The CALayerHost is replaced with WebLayerHostView to be compatible with UIView animations.
Some animation is improved in the conversion.

WebAVPlayerLayerView and WebAVPictureInPicturePlayerLayerView derive from AVKit and UIKit respectively.
Because these frameworks are loaded at runtime, these classes must be generate using objc/runtime.h to
register them from c functions at runtime. The most important part of these UIViews is that their
backing layer is a WebAVPlayerLayer.

WebCALayerHostWrapper and WebAVVideoLayer are combined into WebAVPlayerLayer to simplify the hierarchy.
WebAVPlayerLayer is a stand-in for an AVPlayerLayer.

* platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm:
(WebCore::MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenLayer): -removeFromSuperlayer is redundant.
(WebCore::MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenGravity): syncTextTrackBounds on change.
(WebCore::MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenFrame):
We never want animation here, since all animation will happen in UIViews.
This is just for going into the final size after a transform based animation.

* platform/ios/WebVideoFullscreenControllerAVKit.mm:
(WebVideoFullscreenControllerContext::didSetupFullscreen): layer -> view
(WebVideoFullscreenControllerContext::didCleanupFullscreen): layer -> view
(WebVideoFullscreenControllerContext::setUpFullscreen): layer -> view

(WebVideoFullscreenControllerContext::setVideoLayerFrame): layer -> view
Use fence port to synchronize between the UIThread and the WebThread,
the same way WebKit2 uses a fence port to synchronize between processes.

(WebVideoFullscreenControllerContext::setVideoLayerGravity):
No longer necessary to cache videoGravity at this level.

* platform/ios/WebVideoFullscreenInterfaceAVKit.h:

Source/WebCore/platform/ios/WebVideoFullscreenControllerAVKit.mm
@@ -30,12 +30,17 @@
 #import "WebVideoFullscreenControllerAVKit.h"
 
 #import "Logging.h"
+#import "QuartzCoreSPI.h"
+#import "SoftLinking.h"
 #import "TimeRanges.h"
 #import "WebVideoFullscreenInterfaceAVKit.h"
 #import "WebVideoFullscreenModelVideoElement.h"
 #import <QuartzCore/CoreAnimation.h>
 #import <WebCore/HTMLVideoElement.h>
 #import <WebCore/WebCoreThreadRun.h>
+
+SOFT_LINK_FRAMEWORK(UIKit)
+SOFT_LINK_CLASS(UIKit, UIView)
 
 using namespace WebCore;
 
@@ -129,9 +134,7 @@ private:
     virtual void endScanning() override;
     virtual void requestExitFullscreen() override;
     virtual void setVideoLayerFrame(FloatRect) override;
-    virtual FloatRect videoLayerFrame() const override { return m_frame; }
     virtual void setVideoLayerGravity(WebVideoFullscreenModel::VideoGravity) override;
-    virtual VideoGravity videoLayerGravity() const override { return m_gravity; }
     virtual void selectAudioMediaOption(uint64_t index) override;
     virtual void selectLegibleMediaOption(uint64_t index) override;
     virtual void fullscreenModeChanged(HTMLMediaElementEnums::VideoFullscreenMode) override;
@@ -139,10 +142,8 @@ private:
     RefPtr<WebVideoFullscreenInterfaceAVKit> m_interface;
     RefPtr<WebVideoFullscreenModelVideoElement> m_model;
     RefPtr<HTMLVideoElement> m_videoElement;
-    RetainPtr<PlatformLayer> m_videoFullscreenLayer;
+    RetainPtr<UIView> m_videoFullscreenView;
     RetainPtr<WebVideoFullscreenController> m_controller;
-    FloatRect m_frame;
-    VideoGravity m_gravity;
 };
 
 #pragma mark WebVideoFullscreenChangeObserver
@@ -151,8 +152,9 @@ void WebVideoFullscreenControllerContext::didSetupFullscreen()
 {
     ASSERT(isUIThread());
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
-    WebThreadRun([strongThis, this] {
-        m_model->setVideoFullscreenLayer(m_videoFullscreenLayer.get());
+    RetainPtr<CALayer> videoFullscreenLayer = [m_videoFullscreenView layer];
+    WebThreadRun([strongThis, this, videoFullscreenLayer] {
+        m_model->setVideoFullscreenLayer(videoFullscreenLayer.get());
         dispatch_async(dispatch_get_main_queue(), [strongThis, this] {
             m_interface->enterFullscreen();
         });
@@ -177,6 +179,7 @@ void WebVideoFullscreenControllerContext::didCleanupFullscreen()
     m_interface->setWebVideoFullscreenModel(nullptr);
     m_interface->setWebVideoFullscreenChangeObserver(nullptr);
     m_interface = nullptr;
+    m_videoFullscreenView = nil;
     
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
     WebThreadRun([strongThis, this] {
@@ -185,7 +188,6 @@ void WebVideoFullscreenControllerContext::didCleanupFullscreen()
         m_model->setVideoElement(nullptr);
         m_model = nullptr;
         m_videoElement = nullptr;
-        m_videoFullscreenLayer = nil;
         
         [m_controller didFinishFullscreen:this];
     });
@@ -450,18 +452,25 @@ void WebVideoFullscreenControllerContext::requestExitFullscreen()
 void WebVideoFullscreenControllerContext::setVideoLayerFrame(FloatRect frame)
 {
     ASSERT(isUIThread());
-    m_frame = frame;
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
-    WebThreadRun([strongThis, this, frame] {
+    RetainPtr<CALayer> videoFullscreenLayer = [m_videoFullscreenView layer];
+
+    mach_port_name_t fencePort = [[videoFullscreenLayer context] createFencePort];
+    
+    WebThreadRun([strongThis, this, frame, fencePort, videoFullscreenLayer] {
+        [CATransaction begin];
+        [CATransaction setAnimationDuration:0];
         if (m_model)
             m_model->setVideoLayerFrame(frame);
+        [[videoFullscreenLayer context] setFencePort:fencePort];
+        mach_port_deallocate(mach_task_self(), fencePort);
+        [CATransaction commit];
     });
 }
 
 void WebVideoFullscreenControllerContext::setVideoLayerGravity(WebVideoFullscreenModel::VideoGravity videoGravity)
 {
     ASSERT(isUIThread());
-    m_gravity = videoGravity;
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
     WebThreadRun([strongThis, this, videoGravity] {
         if (m_model)
@@ -510,19 +519,19 @@ void WebVideoFullscreenControllerContext::setUpFullscreen(HTMLVideoElement& vide
     m_interface = WebVideoFullscreenInterfaceAVKit::create();
     m_interface->setWebVideoFullscreenChangeObserver(this);
     m_interface->setWebVideoFullscreenModel(this);
+    m_videoFullscreenView = adoptNS([[getUIViewClass() alloc] init]);
     
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
     WebThreadRun([strongThis, this, viewRef, mode] {
         m_model = WebVideoFullscreenModelVideoElement::create();
         m_model->setWebVideoFullscreenInterface(this);
         m_model->setVideoElement(m_videoElement.get());
-        m_videoFullscreenLayer = [CALayer layer];
         
         bool allowsPictureInPicture = m_videoElement->mediaSession().allowsPictureInPicture(*m_videoElement.get());
         IntRect videoElementClientRect = m_videoElement->clientRect();
         
         dispatch_async(dispatch_get_main_queue(), [strongThis, this, videoElementClientRect, viewRef, mode, allowsPictureInPicture] {
-            m_interface->setupFullscreen(*m_videoFullscreenLayer.get(), videoElementClientRect, viewRef.get(), mode, allowsPictureInPicture);
+            m_interface->setupFullscreen(*m_videoFullscreenView.get(), videoElementClientRect, viewRef.get(), mode, allowsPictureInPicture);
         });
     });
 }

Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
@@ -1124,9 +1124,8 @@ void MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenLayer(PlatformLayer* 
     CAContext *newContext = nil;
     
     if (m_videoFullscreenLayer && m_videoLayer) {
-        [m_videoLayer setFrame:CGRectMake(0, 0, m_videoFullscreenFrame.width(), m_videoFullscreenFrame.height())];
-        [m_videoLayer removeFromSuperlayer];
         [m_videoFullscreenLayer insertSublayer:m_videoLayer.get() atIndex:0];
+        [m_videoLayer setFrame:CGRectMake(0, 0, m_videoFullscreenFrame.width(), m_videoFullscreenFrame.height())];
         newContext = [m_videoFullscreenLayer context];
     } else if (m_videoInlineLayer && m_videoLayer) {
         [m_videoLayer setFrame:[m_videoInlineLayer bounds]];
@@ -1158,11 +1157,7 @@ void MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenFrame(FloatRect frame
         return;
 
     if (m_videoLayer) {
-        [m_videoLayer setStyle:nil]; // This enables actions, i.e. implicit animations.
-        [CATransaction begin];
         [m_videoLayer setFrame:CGRectMake(0, 0, frame.width(), frame.height())];
-        [CATransaction commit];
-        [m_videoLayer web_disableAllActions];
     }
     syncTextTrackBounds();
 }
@@ -1182,8 +1177,12 @@ void MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenGravity(MediaPlayer::
         videoGravity = AVLayerVideoGravityResizeAspectFill;
     else
         ASSERT_NOT_REACHED();
+    
+    if ([m_videoLayer videoGravity] == videoGravity)
+        return;
 
     [m_videoLayer setVideoGravity:videoGravity];
+    syncTextTrackBounds();
 }
 
 void MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenMode(MediaPlayer::VideoFullscreenMode mode)

Source/WebCore/platform/ios/WebVideoFullscreenInterfaceAVKit.h
@@ -45,8 +45,8 @@ OBJC_CLASS UIViewController;
 OBJC_CLASS UIWindow;
 OBJC_CLASS UIView;
 OBJC_CLASS CALayer;
-OBJC_CLASS WebAVVideoLayer;
-OBJC_CLASS WebCALayerHostWrapper;
+OBJC_CLASS WebAVPlayerLayerView;
+OBJC_CLASS WebAVPlayerLayer;
 
 namespace WTF {
 class String;
@@ -91,7 +91,7 @@ public:
     WEBCORE_EXPORT virtual void setLegibleMediaSelectionOptions(const Vector<WTF::String>& options, uint64_t selectedIndex) override;
     WEBCORE_EXPORT virtual void setExternalPlayback(bool enabled, ExternalPlaybackTargetType, WTF::String localizedDeviceName) override;
     
-    WEBCORE_EXPORT virtual void setupFullscreen(PlatformLayer&, const IntRect& initialRect, UIView *, HTMLMediaElementEnums::VideoFullscreenMode, bool allowsPictureInPicturePlayback);
+    WEBCORE_EXPORT virtual void setupFullscreen(UIView&, const IntRect& initialRect, UIView *, HTMLMediaElementEnums::VideoFullscreenMode, bool allowsPictureInPicturePlayback);
     WEBCORE_EXPORT virtual void enterFullscreen();
     WEBCORE_EXPORT virtual void exitFullscreen(const IntRect& finalRect);
     WEBCORE_EXPORT virtual void cleanupFullscreen();
@@ -133,9 +133,6 @@ protected:
 
     RetainPtr<WebAVPlayerController> m_playerController;
     RetainPtr<AVPlayerViewController> m_playerViewController;
-    RetainPtr<CALayer> m_videoLayer;
-    RetainPtr<WebAVVideoLayer> m_videoLayerContainer;
-    RetainPtr<WebCALayerHostWrapper> m_layerHostWrapper;
     WebVideoFullscreenModel* m_videoFullscreenModel { nullptr };
     WebVideoFullscreenChangeObserver* m_fullscreenChangeObserver { nullptr };
 
@@ -144,6 +141,7 @@ protected:
     RetainPtr<UIViewController> m_viewController;
     RetainPtr<UIView> m_parentView;
     RetainPtr<UIWindow> m_parentWindow;
+    RetainPtr<WebAVPlayerLayerView> m_playerLayerView;
     HTMLMediaElementEnums::VideoFullscreenMode m_mode { HTMLMediaElementEnums::VideoFullscreenModeNone };
     std::function<void(bool)> m_prepareToInlineCallback;
     bool m_allowsPictureInPicturePlayback { false };

Source/WebCore/platform/ios/WebVideoFullscreenModel.h
@@ -48,10 +48,8 @@ public:
     virtual void endScanning() = 0;
     virtual void requestExitFullscreen() = 0;
     virtual void setVideoLayerFrame(FloatRect) = 0;
-    virtual FloatRect videoLayerFrame() const = 0;
     enum VideoGravity { VideoGravityResize, VideoGravityResizeAspect, VideoGravityResizeAspectFill };
     virtual void setVideoLayerGravity(VideoGravity) = 0;
-    virtual VideoGravity videoLayerGravity() const = 0;
     virtual void selectAudioMediaOption(uint64_t index) = 0;
     virtual void selectLegibleMediaOption(uint64_t index) = 0;
     virtual void fullscreenModeChanged(HTMLMediaElementEnums::VideoFullscreenMode) = 0;

Source/WebCore/platform/ios/WebVideoFullscreenModelVideoElement.h
@@ -72,9 +72,7 @@ public:
     WEBCORE_EXPORT virtual void endScanning() override;
     WEBCORE_EXPORT virtual void requestExitFullscreen() override;
     WEBCORE_EXPORT virtual void setVideoLayerFrame(FloatRect) override;
-    WEBCORE_EXPORT virtual FloatRect videoLayerFrame() const override;
     WEBCORE_EXPORT virtual void setVideoLayerGravity(VideoGravity) override;
-    WEBCORE_EXPORT virtual VideoGravity videoLayerGravity() const override;
     WEBCORE_EXPORT virtual void selectAudioMediaOption(uint64_t index) override;
     WEBCORE_EXPORT virtual void selectLegibleMediaOption(uint64_t index) override;
     WEBCORE_EXPORT virtual void fullscreenModeChanged(HTMLMediaElementEnums::VideoFullscreenMode) override;

Source/WebCore/platform/ios/WebVideoFullscreenModelVideoElement.mm
@@ -254,11 +254,6 @@ void WebVideoFullscreenModelVideoElement::setVideoLayerFrame(FloatRect rect)
         m_videoElement->setVideoFullscreenFrame(rect);
 }
 
-FloatRect WebVideoFullscreenModelVideoElement::videoLayerFrame() const
-{
-    return m_videoFrame;
-}
-
 void WebVideoFullscreenModelVideoElement::setVideoLayerGravity(WebVideoFullscreenModel::VideoGravity gravity)
 {
     MediaPlayer::VideoGravity videoGravity = MediaPlayer::VideoGravityResizeAspect;
@@ -272,21 +267,6 @@ void WebVideoFullscreenModelVideoElement::setVideoLayerGravity(WebVideoFullscree
         ASSERT_NOT_REACHED();
     
     m_videoElement->setVideoFullscreenGravity(videoGravity);
-}
-
-WebVideoFullscreenModel::VideoGravity WebVideoFullscreenModelVideoElement::videoLayerGravity() const
-{
-    switch (m_videoElement->videoFullscreenGravity()) {
-    case MediaPlayer::VideoGravityResize:
-        return VideoGravityResize;
-    case MediaPlayer::VideoGravityResizeAspect:
-        return VideoGravityResizeAspect;
-    case MediaPlayer::VideoGravityResizeAspectFill:
-        return VideoGravityResizeAspectFill;
-    }
-
-    ASSERT_NOT_REACHED();
-    return VideoGravityResize;
 }
 
 void WebVideoFullscreenModelVideoElement::selectAudioMediaOption(uint64_t selectedAudioIndex)

Source/WebCore/platform/spi/cocoa/AVKitSPI.h
@@ -34,9 +34,12 @@
 #if USE(APPLE_INTERNAL_SDK)
 
 #import <AVKit/AVPlayerController.h>
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wobjc-property-no-attribute"
+#import <AVKit/AVPlayerLayerView.h>
+#pragma clang diagnostic pop
 #import <AVKit/AVPlayerViewController_Private.h>
 #import <AVKit/AVPlayerViewController_WebKitOnly.h>
-#import <AVKit/AVVideoLayer.h>
 
 #else
 
@@ -58,17 +61,17 @@ typedef NS_ENUM(NSInteger, AVPlayerControllerExternalPlaybackType) {
 @property (NS_NONATOMIC_IOSONLY, readonly) AVPlayerControllerStatus status;
 @end
 
-@protocol AVVideoLayer
-typedef NS_ENUM(NSInteger, AVVideoLayerGravity) {
-    AVVideoLayerGravityInvalid = 0,
-    AVVideoLayerGravityResizeAspect = 1,
-    AVVideoLayerGravityResizeAspectFill = 2,
-    AVVideoLayerGravityResize = 3,
-};
-- (void)setPlayerController:(AVPlayerController *)playerController;
-@property (nonatomic) AVVideoLayerGravity videoLayerGravity;
-@property (nonatomic) CGRect videoRect;
-@property (nonatomic, readonly, getter=isReadyForDisplay) BOOL readyForDisplay;
+@class AVPlayerLayer;
+
+@interface AVPictureInPicturePlayerLayerView : UIView
+@property (nonatomic, readonly) AVPlayerLayer *playerLayer;
+@end
+
+@interface AVPlayerLayerView : UIView
+@property (nonatomic, readonly) AVPlayerLayer *playerLayer;
+@property (nonatomic, readonly) AVPictureInPicturePlayerLayerView *pictureInPicturePlayerLayerView;
+- (void)startRoutingVideoToPictureInPicturePlayerLayerView;
+- (void)stopRoutingVideoToPictureInPicturePlayerLayerView;
 @end
 
 @protocol AVPlayerViewControllerDelegate <NSObject>
@@ -85,15 +88,15 @@ typedef NS_ENUM(NSInteger, AVPlayerViewControllerExitFullScreenReason) {
 @end
 
 @interface AVPlayerViewController (Details)
-- (instancetype)initWithVideoLayer:(CALayer <AVVideoLayer> *)videoLayer;
+- (instancetype)initWithPlayerLayerView:(AVPlayerLayerView *)playerLayerView;
 - (void)enterFullScreenAnimated:(BOOL)animated completionHandler:(void (^)(BOOL success, NSError *))completionHandler;
 - (void)exitFullScreenAnimated:(BOOL)animated completionHandler:(void (^)(BOOL success, NSError *))completionHandler;
 
 - (BOOL)isPictureInPicturePossible;
 - (void)startPictureInPicture;
 - (void)stopPictureInPicture;
-- (void)setAllowsPictureInPicturePlayback:(BOOL)allow;
 
+@property (nonatomic) BOOL allowsPictureInPicturePlayback;
 @property (nonatomic, strong) AVPlayerController *playerController;
 @property (nonatomic, weak) id <AVPlayerViewControllerDelegate> delegate;
 @end

commit 80a72eba54828fc30e448c4e31115e2278db0b3b

Fix fullscreen and PiP video animation and sizing regressions. https://bugs.webkit.org/show_bug.cgi?id=147189 <rdar://problem/21930899>
Patch by Jeremy Jones <jeremyj@apple.com> on 2015-07-22
Reviewed by Jer Noble.

Fixes WK1 specific regressions introduced by r187044. Flicker when entering fullscreen,
wrong animation and missing initial animation rects.

* platform/ios/WebVideoFullscreenControllerAVKit.mm:
(elementRectInWindow): Added.
(WebVideoFullscreenControllerContext::didSetupFullscreen): Clear background decreases flash.
(WebVideoFullscreenControllerContext::fullscreenMayReturnToInline): Use consistent inline rect.
(WebVideoFullscreenControllerContext::setVideoLayerFrame):
Clear the transform on the web thread instead of on the UI thread.
(WebVideoFullscreenControllerContext::setUpFullscreen): Use consistent inline rect.
(WebVideoFullscreenControllerContext::exitFullscreen): Use consistent inline rect.
* platform/ios/WebVideoFullscreenInterfaceAVKit.mm:
(-[WebAVPlayerLayer resolveBounds]): Clear transform after setting frame.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187183 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/ios/WebVideoFullscreenControllerAVKit.mm
@@ -36,13 +36,23 @@
 #import "WebVideoFullscreenInterfaceAVKit.h"
 #import "WebVideoFullscreenModelVideoElement.h"
 #import <QuartzCore/CoreAnimation.h>
+#import <WebCore/FrameView.h>
 #import <WebCore/HTMLVideoElement.h>
+#import <WebCore/RenderElement.h>
 #import <WebCore/WebCoreThreadRun.h>
 
 SOFT_LINK_FRAMEWORK(UIKit)
 SOFT_LINK_CLASS(UIKit, UIView)
 
 using namespace WebCore;
+
+static IntRect elementRectInWindow(HTMLVideoElement* videoElement)
+{
+    if (!videoElement || !videoElement->renderer() || !videoElement->document().view())
+        return IntRect();
+    
+    return videoElement->document().view()->convertToContainingWindow(videoElement->renderer()->absoluteBoundingBoxRect());
+}
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED <= 80200 || !HAVE(AVKIT)
 
@@ -154,6 +164,7 @@ void WebVideoFullscreenControllerContext::didSetupFullscreen()
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
     RetainPtr<CALayer> videoFullscreenLayer = [m_videoFullscreenView layer];
     WebThreadRun([strongThis, this, videoFullscreenLayer] {
+        [videoFullscreenLayer setBackgroundColor:cachedCGColor(WebCore::Color::transparent, WebCore::ColorSpaceDeviceRGB)];
         m_model->setVideoFullscreenLayer(videoFullscreenLayer.get());
         dispatch_async(dispatch_get_main_queue(), [strongThis, this] {
             m_interface->enterFullscreen();
@@ -198,7 +209,7 @@ void WebVideoFullscreenControllerContext::fullscreenMayReturnToInline()
     ASSERT(isUIThread());
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
     WebThreadRun([strongThis, this] {
-        IntRect clientRect = m_videoElement->clientRect();
+        IntRect clientRect = elementRectInWindow(m_videoElement.get());
         dispatch_async(dispatch_get_main_queue(), [strongThis, this, clientRect] {
             m_interface->preparedToReturnToInline(true, clientRect);
         });
@@ -454,17 +465,21 @@ void WebVideoFullscreenControllerContext::setVideoLayerFrame(FloatRect frame)
     ASSERT(isUIThread());
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
     RetainPtr<CALayer> videoFullscreenLayer = [m_videoFullscreenView layer];
-
-    mach_port_name_t fencePort = [[videoFullscreenLayer context] createFencePort];
     
-    WebThreadRun([strongThis, this, frame, fencePort, videoFullscreenLayer] {
-        [CATransaction begin];
-        [CATransaction setAnimationDuration:0];
-        if (m_model)
-            m_model->setVideoLayerFrame(frame);
-        [[videoFullscreenLayer context] setFencePort:fencePort];
-        mach_port_deallocate(mach_task_self(), fencePort);
-        [CATransaction commit];
+    [videoFullscreenLayer setSublayerTransform:[videoFullscreenLayer transform]];
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        WebThreadRun([strongThis, this, frame, videoFullscreenLayer] {
+            [CATransaction begin];
+            [CATransaction setDisableActions:YES];
+            [CATransaction setAnimationDuration:0];
+            
+            [videoFullscreenLayer setSublayerTransform:CATransform3DIdentity];
+            
+            if (m_model)
+                m_model->setVideoLayerFrame(frame);
+            [CATransaction commit];
+        });
     });
 }
 
@@ -528,7 +543,10 @@ void WebVideoFullscreenControllerContext::setUpFullscreen(HTMLVideoElement& vide
         m_model->setVideoElement(m_videoElement.get());
         
         bool allowsPictureInPicture = m_videoElement->mediaSession().allowsPictureInPicture(*m_videoElement.get());
-        IntRect videoElementClientRect = m_videoElement->clientRect();
+
+        IntRect videoElementClientRect = elementRectInWindow(m_videoElement.get());
+        FloatRect videoLayerFrame = FloatRect(FloatPoint(), videoElementClientRect.size());
+        m_model->setVideoLayerFrame(videoLayerFrame);
         
         dispatch_async(dispatch_get_main_queue(), [strongThis, this, videoElementClientRect, viewRef, mode, allowsPictureInPicture] {
             m_interface->setupFullscreen(*m_videoFullscreenView.get(), videoElementClientRect, viewRef.get(), mode, allowsPictureInPicture);
@@ -539,11 +557,11 @@ void WebVideoFullscreenControllerContext::setUpFullscreen(HTMLVideoElement& vide
 void WebVideoFullscreenControllerContext::exitFullscreen()
 {
     ASSERT(WebThreadIsCurrent() || isMainThread());
-    IntRect screenRect = m_videoElement->screenRect();
+    IntRect clientRect = elementRectInWindow(m_videoElement.get());
     RefPtr<WebVideoFullscreenControllerContext> strongThis(this);
-    dispatch_async(dispatch_get_main_queue(), [strongThis, this, screenRect] {
+    dispatch_async(dispatch_get_main_queue(), [strongThis, this, clientRect] {
         ASSERT(isUIThread());
-        m_interface->exitFullscreen(screenRect);
+        m_interface->exitFullscreen(clientRect);
     });
 }
 

Source/WebCore/platform/ios/WebVideoFullscreenInterfaceAVKit.mm
@@ -682,10 +682,11 @@ static WebVideoFullscreenInterfaceAVKit::ExitFullScreenReason convertToExitFullS
     
     [CATransaction begin];
     [CATransaction setAnimationDuration:0];
+    [CATransaction setDisableActions:YES];
     
-    [(UIView *)[_videoSublayer delegate] setTransform:CGAffineTransformIdentity];
     self.modelVideoLayerFrame = [self bounds];
     [_avPlayerController delegate]->setVideoLayerFrame(self.modelVideoLayerFrame);
+    [(UIView *)[_videoSublayer delegate] setTransform:CGAffineTransformIdentity];
     
     [CATransaction commit];
 }