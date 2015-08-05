fberlakovich/r2c-webkit

commit 826a060a1c445d1b812c7283b10eed110407b488

[Mac] Unable to dismiss context menu during test runs https://bugs.webkit.org/show_bug.cgi?id=146836
Reviewed by Tim Horton.

Since Context Menus are modal, the test system would block on the displayed context menu,
causing tests to fail. Instead, we should do what WK1 was doing, which is to perform the
mouse click and generate the menu contents, but not ask AppKit to display it.

Drive-by fix: We did not handle 'escape' key presses. This is also fixed.

* DumpRenderTree/mac/EventSendingController.mm:
(-[EventSendingController keyDown:withModifiers:withLocation:]): Handle the 'escape' character.
* WebKitTestRunner/InjectedBundle/EventSendingController.cpp:
(WTR::EventSendingController::mouseMoveTo): Keep track of the current mouse position.
(WTR::EventSendingController::contextClick): Don't emit an actual context menu invocation and then
copy the menu items. Instead, use WKBundlePageCopyContentMenuAtPointInWindow to do all the same
work without actually calling on AppKit to display the menu.
ask WebCore to do all the work EXCEPT displaying the menu.
* WebKitTestRunner/InjectedBundle/EventSendingController.h:
* WebKitTestRunner/mac/EventSenderProxy.mm:
(WTR::EventSenderProxy::keyDown): Handle the 'escape' character.


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@186694 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/WebKitTestRunner/InjectedBundle/EventSendingController.cpp
@@ -250,6 +250,8 @@ void EventSendingController::mouseMoveTo(int x, int y)
     WKRetainPtr<WKDoubleRef> yRef(AdoptWK, WKDoubleCreate(y));
     WKDictionarySetItem(EventSenderMessageBody.get(), yKey.get(), yRef.get());
 
+    m_position = WKPointMake(x, y);
+    
     WKBundlePagePostSynchronousMessage(InjectedBundle::singleton().page()->page(), EventSenderMessageName.get(), EventSenderMessageBody.get(), 0);
 }
 
@@ -473,11 +475,7 @@ JSValueRef EventSendingController::contextClick()
     WKBundleFrameRef mainFrame = WKBundlePageGetMainFrame(page);
     JSContextRef context = WKBundleFrameGetJavaScriptContext(mainFrame);
 #if ENABLE(CONTEXT_MENUS)
-    // Do mouse context click.
-    mouseDown(2, 0);
-    mouseUp(2, 0);
-
-    WKRetainPtr<WKArrayRef> menuEntries = adoptWK(WKBundlePageCopyContextMenuItems(page));
+    WKRetainPtr<WKArrayRef> menuEntries = adoptWK(WKBundlePageCopyContextMenuAtPointInWindow(page, m_position));
     JSValueRef arrayResult = JSObjectMakeArray(context, 0, 0, 0);
     JSObjectRef arrayObj = JSValueToObject(context, arrayResult, 0);
     size_t entriesSize = WKArrayGetSize(menuEntries.get());

Tools/DumpRenderTree/mac/EventSendingController.mm
@@ -968,6 +968,8 @@ static int buildModifierFlags(const WebScriptObject* modifiers)
         keyCode = 0x02;
     else if ([character isEqualToString:@"e"])
         keyCode = 0x0E;
+    else if ([character isEqualToString:@"\x1b"])
+        keyCode = 0x1B;
 
     KeyMappingEntry table[] = {
         {0x2F, 0x41, '.', nil},

Tools/WebKitTestRunner/InjectedBundle/EventSendingController.h
@@ -85,6 +85,7 @@ public:
 
 private:
     EventSendingController();
+    WKPoint m_position;
 };
 
 } // namespace WTR

Tools/WebKitTestRunner/mac/EventSenderProxy.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011, 2014 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2014-2015 Apple Inc. All rights reserved.
  * Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
  *
  * Redistribution and use in source and binary forms, with or without
@@ -563,6 +563,8 @@ void EventSenderProxy::keyDown(WKStringRef key, WKEventModifiers modifiers, unsi
         keyCode = 0x02;
     else if ([character isEqualToString:@"e"])
         keyCode = 0x0E;
+    else if ([character isEqualToString:@"\x1b"])
+        keyCode = 0x1B;
 
     KeyMappingEntry table[] = {
         {0x2F, 0x41, '.', nil},

commit 7654ca28034efcec3f16ad695128c4eef86f0aed

[EFL] Editing tests have been broken since r186694 https://bugs.webkit.org/show_bug.cgi?id=146888
Patch by Hunseop Jeong <hs85.jeong@samsung.com> on 2015-08-04
Reviewed by Brent Fulgham.

Tools:

Restored some codes for the context click on GTK+ and EFL port.

* WebKitTestRunner/InjectedBundle/EventSendingController.cpp:
(WTR::EventSendingController::contextClick): Seperated the codes using the PLATFORM guard.

LayoutTests:

Unskip the failure tests related with r186694.

* platform/efl/TestExpectations:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187934 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/WebKitTestRunner/InjectedBundle/EventSendingController.cpp
@@ -475,7 +475,14 @@ JSValueRef EventSendingController::contextClick()
     WKBundleFrameRef mainFrame = WKBundlePageGetMainFrame(page);
     JSContextRef context = WKBundleFrameGetJavaScriptContext(mainFrame);
 #if ENABLE(CONTEXT_MENUS)
+#if PLATFORM(GTK) || PLATFORM(EFL)
+    // Do mouse context click.
+    mouseDown(2, 0);
+    mouseUp(2, 0);
+    WKRetainPtr<WKArrayRef> menuEntries = adoptWK(WKBundlePageCopyContextMenuItems(page));
+#else
     WKRetainPtr<WKArrayRef> menuEntries = adoptWK(WKBundlePageCopyContextMenuAtPointInWindow(page, m_position));
+#endif
     JSValueRef arrayResult = JSObjectMakeArray(context, 0, 0, 0);
     JSObjectRef arrayObj = JSValueToObject(context, arrayResult, 0);
     size_t entriesSize = WKArrayGetSize(menuEntries.get());

LayoutTests/platform/efl/TestExpectations
@@ -2251,9 +2251,3 @@ webkit.org/b/146887 accessibility/roles-exposed.html [ Failure ]
 webkit.org/b/146887 accessibility/table-fallback-roles-expose-element-attributes.html [ Failure ]
 webkit.org/b/146887 accessibility/table-with-footer-section-above-body.html [ Failure ]
 webkit.org/b/146887 accessibility/table-with-missing-aria-role-rows.html [ Failure ]
-
-webkit.org/b/146888 editing/selection/5354455-1.html [ Failure ]
-webkit.org/b/146888 editing/selection/5354455-2.html [ Failure ]
-webkit.org/b/146888 editing/spelling/context-menu-suggestions-multiword-selection.html [ Failure ]
-webkit.org/b/146888 editing/spelling/context-menu-suggestions-subword-selection.html [ Failure ]
-webkit.org/b/146888 editing/spelling/context-menu-suggestions.html [ Failure ]