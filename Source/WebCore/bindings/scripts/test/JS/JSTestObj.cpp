fberlakovich/r2c-webkit

commit 6a37585dbd351d0b3eec3662106e5161fc1eea20

HTMLTableElement.tHead / tFoot / caption should be nullable https://bugs.webkit.org/show_bug.cgi?id=148991
Reviewed by Ryosuke Niwa.

Source/WebCore:

According to the specification, HTMLTableElement.tHead / tFoot / caption
should be nullable:
https://html.spec.whatwg.org/multipage/tables.html#htmltableelement

Upon assigning null, we are supposed to remove the existing tHead / tFoot
/ caption element. However, we had a bug causing us to throw an exception
after removing the element. This is because we would try to insert a null
element and ContainerNode::insertBefore() throws when doing so.

Also, as per the specification, setting tHead / tFoot to something else
than a thead / tfoot element should throw a HierarchyRequestError:
https://html.spec.whatwg.org/multipage/tables.html#dom-table-thead
https://html.spec.whatwg.org/multipage/tables.html#dom-table-tfoot

Previously, WebKit did not check the tag and was happy inserting the
element as long as it was an HTMLTableSectionElement. This means that
you could set a tfoot by assigning table.tHead.

This patch corrects both bugs and adds test coverage for it.

Test: fast/dom/HTMLTableElement/nullable-attributes.html

* html/HTMLTableElement.cpp:
(WebCore::HTMLTableElement::setCaption):
Only call insertBefore() if newCaption is not null as insertBefore()
will throw an exception otherwise.

(WebCore::HTMLTableElement::setTHead):
- Throw a HierarchyRequestError if the HTMLTableSectionElement is not
  null or a <thead> element, as per the specification.
- Only call insertBefore() if newHead is not null as insertBefore()
  will throw an exception otherwise.

(WebCore::HTMLTableElement::setTFoot):
- Throw a HierarchyRequestError if the HTMLTableSectionElement is not
  null or a <tfoot> element, as per the specification.
- Only call insertBefore() if newFoot is not null as insertBefore()
  will throw an exception otherwise.

* html/HTMLTableElement.idl:
Use [StrictTypeChecking] for these 3 attributes so that the bindings

Source/WebCore/html/HTMLTableElement.cpp
@@ -80,7 +80,8 @@ HTMLTableCaptionElement* HTMLTableElement::caption() const
 void HTMLTableElement::setCaption(PassRefPtr<HTMLTableCaptionElement> newCaption, ExceptionCode& ec)
 {
     deleteCaption();
-    insertBefore(newCaption, firstChild(), ec);
+    if (newCaption)
+        insertBefore(newCaption, firstChild(), ec);
 }
 
 HTMLTableSectionElement* HTMLTableElement::tHead() const
@@ -94,7 +95,15 @@ HTMLTableSectionElement* HTMLTableElement::tHead() const
 
 void HTMLTableElement::setTHead(PassRefPtr<HTMLTableSectionElement> newHead, ExceptionCode& ec)
 {
+    if (newHead && !newHead->hasTagName(theadTag)) {
+        ec = HIERARCHY_REQUEST_ERR;
+        return;
+    }
+
     deleteTHead();
+
+    if (!newHead)
+        return;
 
     Node* child;
     for (child = firstChild(); child; child = child->nextSibling())
@@ -115,7 +124,15 @@ HTMLTableSectionElement* HTMLTableElement::tFoot() const
 
 void HTMLTableElement::setTFoot(PassRefPtr<HTMLTableSectionElement> newFoot, ExceptionCode& ec)
 {
+    if (newFoot && !newFoot->hasTagName(tfootTag)) {
+        ec = HIERARCHY_REQUEST_ERR;
+        return;
+    }
+
     deleteTFoot();
+
+    if (!newFoot)
+        return;
 
     Node* child;
     for (child = firstChild(); child; child = child->nextSibling())

LayoutTests/fast/dom/HTMLTableElement/nullable-attributes-expected.txt
@@ -1,0 +1,56 @@
+Tests that HTMLTableElement.caption / tHead / tFoot are nullable and behave according to specification.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS table.__proto__ is HTMLTableElement.prototype
+PASS caption.__proto__ is HTMLTableCaptionElement.prototype
+PASS thead.__proto__ is HTMLTableSectionElement.prototype
+PASS tfoot.__proto__ is HTMLTableSectionElement.prototype
+
+* HTMLTableElement.caption
+PASS table.caption is null
+table.caption = caption
+PASS table.caption is caption
+PASS table.caption = document.body threw exception TypeError: The HTMLTableElement.caption attribute must be an instance of HTMLTableCaptionElement.
+PASS table.caption is caption
+table.caption = null
+PASS table.caption is null
+table.caption = caption
+PASS table.caption is caption
+table.caption = undefined
+PASS table.caption is null
+
+* HTMLTableElement.tHead
+PASS table.tHead is null
+table.tHead = thead
+PASS table.tHead is thead
+PASS table.tHead = document.body threw exception TypeError: The HTMLTableElement.tHead attribute must be an instance of HTMLTableSectionElement.
+PASS table.tHead is thead
+PASS table.tHead = tfoot threw exception Error: HierarchyRequestError: DOM Exception 3.
+PASS table.tHead is thead
+table.tHead = null
+PASS table.tHead is null
+table.tHead = thead
+PASS table.tHead is thead
+table.tHead = undefined
+PASS table.tHead is null
+
+* HTMLTableElement.tFoot
+PASS table.tFoot is null
+table.tFoot = tfoot
+PASS table.tFoot is tfoot
+PASS table.tFoot = document.body threw exception TypeError: The HTMLTableElement.tFoot attribute must be an instance of HTMLTableSectionElement.
+PASS table.tFoot is tfoot
+PASS table.tFoot = thead threw exception Error: HierarchyRequestError: DOM Exception 3.
+PASS table.tFoot is tfoot
+table.tFoot = null
+PASS table.tFoot is null
+table.tFoot = tfoot
+PASS table.tFoot is tfoot
+table.tFoot = undefined
+PASS table.tFoot is null
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/dom/HTMLTableElement/nullable-attributes.html
@@ -1,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script src="../../../resources/js-test-pre.js"></script>
+<script>
+description("Tests that HTMLTableElement.caption / tHead / tFoot are nullable and behave according to specification.");
+
+var table = document.createElement("table");
+shouldBe("table.__proto__", "HTMLTableElement.prototype");
+var caption = document.createElement("caption");
+shouldBe("caption.__proto__", "HTMLTableCaptionElement.prototype");
+var thead = document.createElement("thead");
+shouldBe("thead.__proto__", "HTMLTableSectionElement.prototype");
+var tfoot = document.createElement("tFoot");
+shouldBe("tfoot.__proto__", "HTMLTableSectionElement.prototype");
+
+debug("");
+debug("* HTMLTableElement.caption");
+shouldBeNull("table.caption");
+evalAndLog("table.caption = caption");
+shouldBe("table.caption", "caption");
+shouldThrow("table.caption = document.body", "'TypeError: The HTMLTableElement.caption attribute must be an instance of HTMLTableCaptionElement'");
+shouldBe("table.caption", "caption");
+evalAndLog("table.caption = null");
+shouldBeNull("table.caption");
+evalAndLog("table.caption = caption");
+shouldBe("table.caption", "caption");
+evalAndLog("table.caption = undefined"); // undefined is converted to null for nullable attributes.
+shouldBeNull("table.caption");
+
+debug("");
+debug("* HTMLTableElement.tHead");
+shouldBeNull("table.tHead");
+evalAndLog("table.tHead = thead");
+shouldBe("table.tHead", "thead");
+shouldThrow("table.tHead = document.body", "'TypeError: The HTMLTableElement.tHead attribute must be an instance of HTMLTableSectionElement'");
+shouldBe("table.tHead", "thead");
+shouldThrow("table.tHead = tfoot", "'Error: HierarchyRequestError: DOM Exception 3'");
+shouldBe("table.tHead", "thead");
+evalAndLog("table.tHead = null");
+shouldBeNull("table.tHead");
+evalAndLog("table.tHead = thead");
+shouldBe("table.tHead", "thead");
+evalAndLog("table.tHead = undefined"); // undefined is converted to null for nullable attributes.
+shouldBeNull("table.tHead");
+
+debug("");
+debug("* HTMLTableElement.tFoot");
+shouldBeNull("table.tFoot");
+evalAndLog("table.tFoot = tfoot");
+shouldBe("table.tFoot", "tfoot");
+shouldThrow("table.tFoot = document.body", "'TypeError: The HTMLTableElement.tFoot attribute must be an instance of HTMLTableSectionElement'");
+shouldBe("table.tFoot", "tfoot");
+shouldThrow("table.tFoot = thead", "'Error: HierarchyRequestError: DOM Exception 3'");
+shouldBe("table.tFoot", "tfoot");
+evalAndLog("table.tFoot = null");
+shouldBeNull("table.tFoot");
+evalAndLog("table.tFoot = tfoot");
+shouldBe("table.tFoot", "tfoot");
+evalAndLog("table.tFoot = undefined"); // undefined is converted to null for nullable attributes.
+shouldBeNull("table.tFoot");
+</script>
+<script src="../../../resources/js-test-post.js"></script>
+</body>
+</html>

LayoutTests/fast/dom/setter-type-enforcement-expected.txt
@@ -4,7 +4,7 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 
 
 PASS document.body = nodelist; threw exception Error: HierarchyRequestError: DOM Exception 3.
-PASS table.tHead = nodelist; threw exception Error: NotFoundError: DOM Exception 8.
+PASS table.tHead = nodelist; threw exception TypeError: The HTMLTableElement.tHead attribute must be an instance of HTMLTableSectionElement.
 PASS successfullyParsed is true
 
 TEST COMPLETE

Source/WebCore/html/HTMLTableElement.idl
@@ -19,9 +19,9 @@
  */
 
 interface HTMLTableElement : HTMLElement {
-    [SetterRaisesException] attribute HTMLTableCaptionElement caption;
-    [SetterRaisesException] attribute HTMLTableSectionElement tHead;
-    [SetterRaisesException] attribute HTMLTableSectionElement tFoot;
+    [SetterRaisesException, StrictTypeChecking] attribute HTMLTableCaptionElement caption;
+    [SetterRaisesException, StrictTypeChecking] attribute HTMLTableSectionElement tHead;
+    [SetterRaisesException, StrictTypeChecking] attribute HTMLTableSectionElement tFoot;
 
     readonly attribute HTMLCollection rows;
     readonly attribute HTMLCollection tBodies;

commit 184c4db253c3ffadd8610676e9ec0fcb67f93d48

Possible small iOS PLT regression from r189537 https://bugs.webkit.org/show_bug.cgi?id=149232
Reviewed by Ryosuke Niwa.

r189537 may have regressed PLT a bit on iOS. That change added a couple
of extra branches to throw exceptions. This patch marks those branches
as UNLIKELY() as we already do for other similar checks in the JS
bindings.

* bindings/scripts/CodeGeneratorJS.pm:
(GenerateImplementation):
* bindings/scripts/test/GObject/WebKitDOMTestObj.cpp:
(webkit_dom_test_obj_get_property):
(webkit_dom_test_obj_class_init):
(webkit_dom_test_obj_get_strict_type_checking_attribute):
(webkit_dom_test_obj_set_strict_type_checking_attribute):
* bindings/scripts/test/GObject/WebKitDOMTestObj.h:
* bindings/scripts/test/JS/JSTestObj.cpp:
(WebCore::jsTestObjStrictTypeCheckingAttribute):
(WebCore::setJSTestObjStrictTypeCheckingAttribute):
* bindings/scripts/test/ObjC/DOMTestObj.h:
* bindings/scripts/test/ObjC/DOMTestObj.mm:
(-[DOMTestObj strictTypeCheckingAttribute]):
(-[DOMTestObj setStrictTypeCheckingAttribute:]):
* bindings/scripts/test/TestObj.idl:
* html/HTMLTableElement.cpp:
(WebCore::HTMLTableElement::setTHead):
(WebCore::HTMLTableElement::setTFoot):


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@189887 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -2662,7 +2662,7 @@ sub GenerateImplementation
 
                     my $argType = $attribute->signature->type;
                     if ($codeGenerator->IsWrapperType($argType)) {
-                        push(@implContent, "    if (!value.isUndefinedOrNull() && !value.inherits(JS${argType}::info())) {\n");
+                        push(@implContent, "    if (UNLIKELY(!value.isUndefinedOrNull() && !value.inherits(JS${argType}::info()))) {\n");
                         push(@implContent, "        throwAttributeTypeError(*exec, \"$interfaceName\", \"$name\", \"$argType\");\n");
                         push(@implContent, "        return;\n");
                         push(@implContent, "    };\n");

Source/WebCore/html/HTMLTableElement.cpp
@@ -95,7 +95,7 @@ HTMLTableSectionElement* HTMLTableElement::tHead() const
 
 void HTMLTableElement::setTHead(PassRefPtr<HTMLTableSectionElement> newHead, ExceptionCode& ec)
 {
-    if (newHead && !newHead->hasTagName(theadTag)) {
+    if (UNLIKELY(newHead && !newHead->hasTagName(theadTag))) {
         ec = HIERARCHY_REQUEST_ERR;
         return;
     }
@@ -124,7 +124,7 @@ HTMLTableSectionElement* HTMLTableElement::tFoot() const
 
 void HTMLTableElement::setTFoot(PassRefPtr<HTMLTableSectionElement> newFoot, ExceptionCode& ec)
 {
-    if (newFoot && !newFoot->hasTagName(tfootTag)) {
+    if (UNLIKELY(newFoot && !newFoot->hasTagName(tfootTag))) {
         ec = HIERARCHY_REQUEST_ERR;
         return;
     }

Source/WebCore/bindings/scripts/test/GObject/WebKitDOMTestObj.cpp
@@ -105,6 +105,7 @@ enum {
     PROP_ATTR_WITH_SETTER_EXCEPTION,
     PROP_STRING_ATTR_WITH_GETTER_EXCEPTION,
     PROP_STRING_ATTR_WITH_SETTER_EXCEPTION,
+    PROP_STRICT_TYPE_CHECKING_ATTRIBUTE,
     PROP_WITH_SCRIPT_STATE_ATTRIBUTE,
     PROP_WITH_SCRIPT_EXECUTION_CONTEXT_ATTRIBUTE,
     PROP_WITH_SCRIPT_STATE_ATTRIBUTE_RAISES,
@@ -334,6 +335,9 @@ static void webkit_dom_test_obj_get_property(GObject* object, guint propertyId, 
         break;
     case PROP_STRING_ATTR_WITH_SETTER_EXCEPTION:
         g_value_take_string(value, webkit_dom_test_obj_get_string_attr_with_setter_exception(self));
+        break;
+    case PROP_STRICT_TYPE_CHECKING_ATTRIBUTE:
+        g_value_set_object(value, webkit_dom_test_obj_get_strict_type_checking_attribute(self));
         break;
     case PROP_WITH_SCRIPT_STATE_ATTRIBUTE:
         g_value_set_long(value, webkit_dom_test_obj_get_with_script_state_attribute(self));
@@ -714,6 +718,16 @@ static void webkit_dom_test_obj_class_init(WebKitDOMTestObjClass* requestClass)
             "read-write gchar* TestObj:string-attr-with-setter-exception",
             "",
             WEBKIT_PARAM_READWRITE));
+
+    g_object_class_install_property(
+        gobjectClass,
+        PROP_STRICT_TYPE_CHECKING_ATTRIBUTE,
+        g_param_spec_object(
+            "strict-type-checking-attribute",
+            "TestObj:strict-type-checking-attribute",
+            "read-only WebKitDOMTestObj* TestObj:strict-type-checking-attribute",
+            WEBKIT_DOM_TYPE_TEST_OBJ,
+            WEBKIT_PARAM_READABLE));
 
     g_object_class_install_property(
         gobjectClass,
@@ -1975,6 +1989,25 @@ void webkit_dom_test_obj_set_string_attr_with_setter_exception(WebKitDOMTestObj*
         WebCore::ExceptionCodeDescription ecdesc(ec);
         g_set_error_literal(error, g_quark_from_string("WEBKIT_DOM"), ecdesc.code, ecdesc.name);
     }
+}
+
+WebKitDOMTestObj* webkit_dom_test_obj_get_strict_type_checking_attribute(WebKitDOMTestObj* self)
+{
+    WebCore::JSMainThreadNullState state;
+    g_return_val_if_fail(WEBKIT_DOM_IS_TEST_OBJ(self), 0);
+    WebCore::TestObj* item = WebKit::core(self);
+    RefPtr<WebCore::TestObj> gobjectResult = WTF::getPtr(item->strictTypeCheckingAttribute());
+    return WebKit::kit(gobjectResult.get());
+}
+
+void webkit_dom_test_obj_set_strict_type_checking_attribute(WebKitDOMTestObj* self, WebKitDOMTestObj* value)
+{
+    WebCore::JSMainThreadNullState state;
+    g_return_if_fail(WEBKIT_DOM_IS_TEST_OBJ(self));
+    g_return_if_fail(WEBKIT_DOM_IS_TEST_OBJ(value));
+    WebCore::TestObj* item = WebKit::core(self);
+    WebCore::TestObj* convertedValue = WebKit::core(value);
+    item->setStrictTypeCheckingAttribute(convertedValue);
 }
 
 glong webkit_dom_test_obj_get_with_script_state_attribute(WebKitDOMTestObj* self)

Source/WebCore/bindings/scripts/test/GObject/WebKitDOMTestObj.h
@@ -1151,6 +1151,27 @@ WEBKIT_API void
 webkit_dom_test_obj_set_string_attr_with_setter_exception(WebKitDOMTestObj* self, const gchar* value, GError** error);
 
 /**
+ * webkit_dom_test_obj_get_strict_type_checking_attribute:
+ * @self: A #WebKitDOMTestObj
+ *
+ * Returns: (transfer none): A #WebKitDOMTestObj
+ *
+ * Stability: Unstable
+**/
+WEBKIT_API WebKitDOMTestObj*
+webkit_dom_test_obj_get_strict_type_checking_attribute(WebKitDOMTestObj* self);
+
+/**
+ * webkit_dom_test_obj_set_strict_type_checking_attribute:
+ * @self: A #WebKitDOMTestObj
+ * @value: A #WebKitDOMTestObj
+ *
+ * Stability: Unstable
+**/
+WEBKIT_API void
+webkit_dom_test_obj_set_strict_type_checking_attribute(WebKitDOMTestObj* self, WebKitDOMTestObj* value);
+
+/**
  * webkit_dom_test_obj_get_with_script_state_attribute:
  * @self: A #WebKitDOMTestObj
  *

Source/WebCore/bindings/scripts/test/JS/JSTestObj.cpp
@@ -228,6 +228,8 @@ JSC::EncodedJSValue jsTestObjStringAttrWithGetterException(JSC::ExecState*, JSC:
 void setJSTestObjStringAttrWithGetterException(JSC::ExecState*, JSC::JSObject*, JSC::EncodedJSValue, JSC::EncodedJSValue);
 JSC::EncodedJSValue jsTestObjStringAttrWithSetterException(JSC::ExecState*, JSC::JSObject*, JSC::EncodedJSValue, JSC::PropertyName);
 void setJSTestObjStringAttrWithSetterException(JSC::ExecState*, JSC::JSObject*, JSC::EncodedJSValue, JSC::EncodedJSValue);
+JSC::EncodedJSValue jsTestObjStrictTypeCheckingAttribute(JSC::ExecState*, JSC::JSObject*, JSC::EncodedJSValue, JSC::PropertyName);
+void setJSTestObjStrictTypeCheckingAttribute(JSC::ExecState*, JSC::JSObject*, JSC::EncodedJSValue, JSC::EncodedJSValue);
 JSC::EncodedJSValue jsTestObjCustomAttr(JSC::ExecState*, JSC::JSObject*, JSC::EncodedJSValue, JSC::PropertyName);
 void setJSTestObjCustomAttr(JSC::ExecState*, JSC::JSObject*, JSC::EncodedJSValue, JSC::EncodedJSValue);
 JSC::EncodedJSValue jsTestObjOnfoo(JSC::ExecState*, JSC::JSObject*, JSC::EncodedJSValue, JSC::PropertyName);
@@ -524,6 +526,7 @@ static const HashTableValue JSTestObjPrototypeTableValues[] =
     { "attrWithSetterException", DontDelete | CustomAccessor, NoIntrinsic, { (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestObjAttrWithSetterException), (intptr_t) static_cast<PutPropertySlot::PutValueFunc>(setJSTestObjAttrWithSetterException) } },
     { "stringAttrWithGetterException", DontDelete | CustomAccessor, NoIntrinsic, { (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestObjStringAttrWithGetterException), (intptr_t) static_cast<PutPropertySlot::PutValueFunc>(setJSTestObjStringAttrWithGetterException) } },
     { "stringAttrWithSetterException", DontDelete | CustomAccessor, NoIntrinsic, { (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestObjStringAttrWithSetterException), (intptr_t) static_cast<PutPropertySlot::PutValueFunc>(setJSTestObjStringAttrWithSetterException) } },
+    { "strictTypeCheckingAttribute", DontDelete | CustomAccessor, NoIntrinsic, { (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestObjStrictTypeCheckingAttribute), (intptr_t) static_cast<PutPropertySlot::PutValueFunc>(setJSTestObjStrictTypeCheckingAttribute) } },
     { "onfoo", DontDelete | CustomAccessor, NoIntrinsic, { (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestObjOnfoo), (intptr_t) static_cast<PutPropertySlot::PutValueFunc>(setJSTestObjOnfoo) } },
     { "withScriptStateAttribute", DontDelete | CustomAccessor, NoIntrinsic, { (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestObjWithScriptStateAttribute), (intptr_t) static_cast<PutPropertySlot::PutValueFunc>(setJSTestObjWithScriptStateAttribute) } },
     { "withScriptExecutionContextAttribute", DontDelete | CustomAccessor, NoIntrinsic, { (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestObjWithScriptExecutionContextAttribute), (intptr_t) static_cast<PutPropertySlot::PutValueFunc>(setJSTestObjWithScriptExecutionContextAttribute) } },
@@ -1256,6 +1259,23 @@ EncodedJSValue jsTestObjStringAttrWithSetterException(ExecState* exec, JSObject*
     }
     auto& impl = castedThis->impl();
     JSValue result = jsStringWithCache(exec, impl.stringAttrWithSetterException());
+    return JSValue::encode(result);
+}
+
+
+EncodedJSValue jsTestObjStrictTypeCheckingAttribute(ExecState* exec, JSObject* slotBase, EncodedJSValue thisValue, PropertyName)
+{
+    UNUSED_PARAM(exec);
+    UNUSED_PARAM(slotBase);
+    UNUSED_PARAM(thisValue);
+    JSTestObj* castedThis = jsDynamicCast<JSTestObj*>(JSValue::decode(thisValue));
+    if (UNLIKELY(!castedThis)) {
+        if (jsDynamicCast<JSTestObjPrototype*>(slotBase))
+            return reportDeprecatedGetterError(*exec, "TestObj", "strictTypeCheckingAttribute");
+        return throwGetterTypeError(*exec, "TestObj", "strictTypeCheckingAttribute");
+    }
+    auto& impl = castedThis->impl();
+    JSValue result = toJS(exec, castedThis->globalObject(), WTF::getPtr(impl.strictTypeCheckingAttribute()));
     return JSValue::encode(result);
 }
 
@@ -2451,6 +2471,30 @@ void setJSTestObjStringAttrWithSetterException(ExecState* exec, JSObject* baseOb
         return;
     impl.setStringAttrWithSetterException(nativeValue, ec);
     setDOMException(exec, ec);
+}
+
+
+void setJSTestObjStrictTypeCheckingAttribute(ExecState* exec, JSObject* baseObject, EncodedJSValue thisValue, EncodedJSValue encodedValue)
+{
+    JSValue value = JSValue::decode(encodedValue);
+    UNUSED_PARAM(baseObject);
+    JSTestObj* castedThis = jsDynamicCast<JSTestObj*>(JSValue::decode(thisValue));
+    if (UNLIKELY(!castedThis)) {
+        if (jsDynamicCast<JSTestObjPrototype*>(JSValue::decode(thisValue)))
+            reportDeprecatedSetterError(*exec, "TestObj", "strictTypeCheckingAttribute");
+        else
+            throwSetterTypeError(*exec, "TestObj", "strictTypeCheckingAttribute");
+        return;
+    }
+    auto& impl = castedThis->impl();
+    if (UNLIKELY(!value.isUndefinedOrNull() && !value.inherits(JSTestObj::info()))) {
+        throwAttributeTypeError(*exec, "TestObj", "strictTypeCheckingAttribute", "TestObj");
+        return;
+    };
+    TestObj* nativeValue = JSTestObj::toWrapped(value);
+    if (UNLIKELY(exec->hadException()))
+        return;
+    impl.setStrictTypeCheckingAttribute(nativeValue);
 }
 
 

Source/WebCore/bindings/scripts/test/ObjC/DOMTestObj.h
@@ -88,6 +88,7 @@ WEBCORE_EXPORT @interface DOMTestObj : DOMObject
 @property int attrWithSetterException;
 @property (copy) NSString *stringAttrWithGetterException;
 @property (copy) NSString *stringAttrWithSetterException;
+@property (strong) DOMTestObj *strictTypeCheckingAttribute;
 @property int customAttr;
 @property int withScriptStateAttribute;
 @property (strong) DOMTestObj *withScriptExecutionContextAttribute;

Source/WebCore/bindings/scripts/test/ObjC/DOMTestObj.mm
@@ -424,6 +424,20 @@
     WebCore::raiseOnDOMError(ec);
 }
 
+- (DOMTestObj *)strictTypeCheckingAttribute
+{
+    WebCore::JSMainThreadNullState state;
+    return kit(WTF::getPtr(IMPL->strictTypeCheckingAttribute()));
+}
+
+- (void)setStrictTypeCheckingAttribute:(DOMTestObj *)newStrictTypeCheckingAttribute
+{
+    WebCore::JSMainThreadNullState state;
+    ASSERT(newStrictTypeCheckingAttribute);
+
+    IMPL->setStrictTypeCheckingAttribute(core(newStrictTypeCheckingAttribute));
+}
+
 - (int)customAttr
 {
     WebCore::JSMainThreadNullState state;

Source/WebCore/bindings/scripts/test/TestObj.idl
@@ -104,6 +104,9 @@ enum _optional { "", "OptionalValue1", "OptionalValue2", "OptionalValue3" };
     [GetterRaisesException] attribute DOMString stringAttrWithGetterException;
     [SetterRaisesException] attribute DOMString stringAttrWithSetterException;
 
+    // Strict type checking.
+    [StrictTypeChecking] attribute TestObj strictTypeCheckingAttribute;
+
     // 'Custom' extended attribute
     [Custom] attribute long            customAttr;
     [Custom] void customMethod();