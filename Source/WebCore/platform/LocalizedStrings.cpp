fberlakovich/r2c-webkit

commit 46b9db0655706ffa440ab57d0a82fe6306efbcd7

Exit fullscreen button in fullscreen media playback needs an accessibility string https://bugs.webkit.org/show_bug.cgi?id=161380
Reviewed by Beth Dakin.

Adds an accessibility string for the exit fullscreen button.

* English.lproj/Localizable.strings:
* platform/LocalizedStrings.cpp:
(WebCore::exitFullScreenButtonAccessibilityTitle):
* platform/LocalizedStrings.h:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@205184 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/LocalizedStrings.cpp
@@ -1206,6 +1206,11 @@ String insertListTypeNumberedAccessibilityTitle()
 {
     return WEB_UI_STRING("Numbered list", "Option in segmented control for inserting a numbered list in text editing");
 }
+
+String exitFullScreenButtonAccessibilityTitle()
+{
+    return WEB_UI_STRING("Exit fullscreen", "Button for exiting fullscreen when in fullscreen media playback");
+}
 #endif // PLATFORM(MAC)
 
 } // namespace WebCore

Source/WebCore/English.lproj/Localizable.strings
@@ -133,6 +133,9 @@
 /* Option in segmented control for inserting a bulleted list in text editing */
 "Bulleted list" = "Bulleted list";
 
+/* Exit fullscreen button in fullscreen media controls */
+"Exit Fullscreen" = "Exit Fullscreen";
+
 /* menu item title for phone number */
 "Call Using iPhone:" = "Call Using iPhone:";
 

Source/WebCore/platform/LocalizedStrings.h
@@ -286,6 +286,7 @@ namespace WebCore {
     WEBCORE_EXPORT String insertListTypeBulletedAccessibilityTitle();
     WEBCORE_EXPORT String insertListTypeNumbered();
     WEBCORE_EXPORT String insertListTypeNumberedAccessibilityTitle();
+    WEBCORE_EXPORT String exitFullScreenButtonAccessibilityTitle();
 #endif
 
 #define WEB_UI_STRING(string, description) WebCore::localizedString(string)

commit 17c163a435030a09be9ecbba3fcbb33548927b6f

Fix a typo introduced in r205184. https://bugs.webkit.org/show_bug.cgi?id=161380
Reviewed by Tim Horton.

Fixes a typo in an accessibility string: "Exit fullscreen" => "Exit Fullscreen"

* platform/LocalizedStrings.cpp:
(WebCore::exitFullScreenButtonAccessibilityTitle):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@205193 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/LocalizedStrings.cpp
@@ -1209,7 +1209,7 @@ String insertListTypeNumberedAccessibilityTitle()
 
 String exitFullScreenButtonAccessibilityTitle()
 {
-    return WEB_UI_STRING("Exit fullscreen", "Button for exiting fullscreen when in fullscreen media playback");
+    return WEB_UI_STRING("Exit Fullscreen", "Button for exiting fullscreen when in fullscreen media playback");
 }
 #endif // PLATFORM(MAC)
 