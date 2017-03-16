fberlakovich/r2c-webkit

commit b3b98fa31c3adf510cf24e14887584a70395d207

Make the VM Traps mechanism non-polling for the DFG and FTL. https://bugs.webkit.org/show_bug.cgi?id=168920 <rdar://problem/30738588>
Reviewed by Filip Pizlo.

Source/JavaScriptCore:

1. Added a ENABLE(SIGNAL_BASED_VM_TRAPS) configuration in Platform.h.
   This is currently only enabled for OS(DARWIN) and ENABLE(JIT). 
2. Added assembler functions for overwriting an instruction with a breakpoint.
3. Added a new JettisonDueToVMTraps jettison reason.
4. Added CodeBlock and DFG::CommonData utility functions for over-writing
   invalidation points with breakpoint instructions.
5. The BytecodeGenerator now emits the op_check_traps bytecode unconditionally.
6. Remove the JSC_alwaysCheckTraps option because of (4) above.
   For ports that don't ENABLE(SIGNAL_BASED_VM_TRAPS), we'll force
   Options::usePollingTraps() to always be true.  This makes the VMTraps
   implementation fall back to using polling based traps only.

7. Make VMTraps support signal based traps.

Some design and implementation details of signal based VM traps:

- The implementation makes use of 2 signal handlers for SIGUSR1 and SIGTRAP.

- VMTraps::fireTrap() will set the flag for the requested trap and instantiate
  a SignalSender.  The SignalSender will send SIGUSR1 to the mutator thread that
  we want to trap, and check for the occurence of one of the following events:

  a. VMTraps::handleTraps() has been called for the requested trap, or

  b. the VM is inactive and is no longer executing any JS code.  We determine
     this to be the case if the thread no longer owns the JSLock and the VM's
     entryScope is null.

     Note: the thread can relinquish the JSLock while the VM's entryScope is not
     null.  This happens when the thread calls JSLock::dropAllLocks() before
     calling a host function that may block on IO (or whatever).  For our purpose,
     this counts as the VM still running JS code, and VM::fireTrap() will still
     be waiting.

Source/JavaScriptCore/assembler/MIPSAssembler.h
@@ -915,6 +915,15 @@ public:
         cacheFlush(insn, codeSize);
     }
 
+    static void replaceWithBkpt(void* instructionStart)
+    {
+        ASSERT(!(bitwise_cast<uintptr_t>(instructionStart) & 3));
+        MIPSWord* insn = reinterpret_cast<MIPSWord*>(reinterpret_cast<intptr_t>(code));
+        int value = 512; /* BRK_BUG */
+        insn[0] = (0x0000000d | ((value & 0x3ff) << OP_SH_CODE));
+        cacheFlush(instructionStart, sizeof(MIPSWord));
+    }
+
     static void replaceWithJump(void* instructionStart, void* to)
     {
         ASSERT(!(bitwise_cast<uintptr_t>(instructionStart) & 3));

Source/JavaScriptCore/assembler/ARM64Assembler.h
@@ -2536,6 +2536,13 @@ public:
         linkPointer(addressOf(code, where), valuePtr);
     }
 
+    static void replaceWithBrk(void* where)
+    {
+        int insn = excepnGeneration(ExcepnOp_BREAKPOINT, 0, 0);
+        performJITMemcpy(where, &insn, sizeof(int));
+        cacheFlush(where, sizeof(int));
+    }
+
     static void replaceWithJump(void* where, void* to)
     {
         intptr_t offset = (reinterpret_cast<intptr_t>(to) - reinterpret_cast<intptr_t>(where)) >> 2;

Source/JavaScriptCore/assembler/ARMAssembler.h
@@ -995,6 +995,13 @@ namespace JSC {
             return reinterpret_cast<void*>(readPointer(reinterpret_cast<void*>(getAbsoluteJumpAddress(from))));
         }
 
+        static void replaceWithBrk(void* instructionStart)
+        {
+            ARMWord* instruction = reinterpret_cast<ARMWord*>(instructionStart);
+            instruction[0] = BKPT;
+            cacheFlush(instruction, sizeof(ARMWord));
+        }
+
         static void replaceWithJump(void* instructionStart, void* to)
         {
             ARMWord* instruction = reinterpret_cast<ARMWord*>(instructionStart);

Source/JavaScriptCore/assembler/ARMv7Assembler.h
@@ -2327,7 +2327,17 @@ public:
     {
         return reinterpret_cast<void*>(readInt32(where));
     }
-    
+
+    static void replaceWithBkpt(void* instructionStart)
+    {
+        ASSERT(!(bitwise_cast<uintptr_t>(instructionStart) & 1));
+
+        uint16_t* ptr = reinterpret_cast<uint16_t*>(instructionStart);
+        uint16_t instructions = OP_BKPT;
+        performJITMemcpy(ptr, &instructions, sizeof(uint16_t));
+        cacheFlush(ptr, sizeof(uint16_t));
+    }
+
     static void replaceWithJump(void* instructionStart, void* to)
     {
         ASSERT(!(bitwise_cast<uintptr_t>(instructionStart) & 1));

Source/JavaScriptCore/assembler/MacroAssemblerARM.h
@@ -1482,6 +1482,11 @@ public:
         return FunctionPtr(reinterpret_cast<void(*)()>(ARMAssembler::readCallTarget(call.dataLocation())));
     }
 
+    static void replaceWithJump(CodeLocationLabel instructionStart)
+    {
+        ARMAssembler::replaceWithBkpt(instructionStart.executableAddress());
+    }
+
     static void replaceWithJump(CodeLocationLabel instructionStart, CodeLocationLabel destination)
     {
         ARMAssembler::replaceWithJump(instructionStart.dataLocation(), destination.dataLocation());

Source/JavaScriptCore/assembler/MacroAssemblerARM64.h
@@ -3406,6 +3406,11 @@ public:
         return FunctionPtr(reinterpret_cast<void(*)()>(ARM64Assembler::readCallTarget(call.dataLocation())));
     }
 
+    static void replaceWithBreakpoint(CodeLocationLabel instructionStart)
+    {
+        ARM64Assembler::replaceWithBrk(instructionStart.executableAddress());
+    }
+
     static void replaceWithJump(CodeLocationLabel instructionStart, CodeLocationLabel destination)
     {
         ARM64Assembler::replaceWithJump(instructionStart.dataLocation(), destination.dataLocation());

Source/JavaScriptCore/assembler/MacroAssemblerARMv7.h
@@ -1349,6 +1349,11 @@ public:
         m_assembler.dmbISHST();
     }
     
+    static void replaceWithBreakpoint(CodeLocationLabel instructionStart)
+    {
+        ARMv7Assembler::replaceWithBkpt(instructionStart.dataLocation());
+    }
+
     static void replaceWithJump(CodeLocationLabel instructionStart, CodeLocationLabel destination)
     {
         ARMv7Assembler::replaceWithJump(instructionStart.dataLocation(), destination.dataLocation());

Source/JavaScriptCore/assembler/MacroAssemblerMIPS.h
@@ -2978,6 +2978,11 @@ public:
         return FunctionPtr(reinterpret_cast<void(*)()>(MIPSAssembler::readCallTarget(call.dataLocation())));
     }
 
+    static void replaceWithJump(CodeLocationLabel instructionStart)
+    {
+        MIPSAssembler::replaceWithBkpt(instructionStart.executableAddress());
+    }
+
     static void replaceWithJump(CodeLocationLabel instructionStart, CodeLocationLabel destination)
     {
         MIPSAssembler::replaceWithJump(instructionStart.dataLocation(), destination.dataLocation());

commit 186824bd7d8438090ff3ee9ef7a35d8c13129ba1

[jsc][mips] Fix compilation error introduced in r213652 https://bugs.webkit.org/show_bug.cgi?id=169723
Patch by Guillaume Emont <guijemont@igalia.com> on 2017-03-15
Reviewed by Mark Lam.

The new replaceWithBkpt() contains a lapsus in it
(s/code/instructionStart) and won't compile.

* assembler/MIPSAssembler.h:
(JSC::MIPSAssembler::replaceWithBkpt):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@214022 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/assembler/MIPSAssembler.h
@@ -931,7 +931,7 @@ public:
     static void replaceWithBkpt(void* instructionStart)
     {
         ASSERT(!(bitwise_cast<uintptr_t>(instructionStart) & 3));
-        MIPSWord* insn = reinterpret_cast<MIPSWord*>(reinterpret_cast<intptr_t>(code));
+        MIPSWord* insn = reinterpret_cast<MIPSWord*>(reinterpret_cast<intptr_t>(instructionStart));
         int value = 512; /* BRK_BUG */
         insn[0] = (0x0000000d | ((value & 0x3ff) << OP_SH_CODE));
         cacheFlush(instructionStart, sizeof(MIPSWord));