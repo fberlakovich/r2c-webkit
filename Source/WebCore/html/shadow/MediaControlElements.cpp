fberlakovich/r2c-webkit

commit 0c81fd67ccf30240d278069813d07923feab604a

Hide inline captions in fullscreen. Remove fullscreen captions when no longer needed. https://bugs.webkit.org/show_bug.cgi?id=173482
Patch by Jeremy Jones <jeremyj@apple.com> on 2017-06-16
Reviewed by Eric Carlson.

Hide the inline captions while they are presenting to fullscreen.
Remove the text track representation as soon as it is no longer needed.
This allows the text track to render properly when returning to inline.

* html/shadow/MediaControlElements.cpp:
(WebCore::MediaControlTextTrackContainerElement::updateTextTrackRepresentation):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@218403 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/shadow/MediaControlElements.cpp
@@ -1280,8 +1280,13 @@ void MediaControlTextTrackContainerElement::updateTextTrackRepresentation()
     if (!mediaElement)
         return;
 
-    if (!mediaElement->requiresTextTrackRepresentation())
+    if (!mediaElement->requiresTextTrackRepresentation()) {
+        if (m_textTrackRepresentation) {
+            clearTextTrackRepresentation();
+            updateSizes(true);
+        }
         return;
+    }
 
     if (!m_textTrackRepresentation) {
         m_textTrackRepresentation = TextTrackRepresentation::create(*this);
@@ -1289,6 +1294,7 @@ void MediaControlTextTrackContainerElement::updateTextTrackRepresentation()
         mediaElement->setTextTrackRepresentation(m_textTrackRepresentation.get());
     }
 
+    hide();
     m_textTrackRepresentation->update();
     updateStyleForTextTrackRepresentation();
 }

commit 069fc9a61bfec3ef510dc534430d6b92d0ae94f8

Captions and subtitles not showing up in picture-in-picture for MSE content https://bugs.webkit.org/show_bug.cgi?id=174317 rdar://problem/33188591
Patch by Jeremy Jones <jeremyj@apple.com> on 2017-07-10
Reviewed by Eric Carlson.

Reverts a regression created by r218403.

* html/shadow/MediaControlElements.cpp:
(WebCore::MediaControlTextTrackContainerElement::updateTextTrackRepresentation):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@219307 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/shadow/MediaControlElements.cpp
@@ -1296,7 +1296,6 @@ void MediaControlTextTrackContainerElement::updateTextTrackRepresentation()
         mediaElement->setTextTrackRepresentation(m_textTrackRepresentation.get());
     }
 
-    hide();
     m_textTrackRepresentation->update();
     updateStyleForTextTrackRepresentation();
 }