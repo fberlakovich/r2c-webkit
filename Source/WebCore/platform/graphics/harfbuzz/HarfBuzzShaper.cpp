fberlakovich/r2c-webkit

commit fe9345d1f1a5a3a4a14f089243d8cc02eed02e41

[HarfBuzz] Wrong offset returned by HarfBuzzShaper::offsetForPosition in some cases https://bugs.webkit.org/show_bug.cgi?id=176848
Reviewed by Michael Catanzaro.

Source/WebCore:

This patch rewrites HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition() to make it simpler and ensure we
return the right offset in all the cases, also honoring now the includePartialGlyphs parameter that we were
ignoring in FontCascade::offsetForPositionForComplexText().

Fixes several tests that started to fail after r221909.

* platform/graphics/cairo/FontCairoHarfbuzzNG.cpp:
(WebCore::FontCascade::offsetForPositionForComplexText const):
* platform/graphics/harfbuzz/HarfBuzzShaper.cpp:
(WebCore::HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition):
(WebCore::HarfBuzzShaper::offsetForPosition):
* platform/graphics/harfbuzz/HarfBuzzShaper.h:

LayoutTests:

Rebaseline several tests where we are now getting results similar to the mac ones.

* platform/gtk/editing/execCommand/findString-2-expected.txt:
* platform/gtk/editing/selection/14971-expected.png:
* platform/gtk/editing/selection/14971-expected.txt:
* platform/gtk/editing/selection/4895428-2-expected.png:
* platform/gtk/editing/selection/4895428-2-expected.txt:
* platform/gtk/editing/selection/5232159-expected.png:
* platform/gtk/editing/selection/5232159-expected.txt:
* platform/gtk/editing/selection/collapse-selection-in-bidi-expected.txt:
* platform/gtk/editing/selection/drag-select-1-expected.png:
* platform/gtk/editing/selection/drag-select-1-expected.txt:
* platform/gtk/editing/selection/editable-links-expected.png:
* platform/gtk/editing/selection/editable-links-expected.txt:
* platform/gtk/editing/selection/fake-doubleclick-expected.txt:
* platform/gtk/editing/selection/fake-drag-expected.txt:
* platform/gtk/editing/selection/select-across-readonly-input-1-expected.png:
* platform/gtk/editing/selection/select-across-readonly-input-1-expected.txt:

Source/WebCore/platform/graphics/harfbuzz/HarfBuzzShaper.cpp
@@ -101,37 +101,32 @@ void HarfBuzzShaper::HarfBuzzRun::setGlyphAndPositions(unsigned index, uint16_t 
     m_offsets[index] = FloatPoint(offsetX, offsetY);
 }
 
-unsigned HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition(float targetX)
+unsigned HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition(float targetX, bool includePartialGlyphs)
 {
     ASSERT(targetX <= m_width);
     float currentX = 0;
-    float currentAdvance = m_advances[0];
     unsigned glyphIndex = 0;
+    float characterWidth = 0;
+    unsigned characterIndex = 0;
+    do {
+        characterIndex = m_glyphToCharacterIndexes[glyphIndex];
+        characterWidth = m_advances[glyphIndex];
+        while (glyphIndex < m_numGlyphs - 1 && m_glyphToCharacterIndexes[glyphIndex + 1] == characterIndex)
+            characterWidth += m_advances[++glyphIndex];
 
-    // Sum up advances that belong to a character.
-    while (glyphIndex < m_numGlyphs - 1 && m_glyphToCharacterIndexes[glyphIndex] == m_glyphToCharacterIndexes[glyphIndex + 1])
-        currentAdvance += m_advances[++glyphIndex];
-    currentAdvance = currentAdvance / 2.0;
-    if (targetX <= currentAdvance)
-        return rtl() ? m_numCharacters : 0;
+        if ((includePartialGlyphs && (targetX < currentX + characterWidth / 2.0))
+            || (!includePartialGlyphs && (targetX < currentX + characterWidth)))
+            return rtl() ? std::min(m_numCharacters, characterIndex + 1) : characterIndex;
 
-    ++glyphIndex;
-    while (glyphIndex < m_numGlyphs) {
-        unsigned prevCharacterIndex = m_glyphToCharacterIndexes[glyphIndex - 1];
-        float prevAdvance = currentAdvance;
-        currentAdvance = m_advances[glyphIndex];
-        while (glyphIndex < m_numGlyphs - 1 && m_glyphToCharacterIndexes[glyphIndex] == m_glyphToCharacterIndexes[glyphIndex + 1])
-            currentAdvance += m_advances[++glyphIndex];
-        currentAdvance = currentAdvance / 2.0;
-        float nextX = currentX + prevAdvance + currentAdvance;
-        if (currentX <= targetX && targetX <= nextX)
-            return rtl() ? prevCharacterIndex : m_glyphToCharacterIndexes[glyphIndex];
-        currentX = nextX;
-        prevAdvance = currentAdvance;
+        if ((includePartialGlyphs && (targetX >= (currentX + characterWidth / 2.0) && targetX < currentX + characterWidth))
+            || (!includePartialGlyphs && (targetX >= currentX && targetX < currentX + characterWidth)))
+            break;
+
+        currentX += characterWidth;
         ++glyphIndex;
-    }
+    } while (glyphIndex < m_numGlyphs);
 
-    return rtl() ? 0 : m_numCharacters;
+    return rtl() ? characterIndex : std::min(m_numCharacters, characterIndex + 1);
 }
 
 float HarfBuzzShaper::HarfBuzzRun::xPositionForOffset(unsigned offset)
@@ -581,7 +576,7 @@ bool HarfBuzzShaper::fillGlyphBuffer(GlyphBuffer* glyphBuffer)
     return glyphBuffer->size();
 }
 
-int HarfBuzzShaper::offsetForPosition(float targetX)
+int HarfBuzzShaper::offsetForPosition(float targetX, bool includePartialGlyphs)
 {
     int charactersSoFar = 0;
     float currentX = 0;
@@ -594,7 +589,7 @@ int HarfBuzzShaper::offsetForPosition(float targetX)
             float offsetForRun = targetX - currentX;
             if (offsetForRun >= 0 && offsetForRun <= m_harfBuzzRuns[i]->width()) {
                 // The x value in question is within this script run.
-                const unsigned index = m_harfBuzzRuns[i]->characterIndexForXPosition(offsetForRun);
+                const unsigned index = m_harfBuzzRuns[i]->characterIndexForXPosition(offsetForRun, includePartialGlyphs);
                 return charactersSoFar + index;
             }
             currentX = nextX;
@@ -604,7 +599,7 @@ int HarfBuzzShaper::offsetForPosition(float targetX)
             float nextX = currentX + m_harfBuzzRuns[i]->width();
             float offsetForRun = targetX - currentX;
             if (offsetForRun >= 0 && offsetForRun <= m_harfBuzzRuns[i]->width()) {
-                const unsigned index = m_harfBuzzRuns[i]->characterIndexForXPosition(offsetForRun);
+                const unsigned index = m_harfBuzzRuns[i]->characterIndexForXPosition(offsetForRun, includePartialGlyphs);
                 return charactersSoFar + index;
             }
             charactersSoFar += m_harfBuzzRuns[i]->numCharacters();

LayoutTests/platform/gtk/editing/execCommand/findString-2-expected.txt
@@ -1,6 +1,6 @@
-EDITING DELEGATE: shouldChangeSelectedDOMRange:(null) toDOMRange:range from 2 of #text > SPAN > DIV > BODY > HTML > #document to 2 of #text > SPAN > DIV > BODY > HTML > #document affinity:NSSelectionAffinityDownstream stillSelecting:FALSE
+EDITING DELEGATE: shouldChangeSelectedDOMRange:(null) toDOMRange:range from 1 of #text > SPAN > DIV > BODY > HTML > #document to 1 of #text > SPAN > DIV > BODY > HTML > #document affinity:NSSelectionAffinityDownstream stillSelecting:FALSE
 EDITING DELEGATE: webViewDidChangeSelection:WebViewDidChangeSelectionNotification
-EDITING DELEGATE: shouldChangeSelectedDOMRange:range from 2 of #text > SPAN > DIV > BODY > HTML > #document to 2 of #text > SPAN > DIV > BODY > HTML > #document toDOMRange:range from 0 of #text > SPAN > DIV > BODY > HTML > #document to 3 of #text > SPAN > DIV > BODY > HTML > #document affinity:NSSelectionAffinityDownstream stillSelecting:FALSE
+EDITING DELEGATE: shouldChangeSelectedDOMRange:range from 1 of #text > SPAN > DIV > BODY > HTML > #document to 1 of #text > SPAN > DIV > BODY > HTML > #document toDOMRange:range from 0 of #text > SPAN > DIV > BODY > HTML > #document to 3 of #text > SPAN > DIV > BODY > HTML > #document affinity:NSSelectionAffinityDownstream stillSelecting:FALSE
 EDITING DELEGATE: webViewDidChangeSelection:WebViewDidChangeSelectionNotification
 EDITING DELEGATE: webViewDidChangeSelection:WebViewDidChangeSelectionNotification
 layer at (0,0) size 800x600

LayoutTests/platform/gtk/editing/selection/14971-expected.txt
@@ -18,4 +18,4 @@ layer at (0,0) size 800x600
           RenderText {#text} at (39,0) size 39x17
             text run at (39,0) width 39: "World"
 selection start: position 0 of child 0 {#text} of child 0 {SPAN} of child 2 {DIV} of body
-selection end:   position 3 of child 0 {#text} of child 2 {SPAN} of child 2 {DIV} of body
+selection end:   position 2 of child 0 {#text} of child 2 {SPAN} of child 2 {DIV} of body

LayoutTests/platform/gtk/editing/selection/4895428-2-expected.txt
@@ -25,4 +25,4 @@ layer at (0,0) size 800x600
           RenderListMarker at (-17,0) size 7x17: bullet
           RenderText {#text} at (0,0) size 50x17
             text run at (0,0) width 50: "Success"
-caret: position 5 of child 0 {#text} of child 1 {TD} of child 0 {TR} of child 0 {TBODY} of child 0 {TABLE} of child 2 {DIV} of body
+caret: position 4 of child 0 {#text} of child 1 {TD} of child 0 {TR} of child 0 {TBODY} of child 0 {TABLE} of child 2 {DIV} of body

LayoutTests/platform/gtk/editing/selection/5232159-expected.txt
@@ -24,4 +24,4 @@ layer at (0,60) size 800x90
         text run at (0,54) width 753: "natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque dapibus ante et nulla. Cras nec velit."
         text run at (0,72) width 643: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer ut tortor."
 selection start: position 3 of child 0 {#text} of child 1 {SPAN} of child 1 {DIV} of child 1 {DIV} of child 3 {DIV} of body
-selection end:   position 236 of child 2 {#text} of child 1 {DIV} of child 1 {DIV} of child 3 {DIV} of body
+selection end:   position 237 of child 2 {#text} of child 1 {DIV} of child 1 {DIV} of child 3 {DIV} of body

commit 2aeaf3b9f53b9b75f89887b15049bd9fec4615d9

REGRESSION(r221974): [Harfbuzz] Test fast/text/international/hebrew-selection.html is failing since r221974 https://bugs.webkit.org/show_bug.cgi?id=177036
Reviewed by Michael Catanzaro.

In r221974 I rewrote the characterIndexForXPosition implementation without taking into account that there can be
multiple glyphs for the same character, so we can't simply do index++ and index-- to get the next and previous
character index.

* platform/graphics/harfbuzz/HarfBuzzShaper.cpp:
(WebCore::HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition): Always get the character index from
m_glyphToCharacterIndexes array.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222141 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/harfbuzz/HarfBuzzShaper.cpp
@@ -108,6 +108,8 @@ unsigned HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition(float targetX, 
     unsigned glyphIndex = 0;
     float characterWidth = 0;
     unsigned characterIndex = 0;
+    unsigned previousCharacterIndex = m_numCharacters;
+
     do {
         characterIndex = m_glyphToCharacterIndexes[glyphIndex];
         characterWidth = m_advances[glyphIndex];
@@ -116,7 +118,7 @@ unsigned HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition(float targetX, 
 
         if ((includePartialGlyphs && (targetX <= currentX + characterWidth / 2.0))
             || (!includePartialGlyphs && (targetX < currentX + characterWidth)))
-            return rtl() ? std::min(m_numCharacters, characterIndex + 1) : characterIndex;
+            return rtl() ? previousCharacterIndex : characterIndex;
 
         if ((includePartialGlyphs && (targetX > (currentX + characterWidth / 2.0) && targetX < currentX + characterWidth))
             || (!includePartialGlyphs && (targetX >= currentX && targetX < currentX + characterWidth)))
@@ -124,9 +126,10 @@ unsigned HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition(float targetX, 
 
         currentX += characterWidth;
         ++glyphIndex;
+        previousCharacterIndex = characterIndex;
     } while (glyphIndex < m_numGlyphs);
 
-    return rtl() ? characterIndex : std::min(m_numCharacters, characterIndex + 1);
+    return rtl() ? characterIndex : glyphIndex < m_numGlyphs - 1 ? m_glyphToCharacterIndexes[glyphIndex + 1] : m_numCharacters;
 }
 
 float HarfBuzzShaper::HarfBuzzRun::xPositionForOffset(unsigned offset)