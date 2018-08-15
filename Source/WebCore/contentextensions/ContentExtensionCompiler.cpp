fberlakovich/r2c-webkit

commit e5235bcedfc2afab11762e25a9f9448e8db292ea

isValidCSSSelector is unsafe to be called from a non-main thread https://bugs.webkit.org/show_bug.cgi?id=188581 <rdar://problem/40517358>
Reviewed by Sam Weinig.

Source/WebCore:

Parsing and determining whether the css selectors are valid is fast enough to do before
hopping to the background thread for the slow NFA/DFA operations and writing to disk.
Doing it on the main thread avoids the thread safety issues in the CSSParser's use of strings.

* contentextensions/ContentExtensionCompiler.cpp:
(WebCore::ContentExtensions::compileRuleList):
* contentextensions/ContentExtensionCompiler.h:
* contentextensions/ContentExtensionParser.cpp:
(WebCore::ContentExtensions::isValidCSSSelector):
(WebCore::ContentExtensions::loadEncodedRules):
(WebCore::ContentExtensions::parseRuleList):
* contentextensions/ContentExtensionParser.h:
* contentextensions/ContentExtensionRule.cpp:
(WebCore::ContentExtensions::Trigger::isolatedCopy const):
(WebCore::ContentExtensions::Action::isolatedCopy const):
* contentextensions/ContentExtensionRule.h:
(WebCore::ContentExtensions::Trigger::isEmpty const):
(WebCore::ContentExtensions::Trigger::operator== const):
(WebCore::ContentExtensions::Action::Action):
(WebCore::ContentExtensions::ContentExtensionRule::isolatedCopy const):
(WebCore::ContentExtensions::ContentExtensionRule::operator== const):
(WebCore::ContentExtensions::vectorIsolatedCopy):

Source/WebKit:

* UIProcess/API/APIContentRuleListStore.cpp:
(API::compiledToFile):
(API::ContentRuleListStore::lookupContentRuleList):
(API::ContentRuleListStore::getAvailableContentRuleListIdentifiers):
(API::ContentRuleListStore::compileContentRuleList):
(API::ContentRuleListStore::removeContentRuleList):
(API::ContentRuleListStore::getContentRuleListSource):
* UIProcess/API/APIContentRuleListStore.h:
* UIProcess/API/Cocoa/WKContentRuleListStore.mm:

Source/WTF:

* wtf/Vector.h:
(WTF::minCapacity>::isolatedCopy):

Tools:

* TestWebKitAPI/Tests/WebCore/ContentExtensions.cpp:

Source/WebCore/contentextensions/ContentExtensionCompiler.cpp
@@ -283,12 +283,13 @@ static void compileToBytecode(CombinedURLFilters&& filters, UniversalActionSet&&
     LOG_LARGE_STRUCTURES(universalActions, universalActions.capacity() * sizeof(unsigned));
 }
 
-std::error_code compileRuleList(ContentExtensionCompilationClient& client, String&& ruleJSON)
+std::error_code compileRuleList(ContentExtensionCompilationClient& client, String&& ruleJSON, Vector<ContentExtensionRule>&& parsedRuleList)
 {
-    auto ruleList = parseRuleList(WTFMove(ruleJSON));
-    if (!ruleList.has_value())
-        return ruleList.error();
-    Vector<ContentExtensionRule> parsedRuleList = WTFMove(ruleList.value());
+#if !ASSERT_DISABLED
+    callOnMainThread([ruleJSON = ruleJSON.isolatedCopy(), parsedRuleList = parsedRuleList.isolatedCopy()] {
+        ASSERT(parseRuleList(ruleJSON) == parsedRuleList);
+    });
+#endif
 
     bool domainConditionSeen = false;
     bool topURLConditionSeen = false;
@@ -313,7 +314,7 @@ std::error_code compileRuleList(ContentExtensionCompilationClient& client, Strin
     MonotonicTime patternPartitioningStart = MonotonicTime::now();
 #endif
     
-    client.writeSource(ruleJSON);
+    client.writeSource(std::exchange(ruleJSON, String()));
 
     Vector<SerializedActionByte> actions;
     Vector<unsigned> actionLocations = serializeActions(parsedRuleList, actions);

Source/WTF/wtf/Vector.h
@@ -766,6 +766,8 @@ public:
 
     void clear() { shrinkCapacity(0); }
 
+    template<typename U = T> Vector<U> isolatedCopy() const;
+
     ALWAYS_INLINE void append(ValueType&& value) { append<ValueType>(std::forward<ValueType>(value)); }
     template<typename U> void append(U&&);
     template<typename... Args> void constructAndAppend(Args&&...);
@@ -1604,6 +1606,17 @@ template<typename T> struct ValueCheck<Vector<T>> {
 };
 #endif
 
+template<typename T, size_t inlineCapacity, typename OverflowHandler, size_t minCapacity>
+template<typename U>
+inline Vector<U> Vector<T, inlineCapacity, OverflowHandler, minCapacity>::isolatedCopy() const
+{
+    Vector<U> copy;
+    copy.reserveInitialCapacity(size());
+    for (const auto& element : *this)
+        copy.uncheckedAppend(element.isolatedCopy());
+    return copy;
+}
+    
 template<typename VectorType, typename Func>
 size_t removeRepeatedElements(VectorType& vector, const Func& func)
 {

Source/WebCore/contentextensions/ContentExtensionCompiler.h
@@ -40,7 +40,7 @@ public:
     virtual ~ContentExtensionCompilationClient() = default;
     
     // Functions should be called in this order. All except writeActions and finalize can be called multiple times, though.
-    virtual void writeSource(const String&) = 0;
+    virtual void writeSource(String&&) = 0;
     virtual void writeActions(Vector<SerializedActionByte>&&, bool conditionsApplyOnlyToDomain) = 0;
     virtual void writeFiltersWithoutConditionsBytecode(Vector<DFABytecode>&&) = 0;
     virtual void writeFiltersWithConditionsBytecode(Vector<DFABytecode>&&) = 0;
@@ -48,7 +48,7 @@ public:
     virtual void finalize() = 0;
 };
 
-WEBCORE_EXPORT std::error_code compileRuleList(ContentExtensionCompilationClient&, String&&);
+WEBCORE_EXPORT std::error_code compileRuleList(ContentExtensionCompilationClient&, String&& ruleJSON, Vector<ContentExtensionRule>&&);
 
 } // namespace ContentExtensions
 } // namespace WebCore

Source/WebCore/contentextensions/ContentExtensionParser.cpp
@@ -230,6 +230,7 @@ static Expected<Trigger, std::error_code> loadTrigger(ExecState& exec, const JSO
 
 bool isValidCSSSelector(const String& selector)
 {
+    ASSERT(isMainThread());
     AtomicString::init();
     QualifiedName::init();
     CSSParserContext context(HTMLQuirksMode);
@@ -299,7 +300,7 @@ static Expected<std::optional<ContentExtensionRule>, std::error_code> loadRule(E
     return { std::nullopt };
 }
 
-static Expected<Vector<ContentExtensionRule>, std::error_code> loadEncodedRules(ExecState& exec, String&& ruleJSON)
+static Expected<Vector<ContentExtensionRule>, std::error_code> loadEncodedRules(ExecState& exec, const String& ruleJSON)
 {
     VM& vm = exec.vm();
     auto scope = DECLARE_THROW_SCOPE(vm);
@@ -347,7 +348,7 @@ static Expected<Vector<ContentExtensionRule>, std::error_code> loadEncodedRules(
     return WTFMove(ruleList);
 }
 
-Expected<Vector<ContentExtensionRule>, std::error_code> parseRuleList(String&& ruleJSON)
+Expected<Vector<ContentExtensionRule>, std::error_code> parseRuleList(const String& ruleJSON)
 {
 #if CONTENT_EXTENSIONS_PERFORMANCE_REPORTING
     MonotonicTime loadExtensionStartTime = MonotonicTime::now();
@@ -358,7 +359,7 @@ Expected<Vector<ContentExtensionRule>, std::error_code> parseRuleList(String&& r
     JSGlobalObject* globalObject = JSGlobalObject::create(*vm, JSGlobalObject::createStructure(*vm, jsNull()));
 
     ExecState* exec = globalObject->globalExec();
-    auto ruleList = loadEncodedRules(*exec, WTFMove(ruleJSON));
+    auto ruleList = loadEncodedRules(*exec, ruleJSON);
 
     vm = nullptr;
 

Source/WebCore/contentextensions/ContentExtensionParser.h
@@ -38,7 +38,7 @@ namespace ContentExtensions {
 
 class ContentExtensionRule;
 
-Expected<Vector<ContentExtensionRule>, std::error_code> parseRuleList(String&&);
+WEBCORE_EXPORT Expected<Vector<ContentExtensionRule>, std::error_code> parseRuleList(const String&);
 WEBCORE_EXPORT bool isValidCSSSelector(const String&);
 
 } // namespace ContentExtensions

Source/WebCore/contentextensions/ContentExtensionRule.cpp
@@ -113,6 +113,28 @@ uint32_t Action::serializedLength(const SerializedActionByte* actions, const uin
     RELEASE_ASSERT_NOT_REACHED();
 }
 
+Trigger Trigger::isolatedCopy() const
+{
+    return {
+        urlFilter.isolatedCopy(),
+        urlFilterIsCaseSensitive,
+        topURLConditionIsCaseSensitive,
+        flags,
+        conditions.isolatedCopy(),
+        conditionType
+    };
+}
+
+Action Action::isolatedCopy() const
+{
+    return {
+        m_extensionIdentifier.isolatedCopy(),
+        m_type,
+        m_actionID,
+        m_stringArgument.isolatedCopy()
+    };
+}
+
 } // namespace ContentExtensions
 
 } // namespace WebCore

Source/WebCore/contentextensions/ContentExtensionRule.h
@@ -55,6 +55,8 @@ struct Trigger {
     };
     ConditionType conditionType { ConditionType::None };
 
+    WEBCORE_EXPORT Trigger isolatedCopy() const;
+    
     ~Trigger()
     {
         ASSERT(conditions.isEmpty() == (conditionType == ConditionType::None));
@@ -66,6 +68,7 @@ struct Trigger {
     {
         return urlFilter.isEmpty()
             && !urlFilterIsCaseSensitive
+            && !topURLConditionIsCaseSensitive
             && !flags
             && conditions.isEmpty()
             && conditionType == ConditionType::None;
@@ -75,6 +78,7 @@ struct Trigger {
     {
         return urlFilter == other.urlFilter
             && urlFilterIsCaseSensitive == other.urlFilterIsCaseSensitive
+            && topURLConditionIsCaseSensitive == other.topURLConditionIsCaseSensitive
             && flags == other.flags
             && conditions == other.conditions
             && conditionType == other.conditionType;
@@ -163,7 +167,16 @@ struct Action {
     uint32_t actionID() const { return m_actionID; }
     const String& stringArgument() const { return m_stringArgument; }
 
+    WEBCORE_EXPORT Action isolatedCopy() const;
+    
 private:
+    Action(String&& extensionIdentifier, ActionType type, uint32_t actionID, String&& stringArgument)
+        : m_extensionIdentifier(WTFMove(extensionIdentifier))
+        , m_type(type)
+        , m_actionID(actionID)
+        , m_stringArgument(WTFMove(stringArgument))
+    { }
+
     String m_extensionIdentifier;
     ActionType m_type;
     uint32_t m_actionID;
@@ -172,10 +185,19 @@ private:
     
 class ContentExtensionRule {
 public:
-    ContentExtensionRule(Trigger&&, Action&&);
+    WEBCORE_EXPORT ContentExtensionRule(Trigger&&, Action&&);
 
     const Trigger& trigger() const { return m_trigger; }
     const Action& action() const { return m_action; }
+
+    ContentExtensionRule isolatedCopy() const
+    {
+        return { m_trigger.isolatedCopy(), m_action.isolatedCopy() };
+    }
+    bool operator==(const ContentExtensionRule& other) const
+    {
+        return m_trigger == other.m_trigger && m_action == other.m_action;
+    }
 
 private:
     Trigger m_trigger;

Source/WebKit/UIProcess/API/APIContentRuleListStore.cpp
@@ -35,8 +35,10 @@
 #include "WebCompiledContentRuleList.h"
 #include <WebCore/ContentExtensionCompiler.h>
 #include <WebCore/ContentExtensionError.h>
+#include <WebCore/ContentExtensionParser.h>
 #include <WebCore/QualifiedName.h>
 #include <string>
+#include <wtf/CompletionHandler.h>
 #include <wtf/NeverDestroyed.h>
 #include <wtf/RunLoop.h>
 #include <wtf/WorkQueue.h>
@@ -209,7 +211,7 @@ static bool writeDataToFile(const Data& fileData, PlatformFileHandle fd)
     return success;
 }
 
-static std::error_code compiledToFile(String&& json, const String& finalFilePath, ContentRuleListMetaData& metaData, Data& mappedData)
+static std::error_code compiledToFile(String&& json, Vector<WebCore::ContentExtensions::ContentExtensionRule>&& parsedRules, const String& finalFilePath, ContentRuleListMetaData& metaData, Data& mappedData)
 {
     using namespace WebCore::ContentExtensions;
 
@@ -227,7 +229,8 @@ static std::error_code compiledToFile(String&& json, const String& finalFilePath
             ASSERT(!metaData.conditionsApplyOnlyToDomain);
         }
         
-        void writeSource(const String& sourceJSON) final {
+        void writeSource(String&& sourceJSON) final
+        {
             ASSERT(!m_filtersWithoutConditionsBytecodeWritten);
             ASSERT(!m_filtersWithConditionBytecodeWritten);
             ASSERT(!m_conditionFiltersBytecodeWritten);
@@ -339,7 +342,7 @@ static std::error_code compiledToFile(String&& json, const String& finalFilePath
 
     CompilationClient compilationClient(temporaryFileHandle, metaData);
     
-    if (auto compilerError = compileRuleList(compilationClient, WTFMove(json))) {
+    if (auto compilerError = compileRuleList(compilationClient, WTFMove(json), WTFMove(parsedRules))) {
         WTFLogAlways("Content Rule List compiling failed: Compiling failed.");
         closeFile(temporaryFileHandle);
         return compilerError;
@@ -391,7 +394,7 @@ static Ref<API::ContentRuleList> createExtension(const String& identifier, const
     return API::ContentRuleList::create(identifier, WTFMove(compiledContentRuleList));
 }
 
-void ContentRuleListStore::lookupContentRuleList(const WTF::String& identifier, Function<void(RefPtr<API::ContentRuleList>, std::error_code)> completionHandler)
+void ContentRuleListStore::lookupContentRuleList(const WTF::String& identifier, CompletionHandler<void(RefPtr<API::ContentRuleList>, std::error_code)> completionHandler)
 {
     m_readQueue->dispatch([protectedThis = makeRef(*this), identifier = identifier.isolatedCopy(), storePath = m_storePath.isolatedCopy(), legacyFilename = m_legacyFilename, completionHandler = WTFMove(completionHandler)]() mutable {
         auto path = constructedPath(storePath, identifier, legacyFilename);
@@ -399,26 +402,26 @@ void ContentRuleListStore::lookupContentRuleList(const WTF::String& identifier, 
         ContentRuleListMetaData metaData;
         Data fileData;
         if (!openAndMapContentRuleList(path, metaData, fileData)) {
-            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)] {
+            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)] () mutable {
                 completionHandler(nullptr, Error::LookupFailed);
             });
             return;
         }
         
         if (metaData.version != ContentRuleListStore::CurrentContentRuleListFileVersion) {
-            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)] {
+            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)] () mutable {
                 completionHandler(nullptr, Error::VersionMismatch);
             });
             return;
         }
         
-        RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), identifier = identifier.isolatedCopy(), fileData = WTFMove(fileData), metaData = WTFMove(metaData), completionHandler = WTFMove(completionHandler)] {
+        RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), identifier = identifier.isolatedCopy(), fileData = WTFMove(fileData), metaData = WTFMove(metaData), completionHandler = WTFMove(completionHandler)] () mutable {
             completionHandler(createExtension(identifier, metaData, fileData), { });
         });
     });
 }
 
-void ContentRuleListStore::getAvailableContentRuleListIdentifiers(Function<void(WTF::Vector<WTF::String>)> completionHandler)
+void ContentRuleListStore::getAvailableContentRuleListIdentifiers(CompletionHandler<void(WTF::Vector<WTF::String>)> completionHandler)
 {
     m_readQueue->dispatch([protectedThis = makeRef(*this), storePath = m_storePath.isolatedCopy(), legacyFilename = m_legacyFilename, completionHandler = WTFMove(completionHandler)]() mutable {
 
@@ -435,43 +438,48 @@ void ContentRuleListStore::getAvailableContentRuleListIdentifiers(Function<void(
     });
 }
 
-void ContentRuleListStore::compileContentRuleList(const WTF::String& identifier, WTF::String&& json, Function<void(RefPtr<API::ContentRuleList>, std::error_code)> completionHandler)
+void ContentRuleListStore::compileContentRuleList(const WTF::String& identifier, WTF::String&& json, CompletionHandler<void(RefPtr<API::ContentRuleList>, std::error_code)> completionHandler)
 {
     AtomicString::init();
     WebCore::QualifiedName::init();
-    m_compileQueue->dispatch([protectedThis = makeRef(*this), identifier = identifier.isolatedCopy(), legacyFilename = m_legacyFilename, json = json.isolatedCopy(), storePath = m_storePath.isolatedCopy(), completionHandler = WTFMove(completionHandler)] () mutable {
+    
+    auto parsedRules = WebCore::ContentExtensions::parseRuleList(json);
+    if (!parsedRules.has_value())
+        return completionHandler(nullptr, parsedRules.error());
+    
+    m_compileQueue->dispatch([protectedThis = makeRef(*this), identifier = identifier.isolatedCopy(), legacyFilename = m_legacyFilename, json = json.isolatedCopy(), parsedRules = parsedRules.value().isolatedCopy(), storePath = m_storePath.isolatedCopy(), completionHandler = WTFMove(completionHandler)] () mutable {
         auto path = constructedPath(storePath, identifier, legacyFilename);
 
         ContentRuleListMetaData metaData;
         Data fileData;
-        auto error = compiledToFile(WTFMove(json), path, metaData, fileData);
+        auto error = compiledToFile(WTFMove(json), WTFMove(parsedRules), path, metaData, fileData);
         if (error) {
-            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), error = WTFMove(error), completionHandler = WTFMove(completionHandler)] {
+            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), error = WTFMove(error), completionHandler = WTFMove(completionHandler)] () mutable {
                 completionHandler(nullptr, error);
             });
             return;
         }
 
-        RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), identifier = WTFMove(identifier), fileData = WTFMove(fileData), metaData = WTFMove(metaData), completionHandler = WTFMove(completionHandler)] {
+        RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), identifier = WTFMove(identifier), fileData = WTFMove(fileData), metaData = WTFMove(metaData), completionHandler = WTFMove(completionHandler)] () mutable {
             RefPtr<API::ContentRuleList> contentRuleList = createExtension(identifier, metaData, fileData);
             completionHandler(contentRuleList, { });
         });
     });
 }
 
-void ContentRuleListStore::removeContentRuleList(const WTF::String& identifier, Function<void(std::error_code)> completionHandler)
+void ContentRuleListStore::removeContentRuleList(const WTF::String& identifier, CompletionHandler<void(std::error_code)> completionHandler)
 {
     m_removeQueue->dispatch([protectedThis = makeRef(*this), identifier = identifier.isolatedCopy(), storePath = m_storePath.isolatedCopy(), legacyFilename = m_legacyFilename, completionHandler = WTFMove(completionHandler)]() mutable {
         auto path = constructedPath(storePath, identifier, legacyFilename);
 
         if (!deleteFile(path)) {
-            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)] {
+            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)] () mutable {
                 completionHandler(Error::RemoveFailed);
             });
             return;
         }
 
-        RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)] {
+        RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)] () mutable {
             completionHandler({ });
         });
     });
@@ -494,13 +502,13 @@ void ContentRuleListStore::invalidateContentRuleListVersion(const WTF::String& i
     closeFile(file);
 }
 
-void ContentRuleListStore::getContentRuleListSource(const WTF::String& identifier, Function<void(WTF::String)> completionHandler)
+void ContentRuleListStore::getContentRuleListSource(const WTF::String& identifier, CompletionHandler<void(WTF::String)> completionHandler)
 {
     m_readQueue->dispatch([protectedThis = makeRef(*this), identifier = identifier.isolatedCopy(), storePath = m_storePath.isolatedCopy(), legacyFilename = m_legacyFilename, completionHandler = WTFMove(completionHandler)]() mutable {
         auto path = constructedPath(storePath, identifier, legacyFilename);
         
         auto complete = [protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler)](String source) mutable {
-            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler), source = source.isolatedCopy()] {
+            RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis), completionHandler = WTFMove(completionHandler), source = source.isolatedCopy()] () mutable {
                 completionHandler(source);
             });
         };

commit 11b9ff24fca2929d3cd0e01d91cb9bcf52cb26a7

Remove failing assertion introduced in r234873 https://bugs.webkit.org/show_bug.cgi?id=188581
* contentextensions/ContentExtensionCompiler.cpp:
(WebCore::ContentExtensions::compileRuleList):
The assertion is correct but failing because VectorTraits<String> is incorrect.
I'll re-add it and fix VectorTraits<String> in a separate patch.



git-svn-id: https://svn.webkit.org/repository/webkit/trunk@234890 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/contentextensions/ContentExtensionCompiler.cpp
@@ -285,12 +285,6 @@ static void compileToBytecode(CombinedURLFilters&& filters, UniversalActionSet&&
 
 std::error_code compileRuleList(ContentExtensionCompilationClient& client, String&& ruleJSON, Vector<ContentExtensionRule>&& parsedRuleList)
 {
-#if !ASSERT_DISABLED
-    callOnMainThread([ruleJSON = ruleJSON.isolatedCopy(), parsedRuleList = parsedRuleList.isolatedCopy()] {
-        ASSERT(parseRuleList(ruleJSON) == parsedRuleList);
-    });
-#endif
-
     bool domainConditionSeen = false;
     bool topURLConditionSeen = false;
     for (const auto& rule : parsedRuleList) {