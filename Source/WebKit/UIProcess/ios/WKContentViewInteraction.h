fberlakovich/r2c-webkit

commit 152e761145a74a440f310b19e3fcf3aa611e9e8a

REGRESSION: Layout test editing/selection/ios/selection-after-changing-text-with-callout-menu.html is failing https://bugs.webkit.org/show_bug.cgi?id=197532 <rdar://problem/50177144>
Reviewed by Ryosuke Niwa.

Fixes layout tests that began failing after r244546. See below for details.

* UIProcess/RemoteLayerTree/RemoteLayerTreeDrawingAreaProxy.mm:
(WebKit::RemoteLayerTreeDrawingAreaProxy::commitLayerTree):

Partially reverts a change in r244546, after which we commit the layer tree and scroll before updating the
page's editor state. The purpose of this change was to ensure that UI process-side element focus scrolling logic
would not conflict with web-process-driven scrolling logic.

Instead, we split the existing logic in WebPageProxy::editorStateChanged into two pieces: one that updates the
editor state (by setting m_editorState), and a second that dispatches updates to PageClient when the first
editor state is received after focusing an element. During a layer tree commit in the UI process, we first
update the editor state, then commit the layer tree and apply scroll position changes, and finally scroll to
reveal the focused element if necessary.

When an editor state is delivered to the UI process in an out-of-band update (i.e. not in a layer tree commit),
simply dispatch the initial editor state for a focused element immediately.

* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::scheduleFullEditorStateUpdate):

Add an IPC message to schedule an editor state update in the next remote layer tree commit. See below for more
details.

(WebKit::WebPageProxy::editorStateChanged):
(WebKit::WebPageProxy::dispatchDidReceiveEditorStateAfterFocus):
* UIProcess/WebPageProxy.h:
* UIProcess/gtk/WebPageProxyGtk.cpp:
(WebKit::WebPageProxy::updateEditorState):
(WebKit::WebPageProxy::editorStateChanged): Deleted.
* UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView willFinishIgnoringCalloutBarFadeAfterPerformingAction]):

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -3920,6 +3920,7 @@ static void selectionChangedWithTouch(WKContentView *view, const WebCore::IntPoi
 - (void)willFinishIgnoringCalloutBarFadeAfterPerformingAction
 {
     _ignoreSelectionCommandFadeCount++;
+    _page->scheduleFullEditorStateUpdate();
     _page->callAfterNextPresentationUpdate([weakSelf = WeakObjCPtr<WKContentView>(self)] (auto) {
         if (auto strongSelf = weakSelf.get())
             strongSelf->_ignoreSelectionCommandFadeCount--;

Source/WebKit/UIProcess/ios/WebPageProxyIOS.mm
@@ -430,6 +430,11 @@ bool WebPageProxy::updateLayoutViewportParameters(const WebKit::RemoteLayerTreeT
 void WebPageProxy::layerTreeCommitComplete()
 {
     pageClient().layerTreeCommitComplete();
+
+    if (m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement && !m_editorState.isMissingPostLayoutData) {
+        pageClient().didReceiveEditorStateUpdateAfterFocus();
+        m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement = false;
+    }
 }
 
 void WebPageProxy::selectWithGesture(const WebCore::IntPoint point, WebCore::TextGranularity granularity, uint32_t gestureType, uint32_t gestureState, bool isInteractingWithFocusedElement, WTF::Function<void(const WebCore::IntPoint&, uint32_t, uint32_t, uint32_t, CallbackBase::Error)>&& callbackFunction)
@@ -1102,7 +1107,7 @@ void WebPageProxy::contentSizeCategoryDidChange(const String& contentSizeCategor
     process().send(Messages::WebPage::ContentSizeCategoryDidChange(contentSizeCategory), m_pageID);
 }
 
-void WebPageProxy::editorStateChanged(const EditorState& editorState)
+void WebPageProxy::updateEditorState(const EditorState& editorState)
 {
     bool couldChangeSecureInputState = m_editorState.isInPasswordField != editorState.isInPasswordField || m_editorState.selectionIsNone;
     
@@ -1119,11 +1124,15 @@ void WebPageProxy::editorStateChanged(const EditorState& editorState)
     // even during composition to support phrase boundary gesture.
     pageClient().selectionDidChange();
     updateFontAttributesAfterEditorStateChange();
+}
 
-    if (m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement && !m_editorState.isMissingPostLayoutData) {
-        pageClient().didReceiveEditorStateUpdateAfterFocus();
-        m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement = false;
-    }
+void WebPageProxy::dispatchDidReceiveEditorStateAfterFocus()
+{
+    if (!m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement || m_editorState.isMissingPostLayoutData)
+        return;
+
+    pageClient().didReceiveEditorStateUpdateAfterFocus();
+    m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement = false;
 }
 
 void WebPageProxy::showValidationMessage(const IntRect& anchorClientRect, const String& message)

Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeDrawingAreaProxy.mm
@@ -199,6 +199,9 @@ void RemoteLayerTreeDrawingAreaProxy::commitLayerTree(const RemoteLayerTreeTrans
     m_transactionIDForPendingCACommit = layerTreeTransaction.transactionID();
     m_activityStateChangeID = layerTreeTransaction.activityStateChangeID();
 
+    if (layerTreeTransaction.hasEditorState())
+        m_webPageProxy.updateEditorState(layerTreeTransaction.editorState());
+
     if (m_remoteLayerTreeHost->updateLayerTree(layerTreeTransaction)) {
         if (layerTreeTransaction.transactionID() >= m_transactionIDForUnhidingContent)
             m_webPageProxy.setRemoteLayerTreeRootNode(m_remoteLayerTreeHost->rootNode());
@@ -253,7 +256,7 @@ void RemoteLayerTreeDrawingAreaProxy::commitLayerTree(const RemoteLayerTreeTrans
 #endif
 
     if (layerTreeTransaction.hasEditorState())
-        m_webPageProxy.editorStateChanged(layerTreeTransaction.editorState());
+        m_webPageProxy.dispatchDidReceiveEditorStateAfterFocus();
 
     if (auto milestones = layerTreeTransaction.newlyReachedPaintingMilestones())
         m_webPageProxy.didReachLayoutMilestone(milestones);

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -2006,6 +2006,14 @@ void WebPageProxy::setMaintainsInactiveSelection(bool newValue)
     m_maintainsInactiveSelection = newValue;
 }
 
+void WebPageProxy::scheduleFullEditorStateUpdate()
+{
+    if (!hasRunningProcess())
+        return;
+
+    m_process->send(Messages::WebPage::ScheduleFullEditorStateUpdate(), m_pageID);
+}
+
 void WebPageProxy::executeEditCommand(const String& commandName, const String& argument, WTF::Function<void(CallbackBase::Error)>&& callbackFunction)
 {
     if (!hasRunningProcess()) {
@@ -6502,6 +6510,20 @@ void WebPageProxy::editingRangeCallback(const EditingRange& range, CallbackID ca
 
     callback->performCallbackWithReturnValue(range);
 }
+
+void WebPageProxy::editorStateChanged(const EditorState& editorState)
+{
+    updateEditorState(editorState);
+    dispatchDidReceiveEditorStateAfterFocus();
+}
+
+#if !PLATFORM(IOS_FAMILY)
+
+void WebPageProxy::dispatchDidReceiveEditorStateAfterFocus()
+{
+}
+
+#endif
 
 #if ENABLE(APPLICATION_MANIFEST)
 void WebPageProxy::applicationManifestCallback(const Optional<WebCore::ApplicationManifest>& manifestOrNull, CallbackID callbackID)

Source/WebKit/UIProcess/WebPageProxy.h
@@ -1423,6 +1423,9 @@ public:
     void createSandboxExtensionsIfNeeded(const Vector<String>& files, SandboxExtension::Handle& fileReadHandle, SandboxExtension::HandleArray& fileUploadHandles);
 #endif
     void editorStateChanged(const EditorState&);
+    void updateEditorState(const EditorState&);
+    void scheduleFullEditorStateUpdate();
+    void dispatchDidReceiveEditorStateAfterFocus();
 
 #if PLATFORM(COCOA)
     void touchBarMenuDataRemoved();

Source/WebKit/UIProcess/gtk/WebPageProxyGtk.cpp
@@ -79,7 +79,7 @@ void WebsiteDataStore::platformRemoveRecentSearches(WallTime oldestTimeToRemove)
     notImplemented();
 }
 
-void WebPageProxy::editorStateChanged(const EditorState& editorState)
+void WebPageProxy::updateEditorState(const EditorState& editorState)
 {
     m_editorState = editorState;
     

Source/WebKit/UIProcess/mac/WebPageProxyMac.mm
@@ -607,7 +607,7 @@ bool WebPageProxy::appleMailLinesClampEnabled()
     return MacApplication::isAppleMail();
 }
 
-void WebPageProxy::editorStateChanged(const EditorState& editorState)
+void WebPageProxy::updateEditorState(const EditorState& editorState)
 {
     bool couldChangeSecureInputState = m_editorState.isInPasswordField != editorState.isInPasswordField || m_editorState.selectionIsNone;
     

Source/WebKit/UIProcess/win/WebPageProxyWin.cpp
@@ -60,7 +60,7 @@ void WebPageProxy::loadRecentSearches(const String& name, CompletionHandler<void
     completionHandler(WTFMove(searchItems));
 }
 
-void WebPageProxy::editorStateChanged(const EditorState& editorState)
+void WebPageProxy::updateEditorState(const EditorState& editorState)
 {
     m_editorState = editorState;
 }

commit 460361602fbd4d68942a15cba5283fcd7868bbbd

REGRESSION (r244897): Caret may appear wider than normal after zooming to focus an editable element https://bugs.webkit.org/show_bug.cgi?id=197579
Reviewed by Tim Horton.

Source/WebKit:

Fixes a couple of flaky tests (CaretSelectionRectAfterRestoringFirstResponderWithRetainActiveFocusedState and
CaretSelectionRectAfterRestoringFirstResponder) that began failing after r244897. These tests both begin by
focusing an editable element, which causes the web view to zoom in. The tests subsequently check that the caret
rect is {{ 16, 13 }, { 2, 15 }}. While the specified caret rect (coming from EditorState) is {{ 16, 13 }, { 3,
15 }}, the narrower caret rect is used because we take measures to preserve the width of the caret relative to
the view (see the inverse scaling logic in -[WKContentView selectedTextRange] for more details).

See below for more details.

* UIProcess/ios/WKContentViewInteraction.h:

Remove _isZoomingToRevealFocusedElement, now that we don't need it anymore (see -observeValueForKeyPath:).

* UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView cleanupInteraction]):
(-[WKContentView observeValueForKeyPath:ofObject:change:context:]):

Stop bailing from a selection update when changing scale, while zooming to reveal the focused element. This
check was added in r239441 to prevent UIWKTextInteractionAssistant's selection scrolling logic from interfering
with WKContentView-driven logic for zooming to the focused element. However, since r244141, this is no longer
necessary since selection scrolling is only driven by code in the web process.

This new update while zooming to reveal the focused element ensures that the WKTextRange returned by
-selectedTextRange after zooming will have a width that is inversely scaled using the content view's current
scale, such that it has a consistent width (relative to the web view) across different scales.

(-[WKContentView _zoomToRevealFocusedElement]):
* UIProcess/ios/WebPageProxyIOS.mm:

Source/WebKit/UIProcess/ios/WKContentViewInteraction.h
@@ -321,7 +321,6 @@ struct WKAutoCorrectionData {
     BOOL _didAccessoryTabInitiateFocus;
     BOOL _isExpectingFastSingleTapCommit;
     BOOL _showDebugTapHighlightsForFastClicking;
-    BOOL _isZoomingToRevealFocusedElement;
 
     BOOL _keyboardDidRequestDismissal;
 

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -934,7 +934,6 @@ static inline bool hasFocusedElement(WebKit::FocusedElementInformation focusedEl
 
     _hasSetUpInteractions = NO;
     _suppressSelectionAssistantReasons = { };
-    _isZoomingToRevealFocusedElement = NO;
 
 #if ENABLE(POINTER_EVENTS)
     [self _resetPanningPreventionFlags];
@@ -1030,12 +1029,6 @@ static inline bool hasFocusedElement(WebKit::FocusedElementInformation focusedEl
     }
 
     [self _updateTapHighlight];
-
-    if (_isZoomingToRevealFocusedElement) {
-        // When zooming to the focused element, avoid additionally scrolling to reveal the selection. Since the scale transform has not yet been
-        // applied to the content view at this point, we'll end up scrolling to reveal a rect that is computed using the wrong scale.
-        return;
-    }
 
     _selectionNeedsUpdate = YES;
     [self _updateChangedSelection:YES];
@@ -1671,7 +1664,6 @@ static NSValue *nsSizeForTapHighlightBorderRadius(WebCore::IntSize borderRadius,
     if (_suppressSelectionAssistantReasons.contains(WebKit::EditableRootIsTransparentOrFullyClipped) || _suppressSelectionAssistantReasons.contains(WebKit::FocusedElementIsTooSmall))
         return;
 
-    SetForScope<BOOL> isZoomingToRevealFocusedElementForScope { _isZoomingToRevealFocusedElement, YES };
     // In case user scaling is force enabled, do not use that scaling when zooming in with an input field.
     // Zooming above the page's default scale factor should only happen when the user performs it.
     [self _zoomToFocusRect:_focusedElementInformation.elementRect

Source/WebKit/UIProcess/ios/WebPageProxyIOS.mm
@@ -430,11 +430,6 @@ bool WebPageProxy::updateLayoutViewportParameters(const WebKit::RemoteLayerTreeT
 void WebPageProxy::layerTreeCommitComplete()
 {
     pageClient().layerTreeCommitComplete();
-
-    if (m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement && !m_editorState.isMissingPostLayoutData) {
-        pageClient().didReceiveEditorStateUpdateAfterFocus();
-        m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement = false;
-    }
 }
 
 void WebPageProxy::selectWithGesture(const WebCore::IntPoint point, WebCore::TextGranularity granularity, uint32_t gestureType, uint32_t gestureState, bool isInteractingWithFocusedElement, WTF::Function<void(const WebCore::IntPoint&, uint32_t, uint32_t, uint32_t, CallbackBase::Error)>&& callbackFunction)

Tools/TestWebKitAPI/Tests/ios/KeyboardInputTestsIOS.mm
@@ -213,25 +213,18 @@ TEST(KeyboardInputTests, ModifyInputAssistantItemBarButtonGroups)
     item.leadingBarButtonGroups = @[ leadingItems ];
     item.trailingBarButtonGroups = @[ trailingItems ];
 
-    bool doneWaitingForInputSession = false;
     [inputDelegate setFocusStartsInputSessionPolicyHandler:[&] (WKWebView *, id <_WKFocusedElementInfo>) -> _WKFocusStartsInputSessionPolicy {
-        doneWaitingForInputSession = true;
         return _WKFocusStartsInputSessionPolicyAllow;
     }];
-    [webView evaluateJavaScript:@"document.body.focus()" completionHandler:nil];
-    TestWebKitAPI::Util::run(&doneWaitingForInputSession);
+    [webView evaluateJavaScriptAndWaitForInputSessionToChange:@"document.body.focus()"];
 
     EXPECT_EQ([webView firstResponder], [webView textInputContentView]);
     EXPECT_TRUE([[webView firstResponder].inputAssistantItem.leadingBarButtonGroups containsObject:leadingItems]);
     EXPECT_TRUE([[webView firstResponder].inputAssistantItem.trailingBarButtonGroups containsObject:trailingItems]);
 
     // Now blur and refocus the editable area, and check that the same leading and trailing button items are present.
-    [webView resignFirstResponder];
-    [webView waitForNextPresentationUpdate];
-
-    doneWaitingForInputSession = false;
-    [webView evaluateJavaScript:@"document.body.focus()" completionHandler:nil];
-    TestWebKitAPI::Util::run(&doneWaitingForInputSession);
+    [webView evaluateJavaScriptAndWaitForInputSessionToChange:@"document.body.blur()"];
+    [webView evaluateJavaScriptAndWaitForInputSessionToChange:@"document.body.focus()"];
 
     EXPECT_EQ([webView firstResponder], [webView textInputContentView]);
     EXPECT_TRUE([[webView firstResponder].inputAssistantItem.leadingBarButtonGroups containsObject:leadingItems]);
@@ -244,14 +237,10 @@ TEST(KeyboardInputTests, OverrideInputAssistantItemBarButtonGroups)
     auto webView = adoptNS([[InputAssistantItemTestingWebView alloc] initWithFrame:CGRectMake(0, 0, 320, 500)]);
     [webView _setInputDelegate:inputDelegate.get()];
     [webView synchronouslyLoadHTMLString:@"<body contenteditable>"];
-
-    bool doneWaitingForInputSession = false;
     [inputDelegate setFocusStartsInputSessionPolicyHandler:[&] (WKWebView *, id <_WKFocusedElementInfo>) -> _WKFocusStartsInputSessionPolicy {
-        doneWaitingForInputSession = true;
         return _WKFocusStartsInputSessionPolicyAllow;
     }];
-    [webView evaluateJavaScript:@"document.body.focus()" completionHandler:nil];
-    TestWebKitAPI::Util::run(&doneWaitingForInputSession);
+    [webView evaluateJavaScriptAndWaitForInputSessionToChange:@"document.body.focus()"];
 
     UIBarButtonItemGroup *leadingItems = [InputAssistantItemTestingWebView leadingItemsForWebView:webView.get()];
     UIBarButtonItemGroup *trailingItems = [InputAssistantItemTestingWebView trailingItemsForWebView:webView.get()];
@@ -260,12 +249,8 @@ TEST(KeyboardInputTests, OverrideInputAssistantItemBarButtonGroups)
     EXPECT_TRUE([[webView firstResponder].inputAssistantItem.trailingBarButtonGroups containsObject:trailingItems]);
 
     // Now blur and refocus the editable area, and check that the same leading and trailing button items are present.
-    [webView resignFirstResponder];
-    [webView waitForNextPresentationUpdate];
-
-    doneWaitingForInputSession = false;
-    [webView evaluateJavaScript:@"document.body.focus()" completionHandler:nil];
-    TestWebKitAPI::Util::run(&doneWaitingForInputSession);
+    [webView evaluateJavaScriptAndWaitForInputSessionToChange:@"document.body.blur()"];
+    [webView evaluateJavaScriptAndWaitForInputSessionToChange:@"document.body.focus()"];
 
     EXPECT_EQ([webView firstResponder], [webView textInputContentView]);
     EXPECT_TRUE([[webView firstResponder].inputAssistantItem.leadingBarButtonGroups containsObject:leadingItems]);