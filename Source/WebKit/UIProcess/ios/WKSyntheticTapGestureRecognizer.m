fberlakovich/r2c-webkit

commit 742f58191040f15d8fb044c30613c142312c94c8

[iOS] Compatibility mouse events aren't prevented by calling preventDefault() on pointerdown https://bugs.webkit.org/show_bug.cgi?id=198124 <rdar://problem/50410863>
Reviewed by Tim Horton.

LayoutTests/imported/w3c:

We add basic support to run a test that wasn't specifically designed for a touch-based interaction such that the test
at imported/w3c/web-platform-tests/pointerevents/pointerevent_suppress_compat_events_on_click.html may run on iOS. The
trick here is to add a pause after a touch ends to avoid the likelihood or two tap gestures triggering a double tap.

* web-platform-tests/resources/testdriver-vendor.js:

Source/WebCore:

This fix builds atop the one made for wkb.ug/198072 which fixes this bug on macOS alone.

In order to correctly prevent "compatibility" mouse events from being dispatched when the initial "pointerdown" event had preventDefault()
called while handled, we need to pass the PointerID for the touch that triggered a tap gesture in the UI process down in the Web process
and into the resulting PlatformMouseEvent. This will allow upon dispatch of a PlatformMouseEvent to call into PointerCaptureController
to identify if the dispatch of mouse events is allowed for the event's PointerID.

To support this, some refactoring was required. The PointerID header is now under platform/ such that PlatformMouseEvent may safely use it.
Additionally, PointerEvent::defaultMousePointerIdentifier() is now a global mousePointerID defined in PointerID.h.

Finally, PointerCaptureController::touchEndedOrWasCancelledForIdentifier() has been renamed to PointerCaptureController::touchWithIdentifierWasRemoved() and
has WEBCORE_EXPORT such that it may be called from WebKit as the indication that a pointer is no longer active will now be initiated in WebKit
on the UI process side.

Testing is covered by the pre-existing imported/w3c/web-platform-tests/pointerevents/pointerevent_suppress_compat_events_on_click.html

Source/WebKit/UIProcess/ios/WKSyntheticTapGestureRecognizer.m
@@ -70,6 +70,26 @@
 {
     [super reset];
     [_resetTarget performSelector:_resetAction withObject:self];
+    _lastActiveTouchIdentifier = nil;
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesEnded:touches withEvent:event];
+    if (!_supportingWebTouchEventsGestureRecognizer)
+        return;
+
+#if ENABLE(POINTER_EVENTS) && HAVE(UI_WEB_TOUCH_EVENTS_GESTURE_RECOGNIZER_WITH_ACTIVE_TOUCHES_BY_ID)
+    // FIXME: <rdar://problem/48035706>
+    NSMapTable<NSNumber *, UITouch *> *activeTouches = [_supportingWebTouchEventsGestureRecognizer activeTouchesByIdentifier];
+    for (NSNumber *touchIdentifier in activeTouches) {
+        UITouch *touch = [activeTouches objectForKey:touchIdentifier];
+        if ([touch.gestureRecognizers containsObject:self]) {
+            _lastActiveTouchIdentifier = touchIdentifier;
+            break;
+        }
+    }
+#endif
 }
 
 @end

LayoutTests/imported/w3c/web-platform-tests/resources/testdriver-vendor.js
@@ -46,7 +46,7 @@ function dispatchMouseActions(actions)
     });
 }
 
-function dispatchTouchActions(actions)
+function dispatchTouchActions(actions, options = { insertPauseAfterPointerUp: false })
 {
     if (!window.testRunner || typeof window.testRunner.runUIScript !== "function")
         return Promise.reject(new Error("window.testRunner.runUIScript() is undefined."));
@@ -99,6 +99,9 @@ function dispatchTouchActions(actions)
             touch.x = x;
             touch.y = y;
             id++;
+            // We need to add a pause after a pointer up to ensure that a subsequent tap may be recognized as such.
+            if (options.insertPauseAfterPointerUp)
+                timeOffsetIncrease = 0.5;
             break;
         case "pause":
             timeOffsetIncrease = action.duration / 1000;
@@ -173,8 +176,10 @@ window.test_driver_internal.action_sequence = function(sources)
 
     logDebug(() => JSON.stringify(pointerSource));
 
-    if (pointerType === "mouse")
-        return dispatchMouseActions(pointerSource.actions);
     if (pointerType === "touch")
         return dispatchTouchActions(pointerSource.actions);
+    if ("createTouch" in document)
+        return dispatchTouchActions(pointerSource.actions, { insertPauseAfterPointerUp: true });
+    if (pointerType === "mouse")
+        return dispatchMouseActions(pointerSource.actions);
 };

LayoutTests/platform/ios/imported/w3c/web-platform-tests/pointerevents/pointerevent_suppress_compat_events_on_click-expected.txt
@@ -1,0 +1,20 @@
+Pointer Event: Suppress compatibility mouse events on click
+
+When a pointerdown is canceled, a click/tap shouldn't fire any compatibility mouse events except click event.
+
+Click or tap on Target0.
+Click or tap on Target1.
+Click Done.
+Target0
+Target1
+Done
+The following pointer types were detected: touch.
+
+The following events were logged: click@target0, mousedown@target1, mouseup@target1, click@target1.
+
+
+PASS Suppress compat mouse events on click 
+PASS primary pointer pointerdown@target0 
+PASS primary pointer pointerdown@target1 
+PASS Event log 
+

Source/WebCore/Headers.cmake
@@ -420,7 +420,6 @@ set(WebCore_PRIVATE_FRAMEWORK_HEADERS
     dom/NodeRenderStyle.h
     dom/NodeTraversal.h
     dom/OverflowEvent.h
-    dom/PointerID.h
     dom/Position.h
     dom/ProcessingInstruction.h
     dom/ProgressEvent.h
@@ -928,6 +927,7 @@ set(WebCore_PRIVATE_FRAMEWORK_HEADERS
     platform/PlatformTouchEvent.h
     platform/PlatformTouchPoint.h
     platform/PlatformWheelEvent.h
+    platform/PointerID.h
     platform/PopupMenu.h
     platform/PopupMenuClient.h
     platform/PopupMenuStyle.h

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -931,7 +931,7 @@
 		316FE1180E6E1DA700BF6088 /* ImplicitAnimation.h in Headers */ = {isa = PBXBuildFile; fileRef = 316FE10E0E6E1DA700BF6088 /* ImplicitAnimation.h */; };
 		316FE11A0E6E1DA700BF6088 /* KeyframeAnimation.h in Headers */ = {isa = PBXBuildFile; fileRef = 316FE1100E6E1DA700BF6088 /* KeyframeAnimation.h */; };
 		31741AAD16636609008A5B7E /* SimulatedClickOptions.h in Headers */ = {isa = PBXBuildFile; fileRef = 31741AAB16635E45008A5B7E /* SimulatedClickOptions.h */; settings = {ATTRIBUTES = (Private, ); }; };
-		317D3FF3215599F40034E3B9 /* PointerEvent.h in Headers */ = {isa = PBXBuildFile; fileRef = 317D3FF2215599E30034E3B9 /* PointerEvent.h */; };
+		317D3FF3215599F40034E3B9 /* PointerEvent.h in Headers */ = {isa = PBXBuildFile; fileRef = 317D3FF2215599E30034E3B9 /* PointerEvent.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		31815A311F9A6C8F00FCBF89 /* ImageBitmap.h in Headers */ = {isa = PBXBuildFile; fileRef = 31D26BBF1F86D189008FF255 /* ImageBitmap.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		318436DE21B9DAAF00ED383E /* WebGPULayer.h in Headers */ = {isa = PBXBuildFile; fileRef = 318436DB21B9DAA000ED383E /* WebGPULayer.h */; };
 		318891611AB7EEA100EA627B /* missingImage@3x.png in Resources */ = {isa = PBXBuildFile; fileRef = CDF419991AB0DA14004E64E1 /* missingImage@3x.png */; };
@@ -17009,11 +17009,11 @@
 		1CA0C2F621EEDAD200A11860 /* AST */ = {
 			isa = PBXGroup;
 			children = (
-				5215862C229377B7005925EF /* WHLSLAST.h */,
 				1C840B9021EC30F900D0500D /* WHLSLAddressSpace.h */,
 				C21BF72521CD89E200227979 /* WHLSLArrayReferenceType.h */,
 				C21BF70921CD89CA00227979 /* WHLSLArrayType.h */,
 				C21BF73021CD89ED00227979 /* WHLSLAssignmentExpression.h */,
+				5215862C229377B7005925EF /* WHLSLAST.h */,
 				C21BF70A21CD89CB00227979 /* WHLSLBaseFunctionAttribute.h */,
 				C21BF6FA21CD89BE00227979 /* WHLSLBaseSemantic.h */,
 				C21BF71E21CD89DC00227979 /* WHLSLBlock.h */,
@@ -25314,6 +25314,7 @@
 				BCBB8AB513F1AFB000734DF0 /* PODInterval.h */,
 				BCBB8AB613F1AFB000734DF0 /* PODIntervalTree.h */,
 				BCBB8AB713F1AFB000734DF0 /* PODRedBlackTree.h */,
+				71EADCD622087E6D0065A45F /* PointerID.h */,
 				0668E1890ADD9624004128E0 /* PopupMenu.h */,
 				ABC128760B33AA6D00C693D5 /* PopupMenuClient.h */,
 				BC3BE12A0E98092F00835588 /* PopupMenuStyle.h */,
@@ -27475,7 +27476,6 @@
 				317D3FF1215599E20034E3B9 /* PointerEvent.cpp */,
 				317D3FF2215599E30034E3B9 /* PointerEvent.h */,
 				317D3FEF215599E10034E3B9 /* PointerEvent.idl */,
-				71EADCD622087E6D0065A45F /* PointerID.h */,
 				5189F0DD10B46B0E00F3C739 /* PopStateEvent.cpp */,
 				5174E20810A1F44F00F95E6F /* PopStateEvent.h */,
 				5174E20B10A1F49A00F95E6F /* PopStateEvent.idl */,

Source/WebCore/dom/Element.cpp
@@ -303,8 +303,14 @@ bool Element::dispatchMouseEvent(const PlatformMouseEvent& platformEvent, const 
 
     bool didNotSwallowEvent = true;
 
-#if ENABLE(POINTER_EVENTS) && !ENABLE(TOUCH_EVENTS)
+#if ENABLE(POINTER_EVENTS)
     if (RuntimeEnabledFeatures::sharedFeatures().pointerEventsEnabled()) {
+#if ENABLE(TOUCH_EVENTS)
+        if (auto* page = document().page()) {
+            if (mouseEvent->type() != eventNames().clickEvent && page->pointerCaptureController().preventsCompatibilityMouseEventsForIdentifier(platformEvent.pointerId()))
+                return false;
+        }
+#else
         if (auto pointerEvent = PointerEvent::create(mouseEvent)) {
             if (auto* page = document().page()) {
                 page->pointerCaptureController().dispatchEvent(*pointerEvent, this);
@@ -317,6 +323,7 @@ bool Element::dispatchMouseEvent(const PlatformMouseEvent& platformEvent, const 
                     return false;
             }
         }
+#endif
     }
 #endif
 

Source/WebCore/dom/PointerEvent.h
@@ -40,7 +40,7 @@ namespace WebCore {
 class PointerEvent final : public MouseEvent {
 public:
     struct Init : MouseEventInit {
-        PointerID pointerId { PointerEvent::defaultMousePointerIdentifier() };
+        PointerID pointerId { mousePointerID };
         double width { 1 };
         double height { 1 };
         float pressure { 0 };
@@ -85,7 +85,6 @@ public:
     static const String& mousePointerType();
     static const String& penPointerType();
     static const String& touchPointerType();
-    static PointerID defaultMousePointerIdentifier() { return 1; }
 
     virtual ~PointerEvent();
 
@@ -113,7 +112,7 @@ private:
     PointerEvent(const AtomicString& type, const PlatformTouchEvent&, IsCancelable isCancelable, unsigned touchIndex, bool isPrimary, Ref<WindowProxy>&&);
 #endif
 
-    PointerID m_pointerId { PointerEvent::defaultMousePointerIdentifier() };
+    PointerID m_pointerId { mousePointerID };
     double m_width { 1 };
     double m_height { 1 };
     float m_pressure { 0 };

Source/WebCore/dom/PointerID.h
@@ -1,36 +1,0 @@
-/*
- * Copyright (C) 2019 Apple Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
- */
-
-#pragma once
-
-#if ENABLE(POINTER_EVENTS)
-
-namespace WebCore {
-
-using PointerID = int32_t;
-
-}
-
-#endif // ENABLE(POINTER_EVENTS)

commit 3bf4f06cf05f0fad40b7dd942472d6f2ae1b14ab

[tvOS] Build broken by r245639 https://bugs.webkit.org/show_bug.cgi?id=198172 <rdar://problem/51048991>
Unreviewed build fix.

* UIProcess/ios/WKSyntheticTapGestureRecognizer.m:
(-[WKSyntheticTapGestureRecognizer reset]):
(-[WKSyntheticTapGestureRecognizer touchesEnded:withEvent:]):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245673 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/ios/WKSyntheticTapGestureRecognizer.m
@@ -70,16 +70,18 @@
 {
     [super reset];
     [_resetTarget performSelector:_resetAction withObject:self];
+#if ENABLE(POINTER_EVENTS) 
     _lastActiveTouchIdentifier = nil;
+#endif
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     [super touchesEnded:touches withEvent:event];
+#if ENABLE(POINTER_EVENTS) && HAVE(UI_WEB_TOUCH_EVENTS_GESTURE_RECOGNIZER_WITH_ACTIVE_TOUCHES_BY_ID)
     if (!_supportingWebTouchEventsGestureRecognizer)
         return;
 
-#if ENABLE(POINTER_EVENTS) && HAVE(UI_WEB_TOUCH_EVENTS_GESTURE_RECOGNIZER_WITH_ACTIVE_TOUCHES_BY_ID)
     // FIXME: <rdar://problem/48035706>
     NSMapTable<NSNumber *, UITouch *> *activeTouches = [_supportingWebTouchEventsGestureRecognizer activeTouchesByIdentifier];
     for (NSNumber *touchIdentifier in activeTouches) {