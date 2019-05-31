fberlakovich/r2c-webkit

commit e9323ca89f2707ef8afdc6f3a16d3a3ed4bba00e

[GTK] Use WPEBackend-fdo for accelerating compositing in Wayland instead of the nested compositor https://bugs.webkit.org/show_bug.cgi?id=197944
Reviewed by Michael Catanzaro.

.:

Add USE_WPE_RENDERER option to use WPEBackend-fdo.

* Source/cmake/OptionsGTK.cmake:
* Source/cmake/OptionsPlayStation.cmake:
* Source/cmake/OptionsWPE.cmake:

Source/WebCore:

* PlatformGTK.cmake:
* SourcesGTK.txt:
* platform/graphics/GLContext.cpp:
(WebCore::GLContext::createContextForWindow): Check current display is X11 before trying to create a GLX context.
* platform/graphics/PlatformDisplay.cpp:
(WebCore::PlatformDisplay::createPlatformDisplay): Use USE(WPE_RENDERER) instead of USE(LIBWPE).
* platform/graphics/PlatformDisplay.h:
* platform/graphics/egl/GLContextEGL.cpp:
(WebCore::GLContextEGL::createWindowContext): Use USE(WPE_RENDERER) instead of PLATFORM(WPE).
(WebCore::GLContextEGL::createContext): Ditto.
(WebCore::GLContextEGL::createSharingContext): Ditto.
(WebCore::GLContextEGL::~GLContextEGL): Ditto.
* platform/graphics/egl/GLContextEGL.h:
* platform/graphics/egl/GLContextEGLLibWPE.cpp:
* platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp:
(WebCore::MediaPlayerPrivateGStreamerBase::ensureGstGLContext): Ditto.
* platform/graphics/libwpe/PlatformDisplayLibWPE.cpp:
(WebCore::PlatformDisplayLibWPE::PlatformDisplayLibWPE): In GTK port set the display as the shared one for compositing.
* platform/graphics/libwpe/PlatformDisplayLibWPE.h:

Source/WebKit:

The WPEBackend-fdo implementation is quite similar to our Wayland nested compositor, but more efficient (using
linux dmabuf, resource caching, etc.). This allows us to share even more code with WPE port too.

* PlatformGTK.cmake:
* Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.cpp:
(WebKit::ThreadedCompositor::forceRepaint): WPE doesn't support force repaint yet.
* Shared/WebPageCreationParameters.cpp:
(WebKit::WebPageCreationParameters::encode const): Use USE(WPE_RENDERER) instead of PLATFORM(WPE).

Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreWayland.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Igalia S.L.
+ * Copyright (C) 2016, 2019 Igalia S.L.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,15 +28,36 @@
 
 #if PLATFORM(WAYLAND) && USE(EGL)
 
-#include "WaylandCompositor.h"
 #include "WebPageProxy.h"
+// These includes need to be in this order because wayland-egl.h defines WL_EGL_PLATFORM
+// and eglplatform.h, included by egl.h, checks that to decide whether it's Wayland platform.
+#include <gdk/gdkwayland.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
 #include <WebCore/CairoUtilities.h>
 #include <WebCore/GLContext.h>
 
 #if USE(OPENGL_ES)
 #include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <WebCore/Extensions3DOpenGLES.h>
 #else
+#include <WebCore/Extensions3DOpenGL.h>
 #include <WebCore/OpenGLShims.h>
+#endif
+
+#if USE(WPE_RENDERER)
+#include <wpe/fdo-egl.h>
+#else
+#include "WaylandCompositor.h"
+#endif
+
+#if USE(WPE_RENDERER)
+#if !defined(PFNGLEGLIMAGETARGETTEXTURE2DOESPROC)
+typedef void (*PFNGLEGLIMAGETARGETTEXTURE2DOESPROC) (GLenum target, GLeglImageOES);
+#endif
+
+static PFNGLEGLIMAGETARGETTEXTURE2DOESPROC glImageTargetTexture2D;
 #endif
 
 namespace WebKit {
@@ -44,20 +65,76 @@ using namespace WebCore;
 
 std::unique_ptr<AcceleratedBackingStoreWayland> AcceleratedBackingStoreWayland::create(WebPageProxy& webPage)
 {
+#if USE(WPE_RENDERER)
+    if (!glImageTargetTexture2D) {
+        if (!wpe_fdo_initialize_for_egl_display(PlatformDisplay::sharedDisplay().eglDisplay()))
+            return nullptr;
+
+        std::unique_ptr<WebCore::GLContext> eglContext = GLContext::createOffscreenContext();
+        if (!eglContext)
+            return nullptr;
+
+        if (!eglContext->makeContextCurrent())
+            return nullptr;
+
+#if USE(OPENGL_ES)
+        std::unique_ptr<Extensions3DOpenGLES> glExtensions = std::make_unique<Extensions3DOpenGLES>(nullptr,  false);
+#else
+        std::unique_ptr<Extensions3DOpenGL> glExtensions = std::make_unique<Extensions3DOpenGL>(nullptr, GLContext::current()->version() >= 320);
+#endif
+        if (glExtensions->supports("GL_OES_EGL_image") || glExtensions->supports("GL_OES_EGL_image_external"))
+            glImageTargetTexture2D = reinterpret_cast<PFNGLEGLIMAGETARGETTEXTURE2DOESPROC>(eglGetProcAddress("glEGLImageTargetTexture2DOES"));
+    }
+
+    if (!glImageTargetTexture2D) {
+        WTFLogAlways("AcceleratedBackingStoreWPE requires glEGLImageTargetTexture2D.");
+        return nullptr;
+    }
+#else
     if (!WaylandCompositor::singleton().isRunning())
         return nullptr;
+#endif
     return std::unique_ptr<AcceleratedBackingStoreWayland>(new AcceleratedBackingStoreWayland(webPage));
 }
 
 AcceleratedBackingStoreWayland::AcceleratedBackingStoreWayland(WebPageProxy& webPage)
     : AcceleratedBackingStore(webPage)
 {
+#if USE(WPE_RENDERER)
+    static struct wpe_view_backend_exportable_fdo_egl_client exportableClient = {
+        // export_egl_image
+        nullptr,
+        // export_fdo_egl_image
+        [](void* data, struct wpe_fdo_egl_exported_image* image)
+        {
+            static_cast<AcceleratedBackingStoreWayland*>(data)->displayBuffer(image);
+        },
+        // padding
+        nullptr, nullptr, nullptr
+    };
+
+    auto viewSize = webPage.viewSize();
+    m_exportable = wpe_view_backend_exportable_fdo_egl_create(&exportableClient, this, viewSize.width(), viewSize.height());
+    wpe_view_backend_initialize(wpe_view_backend_exportable_fdo_get_view_backend(m_exportable));
+#else
     WaylandCompositor::singleton().registerWebPage(m_webPage);
+#endif
 }
 
 AcceleratedBackingStoreWayland::~AcceleratedBackingStoreWayland()
 {
+#if USE(WPE_RENDERER)
+    if (m_pendingImage)
+        wpe_view_backend_exportable_fdo_egl_dispatch_release_exported_image(m_exportable, m_pendingImage);
+    if (m_committedImage)
+        wpe_view_backend_exportable_fdo_egl_dispatch_release_exported_image(m_exportable, m_committedImage);
+    if (m_viewTexture) {
+        if (makeContextCurrent())
+            glDeleteTextures(1, &m_viewTexture);
+    }
+#else
     WaylandCompositor::singleton().unregisterWebPage(m_webPage);
+#endif
 
 #if GTK_CHECK_VERSION(3, 16, 0)
     if (m_gdkGLContext && m_gdkGLContext.get() == gdk_gl_context_get_current())
@@ -102,12 +179,64 @@ bool AcceleratedBackingStoreWayland::makeContextCurrent()
     return m_glContext ? m_glContext->makeContextCurrent() : false;
 }
 
+#if USE(WPE_RENDERER)
+int AcceleratedBackingStoreWayland::renderHostFileDescriptor()
+{
+    return wpe_view_backend_get_renderer_host_fd(wpe_view_backend_exportable_fdo_get_view_backend(m_exportable));
+}
+
+void AcceleratedBackingStoreWayland::displayBuffer(struct wpe_fdo_egl_exported_image* image)
+{
+    if (!m_viewTexture) {
+        if (!makeContextCurrent())
+            return;
+
+        glGenTextures(1, &m_viewTexture);
+        glBindTexture(GL_TEXTURE_2D, m_viewTexture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    }
+
+    if (m_pendingImage)
+        wpe_view_backend_exportable_fdo_egl_dispatch_release_image(m_exportable, m_pendingImage);
+    m_pendingImage = image;
+
+    m_webPage.setViewNeedsDisplay(IntRect(IntPoint::zero(), m_webPage.viewSize()));
+}
+#endif
+
 bool AcceleratedBackingStoreWayland::paint(cairo_t* cr, const IntRect& clipRect)
 {
     GLuint texture;
     IntSize textureSize;
+
+#if USE(WPE_RENDERER)
+    if (!makeContextCurrent())
+        return false;
+
+    if (m_pendingImage) {
+        wpe_view_backend_exportable_fdo_dispatch_frame_complete(m_exportable);
+
+        if (m_committedImage)
+            wpe_view_backend_exportable_fdo_egl_dispatch_release_exported_image(m_exportable, m_committedImage);
+        m_committedImage = m_pendingImage;
+        m_pendingImage = nullptr;
+    }
+
+    if (!m_committedImage)
+        return true;
+
+    glBindTexture(GL_TEXTURE_2D, m_viewTexture);
+    glImageTargetTexture2D(GL_TEXTURE_2D, wpe_fdo_egl_exported_image_get_egl_image(m_committedImage));
+
+    texture = m_viewTexture;
+    textureSize = { static_cast<int>(wpe_fdo_egl_exported_image_get_width(m_committedImage)), static_cast<int>(wpe_fdo_egl_exported_image_get_height(m_committedImage)) };
+#else
     if (!WaylandCompositor::singleton().getTexture(m_webPage, texture, textureSize))
         return false;
+#endif
 
     cairo_save(cr);
 

Source/WebCore/PlatformGTK.cmake
@@ -36,6 +36,12 @@ list(APPEND WebCore_PRIVATE_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/platform/text/gtk"
 )
 
+if (USE_WPE_RENDERER)
+    list(APPEND WebCore_INCLUDE_DIRECTORIES
+        "${WEBCORE_DIR}/platform/graphics/libwpe"
+    )
+endif ()
+
 list(APPEND WebCorePlatformGTK_SOURCES
     editing/gtk/EditorGtk.cpp
 
@@ -120,6 +126,12 @@ list(APPEND WebCore_LIBRARIES
     ${ZLIB_LIBRARIES}
 )
 
+if (USE_WPE_RENDERER)
+    list(APPEND WebCore_LIBRARIES
+        ${WPE_LIBRARIES}
+    )
+endif ()
+
 list(APPEND WebCore_SYSTEM_INCLUDE_DIRECTORIES
     ${ATK_INCLUDE_DIRS}
     ${ENCHANT_INCLUDE_DIRS}
@@ -131,6 +143,12 @@ list(APPEND WebCore_SYSTEM_INCLUDE_DIRECTORIES
     ${UPOWERGLIB_INCLUDE_DIRS}
     ${ZLIB_INCLUDE_DIRS}
 )
+
+if (USE_WPE_RENDERER)
+    list(APPEND WebCore_SYSTEM_INCLUDE_DIRECTORIES
+        ${WPE_INCLUDE_DIRS}
+    )
+endif ()
 
 if (USE_OPENGL_ES)
     list(APPEND WebCore_SOURCES

Source/WebCore/platform/graphics/GLContext.cpp
@@ -90,8 +90,10 @@ std::unique_ptr<GLContext> GLContext::createContextForWindow(GLNativeWindowType 
 #endif
 
 #if USE(GLX)
-    if (auto glxContext = GLContextGLX::createContext(windowHandle, display))
-        return glxContext;
+    if (display.type() == PlatformDisplay::Type::X11) {
+        if (auto glxContext = GLContextGLX::createContext(windowHandle, display))
+            return glxContext;
+    }
 #endif
 #if USE(EGL)
     if (auto eglContext = GLContextEGL::createContext(windowHandle, display))

Source/WebCore/platform/graphics/PlatformDisplay.cpp
@@ -42,7 +42,7 @@
 #include "PlatformDisplayWin.h"
 #endif
 
-#if USE(LIBWPE)
+#if USE(WPE_RENDERER)
 #include "PlatformDisplayLibWPE.h"
 #endif
 
@@ -91,7 +91,7 @@ std::unique_ptr<PlatformDisplay> PlatformDisplay::createPlatformDisplay()
 #endif
 #endif // PLATFORM(GTK)
 
-#if USE(LIBWPE)
+#if USE(WPE_RENDERER)
     return PlatformDisplayLibWPE::create();
 #elif PLATFORM(WIN)
     return PlatformDisplayWin::create();

Source/WebCore/platform/graphics/PlatformDisplay.h
@@ -54,7 +54,7 @@ public:
 #if PLATFORM(WIN)
         Windows,
 #endif
-#if USE(LIBWPE)
+#if USE(WPE_RENDERER)
         WPE,
 #endif
     };

Source/WebCore/platform/graphics/egl/GLContextEGL.cpp
@@ -182,7 +182,9 @@ std::unique_ptr<GLContextEGL> GLContextEGL::createWindowContext(GLNativeWindowTy
     if (platformDisplay.type() == PlatformDisplay::Type::Wayland)
         surface = createWindowSurfaceWayland(display, config, window);
 #endif
-#elif PLATFORM(WPE)
+#endif
+
+#if USE(WPE_RENDERER)
     if (platformDisplay.type() == PlatformDisplay::Type::WPE)
         surface = createWindowSurfaceWPE(display, config, window);
 #else
@@ -279,7 +281,7 @@ std::unique_ptr<GLContextEGL> GLContextEGL::createContext(GLNativeWindowType win
         if (platformDisplay.type() == PlatformDisplay::Type::Wayland)
             context = createWaylandContext(platformDisplay, eglSharingContext);
 #endif
-#if PLATFORM(WPE)
+#if USE(WPE_RENDERER)
         if (platformDisplay.type() == PlatformDisplay::Type::WPE)
             context = createWPEContext(platformDisplay, eglSharingContext);
 #endif
@@ -316,7 +318,7 @@ std::unique_ptr<GLContextEGL> GLContextEGL::createSharingContext(PlatformDisplay
         if (platformDisplay.type() == PlatformDisplay::Type::Wayland)
             context = createWaylandContext(platformDisplay);
 #endif
-#if PLATFORM(WPE)
+#if USE(WPE_RENDERER)
         if (platformDisplay.type() == PlatformDisplay::Type::WPE)
             context = createWPEContext(platformDisplay);
 #endif
@@ -359,7 +361,7 @@ GLContextEGL::~GLContextEGL()
 #if PLATFORM(WAYLAND)
     destroyWaylandWindow();
 #endif
-#if PLATFORM(WPE)
+#if USE(WPE_RENDERER)
     destroyWPETarget();
 #endif
 }

Source/WebCore/platform/graphics/egl/GLContextEGL.h
@@ -32,7 +32,7 @@
 struct wl_egl_window;
 #endif
 
-#if USE(LIBWPE)
+#if USE(WPE_RENDERER)
 struct wpe_renderer_backend_egl_offscreen_target;
 #endif
 
@@ -82,7 +82,7 @@ private:
     GLContextEGL(PlatformDisplay&, EGLContext, EGLSurface, WlUniquePtr<struct wl_surface>&&, struct wl_egl_window*);
     void destroyWaylandWindow();
 #endif
-#if USE(LIBWPE)
+#if USE(WPE_RENDERER)
     GLContextEGL(PlatformDisplay&, EGLContext, EGLSurface, struct wpe_renderer_backend_egl_offscreen_target*);
     void destroyWPETarget();
 #endif
@@ -98,7 +98,7 @@ private:
     static std::unique_ptr<GLContextEGL> createWaylandContext(PlatformDisplay&, EGLContext sharingContext = nullptr);
     static EGLSurface createWindowSurfaceWayland(EGLDisplay, EGLConfig, GLNativeWindowType);
 #endif
-#if USE(LIBWPE)
+#if USE(WPE_RENDERER)
     static std::unique_ptr<GLContextEGL> createWPEContext(PlatformDisplay&, EGLContext sharingContext = nullptr);
     static EGLSurface createWindowSurfaceWPE(EGLDisplay, EGLConfig, GLNativeWindowType);
 #endif
@@ -115,7 +115,7 @@ private:
     WlUniquePtr<struct wl_surface> m_wlSurface;
     struct wl_egl_window* m_wlWindow { nullptr };
 #endif
-#if USE(LIBWPE)
+#if USE(WPE_RENDERER)
     struct wpe_renderer_backend_egl_offscreen_target* m_wpeTarget { nullptr };
 #endif
 #if USE(CAIRO)

Source/WebCore/platform/graphics/egl/GLContextEGLLibWPE.cpp
@@ -19,8 +19,7 @@
 #include "config.h"
 #include "GLContextEGL.h"
 
-#if USE(EGL) && USE(LIBWPE)
-
+#if USE(EGL) && USE(WPE_RENDERER)
 #include "PlatformDisplayLibWPE.h"
 
 #if USE(LIBEPOXY)
@@ -29,6 +28,11 @@
 #define __GBM__ 1
 #include "EpoxyEGL.h"
 #else
+#if PLATFORM(WAYLAND)
+// These includes need to be in this order because wayland-egl.h defines WL_EGL_PLATFORM
+// and eglplatform.h, included by egl.h, checks that to decide whether it's Wayland platform.
+#include <wayland-egl.h>
+#endif
 #include <EGL/egl.h>
 #endif
 
@@ -96,4 +100,4 @@ void GLContextEGL::destroyWPETarget()
 
 } // namespace WebCore
 
-#endif // USE(EGL) && USE(LIBWPE)
+#endif // USE(EGL) && USE(WPE_RENDERER)

commit 3f669177a966c5522443ba4b944ce2c0dbac17dc

[GTK] Fix memory leak introduced in r245807 https://bugs.webkit.org/show_bug.cgi?id=198369
Reviewed by Michael Catanzaro.

We should destroy the wpe_view_backend_exportable_fdo created in AcceleratedBackingStoreWayland.

* UIProcess/gtk/AcceleratedBackingStoreWayland.cpp:
(WebKit::AcceleratedBackingStoreWayland::~AcceleratedBackingStoreWayland):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245955 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreWayland.cpp
@@ -132,6 +132,7 @@ AcceleratedBackingStoreWayland::~AcceleratedBackingStoreWayland()
         if (makeContextCurrent())
             glDeleteTextures(1, &m_viewTexture);
     }
+    wpe_view_backend_exportable_fdo_destroy(m_exportable);
 #else
     WaylandCompositor::singleton().unregisterWebPage(m_webPage);
 #endif