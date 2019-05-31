fberlakovich/r2c-webkit

commit edad43969d6183909a9b35c3f4e6afb4113fdce4

[IntersectionObserver] Regression: No initial observation when nothing else triggers rendering https://bugs.webkit.org/show_bug.cgi?id=197891
Reviewed by Simon Fraser.

Source/WebCore:

Schedule a rendering update whenever a new IntersectionObserver target is added.

Test: intersection-observer/initial-observation.html

* page/IntersectionObserver.cpp:
(WebCore::IntersectionObserver::observe):

LayoutTests:

Add a test where other timers that can schedule rendering updates are disabled,
in order to verify that IntersectionObserver is scheduling rendering updates
itself.

* intersection-observer/initial-observation.html: Added.


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245396 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/IntersectionObserver.cpp
@@ -158,6 +158,7 @@ void IntersectionObserver::observe(Element& target)
     auto* document = trackingDocument();
     if (!hadObservationTargets)
         document->addIntersectionObserver(*this);
+    document->scheduleRenderingUpdate();
 }
 
 void IntersectionObserver::unobserve(Element& target)

LayoutTests/intersection-observer/initial-observation-expected.txt
@@ -1,0 +1,3 @@
+
+PASS An initial observation is fired even when nothing else triggers rendering 
+

LayoutTests/intersection-observer/initial-observation.html
@@ -1,0 +1,39 @@
+<!DOCTYPE html>
+<script src="../resources/testharness.js"></script>
+<script src="../resources/testharnessreport.js"></script>
+
+<script>
+if (window.internals) {
+    // Disable timers that trigger delayed flushes, since these will make
+    // this test pass spuriously, even if IntersectionObserver never
+    // schedules any rendering updates.
+    internals.setSpeculativeTilingDelayDisabledForTesting(true);
+    internals.disableTileSizeUpdateDelay();
+}
+
+window.onload = function() {
+async_test((t) => {
+    var observer = new IntersectionObserver(t.step_func_done(() => {
+        observer.disconnect();
+    }));
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            setTimeout(() => {
+                observer.observe(document.getElementById("target"));
+            }, 0);
+        });
+    });
+}, "An initial observation is fired even when nothing else triggers rendering");
+};
+</script>
+
+<style>
+#target {
+  width: 100px;
+  height: 100px;
+  background-color: green;
+}
+</style>
+
+<div id="target"></div>
+

commit 6193928a6a6b2ea02c551e9bf9dc693292b92439

REGRESSION (r245396): Page load time performance regression https://bugs.webkit.org/show_bug.cgi?id=198382
Reviewed by Simon Fraser.

Delay the scheduling of a rendering update by 500ms when a new
IntersectionObserver target is added during page load. This addresses
a page load time regression from r245396, which immediately scheduled a
rendering update when a target is added. Note that even with this change,
if anything else triggers a rendering update before the 500ms delay expires,
intersection observations will be updated during that rendering update.

Covered by intersection-observer/initial-observation.html

* dom/Document.cpp:
(WebCore::Document::updateIntersectionObservations):
(WebCore::Document::scheduleInitialIntersectionObservationUpdate):
* dom/Document.h:
* page/IntersectionObserver.cpp:
(WebCore::IntersectionObserver::observe):


git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245958 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/Document.cpp
@@ -352,6 +352,10 @@ private:
     bool m_disallowLayout { false };
 };
 
+#if ENABLE(INTERSECTION_OBSERVER)
+static const Seconds intersectionObserversInitialUpdateDelay { 500_ms };
+#endif
+
 // DOM Level 2 says (letters added):
 //
 // a) Name start characters must have one of the categories Ll, Lu, Lo, Lt, Nl.
@@ -536,6 +540,7 @@ Document::Document(Frame* frame, const URL& url, unsigned documentClasses, unsig
 #endif
 #if ENABLE(INTERSECTION_OBSERVER)
     , m_intersectionObserversNotifyTimer(*this, &Document::notifyIntersectionObserversTimerFired)
+    , m_intersectionObserversInitialUpdateTimer(*this, &Document::scheduleRenderingUpdate)
 #endif
     , m_loadEventDelayTimer(*this, &Document::loadEventDelayTimerFired)
 #if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
@@ -7416,6 +7421,8 @@ void Document::updateIntersectionObservations()
     if (!frameView)
         return;
 
+    m_intersectionObserversInitialUpdateTimer.stop();
+
     bool needsLayout = frameView->layoutContext().isLayoutPending() || (renderView() && renderView()->needsLayout());
     if (needsLayout || hasPendingStyleRecalc())
         return;
@@ -7503,6 +7510,14 @@ void Document::notifyIntersectionObserversTimerFired()
             observer->notify();
     }
     m_intersectionObserversWithPendingNotifications.clear();
+}
+
+void Document::scheduleInitialIntersectionObservationUpdate()
+{
+    if (m_readyState == Complete)
+        scheduleRenderingUpdate();
+    else if (!m_intersectionObserversInitialUpdateTimer.isActive())
+        m_intersectionObserversInitialUpdateTimer.startOneShot(intersectionObserversInitialUpdateDelay);
 }
 #endif
 

Source/WebCore/dom/Document.h
@@ -1387,6 +1387,7 @@ public:
     void removeIntersectionObserver(IntersectionObserver&);
     unsigned numberOfIntersectionObservers() const { return m_intersectionObservers.size(); }
     void updateIntersectionObservations();
+    void scheduleInitialIntersectionObservationUpdate();
 #endif
 
 #if ENABLE(RESIZE_OBSERVER)
@@ -1821,6 +1822,7 @@ private:
     Vector<WeakPtr<IntersectionObserver>> m_intersectionObservers;
     Vector<WeakPtr<IntersectionObserver>> m_intersectionObserversWithPendingNotifications;
     Timer m_intersectionObserversNotifyTimer;
+    Timer m_intersectionObserversInitialUpdateTimer;
 #endif
 
 #if ENABLE(RESIZE_OBSERVER)

Source/WebCore/page/IntersectionObserver.cpp
@@ -158,7 +158,7 @@ void IntersectionObserver::observe(Element& target)
     auto* document = trackingDocument();
     if (!hadObservationTargets)
         document->addIntersectionObserver(*this);
-    document->scheduleRenderingUpdate();
+    document->scheduleInitialIntersectionObservationUpdate();
 }
 
 void IntersectionObserver::unobserve(Element& target)