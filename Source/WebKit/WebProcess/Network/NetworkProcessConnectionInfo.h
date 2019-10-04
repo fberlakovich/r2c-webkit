fberlakovich/r2c-webkit

commit 0d1bc2deecd52ac48db99bf0c446025176f2d364

REGRESSION(249649): Unable to open local files in MiniBrowser on macOS https://bugs.webkit.org/show_bug.cgi?id=201798
Reviewed by Brent Fulgham.

Source/WebKit:

The commit <https://trac.webkit.org/changeset/249649> introduced a MiniBrowser regression on macOS where MiniBrowser
is not able to open local files. The change set r249649 fixed a problem where the WebContent process PID was not ready
to be used when creating a sandbox extension. This happened in the cases where the WebContent process had not finished
launching when the load started. The WebContent process is also creating sandbox extensions for the Networking process
for the files being loaded, and also needs to be passing the audit token of the Networking process when creating these.
This patch implements getting the audit token for the Networking process when the WebProcess is sending a message to
the UI process to establish the Networking process connection. The audit token is stored in the NetworkProcessConnection
object, and passed to the NetworkLoadParameters object when the load is started. Finally, when encoding the Network
resources load parameters, the audit token is used to create an extension for the Networking process.

API test: WKWebView.LoadFileWithLoadRequest

* NetworkProcess/NetworkLoadParameters.h:
* NetworkProcess/NetworkResourceLoadParameters.cpp:
(WebKit::NetworkResourceLoadParameters::encode const):
* Platform/IPC/ArgumentCoders.cpp:
(IPC::ArgumentCoder<audit_token_t>::encode):
(IPC::ArgumentCoder<audit_token_t>::decode):
* Platform/IPC/ArgumentCoders.h:
* Shared/Cocoa/SandboxExtensionCocoa.mm:
(WebKit::SandboxExtensionImpl::create):
(WebKit::SandboxExtensionImpl::sandboxExtensionForType):
(WebKit::SandboxExtensionImpl::SandboxExtensionImpl):
(WebKit::SandboxExtension::createHandleForMachLookupByPid):
(WebKit::SandboxExtension::createHandleForReadByPid):
(WebKit::SandboxExtension::createHandleForReadByAuditToken):
* Shared/SandboxExtension.h:

Source/WebKit/WebProcess/Network/NetworkProcessConnectionInfo.h
@@ -1,0 +1,70 @@
+/*
+* Copyright (C) 2019 Apple Inc. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+* THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+namespace WebKit {
+
+struct NetworkProcessConnectionInfo {
+    IPC::Attachment connection;
+#if HAVE(AUDIT_TOKEN)
+    Optional<audit_token_t> auditToken;
+#endif
+
+    IPC::Connection::Identifier identifier()
+    {
+#if USE(UNIX_DOMAIN_SOCKETS)
+        return IPC::Connection::Identifier(connection.fileDescriptor());
+#elif OS(DARWIN)
+        return IPC::Connection::Identifier(connection.port());
+#elif OS(WINDOWS)
+        return IPC::Connection::Identifier(connection.handle());
+#else
+        ASSERT_NOT_REACHED();
+        return IPC::Connection::Identifier();
+#endif
+    }
+    
+    void encode(IPC::Encoder& encoder) const
+    {
+        encoder << connection;
+#if HAVE(AUDIT_TOKEN)
+        encoder << auditToken;
+#endif
+    }
+    
+    static bool decode(IPC::Decoder& decoder, NetworkProcessConnectionInfo& info)
+    {
+        if (!decoder.decode(info.connection))
+            return false;
+#if HAVE(AUDIT_TOKEN)
+        if (!decoder.decode(info.auditToken))
+            return false;
+#endif
+        return true;
+    }
+};
+
+};

Source/WebKit/WebProcess/WebProcess.cpp
@@ -38,6 +38,7 @@
 #include "Logging.h"
 #include "NetworkConnectionToWebProcessMessages.h"
 #include "NetworkProcessConnection.h"
+#include "NetworkProcessConnectionInfo.h"
 #include "NetworkSession.h"
 #include "NetworkSessionCreationParameters.h"
 #include "PluginProcessConnectionManager.h"
@@ -1137,10 +1138,10 @@ void WebProcess::setInjectedBundleParameters(const IPC::DataReference& value)
     injectedBundle->setBundleParameters(value);
 }
 
-static IPC::Connection::Identifier getNetworkProcessConnection(IPC::Connection& connection)
+static NetworkProcessConnectionInfo getNetworkProcessConnection(IPC::Connection& connection)
 {
-    IPC::Attachment encodedConnectionIdentifier;
-    if (!connection.sendSync(Messages::WebProcessProxy::GetNetworkProcessConnection(), Messages::WebProcessProxy::GetNetworkProcessConnection::Reply(encodedConnectionIdentifier), 0)) {
+    NetworkProcessConnectionInfo connectionInfo;
+    if (!connection.sendSync(Messages::WebProcessProxy::GetNetworkProcessConnection(), Messages::WebProcessProxy::GetNetworkProcessConnection::Reply(connectionInfo), 0)) {
 #if PLATFORM(GTK) || PLATFORM(WPE)
         // GTK+ and WPE ports don't exit on send sync message failure.
         // In this particular case, the network process can be terminated by the UI process while the
@@ -1153,16 +1154,7 @@ static IPC::Connection::Identifier getNetworkProcessConnection(IPC::Connection& 
 #endif
     }
 
-#if USE(UNIX_DOMAIN_SOCKETS)
-    return encodedConnectionIdentifier.releaseFileDescriptor();
-#elif OS(DARWIN)
-    return encodedConnectionIdentifier.port();
-#elif OS(WINDOWS)
-    return encodedConnectionIdentifier.handle();
-#else
-    ASSERT_NOT_REACHED();
-    return IPC::Connection::Identifier();
-#endif
+    return connectionInfo;
 }
 
 NetworkProcessConnection& WebProcess::ensureNetworkProcessConnection()
@@ -1172,17 +1164,20 @@ NetworkProcessConnection& WebProcess::ensureNetworkProcessConnection()
 
     // If we've lost our connection to the network process (e.g. it crashed) try to re-establish it.
     if (!m_networkProcessConnection) {
-        IPC::Connection::Identifier connectionIdentifier = getNetworkProcessConnection(*parentProcessConnection());
+        auto connectionInfo = getNetworkProcessConnection(*parentProcessConnection());
 
         // Retry once if the IPC to get the connectionIdentifier succeeded but the connectionIdentifier we received
         // is invalid. This may indicate that the network process has crashed.
-        if (!IPC::Connection::identifierIsValid(connectionIdentifier))
-            connectionIdentifier = getNetworkProcessConnection(*parentProcessConnection());
+        if (!IPC::Connection::identifierIsValid(connectionInfo.identifier()))
+            connectionInfo = getNetworkProcessConnection(*parentProcessConnection());
 
-        if (!IPC::Connection::identifierIsValid(connectionIdentifier))
+        if (!IPC::Connection::identifierIsValid(connectionInfo.identifier()))
             CRASH();
 
-        m_networkProcessConnection = NetworkProcessConnection::create(connectionIdentifier);
+        m_networkProcessConnection = NetworkProcessConnection::create(connectionInfo.identifier());
+#if HAVE(AUDIT_TOKEN)
+        m_networkProcessConnection->setNetworkProcessAuditToken(WTFMove(connectionInfo.auditToken));
+#endif
     }
     
     return *m_networkProcessConnection;

Source/WTF/wtf/Platform.h
@@ -693,6 +693,7 @@
 #define HAVE_PTHREAD_SETNAME_NP 1
 #define HAVE_READLINE 1
 #define HAVE_SYS_TIMEB_H 1
+#define HAVE_AUDIT_TOKEN 1
 
 #if __has_include(<mach/mach_exc.defs>) && !PLATFORM(GTK)
 #define HAVE_MACH_EXCEPTIONS 1
@@ -1525,6 +1526,10 @@
 
 #if (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
 #define HAVE_SANDBOX_ISSUE_READ_EXTENSION_TO_PROCESS_BY_PID 1
+#endif
+
+#if (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
+#define HAVE_SANDBOX_ISSUE_READ_EXTENSION_TO_PROCESS_BY_AUDIT_TOKEN 1
 #endif
 
 #if (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500) || (PLATFORM(IOS_FAMILY) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 130000)

Source/WebKit/NetworkProcess/NetworkLoadParameters.h
@@ -46,6 +46,9 @@ public:
     WebCore::FrameIdentifier webFrameID;
     RefPtr<WebCore::SecurityOrigin> topOrigin;
     WTF::ProcessID parentPID { 0 };
+#if HAVE(AUDIT_TOKEN)
+    Optional<audit_token_t> networkProcessAuditToken;
+#endif
     WebCore::ResourceRequest request;
     WebCore::ContentSniffingPolicy contentSniffingPolicy { WebCore::ContentSniffingPolicy::SniffContent };
     WebCore::ContentEncodingSniffingPolicy contentEncodingSniffingPolicy { WebCore::ContentEncodingSniffingPolicy::Sniff };

Source/WebKit/NetworkProcess/NetworkResourceLoadParameters.cpp
@@ -66,7 +66,14 @@ void NetworkResourceLoadParameters::encode(IPC::Encoder& encoder) const
 
     if (request.url().isLocalFile()) {
         SandboxExtension::Handle requestSandboxExtension;
+#if HAVE(SANDBOX_ISSUE_READ_EXTENSION_TO_PROCESS_BY_AUDIT_TOKEN)
+        if (networkProcessAuditToken)
+            SandboxExtension::createHandleForReadByAuditToken(request.url().fileSystemPath(), *networkProcessAuditToken, requestSandboxExtension);
+        else
+            SandboxExtension::createHandle(request.url().fileSystemPath(), SandboxExtension::Type::ReadOnly, requestSandboxExtension);
+#else
         SandboxExtension::createHandle(request.url().fileSystemPath(), SandboxExtension::Type::ReadOnly, requestSandboxExtension);
+#endif
         encoder << requestSandboxExtension;
     }
 

Source/WebKit/Platform/IPC/ArgumentCoders.cpp
@@ -188,4 +188,21 @@ bool ArgumentCoder<SHA1::Digest>::decode(Decoder& decoder, SHA1::Digest& digest)
     return decoder.decodeFixedLengthData(digest.data(), sizeof(digest), 1);
 }
 
+#if HAVE(AUDIT_TOKEN)
+void ArgumentCoder<audit_token_t>::encode(Encoder& encoder, const audit_token_t& auditToken)
+{
+    for (unsigned i = 0; i < WTF_ARRAY_LENGTH(auditToken.val); i++)
+        encoder << auditToken.val[i];
+}
+
+bool ArgumentCoder<audit_token_t>::decode(Decoder& decoder, audit_token_t& auditToken)
+{
+    for (unsigned i = 0; i < WTF_ARRAY_LENGTH(auditToken.val); i++) {
+        if (!decoder.decode(auditToken.val[i]))
+            return false;
+    }
+    return true;
+}
+#endif
+
 } // namespace IPC

Source/WebKit/Platform/IPC/ArgumentCoders.h
@@ -621,4 +621,11 @@ template<> struct ArgumentCoder<SHA1::Digest> {
     static bool decode(Decoder&, SHA1::Digest&);
 };
 
+#if HAVE(AUDIT_TOKEN)
+template<> struct ArgumentCoder<audit_token_t> {
+    static void encode(Encoder&, const audit_token_t&);
+    static bool decode(Decoder&, audit_token_t&);
+};
+#endif
+
 } // namespace IPC

Source/WebKit/Shared/Cocoa/SandboxExtensionCocoa.mm
@@ -32,6 +32,7 @@
 #import "Decoder.h"
 #import "Encoder.h"
 #import <wtf/FileSystem.h>
+#import <wtf/Variant.h>
 #import <wtf/spi/darwin/SandboxSPI.h>
 #import <wtf/text/CString.h>
 
@@ -40,9 +41,9 @@ namespace WebKit {
 class SandboxExtensionImpl {
     WTF_MAKE_FAST_ALLOCATED;
 public:
-    static std::unique_ptr<SandboxExtensionImpl> create(const char* path, SandboxExtension::Type type, Optional<pid_t> pid = WTF::nullopt)
+    static std::unique_ptr<SandboxExtensionImpl> create(const char* path, SandboxExtension::Type type, Optional<Variant<pid_t, audit_token_t>> processToken = WTF::nullopt)
     {
-        std::unique_ptr<SandboxExtensionImpl> impl { new SandboxExtensionImpl(path, type, pid) };
+        std::unique_ptr<SandboxExtensionImpl> impl { new SandboxExtensionImpl(path, type, processToken) };
         if (!impl->m_token)
             return nullptr;
         return impl;
@@ -84,7 +85,7 @@ public:
     }
 
 private:
-    char* sandboxExtensionForType(const char* path, SandboxExtension::Type type, Optional<pid_t> pid = WTF::nullopt)
+    char* sandboxExtensionForType(const char* path, SandboxExtension::Type type, Optional<Variant<pid_t, audit_token_t>> processToken)
     {
         switch (type) {
         case SandboxExtension::Type::ReadOnly:
@@ -93,27 +94,37 @@ private:
             return sandbox_extension_issue_file(APP_SANDBOX_READ_WRITE, path, 0);
         case SandboxExtension::Type::Mach:
 #if HAVE(SANDBOX_ISSUE_MACH_EXTENSION_TO_PROCESS_BY_PID)
-            return sandbox_extension_issue_mach_to_process_by_pid("com.apple.webkit.extension.mach"_s, path, 0, pid.value());
+            if (!processToken)
+                return nullptr;
+            if (!WTF::holds_alternative<pid_t>(*processToken))
+                return nullptr;
+            return sandbox_extension_issue_mach_to_process_by_pid("com.apple.webkit.extension.mach"_s, path, 0, WTF::get<pid_t>(*processToken));
 #else
-            UNUSED_PARAM(pid);
+            UNUSED_PARAM(processToken);
             ASSERT_NOT_REACHED();
             return nullptr;
 #endif
         case SandboxExtension::Type::Generic:
             return sandbox_extension_issue_generic(path, 0);
-        case SandboxExtension::Type::ReadByPid:
+        case SandboxExtension::Type::ReadByProcess:
 #if HAVE(SANDBOX_ISSUE_READ_EXTENSION_TO_PROCESS_BY_PID)
-            return sandbox_extension_issue_file_to_process_by_pid(APP_SANDBOX_READ, path, SANDBOX_EXTENSION_USER_INTENT, pid.value());
+            if (!processToken)
+                return nullptr;
+            if (WTF::holds_alternative<pid_t>(*processToken))
+                return sandbox_extension_issue_file_to_process_by_pid(APP_SANDBOX_READ, path, SANDBOX_EXTENSION_USER_INTENT, WTF::get<pid_t>(*processToken));
+            if (WTF::holds_alternative<audit_token_t>(*processToken))
+                return sandbox_extension_issue_file_to_process(APP_SANDBOX_READ, path, SANDBOX_EXTENSION_USER_INTENT, WTF::get<audit_token_t>(*processToken));
+            return nullptr;
 #else
-            UNUSED_PARAM(pid);
+            UNUSED_PARAM(processToken);
             ASSERT_NOT_REACHED();
             return nullptr;
 #endif
         }
     }
 
-    SandboxExtensionImpl(const char* path, SandboxExtension::Type type, Optional<pid_t> pid = WTF::nullopt)
-        : m_token { sandboxExtensionForType(path, type, pid) }
+    SandboxExtensionImpl(const char* path, SandboxExtension::Type type, Optional<Variant<pid_t, audit_token_t>> processToken)
+        : m_token { sandboxExtensionForType(path, type, processToken) }
     {
     }
 
@@ -336,7 +347,8 @@ bool SandboxExtension::createHandleForMachLookupByPid(const String& service, pid
 {
     ASSERT(!handle.m_sandboxExtension);
     
-    handle.m_sandboxExtension = SandboxExtensionImpl::create(service.utf8().data(), Type::Mach, pid);
+    Variant<pid_t, audit_token_t> processToken(pid);
+    handle.m_sandboxExtension = SandboxExtensionImpl::create(service.utf8().data(), Type::Mach, WTFMove(processToken));
     if (!handle.m_sandboxExtension) {
         WTFLogAlways("Could not create a '%s' sandbox extension", service.utf8().data());
         return false;
@@ -352,7 +364,22 @@ bool SandboxExtension::createHandleForReadByPid(const String& path, ProcessID pi
     if (!pid)
         return false;
 
-    handle.m_sandboxExtension = SandboxExtensionImpl::create(path.utf8().data(), Type::ReadByPid, pid);
+    Variant<pid_t, audit_token_t> processToken(pid);
+    handle.m_sandboxExtension = SandboxExtensionImpl::create(path.utf8().data(), Type::ReadByProcess, WTFMove(processToken));
+    if (!handle.m_sandboxExtension) {
+        WTFLogAlways("Could not create sandbox extension");
+        return false;
+    }
+    
+    return true;
+}
+
+bool SandboxExtension::createHandleForReadByAuditToken(const String& path, audit_token_t auditToken, Handle& handle)
+{
+    ASSERT(!handle.m_sandboxExtension);
+
+    Variant<pid_t, audit_token_t> processToken(auditToken);
+    handle.m_sandboxExtension = SandboxExtensionImpl::create(path.utf8().data(), Type::ReadByProcess, WTFMove(processToken));
     if (!handle.m_sandboxExtension) {
         WTFLogAlways("Could not create sandbox extension");
         return false;

commit 5a1682a6cba549bb0604f84e4d7961108d0ba222

[GTK][WPE] REGRESSION(r250673): connection to network process is broken after r250673 https://bugs.webkit.org/show_bug.cgi?id=202575
Reviewed by Carlos Alberto Lopez Perez.

The file descriptor is now closed when the IPC::Attachment is destroyed in NetworkProcessConnectionInfo
destructor. In case of unix domain sockets we need to transfer the ownership of the fd to
NetworkProcessConnection, but NetworkProcessConnectionInfo is ow keeping the ownership.

* WebProcess/Network/NetworkProcessConnectionInfo.h:
(WebKit::NetworkProcessConnectionInfo::releaseIdentifier): Use IPC::Attachment::releaseFileDescriptor() in case
of unix domain sockets.
* WebProcess/WebProcess.cpp:
(WebKit::WebProcess::ensureNetworkProcessConnection): Use NetworkProcessConnectionInfo::releaseIdentifier() when
passing the identifier to NetworkProcessConnection.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@250713 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/WebProcess/Network/NetworkProcessConnectionInfo.h
@@ -46,7 +46,18 @@ struct NetworkProcessConnectionInfo {
         return IPC::Connection::Identifier();
 #endif
     }
-    
+
+    IPC::Connection::Identifier releaseIdentifier()
+    {
+#if USE(UNIX_DOMAIN_SOCKETS)
+        auto returnValue = IPC::Connection::Identifier(connection.releaseFileDescriptor());
+#else
+        auto returnValue = identifier();
+#endif
+        connection = { };
+        return returnValue;
+    }
+
     void encode(IPC::Encoder& encoder) const
     {
         encoder << connection;

Source/WebKit/WebProcess/WebProcess.cpp
@@ -1174,7 +1174,7 @@ NetworkProcessConnection& WebProcess::ensureNetworkProcessConnection()
         if (!IPC::Connection::identifierIsValid(connectionInfo.identifier()))
             CRASH();
 
-        m_networkProcessConnection = NetworkProcessConnection::create(connectionInfo.identifier());
+        m_networkProcessConnection = NetworkProcessConnection::create(connectionInfo.releaseIdentifier());
 #if HAVE(AUDIT_TOKEN)
         m_networkProcessConnection->setNetworkProcessAuditToken(WTFMove(connectionInfo.auditToken));
 #endif