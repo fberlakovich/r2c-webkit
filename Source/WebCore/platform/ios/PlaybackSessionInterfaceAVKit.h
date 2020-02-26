fberlakovich/r2c-webkit

commit 555006d628c2758b076e55e49b6559acd19ee8d5

Stop using QTKit for rendering WK1 video fullscreen controls https://bugs.webkit.org/show_bug.cgi?id=207795 <rdar://problem/48894915>
Reviewed by Jon Lee.

Source/WebCore:

Drive-by fixes:

- Don't set the video layer to not-visible when in video fullscreen mode.
- Export previously un-exported methods.
- Make some ios-only classes available on mac.
- Add a couple previously unimplemented properties to WebAVPlayerController needed by AVKit.
- WebCoreFullscreenWindow doesn't depend on FULLSCREEN_API.

* WebCore.xcodeproj/project.pbxproj:
* html/HTMLMediaElement.cpp:
(WebCore::HTMLMediaElement::visibilityStateChanged):
* html/HTMLMediaElement.h:
(WebCore::HTMLMediaElement::setVideoFullscreenLayer):
* platform/ios/PlaybackSessionInterfaceAVKit.h:
* platform/ios/PlaybackSessionInterfaceAVKit.mm:
(WebCore::PlaybackSessionInterfaceAVKit::externalPlaybackChanged):
* platform/ios/WebAVPlayerController.h:
* platform/ios/WebAVPlayerController.mm:
(-[WebAVPlayerController canSeekFrameBackward]):
(-[WebAVPlayerController canSeekFrameForward]):
* platform/mac/WebCoreFullScreenWindow.h:
* platform/mac/WebCoreFullScreenWindow.mm:

Source/WebCore/PAL:

* PAL.xcodeproj/project.pbxproj:
* pal/spi/cocoa/AVKitSPI.h:
* pal/spi/mac/NSWindowSPI.h:
* pal/spi/mac/QTKitSPI.h: Removed.

Source/WebKit:

Use NSWindowSPI.h rather than declaring SPI in the source file.

* UIProcess/mac/WKFullScreenWindowController.mm:

Source/WebKitLegacy/mac:

Switch to using AVPlayerView instead of QTHUD to render our fullscreen controls.

In addition, switch to a modern NSWindow fullscreen API, with custom, smooth animations which
move the fullscreen window to a separate space. This requires subclassing AVPlayerView so that
fullscreen toggling will signal to the controller, so that our fullscreen button and the inline
version of same don't get out of sync. Because AVPlayerView requires an AVPlayer in order to
display the video layer, and we won't necessarily have an AVPlayer for MSE-backed videos, instead

Source/WebCore/platform/ios/PlaybackSessionInterfaceAVKit.h
@@ -26,7 +26,7 @@
 
 #pragma once
 
-#if PLATFORM(IOS_FAMILY)
+#if HAVE(AVKIT)
 
 #include "EventListener.h"
 #include "HTMLMediaElementEnums.h"
@@ -41,10 +41,6 @@
 #include <wtf/RetainPtr.h>
 
 OBJC_CLASS WebAVPlayerController;
-OBJC_CLASS AVPlayerViewController;
-OBJC_CLASS UIViewController;
-OBJC_CLASS UIWindow;
-OBJC_CLASS UIView;
 
 namespace WebCore {
 class IntRect;
@@ -92,5 +88,4 @@ protected:
 
 }
 
-#endif
-
+#endif // HAVE(AVKIT)

Source/WebCore/platform/ios/PlaybackSessionInterfaceAVKit.mm
@@ -27,7 +27,6 @@
 #import "config.h"
 #import "PlaybackSessionInterfaceAVKit.h"
 
-#if PLATFORM(IOS_FAMILY)
 #if HAVE(AVKIT)
 
 #import "Logging.h"
@@ -185,9 +184,9 @@ void PlaybackSessionInterfaceAVKit::legibleMediaSelectionOptionsChanged(const Ve
 void PlaybackSessionInterfaceAVKit::externalPlaybackChanged(bool enabled, PlaybackSessionModel::ExternalPlaybackTargetType targetType, const String& localizedDeviceName)
 {
     AVPlayerControllerExternalPlaybackType externalPlaybackType = AVPlayerControllerExternalPlaybackTypeNone;
-    if (targetType == PlaybackSessionModel::TargetTypeAirPlay)
+    if (enabled && targetType == PlaybackSessionModel::TargetTypeAirPlay)
         externalPlaybackType = AVPlayerControllerExternalPlaybackTypeAirPlay;
-    else if (targetType == PlaybackSessionModel::TargetTypeTVOut)
+    else if (enabled && targetType == PlaybackSessionModel::TargetTypeTVOut)
         externalPlaybackType = AVPlayerControllerExternalPlaybackTypeTVOut;
 
     WebAVPlayerController* playerController = m_playerController.get();
@@ -231,4 +230,3 @@ void PlaybackSessionInterfaceAVKit::modelDestroyed()
 }
 
 #endif // HAVE(AVKIT)
-#endif // PLATFORM(IOS_FAMILY)

Source/WebCore/platform/ios/WebAVPlayerController.h
@@ -23,7 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#if PLATFORM(IOS_FAMILY) && HAVE(AVKIT)
+#if HAVE(AVKIT)
 
 #import <pal/spi/cocoa/AVKitSPI.h>
 
@@ -36,7 +36,7 @@ class PlaybackSessionInterfaceAVKit;
 @property (retain) NSString *localizedDisplayName;
 @end
 
-@interface WebAVPlayerController : NSObject {
+WEBCORE_EXPORT @interface WebAVPlayerController : NSObject {
     WebAVMediaSelectionOption *_currentAudioMediaSelectionOption;
     WebAVMediaSelectionOption *_currentLegibleMediaSelectionOption;
     BOOL _pictureInPictureInterrupted;
@@ -52,6 +52,9 @@ class PlaybackSessionInterfaceAVKit;
 @property (readonly) BOOL canSeekToBeginning;
 @property (readonly) BOOL canSeekToEnd;
 @property (readonly) BOOL isScrubbing;
+@property (readonly) BOOL canSeekFrameBackward;
+@property (readonly) BOOL canSeekFrameForward;
+@property (readonly) BOOL hasContentChapters;
 
 @property BOOL canPlay;
 @property (getter=isPlaying) BOOL playing;
@@ -100,4 +103,3 @@ class PlaybackSessionInterfaceAVKit;
 @end
 
 #endif
-

Source/WebCore/platform/ios/WebAVPlayerController.mm
@@ -27,7 +27,7 @@
 #import "config.h"
 #import "WebAVPlayerController.h"
 
-#if PLATFORM(IOS_FAMILY) && HAVE(AVKIT)
+#if HAVE(AVKIT)
 
 #import "Logging.h"
 #import "PlaybackSessionInterfaceAVKit.h"
@@ -397,6 +397,16 @@ static double WebAVPlayerControllerLiveStreamSeekableTimeRangeMinimumDuration = 
     [self seekToEnd:sender];
 }
 
+- (BOOL)canSeekFrameBackward
+{
+    return YES;
+}
+
+- (BOOL)canSeekFrameForward
+{
+    return YES;
+}
+
 - (BOOL)hasMediaSelectionOptions
 {
     return [self hasAudioMediaSelectionOptions] || [self hasLegibleMediaSelectionOptions];
@@ -650,5 +660,5 @@ static double WebAVPlayerControllerLiveStreamSeekableTimeRangeMinimumDuration = 
 
 @end
 
-#endif // PLATFORM(IOS_FAMILY)
+#endif // HAVE(AVKIT)
 

Source/WebCore/PAL/PAL.xcodeproj/project.pbxproj
@@ -95,7 +95,6 @@
 		0C77859C1F45130F00F4EBB6 /* NSViewSPI.h in Headers */ = {isa = PBXBuildFile; fileRef = 0C7785831F45130F00F4EBB6 /* NSViewSPI.h */; };
 		0C77859D1F45130F00F4EBB6 /* NSWindowSPI.h in Headers */ = {isa = PBXBuildFile; fileRef = 0C7785841F45130F00F4EBB6 /* NSWindowSPI.h */; };
 		0C77859E1F45130F00F4EBB6 /* PIPSPI.h in Headers */ = {isa = PBXBuildFile; fileRef = 0C7785851F45130F00F4EBB6 /* PIPSPI.h */; };
-		0C77859F1F45130F00F4EBB6 /* QTKitSPI.h in Headers */ = {isa = PBXBuildFile; fileRef = 0C7785861F45130F00F4EBB6 /* QTKitSPI.h */; };
 		0C7785A01F45130F00F4EBB6 /* QuickLookMacSPI.h in Headers */ = {isa = PBXBuildFile; fileRef = 0C7785871F45130F00F4EBB6 /* QuickLookMacSPI.h */; };
 		0C7785A11F45130F00F4EBB6 /* TelephonyUtilitiesSPI.h in Headers */ = {isa = PBXBuildFile; fileRef = 0C7785881F45130F00F4EBB6 /* TelephonyUtilitiesSPI.h */; };
 		0CF99CA41F736375007EE793 /* MediaTimeAVFoundation.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 0C00CFD11F68CE4600AAC26D /* MediaTimeAVFoundation.cpp */; };
@@ -264,7 +263,6 @@
 		0C7785831F45130F00F4EBB6 /* NSViewSPI.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = NSViewSPI.h; sourceTree = "<group>"; };
 		0C7785841F45130F00F4EBB6 /* NSWindowSPI.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = NSWindowSPI.h; sourceTree = "<group>"; };
 		0C7785851F45130F00F4EBB6 /* PIPSPI.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = PIPSPI.h; sourceTree = "<group>"; };
-		0C7785861F45130F00F4EBB6 /* QTKitSPI.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = QTKitSPI.h; sourceTree = "<group>"; };
 		0C7785871F45130F00F4EBB6 /* QuickLookMacSPI.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = QuickLookMacSPI.h; sourceTree = "<group>"; };
 		0C7785881F45130F00F4EBB6 /* TelephonyUtilitiesSPI.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = TelephonyUtilitiesSPI.h; sourceTree = "<group>"; };
 		0CF99CA61F738436007EE793 /* CoreMediaSoftLink.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = CoreMediaSoftLink.cpp; sourceTree = "<group>"; };
@@ -513,7 +511,6 @@
 				0C7785831F45130F00F4EBB6 /* NSViewSPI.h */,
 				0C7785841F45130F00F4EBB6 /* NSWindowSPI.h */,
 				0C7785851F45130F00F4EBB6 /* PIPSPI.h */,
-				0C7785861F45130F00F4EBB6 /* QTKitSPI.h */,
 				0C7785871F45130F00F4EBB6 /* QuickLookMacSPI.h */,
 				A1175B481F6AFF8E00C4B9F0 /* SpeechSynthesisSPI.h */,
 				0C7785881F45130F00F4EBB6 /* TelephonyUtilitiesSPI.h */,
@@ -817,7 +814,6 @@
 				0C77859E1F45130F00F4EBB6 /* PIPSPI.h in Headers */,
 				A1175B4E1F6B337300C4B9F0 /* PopupMenu.h in Headers */,
 				0C2DA1561F3BEB4900DBC317 /* pthreadSPI.h in Headers */,
-				0C77859F1F45130F00F4EBB6 /* QTKitSPI.h in Headers */,
 				0C2DA1571F3BEB4900DBC317 /* QuartzCoreSPI.h in Headers */,
 				0C7785A01F45130F00F4EBB6 /* QuickLookMacSPI.h in Headers */,
 				4450FCA021F5F602004DFA56 /* QuickLookSoftLink.h in Headers */,

Source/WebCore/PAL/pal/spi/cocoa/AVKitSPI.h
@@ -26,6 +26,34 @@
 #import <objc/runtime.h>
 #import <wtf/SoftLinking.h>
 
+#if USE(APPLE_INTERNAL_SDK)
+#import <AVKit/AVPlayerController.h>
+#else
+#if PLATFORM(IOS_FAMILY)
+#import <UIKit/UIResponder.h>
+@interface AVPlayerController : UIResponder
+#else
+#import <AppKit/NSResponder.h>
+@interface AVPlayerController : NSResponder <NSUserInterfaceValidations>
+#endif
+@end
+
+@interface AVPlayerController ()
+typedef NS_ENUM(NSInteger, AVPlayerControllerStatus) {
+    AVPlayerControllerStatusUnknown = 0,
+    AVPlayerControllerStatusReadyToPlay = 2,
+};
+
+typedef NS_ENUM(NSInteger, AVPlayerControllerExternalPlaybackType) {
+    AVPlayerControllerExternalPlaybackTypeNone = 0,
+    AVPlayerControllerExternalPlaybackTypeAirPlay = 1,
+    AVPlayerControllerExternalPlaybackTypeTVOut = 2,
+};
+
+@property (NS_NONATOMIC_IOSONLY, readonly) AVPlayerControllerStatus status;
+@end
+#endif // USE(APPLE_INTERNAL_SDK)
+
 #if PLATFORM(IOS_FAMILY)
 #import <AVKit/AVKit.h>
 #import <QuartzCore/QuartzCore.h>
@@ -37,7 +65,6 @@
 #import <AVKit/AVBackgroundView.h>
 #endif
 
-#import <AVKit/AVPlayerController.h>
 IGNORE_WARNINGS_BEGIN("objc-property-no-attribute")
 #import <AVKit/AVPlayerLayerView.h>
 IGNORE_WARNINGS_END
@@ -115,24 +142,6 @@ typedef NS_ENUM(NSInteger, AVBackgroundViewTintEffectStyle) {
 - (void)addSubview:(UIView *)subview applyingMaterialStyle:(AVBackgroundViewMaterialStyle)materialStyle tintEffectStyle:(AVBackgroundViewTintEffectStyle)tintEffectStyle;
 @end
 
-@interface AVPlayerController : UIResponder
-@end
-
-@interface AVPlayerController ()
-typedef NS_ENUM(NSInteger, AVPlayerControllerStatus) {
-    AVPlayerControllerStatusUnknown = 0,
-    AVPlayerControllerStatusReadyToPlay = 2,
-};
-
-typedef NS_ENUM(NSInteger, AVPlayerControllerExternalPlaybackType) {
-    AVPlayerControllerExternalPlaybackTypeNone = 0,
-    AVPlayerControllerExternalPlaybackTypeAirPlay = 1,
-    AVPlayerControllerExternalPlaybackTypeTVOut = 2,
-};
-
-@property (NS_NONATOMIC_IOSONLY, readonly) AVPlayerControllerStatus status;
-@end
-
 @class AVPlayerLayer;
 
 @interface AVPictureInPicturePlayerLayerView : UIView
@@ -177,6 +186,26 @@ NS_ASSUME_NONNULL_END
 
 #endif // USE(APPLE_INTERNAL_SDK)
 #endif // PLATFORM(IOS_FAMILY)
+
+#if PLATFORM(MAC)
+#if USE(APPLE_INTERNAL_SDK)
+#import <AVKit/AVPlayerView_Private.h>
+#else
+#import <AVKit/AVPlayerView.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AVPlayerView (WebKitFullscreenSPI)
+@property AVPlayerController *playerController;
+@property (readonly) BOOL isFullScreen;
+- (void)enterFullScreen:(id)sender;
+- (void)exitFullScreen:(id)sender;
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif // USE(APPLE_INTERNAL_SDK)
+#endif // PLATFORM(MAC)
 
 #if ENABLE(WIRELESS_PLAYBACK_TARGET) && !PLATFORM(IOS_FAMILY)
 

Source/WebCore/PAL/pal/spi/mac/NSWindowSPI.h
@@ -40,7 +40,8 @@
 - (id)_oldFirstResponderBeforeBecoming;
 - (id)_newFirstResponderAfterResigning;
 - (void)_setCursorForMouseLocation:(NSPoint)point;
-
+- (void)exitFullScreenMode:(id)sender;
+- (void)enterFullScreenMode:(id)sender;
 @end
 
 enum {

Source/WebCore/PAL/pal/spi/mac/QTKitSPI.h
@@ -1,150 +1,0 @@
-/*
- * Copyright (C) 2016-2017 Apple Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#pragma once
-
-#if USE(APPLE_INTERNAL_SDK)
-
-#import <QTKit/QTHUDBackgroundView_Private.h>
-#import <QTKit/QTKit_Private.h>
-#import <QTKit/QTUtilities_Private.h>
-
-#else
-
-enum {
-    kQTTimeIsIndefinite = 1 << 0
-};
-
-typedef struct {
-    long long timeValue;
-    long timeScale;
-    long flags;
-} QTTime;
-
-typedef struct {
-    QTTime time;
-    QTTime duration;
-} QTTimeRange;
-
-typedef enum {
-    QTIncludeCommonTypes = 0,
-    QTIncludeOnlyFigMediaFileTypes = 1 << 8,
-} QTMovieFileTypeOptions;
-
-enum {
-    QTMovieTypeUnknown = 0,
-    QTMovieTypeLocal,
-    QTMovieTypeFastStart,
-    QTMovieTypeLiveStream,
-    QTMovieTypeStoredStream
-};
-
-@class QTTrack;
-
-@interface QTMedia : NSObject
-
-- (id)attributeForKey:(NSString *)attributeKey;
-
-@end
-
-@interface QTMovie : NSObject <NSCoding, NSCopying>
-
-+ (NSArray *)movieFileTypes:(QTMovieFileTypeOptions)types;
-
-+ (void)disableComponent:(ComponentDescription)component;
-
-- (id)initWithAttributes:(NSDictionary *)attributes error:(NSError **)errorPtr;
-
-- (void)play;
-- (void)stop;
-
-- (QTTime)duration;
-
-- (QTTime)currentTime;
-- (void)setCurrentTime:(QTTime)time;
-
-- (float)rate;
-- (void)setRate:(float)rate;
-
-- (float)volume;
-- (void)setVolume:(float)volume;
-
-- (NSDictionary *)movieAttributes;
-
-- (id)attributeForKey:(NSString *)attributeKey;
-- (void)setAttribute:(id)value forKey:(NSString *)attributeKey;
-
-- (NSArray *)tracks;
-
-- (NSArray *)alternateGroupTypes;
-- (NSArray *)alternatesForMediaType:(NSString *)type;
-- (void)deselectAlternateGroupTrack:(QTTrack *)alternateTrack;
-- (void)selectAlternateGroupTrack:(QTTrack *)alternateTrack;
-
-- (NSURL *)URL;
-- (UInt32)movieType;
-
-@end
-
-@interface QTMovieLayer : CALayer
-
-- (void)setMovie:(QTMovie *)movie;
-- (QTMovie *)movie;
-
-@end
-
-@interface NSValue (NSValueQTTimeRangeExtensions)
-+ (NSValue *)valueWithQTTimeRange:(QTTimeRange)range;
-- (QTTimeRange)QTTimeRangeValue;
-@end
-
-@interface QTTrack : NSObject
-
-- (QTMedia *)media;
-
-- (BOOL)isEnabled;
-- (void)setEnabled:(BOOL)enabled;
-
-- (id)attributeForKey:(NSString *)attributeKey;
-
-@end
-
-@interface QTHUDBackgroundView : NSView
-
-- (void)setContentBorderPosition:(CGFloat)contentBorderPosition;
-
-@end
-
-@interface QTUtilities : NSObject
-
-+ (id)qtUtilities;
-
-- (NSArray *)sitesInDownloadCache;
-- (void)clearDownloadCache;
-- (void)clearDownloadCacheForSite:(NSString *)site;
-
-@end
-
-#endif

commit 12853edcc63af667118053c3479a11babe39eaca

Unreviewed tvOS build fix; fix the compiler guards broken by r257189.
* platform/ios/PlaybackSessionInterfaceAVKit.h:
* platform/ios/PlaybackSessionInterfaceAVKit.mm:
* platform/ios/WebAVPlayerController.h:
* platform/ios/WebAVPlayerController.mm:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@257409 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/ios/PlaybackSessionInterfaceAVKit.h
@@ -26,7 +26,7 @@
 
 #pragma once
 
-#if HAVE(AVKIT)
+#if PLATFORM(COCOA)
 
 #include "EventListener.h"
 #include "HTMLMediaElementEnums.h"
@@ -88,4 +88,4 @@ protected:
 
 }
 
-#endif // HAVE(AVKIT)
+#endif // PLATFORM(COCOA)

Source/WebCore/platform/ios/PlaybackSessionInterfaceAVKit.mm
@@ -27,6 +27,7 @@
 #import "config.h"
 #import "PlaybackSessionInterfaceAVKit.h"
 
+#if PLATFORM(COCOA)
 #if HAVE(AVKIT)
 
 #import "Logging.h"
@@ -230,3 +231,4 @@ void PlaybackSessionInterfaceAVKit::modelDestroyed()
 }
 
 #endif // HAVE(AVKIT)
+#endif // PLATFORM(COCOA)

Source/WebCore/platform/ios/WebAVPlayerController.h
@@ -23,7 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#if HAVE(AVKIT)
+#if PLATFORM(COCOA) && HAVE(AVKIT)
 
 #import <pal/spi/cocoa/AVKitSPI.h>
 

Source/WebCore/platform/ios/WebAVPlayerController.mm
@@ -27,7 +27,7 @@
 #import "config.h"
 #import "WebAVPlayerController.h"
 
-#if HAVE(AVKIT)
+#if PLATFORM(COCOA) && HAVE(AVKIT)
 
 #import "Logging.h"
 #import "PlaybackSessionInterfaceAVKit.h"
@@ -660,5 +660,5 @@ static double WebAVPlayerControllerLiveStreamSeekableTimeRangeMinimumDuration = 
 
 @end
 
-#endif // HAVE(AVKIT)
+#endif // PLATFORM(COCOA) && HAVE(AVKIT)
 