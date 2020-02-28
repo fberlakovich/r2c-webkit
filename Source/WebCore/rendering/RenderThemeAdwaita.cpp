fberlakovich/r2c-webkit

commit aebb04b39632d0dd6c1a57348032065950c22575

[GTK] Stop using gtk foreign drawing API to style form controls https://bugs.webkit.org/show_bug.cgi?id=208129
Reviewed by Adrian Perez de Castro.

.:

Add a test case for datalist element.

* ManualTests/gtk/theme.html:

Source/WebCore:

It causes layout issues in some websites, it doesn't really work with all GTK themes and it won't be possible
with GTK4 because foreign drawing APIs have been removed. Instead, we can use the new custom style used by WPE
port, which is based on adwaita, but simplified to avoid the huge minimum control sizes, the usage of gradients
and transparencies, etc. We can still use the GTK API to get the selection colors, to keep some consistency with
the actual GTK theme, but that won't be possible with GTK4 either. This also means we won't be rendering
directly to the cairo context anymore (which was required by GTK foreign drawing), so we can use things like
threaded rendering in the GTK port now. This patch renames ScrollbarThemeWPE, RenderThemeWPE and ThemeWPE as
ScrollbarThemeAdwaita, RenderThemeAdwaita and ThemeAdwaita and adds ThemeGtk. GTK media controls CSS and
JavaScript files have been removed in favor of using the adwaita ones.

* Modules/mediacontrols/mediaControlsGtk.js: Removed.
* PlatformGTK.cmake:
* PlatformWPE.cmake:
* SourcesGTK.txt:
* SourcesWPE.txt:
* css/mediaControlsGtk.css: Removed.
* platform/adwaita/ScrollbarThemeAdwaita.cpp: Renamed from Source/WebCore/platform/wpe/ScrollbarThemeWPE.cpp.
(WebCore::ScrollbarThemeAdwaita::usesOverlayScrollbars const):
(WebCore::ScrollbarThemeAdwaita::scrollbarThickness):
(WebCore::ScrollbarThemeAdwaita::minimumThumbLength):
(WebCore::ScrollbarThemeAdwaita::hasButtons):
(WebCore::ScrollbarThemeAdwaita::hasThumb):
(WebCore::ScrollbarThemeAdwaita::backButtonRect):
(WebCore::ScrollbarThemeAdwaita::forwardButtonRect):
(WebCore::ScrollbarThemeAdwaita::trackRect):
(WebCore::ScrollbarThemeAdwaita::paint):
(WebCore::ScrollbarThemeAdwaita::handleMousePressEvent):

Source/WebCore/rendering/RenderThemeAdwaita.h
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2020 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "RenderTheme.h"
+
+namespace WebCore {
+
+class RenderThemeAdwaita : public RenderTheme {
+public:
+    virtual ~RenderThemeAdwaita() = default;
+
+private:
+    String extraDefaultStyleSheet() final;
+#if ENABLE(VIDEO)
+    String extraMediaControlsStyleSheet() final;
+    String mediaControlsScript() final;
+#endif
+
+    bool supportsHover(const RenderStyle&) const final { return true; }
+    bool supportsFocusRing(const RenderStyle&) const final;
+    bool shouldHaveCapsLockIndicator(const HTMLInputElement&) const final;
+
+    void updateCachedSystemFontDescription(CSSValueID, FontCascadeDescription&) const override { };
+
+    Color platformActiveSelectionBackgroundColor(OptionSet<StyleColor::Options>) const final;
+    Color platformInactiveSelectionBackgroundColor(OptionSet<StyleColor::Options>) const final;
+    Color platformActiveSelectionForegroundColor(OptionSet<StyleColor::Options>) const final;
+    Color platformInactiveSelectionForegroundColor(OptionSet<StyleColor::Options>) const final;
+    Color platformActiveListBoxSelectionBackgroundColor(OptionSet<StyleColor::Options>) const final;
+    Color platformActiveListBoxSelectionForegroundColor(OptionSet<StyleColor::Options>) const final;
+    Color platformInactiveListBoxSelectionBackgroundColor(OptionSet<StyleColor::Options>) const final;
+    Color platformInactiveListBoxSelectionForegroundColor(OptionSet<StyleColor::Options>) const final;
+    Color platformFocusRingColor(OptionSet<StyleColor::Options>) const final;
+    void platformColorsDidChange() final;
+
+    bool paintTextField(const RenderObject&, const PaintInfo&, const FloatRect&) final;
+    bool paintTextArea(const RenderObject&, const PaintInfo&, const FloatRect&) final;
+    bool paintSearchField(const RenderObject&, const PaintInfo&, const IntRect&) final;
+
+    bool popsMenuBySpaceOrReturn() const final { return true; }
+    LengthBox popupInternalPaddingBox(const RenderStyle&) const final;
+    bool paintMenuList(const RenderObject&, const PaintInfo&, const FloatRect&) final;
+    bool paintMenuListButtonDecorations(const RenderBox&, const PaintInfo&, const FloatRect&) final;
+
+    Seconds animationRepeatIntervalForProgressBar(RenderProgress&) const final;
+    Seconds animationDurationForProgressBar(RenderProgress&) const final;
+    IntRect progressBarRectForBounds(const RenderObject&, const IntRect&) const final;
+    bool paintProgressBar(const RenderObject&, const PaintInfo&, const IntRect&) final;
+
+    bool paintSliderTrack(const RenderObject&, const PaintInfo&, const IntRect&) final;
+    void adjustSliderThumbSize(RenderStyle&, const Element*) const final;
+    bool paintSliderThumb(const RenderObject&, const PaintInfo&, const IntRect&) final;
+
+#if ENABLE(VIDEO)
+    bool paintMediaSliderTrack(const RenderObject&, const PaintInfo&, const IntRect&) final;
+    bool paintMediaVolumeSliderTrack(const RenderObject&, const PaintInfo&, const IntRect&) final;
+#endif
+
+#if ENABLE(DATALIST_ELEMENT)
+    IntSize sliderTickSize() const final;
+    int sliderTickOffsetFromTrackCenter() const final;
+    void adjustListButtonStyle(RenderStyle&, const Element*) const final;
+#endif
+};
+
+} // namespace WebCore

ManualTests/gtk/theme.html
@@ -147,6 +147,16 @@
       </html>">
   </iframe>
 
+  <h1>Datalist</h1>
+  <input list="ports" name="port">
+  <datalist id="ports">
+    <option value="WebKitGTK">
+    <option value="WPEWebKit">
+    <option value="PlayStation">
+    <option value="macOS">
+    <option value="iOS">
+  </datalist>
+
   <h1>Media controls</h1>
   <video controls></video>
   </body>

Source/WTF/wtf/PlatformUse.h
@@ -291,7 +291,7 @@
 #define USE_CFNETWORK_CONTENT_ENCODING_SNIFFING_OVERRIDE 1
 #endif
 
-#if PLATFORM(MAC) || PLATFORM(WPE)
+#if PLATFORM(MAC) || PLATFORM(WPE) || PLATFORM(GTK)
 /* FIXME: This really needs a descriptive name, this "new theme" was added in 2008. */
 #define USE_NEW_THEME 1
 #endif

Source/WebCore/Modules/mediacontrols/mediaControlsGtk.js
@@ -1,261 +1,0 @@
-function createControls(root, video, host)
-{
-    return new ControllerGtk(root, video, host);
-};
-
-function ControllerGtk(root, video, host)
-{
-    Controller.call(this, root, video, host);
-};
-
-function contains(list, obj)
-{
-    var i = list.length;
-    while (i--)
-        if (list[i] === obj)
-            return true;
-    return false;
-};
-
-ControllerGtk.prototype = {
-
-    createControls: function()
-    {
-        Controller.prototype.createControls.apply(this);
-
-        this.controls.currentTime.classList.add(this.ClassNames.hidden);
-        this.controls.remainingTime.classList.add(this.ClassNames.hidden);
-
-        this.controls.volumeBox.classList.add(this.ClassNames.hiding);
-
-        this.listenFor(this.controls.muteBox, 'mouseout', this.handleVolumeBoxMouseOut);
-        this.listenFor(this.controls.muteButton, 'mouseover', this.handleMuteButtonMouseOver);
-        this.listenFor(this.controls.volumeBox, 'mouseover', this.handleMuteButtonMouseOver);
-        this.listenFor(this.controls.volume, 'mouseover', this.handleMuteButtonMouseOver);
-        this.listenFor(this.controls.captionButton, 'mouseover', this.handleCaptionButtonMouseOver);
-        this.listenFor(this.controls.captionButton, 'mouseout', this.handleCaptionButtonMouseOut);
-
-        var enclosure = this.controls.enclosure = document.createElement('div');
-        enclosure.setAttribute('pseudo', '-webkit-media-controls-enclosure');
-    },
-
-    configureInlineControls: function()
-    {
-        this.controls.panel.appendChild(this.controls.playButton);
-        this.controls.panel.appendChild(this.controls.timeline);
-        this.controls.panel.appendChild(this.controls.currentTime);
-        this.controls.panel.appendChild(this.controls.remainingTime);
-        this.controls.panel.appendChild(this.controls.captionButton);
-        this.controls.panel.appendChild(this.controls.fullscreenButton);
-        this.controls.panel.appendChild(this.controls.muteBox);
-        this.controls.muteBox.appendChild(this.controls.muteButton);
-        this.controls.muteBox.appendChild(this.controls.volumeBox);
-        this.controls.volumeBox.appendChild(this.controls.volume);
-        this.controls.enclosure.appendChild(this.controls.panel);
-    },
-
-    shouldHaveControls: function()
-    {
-        if (!this.isAudio() && !this.host.allowsInlineMediaPlayback)
-            return true;
-
-        return this.video.controls || this.isFullScreen();
-    },
-
-    reconnectControls: function()
-    {
-        Controller.prototype.disconnectControls.apply(this, arguments);
-
-        this.configureInlineControls();
-
-        if (this.shouldHaveControls())
-            this.addControls();
-    },
-
-    setStatusHidden: function(hidden)
-    {
-    },
-
-    updateTime: function(forceUpdate)
-    {
-        if (!forceUpdate && this.controlsAreHidden())
-            return;
-
-        var currentTime = this.video.currentTime;
-        var duration = this.video.duration;
-
-        this.controls.currentTime.innerText = this.formatTime(currentTime);
-        this.controls.timeline.value = currentTime;
-        if (duration === Infinity || duration === NaN)
-            this.controls.remainingTime.classList.add(this.ClassNames.hidden);
-        else {
-            this.controls.currentTime.innerText += " / " + this.formatTime(duration);
-            this.controls.remainingTime.innerText = this.formatTime(duration);
-            if (this.controls.currentTime.classList.contains(this.ClassNames.hidden))
-                this.controls.remainingTime.classList.remove(this.ClassNames.hidden);
-        }
-
-        if (currentTime > 0)
-            this.showCurrentTime();
-    },
-
-    showCurrentTime: function()
-    {
-        this.controls.currentTime.classList.remove(this.ClassNames.hidden);
-        this.controls.remainingTime.classList.add(this.ClassNames.hidden);
-    },
-
-    handlePlay: function(event)
-    {
-        Controller.prototype.handlePlay.apply(this, arguments);
-        this.showCurrentTime();
-        if (!this.isLive)
-            this.showCurrentTime();
-    },
-
-    handleTimeUpdate: function(event)
-    {
-        this.updateTime();
-    },
-
-    handleMuteButtonMouseOver: function(event)
-    {
-        if (this.video.offsetTop + this.controls.enclosure.offsetTop < 105) {
-            this.controls.volumeBox.classList.add(this.ClassNames.down);
-            this.controls.panel.classList.add(this.ClassNames.down);
-        } else {
-            this.controls.volumeBox.classList.remove(this.ClassNames.down);
-            this.controls.panel.classList.remove(this.ClassNames.down);
-        }
-        this.controls.volumeBox.classList.remove(this.ClassNames.hiding);
-        return true;
-    },
-
-    handleVolumeBoxMouseOut: function(event)
-    {
-        this.controls.volumeBox.classList.add(this.ClassNames.hiding);
-        return true;
-    },
-
-    removeControls: function()
-    {
-        if (this.controls.enclosure.parentNode)
-            this.controls.enclosure.parentNode.removeChild(this.controls.enclosure);
-        this.destroyCaptionMenu();
-    },
-
-    addControls: function()
-    {
-        this.base.appendChild(this.controls.enclosure);
-    },
-
-    updateReadyState: function()
-    {
-        if (this.host.supportsFullscreen && this.video.videoTracks.length)
-            this.controls.fullscreenButton.classList.remove(this.ClassNames.hidden);
-        else
-            this.controls.fullscreenButton.classList.add(this.ClassNames.hidden);
-        this.updateVolume();
-    },
-
-    updateDuration: function()
-    {
-        Controller.prototype.updateDuration.apply(this, arguments);
-        if (this.isLive)
-            this.controls.timeline.max = 0;
-    },
-
-    setIsLive: function(live)
-    {
-        Controller.prototype.setIsLive.apply(this, arguments);
-        this.controls.timeline.disabled = this.isLive;
-    },
-
-    updatePlaying: function()
-    {
-        Controller.prototype.updatePlaying.apply(this, arguments);
-        if (!this.canPlay())
-            this.showControls();
-    },
-
-    handleCaptionButtonClicked: function(event)
-    {
-        this.handleCaptionButtonShowMenu(event)
-        return true;
-    },
-
-    buildCaptionMenu: function()
-    {
-        Controller.prototype.buildCaptionMenu.apply(this, arguments);
-
-        this.listenFor(this.captionMenu, 'mouseout', this.handleCaptionMouseOut);
-        this.listenFor(this.captionMenu, 'transitionend', this.captionMenuTransitionEnd);
-
-        this.captionMenu.captionMenuTreeElements = this.captionMenu.getElementsByTagName("*");
-
-        // Caption menu has to be centered to the caption button.
-        var captionButtonCenter =  this.controls.panel.offsetLeft + this.controls.captionButton.offsetLeft +
-            this.controls.captionButton.offsetWidth / 2;
-        var captionMenuLeft = (captionButtonCenter - this.captionMenu.offsetWidth / 2);
-        if (captionMenuLeft + this.captionMenu.offsetWidth > this.controls.panel.offsetLeft + this.controls.panel.offsetWidth)
-            this.captionMenu.classList.add(this.ClassNames.out);
-        this.captionMenu.style.left = captionMenuLeft + 'px';
-        // As height is not in the css, it needs to be specified to animate it.
-        this.captionMenu.height = this.captionMenu.offsetHeight;
-        this.captionMenu.style.height = 0;
-    },
-
-    destroyCaptionMenu: function()
-    {
-        this.hideCaptionMenu();
-    },
-
-    showCaptionMenu: function()
-    {
-        this.captionMenu.style.height = this.captionMenu.height + 'px';
-    },
-
-    hideCaptionMenu: function()
-    {
-        this.captionMenu.style.height = 0;
-    },
-
-    captionMenuTransitionEnd: function(event)
-    {
-        if (this.captionMenu.offsetHeight === 0)
-            Controller.prototype.destroyCaptionMenu.apply(this, arguments);
-    },
-
-    handleCaptionButtonMouseOver: function(event)
-    {
-        this.handleCaptionButtonShowMenu(event);
-        return true;
-    },
-
-    handleCaptionButtonShowMenu: function(event)
-    {
-        if (!this.captionMenu)
-            this.buildCaptionMenu();
-        if (!contains(this.captionMenu.captionMenuTreeElements, event.relatedTarget))
-            this.showCaptionMenu();
-        return true;
-    },
-
-    handleCaptionButtonMouseOut: function(event)
-    {
-        if (this.captionMenu && !contains(this.captionMenu.captionMenuTreeElements, event.relatedTarget))
-            this.hideCaptionMenu();
-        return true;
-    },
-
-    handleCaptionMouseOut: function(event)
-    {
-        if (event.relatedTarget != this.controls.captionButton &&
-            !contains(this.captionMenu.captionMenuTreeElements, event.relatedTarget))
-            this.hideCaptionMenu();
-        return true;
-    },
-};
-
-Object.create(Controller.prototype).extend(ControllerGtk.prototype);
-Object.defineProperty(ControllerGtk.prototype, 'constructor', { enumerable:false, value:ControllerGtk });

Source/WebCore/PlatformGTK.cmake
@@ -18,6 +18,7 @@ list(APPEND WebCore_PRIVATE_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/accessibility/atk"
     "${WEBCORE_DIR}/editing/atk"
     "${WEBCORE_DIR}/page/gtk"
+    "${WEBCORE_DIR}/platform/adwaita"
     "${WEBCORE_DIR}/platform/generic"
     "${WEBCORE_DIR}/platform/gtk"
     "${WEBCORE_DIR}/platform/graphics/egl"
@@ -64,16 +65,17 @@ list(APPEND WebCore_PRIVATE_FRAMEWORK_HEADERS
 )
 
 list(APPEND WebCore_USER_AGENT_STYLE_SHEETS
-    ${WEBCORE_DIR}/css/mediaControlsGtk.css
+    ${WEBCORE_DIR}/Modules/mediacontrols/mediaControlsAdwaita.css
+    ${WEBCORE_DIR}/css/themeAdwaita.css
 )
 
 set(WebCore_USER_AGENT_SCRIPTS
     ${WEBCORE_DIR}/en.lproj/mediaControlsLocalizedStrings.js
+    ${WEBCORE_DIR}/Modules/mediacontrols/mediaControlsAdwaita.js
     ${WEBCORE_DIR}/Modules/mediacontrols/mediaControlsBase.js
-    ${WEBCORE_DIR}/Modules/mediacontrols/mediaControlsGtk.js
 )
 
-set(WebCore_USER_AGENT_SCRIPTS_DEPENDENCIES ${WEBCORE_DIR}/platform/gtk/RenderThemeGtk.cpp)
+set(WebCore_USER_AGENT_SCRIPTS_DEPENDENCIES ${WEBCORE_DIR}/rendering/RenderThemeAdwaita.cpp)
 
 list(APPEND WebCore_LIBRARIES
     ${ATK_LIBRARIES}

Source/WebCore/PlatformWPE.cmake
@@ -18,6 +18,7 @@ list(APPEND WebCore_UNIFIED_SOURCE_LIST_FILES
 
 list(APPEND WebCore_PRIVATE_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/accessibility/atk"
+    "${WEBCORE_DIR}/platform/adwaita"
     "${WEBCORE_DIR}/platform/graphics/egl"
     "${WEBCORE_DIR}/platform/graphics/epoxy"
     "${WEBCORE_DIR}/platform/graphics/glx"

commit 11ce4225bd6ae4b05e756ef4dc2af7b27aaed33c

REGRESSION(r257299): [GTK] Test fast/forms/menulist-restrict-line-height.html is failing since r257299 https://bugs.webkit.org/show_bug.cgi?id=208241
Reviewed by Adrian Perez de Castro.

We should not honor line-height for styled combo buttons.

* rendering/RenderThemeAdwaita.cpp:
(WebCore::RenderThemeAdwaita::adjustMenuListStyle const):
(WebCore::RenderThemeAdwaita::adjustMenuListButtonStyle const):
* rendering/RenderThemeAdwaita.h:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@257628 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderThemeAdwaita.cpp
@@ -246,6 +246,16 @@ bool RenderThemeAdwaita::paintSearchField(const RenderObject& renderObject, cons
     return paintTextField(renderObject, paintInfo, rect);
 }
 
+void RenderThemeAdwaita::adjustMenuListStyle(RenderStyle& style, const Element*) const
+{
+    style.setLineHeight(RenderStyle::initialLineHeight());
+}
+
+void RenderThemeAdwaita::adjustMenuListButtonStyle(RenderStyle& style, const Element* element) const
+{
+    adjustMenuListStyle(style, element);
+}
+
 LengthBox RenderThemeAdwaita::popupInternalPaddingBox(const RenderStyle& style) const
 {
     if (style.appearance() == NoControlPart)

Source/WebCore/rendering/RenderThemeAdwaita.h
@@ -62,6 +62,8 @@ private:
     bool paintSearchField(const RenderObject&, const PaintInfo&, const IntRect&) final;
 
     bool popsMenuBySpaceOrReturn() const final { return true; }
+    void adjustMenuListStyle(RenderStyle&, const Element*) const;
+    void adjustMenuListButtonStyle(RenderStyle&, const Element*) const;
     LengthBox popupInternalPaddingBox(const RenderStyle&) const final;
     bool paintMenuList(const RenderObject&, const PaintInfo&, const FloatRect&) final;
     bool paintMenuListButtonDecorations(const RenderBox&, const PaintInfo&, const FloatRect&) final;