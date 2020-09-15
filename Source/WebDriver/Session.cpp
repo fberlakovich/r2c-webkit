fberlakovich/r2c-webkit

commit 7723f464ed8eea0776aef0103079a95c452015f0

Web Inspector: modernize generated backend protocol code https://bugs.webkit.org/show_bug.cgi?id=216302 <rdar://problem/68547649>
Reviewed by Brian Burg.

Source/JavaScriptCore:

Previously, the inspector protocol was expressed in code in a somewhat confusing way:
 - the error string was the first argument
 - required parameters were `T` or `const T&`
 - optional parameters were `const T*`
 - enum parameters were the underlying type requiring the backend dispatcher handler to
   process it instead of it being preprocessed
 - required returns were `T&`
 - optional returns were `T*`
This doesn't really make for easy/obvious reading of code since the order of arguments is
not weird (e.g. error string first), and that there are references/pointers to primitive
types.

This patch cleans up the generated inspector protocol code to be:
 - required parameters are `T` or `Ref<T>&&`
 - optional parameters are `Optional<T>&&` or `RefPtr<T>&&`
 - enum parameters are preprocessed and passed to the backend dispatcher handler if valid
 - synchronous commands return `Expected<X, ErrorString>` using the same types/rules above
   where `X` is either a single return or a `std::tuple` of multiple returns

The one exception to the above is `String`, which is already a tri-state of `nullString()`,
`emptyString()`, and something set, so there's no need to use `Optional<String>`.

Also use `Protocol` objects/`typedefs` wherever possible to further relate the protocol
JSON and the actual backend dispatcher handler implementation.

* inspector/scripts/codegen/generator.py:
(Generator.generate_includes_from_entries):
* inspector/scripts/codegen/cpp_generator_templates.py:
* inspector/scripts/codegen/cpp_generator.py:
(CppGenerator.helpers_namespace):
(CppGenerator.cpp_getter_method_for_type):
(CppGenerator.cpp_setter_method_for_type):
(CppGenerator.cpp_protocol_type_for_type):
(CppGenerator.cpp_type_for_type_member_argument): Added.
(CppGenerator.cpp_type_for_command_parameter): Added.

Source/WebDriver/SessionHost.cpp
@@ -62,31 +62,27 @@ long SessionHost::sendCommandToBackend(const String& command, RefPtr<JSON::Objec
 
 void SessionHost::dispatchMessage(const String& message)
 {
-    RefPtr<JSON::Value> messageValue;
-    if (!JSON::Value::parseJSON(message, messageValue))
+    auto messageValue = JSON::Value::parseJSON(message);
+    if (!messageValue)
         return;
 
-    RefPtr<JSON::Object> messageObject;
-    if (!messageValue->asObject(messageObject))
+    auto messageObject = messageValue->asObject();
+    if (!messageObject)
         return;
 
-    long sequenceID;
-    if (!messageObject->getInteger("id"_s, sequenceID))
+    auto sequenceID = messageObject->getInteger("id"_s);
+    if (!sequenceID)
         return;
 
-    auto responseHandler = m_commandRequests.take(sequenceID);
+    auto responseHandler = m_commandRequests.take(*sequenceID);
     ASSERT(responseHandler);
 
     CommandResponse response;
-    RefPtr<JSON::Object> errorObject;
-    if (messageObject->getObject("error"_s, errorObject)) {
+    if (auto errorObject = messageObject->getObject("error"_s)) {
         response.responseObject = WTFMove(errorObject);
         response.isError = true;
-    } else {
-        RefPtr<JSON::Object> resultObject;
-        if (messageObject->getObject("result"_s, resultObject) && resultObject->size())
-            response.responseObject = WTFMove(resultObject);
-    }
+    } else if (auto resultObject = messageObject->getObject("result"_s))
+        response.responseObject = WTFMove(resultObject);
 
     responseHandler(WTFMove(response));
 }

Source/WebDriver/gtk/WebDriverServiceGtk.cpp
@@ -40,64 +40,48 @@ Capabilities WebDriverService::platformCapabilities()
     return capabilities;
 }
 
-bool WebDriverService::platformValidateCapability(const String& name, const RefPtr<JSON::Value>& value) const
+bool WebDriverService::platformValidateCapability(const String& name, const Ref<JSON::Value>& value) const
 {
     if (name != "webkitgtk:browserOptions")
         return true;
 
-    RefPtr<JSON::Object> browserOptions;
-    if (!value->asObject(browserOptions))
+    auto browserOptions = value->asObject();
+    if (!browserOptions)
         return false;
 
     if (browserOptions->isNull())
         return true;
 
-    RefPtr<JSON::Value> binaryValue;
-    String binary;
-    if (browserOptions->getValue("binary"_s, binaryValue) && !binaryValue->asString(binary))
+    auto binary = browserOptions->getString("binary"_s);
+    if (!binary)
         return false;
 
-    RefPtr<JSON::Value> useOverlayScrollbarsValue;
-    bool useOverlayScrollbars;
-    if (browserOptions->getValue("useOverlayScrollbars"_s, useOverlayScrollbarsValue) && !useOverlayScrollbarsValue->asBoolean(useOverlayScrollbars))
+    auto useOverlayScrollbars = browserOptions->getBoolean("useOverlayScrollbars"_s);
+    if (!useOverlayScrollbars)
         return false;
 
-    RefPtr<JSON::Value> browserArgumentsValue;
-    RefPtr<JSON::Array> browserArguments;
-    if (browserOptions->getValue("args"_s, browserArgumentsValue)) {
-        if (!browserArgumentsValue->asArray(browserArguments))
-            return false;
-
+    if (auto browserArguments = browserOptions->getArray("args"_s)) {
         unsigned browserArgumentsLength = browserArguments->length();
         for (unsigned i = 0; i < browserArgumentsLength; ++i) {
-            RefPtr<JSON::Value> value = browserArguments->get(i);
-            String argument;
-            if (!value->asString(argument))
+            auto argument = browserArguments->get(i)->asString();
+            if (!argument)
                 return false;
         }
     }
 
-    RefPtr<JSON::Value> certificatesValue;
-    if (browserOptions->getValue("certificates"_s, certificatesValue)) {
-        RefPtr<JSON::Array> certificates;
-        if (!certificatesValue->asArray(certificates))
-            return false;
-
+    if (auto certificates = browserOptions->getArray("certificates"_s)) {
         unsigned certificatesLength = certificates->length();
         for (unsigned i = 0; i < certificatesLength; ++i) {
-            RefPtr<JSON::Value> certificateValue = certificates->get(i);
-            RefPtr<JSON::Object> certificate;
-            if (!certificateValue->asObject(certificate))
+            auto certificate = certificates->get(i)->asObject();
+            if (!certificate)
                 return false;
 
-            RefPtr<JSON::Value> hostValue;
-            String host;
-            if (!certificate->getValue("host"_s, hostValue) || !hostValue->asString(host))
+            auto host = certificate->getString("host"_s);
+            if (!host)
                 return false;
 
-            RefPtr<JSON::Value> certificateFileValue;
-            String certificateFile;
-            if (!certificate->getValue("certificateFile"_s, certificateFileValue) || !certificateFileValue->asString(certificateFile))
+            auto certificateFile = certificate->getString("certificateFile"_s);
+            if (!certificateFile)
                 return false;
         }
     }
@@ -105,7 +89,7 @@ bool WebDriverService::platformValidateCapability(const String& name, const RefP
     return true;
 }
 
-bool WebDriverService::platformMatchCapability(const String&, const RefPtr<JSON::Value>&) const
+bool WebDriverService::platformMatchCapability(const String&, const Ref<JSON::Value>&) const
 {
     return true;
 }
@@ -116,53 +100,46 @@ void WebDriverService::platformParseCapabilities(const JSON::Object& matchedCapa
     capabilities.browserArguments = Vector<String> { "--automation"_s };
     capabilities.useOverlayScrollbars = true;
 
-    RefPtr<JSON::Object> browserOptions;
-    if (!matchedCapabilities.getObject("webkitgtk:browserOptions"_s, browserOptions))
+    auto browserOptions = matchedCapabilities.getObject("webkitgtk:browserOptions"_s);
+    if (!browserOptions)
         return;
 
-    String browserBinary;
-    if (browserOptions->getString("binary"_s, browserBinary)) {
+    auto browserBinary = browserOptions->getString("binary"_s);
+    if (!!browserBinary) {
         capabilities.browserBinary = browserBinary;
         capabilities.browserArguments = WTF::nullopt;
     }
 
-    RefPtr<JSON::Array> browserArguments;
-    if (browserOptions->getArray("args"_s, browserArguments) && browserArguments->length()) {
+    auto browserArguments = browserOptions->getArray("args"_s);
+    if (browserArguments && browserArguments->length()) {
         unsigned browserArgumentsLength = browserArguments->length();
         capabilities.browserArguments = Vector<String>();
         capabilities.browserArguments->reserveInitialCapacity(browserArgumentsLength);
         for (unsigned i = 0; i < browserArgumentsLength; ++i) {
-            RefPtr<JSON::Value> value = browserArguments->get(i);
-            String argument;
-            value->asString(argument);
+            auto argument = browserArguments->get(i)->asString();
             ASSERT(!argument.isNull());
             capabilities.browserArguments->uncheckedAppend(WTFMove(argument));
         }
     }
 
-    bool useOverlayScrollbars;
-    if (browserOptions->getBoolean("useOverlayScrollbars"_s, useOverlayScrollbars))
+    if (auto useOverlayScrollbars = browserOptions->getBoolean("useOverlayScrollbars"_s))
         capabilities.useOverlayScrollbars = useOverlayScrollbars;
     else
         capabilities.useOverlayScrollbars = true;
 
-    RefPtr<JSON::Array> certificates;
-    if (browserOptions->getArray("certificates"_s, certificates) && certificates->length()) {
+    auto certificates = browserOptions->getArray("certificates"_s);
+    if (certificates && certificates->length()) {
         unsigned certificatesLength = certificates->length();
         capabilities.certificates = Vector<std::pair<String, String>>();
         capabilities.certificates->reserveInitialCapacity(certificatesLength);
         for (unsigned i = 0; i < certificatesLength; ++i) {
-            RefPtr<JSON::Value> value = certificates->get(i);
-            RefPtr<JSON::Object> certificate;
-            value->asObject(certificate);
+            auto certificate = certificates->get(i)->asObject();
             ASSERT(certificate);
 
-            String host;
-            certificate->getString("host"_s, host);
+            auto host = certificate->getString("host"_s);
             ASSERT(!host.isNull());
 
-            String certificateFile;
-            certificate->getString("certificateFile"_s, certificateFile);
+            auto certificateFile = certificate->getString("certificateFile"_s);
             ASSERT(!certificateFile.isNull());
 
             capabilities.certificates->uncheckedAppend({ WTFMove(host), WTFMove(certificateFile) });

Source/WebDriver/wpe/WebDriverServiceWPE.cpp
@@ -40,57 +40,47 @@ Capabilities WebDriverService::platformCapabilities()
     return capabilities;
 }
 
-bool WebDriverService::platformValidateCapability(const String& name, const RefPtr<JSON::Value>& value) const
+bool WebDriverService::platformValidateCapability(const String& name, const Ref<JSON::Value>& value) const
 {
     if (name != "wpe:browserOptions")
         return true;
 
-    RefPtr<JSON::Object> browserOptions;
-    if (!value->asObject(browserOptions))
+    auto browserOptions = value->asObject();
+    if (!browserOptions)
         return false;
 
     if (browserOptions->isNull())
         return true;
 
     // If browser options are provided, binary is required.
-    String binary;
-    if (!browserOptions->getString("binary"_s, binary))
+    auto binary = browserOptions->getString("binary"_s);
+    if (!binary)
         return false;
 
-    RefPtr<JSON::Value> browserArgumentsValue;
-    RefPtr<JSON::Array> browserArguments;
-    if (browserOptions->getValue("args"_s, browserArgumentsValue) && !browserArgumentsValue->asArray(browserArguments))
+    auto browserArguments = browserOptions->getArray("args"_s);
+    if (!browserArguments)
         return false;
 
     unsigned browserArgumentsLength = browserArguments->length();
     for (unsigned i = 0; i < browserArgumentsLength; ++i) {
-        RefPtr<JSON::Value> value = browserArguments->get(i);
-        String argument;
-        if (!value->asString(argument))
+        auto argument = browserArguments->get(i)->asString();
+        if (!argument)
             return false;
     }
 
-    RefPtr<JSON::Value> certificatesValue;
-    if (browserOptions->getValue("certificates"_s, certificatesValue)) {
-        RefPtr<JSON::Array> certificates;
-        if (!certificatesValue->asArray(certificates))
-            return false;
-
+    if (auto certificates = browserOptions->getArray("certificates"_s)) {
         unsigned certificatesLength = certificates->length();
         for (unsigned i = 0; i < certificatesLength; ++i) {
-            RefPtr<JSON::Value> certificateValue = certificates->get(i);
-            RefPtr<JSON::Object> certificate;
-            if (!certificateValue->asObject(certificate))
+            auto certificate = certificates->get(i)->asObject();
+            if (!certificate)
                 return false;
 
-            RefPtr<JSON::Value> hostValue;
-            String host;
-            if (!certificate->getValue("host"_s, hostValue) || !hostValue->asString(host))
+            auto host = certificate->getString("host"_s);
+            if (!host)
                 return false;
 
-            RefPtr<JSON::Value> certificateFileValue;
-            String certificateFile;
-            if (!certificate->getValue("certificateFile"_s, certificateFileValue) || !certificateFileValue->asString(certificateFile))
+            auto certificateFile = certificate->getString("certificateFile"_s);
+            if (!certificateFile)
                 return false;
         }
     }
@@ -98,7 +88,7 @@ bool WebDriverService::platformValidateCapability(const String& name, const RefP
     return true;
 }
 
-bool WebDriverService::platformMatchCapability(const String&, const RefPtr<JSON::Value>&) const
+bool WebDriverService::platformMatchCapability(const String&, const Ref<JSON::Value>&) const
 {
     return true;
 }
@@ -108,47 +98,41 @@ void WebDriverService::platformParseCapabilities(const JSON::Object& matchedCapa
     capabilities.browserBinary = String("MiniBrowser");
     capabilities.browserArguments = Vector<String> { "--automation"_s };
 
-    RefPtr<JSON::Object> browserOptions;
-    if (!matchedCapabilities.getObject("wpe:browserOptions"_s, browserOptions))
+    auto browserOptions = matchedCapabilities.getObject("wpe:browserOptions"_s);
+    if (!browserOptions)
         return;
 
-    String browserBinary;
-    if (browserOptions->getString("binary"_s, browserBinary)) {
+    auto browserBinary = browserOptions->getString("binary"_s);
+    if (!!browserBinary) {
         capabilities.browserBinary = browserBinary;
         capabilities.browserArguments = WTF::nullopt;
     }
 
-    RefPtr<JSON::Array> browserArguments;
-    if (browserOptions->getArray("args"_s, browserArguments) && browserArguments->length()) {
+    auto browserArguments = browserOptions->getArray("args"_s);
+    if (browserArguments && browserArguments->length()) {
         unsigned browserArgumentsLength = browserArguments->length();
         capabilities.browserArguments = Vector<String>();
         capabilities.browserArguments->reserveInitialCapacity(browserArgumentsLength);
         for (unsigned i = 0; i < browserArgumentsLength; ++i) {
-            RefPtr<JSON::Value> value = browserArguments->get(i);
-            String argument;
-            value->asString(argument);
+            auto argument = browserArguments->get(i)->asString();
             ASSERT(!argument.isNull());
             capabilities.browserArguments->uncheckedAppend(WTFMove(argument));
         }
     }
 
-    RefPtr<JSON::Array> certificates;
-    if (browserOptions->getArray("certificates"_s, certificates) && certificates->length()) {
+    auto certificates = browserOptions->getArray("certificates"_s);
+    if (certificates && certificates->length()) {
         unsigned certificatesLength = certificates->length();
         capabilities.certificates = Vector<std::pair<String, String>>();
         capabilities.certificates->reserveInitialCapacity(certificatesLength);
         for (unsigned i = 0; i < certificatesLength; ++i) {
-            RefPtr<JSON::Value> value = certificates->get(i);
-            RefPtr<JSON::Object> certificate;
-            value->asObject(certificate);
+            auto certificate = certificates->get(i)->asObject();
             ASSERT(certificate);
 
-            String host;
-            certificate->getString("host"_s, host);
+            auto host = certificate->getString("host"_s);
             ASSERT(!host.isNull());
 
-            String certificateFile;
-            certificate->getString("certificateFile"_s, certificateFile);
+            auto certificateFile = certificate->getString("certificateFile"_s);
             ASSERT(!certificateFile.isNull());
 
             capabilities.certificates->uncheckedAppend({ WTFMove(host), WTFMove(certificateFile) });

commit 1c1f3e42e89e108e29c78f4f56bcff038c64d81e

REGRESSION(r266885): WebDriver is broken since r266885 https://bugs.webkit.org/show_bug.cgi?id=216477
Reviewed by Devin Rousso.

There are several issues introduced in r266885, most of them related to JSValue::get methods now returning an
optional. In some cases of boolean values, the optional is checked instead of the contained bool or the logic is
inverted. JSONValue::getString() is problematic too because it doesn't use optional, when you call getString()
and null is returned it could be because the property is not present in the object or because the property is
not a string. In several cases we need to know that so we need to first get the value and then call asString(9
instead of calling getString() directly.

* Session.cpp:
(WebDriver::Session::handleUserPrompts): Check the value of isShowingJavaScriptDialog, not the optional.
(WebDriver::parseAutomationCookie): Check also the value of the session optional bool, not only the optional.
* SessionHost.cpp:
(WebDriver::SessionHost::dispatchMessage): Only set the responseObject if the object is not empty.
* WebDriverService.cpp:
(WebDriver::WebDriverService::newWindow): Unknown values of type hint are not an error.
(WebDriver::WebDriverService::deleteCookie): Fix indentation.
(WebDriver::processKeyAction): Empty key value is an error, not the opposite.
* gtk/WebDriverServiceGtk.cpp:
(WebDriver::WebDriverService::platformValidateCapability const): Only fail if optional values are wrong type,
not if they are not present.
* wpe/WebDriverServiceWPE.cpp:
(WebDriver::WebDriverService::platformValidateCapability const): Ditto.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@267078 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebDriver/Session.cpp
@@ -214,7 +214,7 @@ void Session::handleUserPrompts(Function<void (CommandResult&&)>&& completionHan
             return;
         }
 
-        if (!isShowingJavaScriptDialog) {
+        if (!isShowingJavaScriptDialog.value()) {
             completionHandler(CommandResult::success());
             return;
         }
@@ -2368,7 +2368,7 @@ static Optional<Session::Cookie> parseAutomationCookie(const JSON::Object& cooki
         cookie.httpOnly = *httpOnly;
 
     auto session = cookieObject.getBoolean("session"_s);
-    if (!session) {
+    if (!session || !*session) {
         if (auto expiry = cookieObject.getDouble("expires"_s))
             cookie.expiry = *expiry;
     }

Source/WebDriver/SessionHost.cpp
@@ -81,8 +81,10 @@ void SessionHost::dispatchMessage(const String& message)
     if (auto errorObject = messageObject->getObject("error"_s)) {
         response.responseObject = WTFMove(errorObject);
         response.isError = true;
-    } else if (auto resultObject = messageObject->getObject("result"_s))
-        response.responseObject = WTFMove(resultObject);
+    } else if (auto resultObject = messageObject->getObject("result"_s)) {
+        if (resultObject->size())
+            response.responseObject = WTFMove(resultObject);
+    }
 
     responseHandler(WTFMove(response));
 }

Source/WebDriver/WebDriverService.cpp
@@ -1181,9 +1181,10 @@ void WebDriverService::newWindow(RefPtr<JSON::Object>&& parameters, Function<voi
     Optional<String> typeHint;
     if (auto value = parameters->getValue("type"_s)) {
         auto valueString = value->asString();
-        if (valueString == "window" || valueString == "tab")
-            typeHint = valueString;
-        else if (!value->isNull()) {
+        if (!!valueString) {
+            if (valueString == "window" || valueString == "tab")
+                typeHint = valueString;
+        } else if (!value->isNull()) {
             completionHandler(CommandResult::fail(CommandResult::ErrorCode::InvalidArgument));
             return;
         }
@@ -1761,7 +1762,7 @@ void WebDriverService::deleteCookie(RefPtr<JSON::Object>&& parameters, Function<
     if (!findSessionOrCompleteWithError(*parameters, completionHandler))
         return;
 
-auto name = parameters->getString("name"_s);
+    auto name = parameters->getString("name"_s);
     if (!name) {
         completionHandler(CommandResult::fail(CommandResult::ErrorCode::InvalidArgument));
         return;
@@ -1853,7 +1854,7 @@ static Optional<Action> processKeyAction(const String& id, JSON::Object& actionI
             return WTF::nullopt;
         }
         auto key = keyValue->asString();
-        if (!key.isEmpty()) {
+        if (key.isEmpty()) {
             errorMessage = String("The paramater 'value' is invalid for key up/down action");
             return WTF::nullopt;
         }

Source/WebDriver/gtk/WebDriverServiceGtk.cpp
@@ -52,15 +52,21 @@ bool WebDriverService::platformValidateCapability(const String& name, const Ref<
     if (browserOptions->isNull())
         return true;
 
-    auto binary = browserOptions->getString("binary"_s);
-    if (!binary)
-        return false;
+    if (auto binaryValue = browserOptions->getValue("binary"_s)) {
+        if (!binaryValue->asString())
+            return false;
+    }
 
-    auto useOverlayScrollbars = browserOptions->getBoolean("useOverlayScrollbars"_s);
-    if (!useOverlayScrollbars)
-        return false;
+    if (auto useOverlayScrollbarsValue = browserOptions->getValue("useOverlayScrollbars"_s)) {
+        if (!useOverlayScrollbarsValue->asBoolean())
+            return false;
+    }
 
-    if (auto browserArguments = browserOptions->getArray("args"_s)) {
+    if (auto browserArgumentsValue = browserOptions->getValue("args"_s)) {
+        auto browserArguments = browserArgumentsValue->asArray();
+        if (!browserArguments)
+            return false;
+
         unsigned browserArgumentsLength = browserArguments->length();
         for (unsigned i = 0; i < browserArgumentsLength; ++i) {
             auto argument = browserArguments->get(i)->asString();
@@ -69,7 +75,11 @@ bool WebDriverService::platformValidateCapability(const String& name, const Ref<
         }
     }
 
-    if (auto certificates = browserOptions->getArray("certificates"_s)) {
+    if (auto certificatesValue = browserOptions->getValue("certificates"_s)) {
+        auto certificates = certificatesValue->asArray();
+        if (!certificates)
+            return false;
+
         unsigned certificatesLength = certificates->length();
         for (unsigned i = 0; i < certificatesLength; ++i) {
             auto certificate = certificates->get(i)->asObject();

Source/WebDriver/wpe/WebDriverServiceWPE.cpp
@@ -57,18 +57,24 @@ bool WebDriverService::platformValidateCapability(const String& name, const Ref<
     if (!binary)
         return false;
 
-    auto browserArguments = browserOptions->getArray("args"_s);
-    if (!browserArguments)
-        return false;
-
-    unsigned browserArgumentsLength = browserArguments->length();
-    for (unsigned i = 0; i < browserArgumentsLength; ++i) {
-        auto argument = browserArguments->get(i)->asString();
-        if (!argument)
+    if (auto browserArgumentsValue = browserOptions->getValue("args"_s)) {
+        auto browserArguments = browserArgumentsValue->asArray();
+        if (!browserArguments)
             return false;
+
+        unsigned browserArgumentsLength = browserArguments->length();
+        for (unsigned i = 0; i < browserArgumentsLength; ++i) {
+            auto argument = browserArguments->get(i)->asString();
+            if (!argument)
+                return false;
+        }
     }
 
-    if (auto certificates = browserOptions->getArray("certificates"_s)) {
+    if (auto certificatesValue = browserOptions->getValue("certificates"_s)) {
+        auto certificates = certificatesValue->asArray();
+        if (!certificates)
+            return false;
+
         unsigned certificatesLength = certificates->length();
         for (unsigned i = 0; i < certificatesLength; ++i) {
             auto certificate = certificates->get(i)->asObject();