fberlakovich/r2c-webkit

commit 9a42cac1a7c8366a50c22afeb575cda9feda2232

Values returned by FFTFrame::doFFT() are twice as large as they should be https://bugs.webkit.org/show_bug.cgi?id=216781
Reviewed by Darin Adler.

Source/WebCore:

To provide the best possible execution speeds, the vDSP library's functions don't always adhere strictly
to textbook formulas for Fourier transforms, and must be scaled accordingly [1].

In the case of a Real forward Transform like in FFTFrame::doFFT(): RFimp = RFmath * 2 so we need to
divide the output by 2 to get the correct value. We were failing to do this scaling and this was causing
AnalyserNode tests to fail.

[1] See https://developer.apple.com/library/archive/documentation/Performance/Conceptual/vDSP_Programming_Guide/UsingFourierTransforms/UsingFourierTransforms.html#//apple_ref/doc/uid/TP40005147-CH3-SW5

No new tests, rebaselined existing tests.

* Modules/webaudio/PeriodicWave.cpp:
(WebCore::PeriodicWave::createBandLimitedTables):
Update normalization factor now that FFTFrame::doInverseFFT() has been fixed. The new normalization factor
matches the value used by blink at:
- https://github.com/chromium/chromium/blob/master/third_party/blink/renderer/modules/webaudio/periodic_wave.cc

* platform/audio/FFTFrame.cpp:
(WebCore::FFTFrame::multiply):
We were applying a 0.5 scaling factor to the output of vDSP_zvmul(). However, the documentation for vDSP_zvmul()
does not indicate that its output is twice as large as expected. Odds are that this was done because we used
to have a bug in FFTFrame::doFFT() that was returning values twice as large as expected. SInce this function
relies on VectorMath, there is no need for its implementation to be platform-specific.

* platform/audio/FFTFrameStub.cpp:
* platform/audio/gstreamer/FFTFrameGStreamer.cpp:
(WebCore::FFTFrame::doFFT):
(WebCore::FFTFrame::doInverseFFT):
Drop 2 factor in the GStreamer implementation that was added to try and be consistent with the incorrect Mac
implementation.

* platform/audio/mac/FFTFrameMac.cpp:
(WebCore::FFTFrame::doFFT):

Source/WebCore/platform/audio/FFTFrame.cpp
@@ -179,6 +179,28 @@ void FFTFrame::scaleFFT(float factor)
     VectorMath::vsmul(imagData(), 1, &factor, imagData(), 1, fftSize());
 }
 
+void FFTFrame::multiply(const FFTFrame& frame)
+{
+    FFTFrame& frame1 = *this;
+    const FFTFrame& frame2 = frame;
+
+    float* realP1 = frame1.realData();
+    float* imagP1 = frame1.imagData();
+    const float* realP2 = frame2.realData();
+    const float* imagP2 = frame2.imagData();
+
+    unsigned halfSize = m_FFTSize / 2;
+    float real0 = realP1[0];
+    float imag0 = imagP1[0];
+
+    // Complex multiply
+    VectorMath::zvmul(realP1, imagP1, realP2, imagP2, realP1, imagP1, halfSize);
+
+    // Multiply the packed DC/nyquist component
+    realP1[0] = real0 * realP2[0];
+    imagP1[0] = imag0 * imagP2[0];
+}
+
 double FFTFrame::extractAverageGroupDelay()
 {
     float* realP = realData();

Source/WebCore/platform/audio/FFTFrameStub.cpp
@@ -64,11 +64,6 @@ FFTFrame::~FFTFrame()
     ASSERT_NOT_REACHED();
 }
 
-void FFTFrame::multiply(const FFTFrame& frame)
-{
-    ASSERT_NOT_REACHED();
-}
-
 void FFTFrame::doFFT(const float* data)
 {
     ASSERT_NOT_REACHED();

Source/WebCore/platform/audio/gstreamer/FFTFrameGStreamer.cpp
@@ -103,46 +103,9 @@ FFTFrame::~FFTFrame()
     m_inverseFft = 0;
 }
 
-void FFTFrame::multiply(const FFTFrame& frame)
-{
-    FFTFrame& frame1 = *this;
-    FFTFrame& frame2 = const_cast<FFTFrame&>(frame);
-
-    float* realP1 = frame1.realData();
-    float* imagP1 = frame1.imagData();
-    const float* realP2 = frame2.realData();
-    const float* imagP2 = frame2.imagData();
-
-    size_t size = unpackedFFTDataSize(m_FFTSize);
-    VectorMath::zvmul(realP1, imagP1, realP2, imagP2, realP1, imagP1, size);
-
-    // Scale accounts the peculiar scaling of vecLib on the Mac.
-    // This ensures the right scaling all the way back to inverse FFT.
-    // FIXME: if we change the scaling on the Mac then this scale
-    // factor will need to change too.
-    float scale = 0.5f;
-
-    VectorMath::vsmul(realP1, 1, &scale, realP1, 1, size);
-    VectorMath::vsmul(imagP1, 1, &scale, imagP1, 1, size);
-}
-
 void FFTFrame::doFFT(const float* data)
 {
     gst_fft_f32_fft(m_fft, data, m_complexData.get());
-
-    // Scale the frequency domain data to match vecLib's scale factor
-    // on the Mac. FIXME: if we change the definition of FFTFrame to
-    // eliminate this scale factor then this code will need to change.
-    // Also, if this loop turns out to be hot then we should use SSE
-    // or other intrinsics to accelerate it.
-    float scaleFactor = 2;
-
-    float* imagData = m_imagData.data();
-    float* realData = m_realData.data();
-    for (unsigned i = 0; i < unpackedFFTDataSize(m_FFTSize); ++i) {
-        imagData[i] = m_complexData[i].i * scaleFactor;
-        realData[i] = m_complexData[i].r * scaleFactor;
-    }
 }
 
 void FFTFrame::doInverseFFT(float* data)
@@ -159,7 +122,7 @@ void FFTFrame::doInverseFFT(float* data)
     gst_fft_f32_inverse_fft(m_inverseFft, m_complexData.get(), data);
 
     // Scale so that a forward then inverse FFT yields exactly the original data.
-    const float scaleFactor = 1.0 / (2 * m_FFTSize);
+    const float scaleFactor = 1.0 / m_FFTSize;
     VectorMath::vsmul(data, 1, &scaleFactor, data, 1, m_FFTSize);
 }
 

Source/WebCore/platform/audio/mac/FFTFrameMac.cpp
@@ -97,39 +97,21 @@ FFTFrame::FFTFrame(const FFTFrame& frame)
 
 FFTFrame::~FFTFrame() = default;
 
-void FFTFrame::multiply(const FFTFrame& frame)
-{
-    FFTFrame& frame1 = *this;
-    const FFTFrame& frame2 = frame;
-
-    float* realP1 = frame1.realData();
-    float* imagP1 = frame1.imagData();
-    const float* realP2 = frame2.realData();
-    const float* imagP2 = frame2.imagData();
-
-    unsigned halfSize = m_FFTSize / 2;
-    float real0 = realP1[0];
-    float imag0 = imagP1[0];
-
-    // Complex multiply
-    VectorMath::zvmul(realP1, imagP1, realP2, imagP2, realP1, imagP1, halfSize); 
-
-    // Multiply the packed DC/nyquist component
-    realP1[0] = real0 * realP2[0];
-    imagP1[0] = imag0 * imagP2[0];
-
-    // Scale accounts for vecLib's peculiar scaling
-    // This ensures the right scaling all the way back to inverse FFT
-    float scale = 0.5f;
-
-    VectorMath::vsmul(realP1, 1, &scale, realP1, 1, halfSize);
-    VectorMath::vsmul(imagP1, 1, &scale, imagP1, 1, halfSize);
-}
-
 void FFTFrame::doFFT(const float* data)
 {
-    vDSP_ctoz(reinterpret_cast<const DSPComplex*>(data), 2, &m_frame, 1, m_FFTSize / 2);
+    unsigned halfSize = m_FFTSize / 2;
+    vDSP_ctoz(reinterpret_cast<const DSPComplex*>(data), 2, &m_frame, 1, halfSize);
     vDSP_fft_zrip(m_FFTSetup, &m_frame, 1, m_log2FFTSize, FFT_FORWARD);
+
+    // To provide the best possible execution speeds, the vDSP library's functions don't always adhere strictly
+    // to textbook formulas for Fourier transforms, and must be scaled accordingly.
+    // (See https://developer.apple.com/library/archive/documentation/Performance/Conceptual/vDSP_Programming_Guide/UsingFourierTransforms/UsingFourierTransforms.html#//apple_ref/doc/uid/TP40005147-CH3-SW5)
+    // In the case of a Real forward Transform like above: RFimp = RFmath * 2 so we need to divide the output
+    // by 2 to get the correct value.
+    float scale = 0.5f;
+
+    VectorMath::vsmul(realData(), 1, &scale, realData(), 1, halfSize);
+    VectorMath::vsmul(imagData(), 1, &scale, imagData(), 1, halfSize);
 }
 
 void FFTFrame::doInverseFFT(float* data)
@@ -138,8 +120,8 @@ void FFTFrame::doInverseFFT(float* data)
     vDSP_ztoc(&m_frame, 1, (DSPComplex*)data, 2, m_FFTSize / 2);
 
     // Do final scaling so that x == IFFT(FFT(x))
-    float scale = 0.5f / m_FFTSize;
-    vDSP_vsmul(data, 1, &scale, data, 1, m_FFTSize);
+    float scale = 1.0f / m_FFTSize;
+    VectorMath::vsmul(data, 1, &scale, data, 1, m_FFTSize);
 }
 
 FFTSetup FFTFrame::fftSetupForSize(unsigned fftSize)

LayoutTests/TestExpectations
@@ -330,10 +330,6 @@ imported/w3c/web-platform-tests/webaudio/the-audio-api/the-audioparam-interface/
 imported/w3c/web-platform-tests/webaudio/the-audio-api/the-audiobuffersourcenode-interface/buffer-resampling.html [ Pass Failure ]
 imported/w3c/web-platform-tests/webaudio/the-audio-api/the-audiobuffersourcenode-interface/sub-sample-buffer-stitching.html [ Pass Failure ]
 webaudio/Analyser/handle-silent-inputs.html [ Pass Failure ]
-webaudio/Analyser/realtimeanalyser-fftsize-reset.html [ Pass Failure ]
-webaudio/Analyser/realtimeanalyser-freq-data-smoothing.html [ Pass Failure ]
-webaudio/Analyser/realtimeanalyser-freq-data.html [ Pass Failure ]
-webaudio/Analyser/realtimeanalyser-multiple-calls.html [ Pass Failure ]
 webaudio/AudioBufferSource/audiobuffersource-detune-modulation.html [ Pass Failure ]
 webaudio/AudioBufferSource/audiobuffersource-playbackrate-modulation.html [ Pass Failure ]
 

LayoutTests/platform/mac/TestExpectations
@@ -1616,7 +1616,6 @@ webkit.org/b/136627 media/track/track-cue-rendering-vertical.html [ Failure ]
 
 # <rdar://problem/47088939>
 imported/w3c/web-platform-tests/webaudio/the-audio-api/the-mediaelementaudiosourcenode-interface/mediaElementAudioSourceToScriptProcessorTest.html  [ Failure ]
-webaudio/realtimeanalyser-fft-scaling.html [ Pass Failure ]
 webaudio/oscillator-sine.html [ Skip ]
 
 webkit.org/b/199013 [ Debug ] imported/w3c/web-platform-tests/websockets/Create-Secure-verify-url-set-non-default-port.any.html [ Pass Failure ]

LayoutTests/webaudio/Analyser/realtimeanalyser-multiple-calls-expected.txt
@@ -4,9 +4,9 @@ PASS Executing "test"
 PASS Audit report 
 PASS > [test]  
 PASS   Second call to getFloatFrequencyData is identical to the array [expected array]. 
-PASS   Second call to getByteFrequencyData is identical to the array [189,254,255,255,255,255,245,250,238,239,236,228,233,219,228,218...]. 
-PASS   Output of getByteFrequencyData after getFloatFrequencyData is identical to the array [185,255,255,255,255,255,255,255,251,250,247,241,243,234,238,231...]. 
-PASS   Output of getFloatFrequenycData (converted to byte) after getByteFrequencyData is identical to the array [198,255,255,255,255,255,255,255,255,255,253,247,249,239,244,237...]. 
+PASS   Second call to getByteFrequencyData is identical to the array [167,232,255,255,238,239,223,228,216,217,214,206,211,197,206,196...]. 
+PASS   Output of getByteFrequencyData after getFloatFrequencyData is identical to the array [163,242,255,255,252,250,237,238,229,228,225,219,221,212,216,209...]. 
+PASS   Output of getFloatFrequenycData (converted to byte) after getByteFrequencyData is identical to the array [176,248,255,255,255,255,243,244,235,234,231,225,227,217,222,215...]. 
 PASS < [test] All assertions passed. (total 4 assertions) 
 PASS # AUDIT TASK RUNNER FINISHED: 1 tasks ran successfully. 
 

commit 3775c6577571d310de87cc91c1135a8eadc5f5e9

REGRESSION(r267383): fast/mediastream/getUserMedia-webaudio.html is failing https://bugs.webkit.org/show_bug.cgi?id=216813
Reviewed by Adrian Perez de Castro.

Source/WebCore:

Revert r267428 and fixup the GStreamer FFT implementation which broke in r267383.

* platform/audio/FFTFrame.cpp:
(WebCore::FFTFrame::multiply):
* platform/audio/FFTFrameStub.cpp:
(WebCore::FFTFrame::multiply): Deleted.
* platform/audio/gstreamer/FFTFrameGStreamer.cpp:
(WebCore::FFTFrame::doFFT): Update the real and imaginary arrays after applying the FFT.
(WebCore::FFTFrame::multiply): Deleted.
* platform/audio/mac/FFTFrameMac.cpp:
(WebCore::FFTFrame::multiply): Deleted.

LayoutTests:

Unflag passing tests.

* platform/glib/TestExpectations:
* platform/glib/webaudio/Analyser/realtimeanalyser-fftsize-reset-expected.txt:
* platform/glib/webaudio/Analyser/realtimeanalyser-freq-data-expected.txt: Added. There's
still a FAIL test there though.
* platform/glib/webaudio/Analyser/realtimeanalyser-multiple-calls-expected.txt:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@267471 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/audio/FFTFrame.cpp
@@ -179,6 +179,28 @@ void FFTFrame::scaleFFT(float factor)
     VectorMath::vsmul(imagData(), 1, &factor, imagData(), 1, fftSize());
 }
 
+void FFTFrame::multiply(const FFTFrame& frame)
+{
+    FFTFrame& frame1 = *this;
+    const FFTFrame& frame2 = frame;
+
+    float* realP1 = frame1.realData();
+    float* imagP1 = frame1.imagData();
+    const float* realP2 = frame2.realData();
+    const float* imagP2 = frame2.imagData();
+
+    unsigned halfSize = m_FFTSize / 2;
+    float real0 = realP1[0];
+    float imag0 = imagP1[0];
+
+    // Complex multiply
+    VectorMath::zvmul(realP1, imagP1, realP2, imagP2, realP1, imagP1, halfSize);
+
+    // Multiply the packed DC/nyquist component
+    realP1[0] = real0 * realP2[0];
+    imagP1[0] = imag0 * imagP2[0];
+}
+
 double FFTFrame::extractAverageGroupDelay()
 {
     float* realP = realData();

Source/WebCore/platform/audio/FFTFrameStub.cpp
@@ -64,11 +64,6 @@ FFTFrame::~FFTFrame()
     ASSERT_NOT_REACHED();
 }
 
-void FFTFrame::multiply(const FFTFrame& frame)
-{
-    ASSERT_NOT_REACHED();
-}
-
 void FFTFrame::doFFT(const float* data)
 {
     ASSERT_NOT_REACHED();

Source/WebCore/platform/audio/gstreamer/FFTFrameGStreamer.cpp
@@ -103,23 +103,16 @@ FFTFrame::~FFTFrame()
     m_inverseFft = 0;
 }
 
-void FFTFrame::multiply(const FFTFrame& frame)
-{
-    FFTFrame& frame1 = *this;
-    FFTFrame& frame2 = const_cast<FFTFrame&>(frame);
-
-    float* realP1 = frame1.realData();
-    float* imagP1 = frame1.imagData();
-    const float* realP2 = frame2.realData();
-    const float* imagP2 = frame2.imagData();
-
-    size_t size = unpackedFFTDataSize(m_FFTSize);
-    VectorMath::zvmul(realP1, imagP1, realP2, imagP2, realP1, imagP1, size);
-}
-
 void FFTFrame::doFFT(const float* data)
 {
     gst_fft_f32_fft(m_fft, data, m_complexData.get());
+
+    float* imagData = m_imagData.data();
+    float* realData = m_realData.data();
+    for (unsigned i = 0; i < unpackedFFTDataSize(m_FFTSize); ++i) {
+        imagData[i] = m_complexData[i].i;
+        realData[i] = m_complexData[i].r;
+    }
 }
 
 void FFTFrame::doInverseFFT(float* data)

Source/WebCore/platform/audio/mac/FFTFrameMac.cpp
@@ -97,28 +97,6 @@ FFTFrame::FFTFrame(const FFTFrame& frame)
 
 FFTFrame::~FFTFrame() = default;
 
-void FFTFrame::multiply(const FFTFrame& frame)
-{
-    FFTFrame& frame1 = *this;
-    const FFTFrame& frame2 = frame;
-
-    float* realP1 = frame1.realData();
-    float* imagP1 = frame1.imagData();
-    const float* realP2 = frame2.realData();
-    const float* imagP2 = frame2.imagData();
-
-    unsigned halfSize = m_FFTSize / 2;
-    float real0 = realP1[0];
-    float imag0 = imagP1[0];
-
-    // Complex multiply
-    VectorMath::zvmul(realP1, imagP1, realP2, imagP2, realP1, imagP1, halfSize);
-
-    // Multiply the packed DC/nyquist component
-    realP1[0] = real0 * realP2[0];
-    imagP1[0] = imag0 * imagP2[0];
-}
-
 void FFTFrame::doFFT(const float* data)
 {
     unsigned halfSize = m_FFTSize / 2;

LayoutTests/platform/glib/TestExpectations
@@ -277,18 +277,6 @@ webkit.org/b/216171 imported/w3c/web-platform-tests/webaudio/the-audio-api/the-d
 webkit.org/b/216118 webaudio/convolver-setBuffer-different-samplerate.html [ Skip ]
 webkit.org/b/216118 webaudio/convolver-setBuffer-null.html [ Skip ]
 
-webkit.org/b/216813 fast/mediastream/getUserMedia-webaudio.html [ Failure ]
-webkit.org/b/216813 imported/w3c/web-platform-tests/webaudio/the-audio-api/the-analysernode-interface/realtimeanalyser-fft-scaling.html [ Failure ]
-webkit.org/b/216813 webaudio/Analyser/realtimeanalyser-freq-data-smoothing.html [ Failure ]
-webkit.org/b/216813 webaudio/Analyser/realtimeanalyser-freq-data.html [ Failure ]
-webkit.org/b/216813 webaudio/AudioParam/audioparam-k-rate.html [ Crash ]
-webkit.org/b/216813 webrtc/audio-peer-connection-webaudio.html [ Failure ]
-webkit.org/b/216813 webrtc/peer-connection-audio-unmute.html [ Failure ]
-webkit.org/b/216813 webrtc/peer-connection-remote-audio-mute2.html [ Failure ]
-# Tests below are timing out.
-webkit.org/b/216813 webrtc/peer-connection-audio-mute.html [ Skip ]
-webkit.org/b/216813 webrtc/peer-connection-remote-audio-mute.html [ Skip ]
-
 #////////////////////////////////////////////////////////////////////////////////////////
 # End of GStreamer-related bugs
 #////////////////////////////////////////////////////////////////////////////////////////

LayoutTests/platform/glib/webaudio/Analyser/realtimeanalyser-fftsize-reset-expected.txt
@@ -5,11 +5,11 @@ PASS Executing "512->256"
 PASS Audit report 
 PASS > [128->1024]  
 PASS   Time data equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
-PASS   Linear FFT data after setting fftSize = 1024 equals [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.0000019455}. 
+PASS   Linear FFT data after setting fftSize = 1024 equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.0000019455}. 
 PASS < [128->1024] All assertions passed. (total 2 assertions) 
 PASS > [512->256]  
 PASS   Time data equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
-PASS   Linear FFT data after setting fftSize = 256 equals [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.0000018592}. 
+PASS   Linear FFT data after setting fftSize = 256 equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.0000018592}. 
 PASS < [512->256] All assertions passed. (total 2 assertions) 
 PASS # AUDIT TASK RUNNER FINISHED: 2 tasks ran successfully. 
 

LayoutTests/platform/glib/webaudio/Analyser/realtimeanalyser-freq-data-expected.txt
@@ -1,0 +1,91 @@
+
+PASS # AUDIT TASK RUNNER STARTED. 
+PASS Executing "5-order FFT" 
+PASS Executing "6-order FFT" 
+PASS Executing "7-order FFT" 
+PASS Executing "8-order FFT" 
+PASS Executing "9-order FFT" 
+PASS Executing "10-order FFT" 
+PASS Executing "11-order FFT" 
+PASS Executing "12-order FFT" 
+PASS Executing "13-order FFT" 
+PASS Executing "14-order FFT" 
+PASS Executing "15-order FFT" 
+PASS Executing "no smoothing" 
+PASS Audit report 
+PASS > [5-order FFT]  
+PASS   32-point float FFT equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":9.6549e-7}. 
+PASS   Order: 5: Min FFT value is less than or equal to -50. 
+PASS   Order: 5: Max FFT value is greater than or equal to -30. 
+PASS   32-point byte FFT equals [153,255,255,255,255,255,255,255,255,249,233,207,178,146,79,0...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [5-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [6-order FFT]  
+PASS   64-point float FFT equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.000010084}. 
+PASS   Order: 6: Min FFT value is less than or equal to -100. 
+PASS   Order: 6: Max FFT value is greater than or equal to -30. 
+PASS   64-point byte FFT equals [255,255,255,254,246,239,233,228,224,220,217,214,211,208,206,204...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [6-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [7-order FFT]  
+PASS   128-point float FFT equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.0000011473}. 
+PASS   Order: 7: Min FFT value is less than or equal to -100. 
+PASS   Order: 7: Max FFT value is greater than or equal to -30. 
+PASS   128-point byte FFT equals [255,255,255,255,255,249,239,242,229,232,227,221,224,213,219,212...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [7-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [8-order FFT]  
+PASS   256-point float FFT equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.0000010442}. 
+PASS   Order: 8: Min FFT value is less than or equal to -100. 
+PASS   Order: 8: Max FFT value is greater than or equal to -30. 
+PASS   256-point byte FFT equals [102,169,255,255,255,255,255,255,252,233,255,253,218,242,250,226...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [8-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [9-order FFT]  
+PASS   512-point float FFT equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.000026427}. 
+PASS   Order: 9: Min FFT value is less than or equal to -100. 
+PASS   Order: 9: Max FFT value is greater than or equal to -30. 
+PASS   512-point byte FFT equals [0,16,36,50,65,231,255,255,255,208,59,75,219,255,255,247...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [9-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [10-order FFT]  
+PASS   1024-point float FFT equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.000029771}. 
+PASS   Order: 10: Min FFT value is less than or equal to -100. 
+PASS   Order: 10: Max FFT value is greater than or equal to -30. 
+PASS   1024-point byte FFT equals [0,0,0,0,2,12,23,35,49,63,74,107,240,255,255,255...] with an element-wise tolerance of {"absoluteThreshold":1,"relativeThreshold":0}. 
+PASS < [10-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [11-order FFT]  
+PASS   2048-point float FFT equals [-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.000013456}. 
+PASS   Order: 11: Min FFT value is less than or equal to -100. 
+PASS   Order: 11: Max FFT value is greater than or equal to -30. 
+PASS   2048-point byte FFT equals [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [11-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [12-order FFT]  
+PASS   4096-point float FFT equals [-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":4.6116e-7}. 
+PASS   Order: 12: Min FFT value is less than or equal to -100. 
+PASS   Order: 12: Max FFT value is greater than or equal to -30. 
+PASS   4096-point byte FFT equals [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [12-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [13-order FFT]  
+PASS   8192-point float FFT equals [-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":3.2106e-7}. 
+PASS   Order: 13: Min FFT value is less than or equal to -100. 
+PASS   Order: 13: Max FFT value is greater than or equal to -30. 
+PASS   8192-point byte FFT equals [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [13-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [14-order FFT]  
+PASS   16384-point float FFT equals [-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":1.1756e-7}. 
+PASS   Order: 14: Min FFT value is less than or equal to -100. 
+PASS   Order: 14: Max FFT value is greater than or equal to -30. 
+PASS   16384-point byte FFT equals [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+PASS < [14-order FFT] All assertions passed. (total 4 assertions) 
+PASS > [15-order FFT]  
+FAIL X 32768-point float FFT does not equal [-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":1.1756e-7}.
+	Index	Actual			Expected		AbsError		RelError		Test threshold
+	[14082]	-7.3094314575195313e+1	-7.3094299316406250e+1	1.5258789062500000e-5	2.0875484415615863e-7	8.5929658276367193e-6
+	Max AbsError of 1.5258789062500000e-5 at index of 14082.
+	Max RelError of 2.0875484415615863e-7 at index of 14082.
+ assert_true: expected true got false
+PASS   Order: 15: Min FFT value is less than or equal to -100. 
+PASS   Order: 15: Max FFT value is greater than or equal to -30. 
+PASS   32768-point byte FFT equals [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0}. 
+FAIL < [15-order FFT] 1 out of 4 assertions were failed. assert_true: expected true got false
+PASS > [no smoothing]  
+PASS   128-point float FFT equals [expected array] with an element-wise tolerance of {"absoluteThreshold":0,"relativeThreshold":0.0000015684}. 
+PASS < [no smoothing] All assertions passed. (total 1 assertions) 
+FAIL # AUDIT TASK RUNNER FINISHED: 1 out of 12 tasks were failed. assert_true: expected true got false
+

LayoutTests/platform/glib/webaudio/Analyser/realtimeanalyser-multiple-calls-expected.txt
@@ -3,10 +3,10 @@ PASS # AUDIT TASK RUNNER STARTED.
 PASS Executing "test" 
 PASS Audit report 
 PASS > [test]  
-PASS   Second call to getFloatFrequencyData is identical to the array [-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity,-Infinity...]. 
-PASS   Second call to getByteFrequencyData is identical to the array [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...]. 
-PASS   Output of getByteFrequencyData after getFloatFrequencyData is identical to the array [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...]. 
-PASS   Output of getFloatFrequenycData (converted to byte) after getByteFrequencyData is identical to the array [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0...]. 
+PASS   Second call to getFloatFrequencyData is identical to the array [expected array]. 
+PASS   Second call to getByteFrequencyData is identical to the array [167,232,255,255,238,239,223,228,216,217,214,206,211,197,206,196...]. 
+PASS   Output of getByteFrequencyData after getFloatFrequencyData is identical to the array [163,242,255,255,252,250,237,238,229,228,225,219,221,212,216,209...]. 
+PASS   Output of getFloatFrequenycData (converted to byte) after getByteFrequencyData is identical to the array [176,248,255,255,255,255,243,244,235,234,231,225,227,217,222,215...]. 
 PASS < [test] All assertions passed. (total 4 assertions) 
 PASS # AUDIT TASK RUNNER FINISHED: 1 tasks ran successfully. 
 